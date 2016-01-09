d03a1ad4bbf3b713a248ce84fec4830ab864d3a0
Ahmed ElSayed <ahmels@microsoft.com>
2016-01-08 17:26:44 -0800

    Adding AADOAuth Module

--- AzureFunctions/App_Start/WebApiConfig.cs
+++ AzureFunctions/App_Start/WebApiConfig.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.Routing;
 
 namespace AzureFunctions.App_Start
---
+++ AzureFunctions/Common/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureFunctions.Common
+{
+    public static class Constants
+    {
+        public const string SubscriptionTemplate = "{0}/subscriptions/{1}?api-version={2}";
+        public const string CSMApiVersion = "2014-04-01";
+        public const string CSMUrl = "https://management.azure.com";
+    }
+}
