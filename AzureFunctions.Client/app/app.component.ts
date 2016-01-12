4c752249bc6bed7dd1e0a4f8160e48d46ace43c7
Ahmed ElSayed <ahmels@microsoft.com>
2016-01-11 17:45:48 -0800

    Initial Client

---
+++ AzureFunctions.Client/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from 'angular2/core';
+
+@Component({
+    selector: 'azure-functions-app',
+    templateUrl: 'templates/app.html',
+    styleUrls: ['styles/app.css']
+})
+export class AppComponent {
+    
+}
