118437957b0926330b3b3a93eff756fdacf62a9a
Ahmed ElSayed <ahmels@microsoft.com>
2016-01-13 12:03:25 -0800

    Adding missing file

---
+++ AzureFunctions.Client/mocks/WebHook/index.js
@@ -0,0 +1,9 @@
+﻿var util = require('util');
+
+module.exports = function (context) {
+    // expect the request body to be json
+    var json = util.inspect(context.input);
+
+    context.log('Node.js webHook trigger function invoked! ' + json);
+    context.done();
+}
