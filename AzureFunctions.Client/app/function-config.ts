2f34c1dc95de86b2ed7e035cbd9100226c8fa9ef
Ahmed ElSayed <ahmels@microsoft.com>
2016-01-20 16:02:12 -0800

    Add FunctionConfig model

--- AzureFunctions.Client/app/function-config.ts
+++ AzureFunctions.Client/app/function-config.ts
@@ -1,3 +1,21 @@
 export interface FunctionConfig {
-    
+    bindings: FunctionBindings;
+}
+
+export interface FunctionBindings {
+    input: FunctionBinding[],
+    output: FunctionBinding[]
+}
+
+export interface FunctionBinding {
+    name: string;
+    type: string;
+    path: string;
+    queueName: string;
+    schedule: string;
+    runOnStartup: boolean;
+    partitionKey: string;
+    filter: string;
+    tableName: string;
+    rowKey: string;
 }
