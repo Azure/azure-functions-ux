739027a0929fc6706b705f2f031034482cde8801
Ahmed ElSayed <ahmels@microsoft.com>
2016-02-12 12:32:19 -0800

    remove unused imports and use angular.dev

--- AzureFunctions.Client/app/boot.ts
+++ AzureFunctions.Client/app/boot.ts
@@ -1,10 +1,3 @@
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/zip';
-import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/observable/fromArray';
-import 'rxjs/add/observable/empty'
-import 'rxjs/add/operator/catch';
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {AppComponent} from './components/app.component';
