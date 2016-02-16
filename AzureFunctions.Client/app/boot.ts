658383d9de8795c0dff7cb39281b686c749d4790
Ahmed ElSayed <ahmels@microsoft.com>
2016-02-16 13:13:55 -0800

    Adding es6 shim to vs

--- AzureFunctions.Client/app/boot.ts
+++ AzureFunctions.Client/app/boot.ts
@@ -1,3 +1,5 @@
+/// <reference path="..\typings\main.d.ts" />
+
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS} from 'angular2/http';
 import {AppComponent} from './components/app.component';
