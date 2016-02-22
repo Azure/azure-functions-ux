8d41c65046e340c23f0884738a880d1a6510a624
Elliott Hamai <ellhamai@microsoft.com>
2016-02-22 13:50:52 -0800

    Added tab support

---
+++ AzureFunctions.Client/app/components/tab.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component, Input } from 'angular2/core';
+
+@Component({
+    selector: 'tab',
+    styleUrls: ['styles/tabs.style.css'],
+    templateUrl: 'templates/tab.component.html'
+})
+export class TabComponent {
+    @Input('tabTitle') title: string;
+    @Input() active = false;
+}
