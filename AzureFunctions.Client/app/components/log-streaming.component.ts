598f616105d9addfc7157a29273fd503ad5cf4e7
Elliott Hamai <ellhamai@microsoft.com>
2016-02-24 17:48:01 -0800

    Implemented "develop" tab

--- AzureFunctions.Client/app/components/log-streaming.component.ts
+++ AzureFunctions.Client/app/components/log-streaming.component.ts
@@ -5,14 +5,17 @@ import {FunctionsService} from '../services/functions.service';
 @Component({
     selector: 'log-streaming',
     templateUrl: 'templates/log-streaming.component.html',
-    inputs: ['functionInfo']
+    inputs: ['functionInfo'],
+    styleUrls: ['styles/function-dev.style.css']
 })
 export class LogStreamingComponent implements OnDestroy {
     public log: string;
+    public stopped: boolean;
     private xhReq: XMLHttpRequest;
     private basic: string;
     private scmUrl: string;
     private timerId: number;
+    private _functionInfo: FunctionInfo;
 
     constructor(private _functionsService: FunctionsService) {
         this.basic = _functionsService.getBasicHeader();
@@ -20,25 +23,9 @@ export class LogStreamingComponent implements OnDestroy {
     }
 
     set functionInfo(value: FunctionInfo) {
-        if (this.xhReq) {
-            window.clearInterval(this.timerId);
-            this.log = '';
-            this.xhReq.abort();
-        }
-        this.xhReq = new XMLHttpRequest();
-        this.xhReq.open('GET', `${this.scmUrl}/api/logstream/application/functions/function/${value.name}`, true);
-        this.xhReq.setRequestHeader('Authorization', this.basic);
-        this.xhReq.setRequestHeader('FunctionsPortal', '1');
-        this.xhReq.send(null);
-        this.timerId = window.setInterval(() => {
-            this.log = this.xhReq.responseText;
-            window.setTimeout(() => {
-                var el = document.getElementById('log-stream');
-                if (el) {
-                    el.scrollTop = el.scrollHeight;
-                }
-            });
-        }, 1000);
+        this._functionInfo = value;
+        this.initLogs();
+        this.startLogs()
     }
 
     ngOnDestroy() {
@@ -46,5 +33,44 @@ export class LogStreamingComponent implements OnDestroy {
             window.clearInterval(this.timerId);
             this.xhReq.abort();
         }
+    }
+
+    startLogs(){
+        this.stopped = false;
+    }
+
+    stopLogs(){
+        this.stopped = true;
+    }
+
+    clearLogs(){
+        this.initLogs();
+    }
+
+    private initLogs(){
+        if (this.xhReq) {
+            window.clearInterval(this.timerId);
+            this.log = '';
+            this.xhReq.abort();
+        }
+
+        this.xhReq = new XMLHttpRequest();
+        this.xhReq.open('GET', `${this.scmUrl}/api/logstream/application/functions/function/${this._functionInfo.name}`, true);
+        this.xhReq.setRequestHeader('Authorization', this.basic);
+        this.xhReq.setRequestHeader('FunctionsPortal', '1');
+        this.xhReq.send(null);
+
+        this.timerId = window.setInterval(() => {
+            if (!this.stopped) {
+                this.log = this.xhReq.responseText;
+            }
+
+            window.setTimeout(() => {
+                var el = document.getElementById('log-stream');
+                if (el) {
+                    el.scrollTop = el.scrollHeight;
+                }
+            });
+        }, 1000);           
     }
 }
