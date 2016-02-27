20af6a9e26dc95dc88880fa6e811d02b38c59e9e
Ahmed ElSayed <ahmels@microsoft.com>
2016-02-26 16:06:49 -0800

    Add production deployment steps

--- deploy.cmd
+++ deploy.cmd
@@ -103,12 +103,31 @@ IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
 :: 6. Install npm packages
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
+
   call :ExecuteCmd npm install
   IF !ERRORLEVEL! NEQ 0 goto error
+
   call :ExecuteCmd npm run typings install
   IF !ERRORLEVEL! NEQ 0 goto error
+
+  call :ExecuteCmd npm install -g jspm
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  call :ExecuteCmd jspm install
+  IF !ERRORLEVEL! NEQ 0 goto error
+
   call :ExecuteCmd npm run tsc
   IF !ERRORLEVEL! NEQ 0 goto error
+
+  call :ExecuteCmd rm index.html
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  call :ExecuteCmd ren index.html.prod index.html
+  IF !ERRORLEVEL! NEQ 0 goto error
+
+  call :ExecuteCmd jspm bundle-sfx app sfx/app.js --minify
+  IF !ERRORLEVEL! NEQ 0 goto error
+
   popd
 )
 
