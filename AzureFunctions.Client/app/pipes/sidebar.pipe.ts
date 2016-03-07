085de8cc537f5ea588266689d24bc1d902af47de
Ahmed ElSayed <ahmels@microsoft.com>
2016-03-07 14:28:08 -0800

    Don't show 'New Function' during search

--- AzureFunctions.Client/app/pipes/sidebar.pipe.ts
+++ AzureFunctions.Client/app/pipes/sidebar.pipe.ts
@@ -9,7 +9,7 @@ import {FunctionInfo} from '../models/function-info';
 export class SideBarFilterPipe implements PipeTransform {
     transform(items: FunctionInfo[], args: string[]): any {
         if (args && args.length > 0 && args[0] && args[0].length > 0) {
-            return items.filter(item => item.name.toLocaleLowerCase().indexOf(args[0].toLocaleLowerCase()) !== -1);
+            return items.filter(item => !item.clientOnly && item.name.toLocaleLowerCase().indexOf(args[0].toLocaleLowerCase()) !== -1);
         } else {
             return items;
         }
