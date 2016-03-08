fa502a31a47b204714f121b0fac7bb6a54471d5e
Alexey Rodionov <alrod@microsoft.com>
2016-03-08 15:01:21 -0800

    Fix swiching integrate(V2) tabs

--- AzureFunctions.Client/app/components/function-integrate-v2.component.ts
+++ AzureFunctions.Client/app/components/function-integrate-v2.component.ts
@@ -32,19 +32,18 @@ export class FunctionIntegrateV2Component {
     private _bindingManager: BindingManager = new BindingManager();
     
     set selectedFunction(fi: FunctionInfo) {
-        if (!this._functionInfo) {
-            this._functionInfo = JSON.parse(JSON.stringify(fi));  //clone functionInfo
-            this._functionsService.getBindingConfig().subscribe((bindings) => {
-                this.model.config = this._bindingManager.functionConfigToUI(fi.config, bindings.bindings);
-                if (this.model.config.bindings.length > 0) {
-                    this.currentBinding = this.model.config.bindings[0];
-                    this.currentBindingId = this.currentBinding.id;
-                }
+        this._functionInfo = JSON.parse(JSON.stringify(fi));  //clone functionInfo
+        this._functionsService.getBindingConfig().subscribe((bindings) => {
+            this.model.config = this._bindingManager.functionConfigToUI(fi.config, bindings.bindings);
+            if (this.model.config.bindings.length > 0) {
+                this.currentBinding = this.model.config.bindings[0];
+                this.currentBindingId = this.currentBinding.id;
+            }
 
-                this.model.setBindings();
-                jQuery(this._elementRef.nativeElement).find('[data-toggle="popover"]').popover({ html: true, container: 'body' });
-            });
-        }
+            this.model.setBindings();
+            jQuery(this._elementRef.nativeElement).find('[data-toggle="popover"]').popover({ html: true, container: 'body' });
+        });
+        
     }
 
     constructor( @Inject(ElementRef) elementRef: ElementRef, private _functionsService: FunctionsService) {
