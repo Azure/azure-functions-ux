cb7e4c6b7c980d53ab1b1c088fff01f53501c3ca
Ahmed ElSayed <ahmels@microsoft.com>
2016-03-09 18:34:39 -0800

    adding more steps to the getting started page

--- AzureFunctions.Client/app/components/app.component.ts
+++ AzureFunctions.Client/app/components/app.component.ts
@@ -1,18 +1,50 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {DashboardComponent} from './dashboard.component';
 import {GettingStartedComponent} from './getting-started.component';
+import {PortalService} from '../services/portal.service';
+import {FunctionsService} from '../services/functions.service';
+import {BroadcastEvent, IBroadcastService} from '../services/ibroadcast.service';
 
 @Component({
     selector: 'azure-functions-app',
     template: `<busy-state></busy-state>
-<functions-dashboard *ngIf="loggedin"></functions-dashboard>
-<getting-started *ngIf="!loggedin"></getting-started>`,
+<functions-dashboard *ngIf="!gettingStarted"></functions-dashboard>
+<getting-started [loggedIn]="loggedIn" (userReady)="onUserReady($event)" *ngIf="gettingStarted"></getting-started>`,
     directives: [DashboardComponent, GettingStartedComponent]
 })
-export class AppComponent {
-    public loggedin: boolean;
-    constructor() {
-        this.loggedin = document.cookie.indexOf('authenticated=true') !== -1 ||
-            window.location.protocol === 'http:';
+export class AppComponent implements OnInit {
+    public gettingStarted: boolean;
+    public loggedIn: boolean;
+
+    constructor(
+        private _portalService: PortalService,
+        private _functionsService: FunctionsService,
+        private _broadcastService: IBroadcastService
+    ) {
+        if (_portalService.inIFrame ||
+            window.location.protocol === 'http:') {
+            this.gettingStarted = false;
+            return;
+        }
+
+        this.loggedIn = document.cookie.indexOf('authenticated=true') !== -1;
+        this.gettingStarted = !this.loggedIn || false;
+    }
+
+    ngOnInit() {
+        if (!this.gettingStarted) return;
+
+        this._broadcastService.setBusyState();
+
+        this._functionsService.initializeUser()
+            .subscribe(
+                res => this.gettingStarted = !!res,
+                e => console.log(e),
+                () => this._broadcastService.clearBusyState()
+            );
+    }
+
+    onUserReady(userReady: boolean) {
+        this.gettingStarted = !userReady;
     }
 }
--- AzureFunctions.Client/app/components/dashboard.component.ts
+++ AzureFunctions.Client/app/components/dashboard.component.ts
@@ -36,36 +36,16 @@ export class DashboardComponent implements OnInit {
     @ViewChild(SideBarComponent) sideBar: SideBarComponent;
 
     public functionsInfo: FunctionInfo[];
-    public subscriptions: DropDownElement<Subscription>[];
-    public serverFarms: DropDownElement<ServerFarm>[];
-    public _serverFarms: DropDownElement<ServerFarm>[];
-    public selectedSubscription: Subscription;
-    public selectedServerFarm: ServerFarm;
     public functionTemplates: FunctionTemplate[];
     public selectedFunction: FunctionInfo;
-    public noContainerFound: boolean;
-    public noTenantsFound: boolean;
-    public subscriptionPickerPlaceholder: string;
-    public serverFarmPickerPlaceholder: string;
-    public geoRegions: DropDownElement<string>[];
-    public selectedGeoRegion: string;
-    public resetServerFarm: boolean;
     public openAppSettings: boolean;
 
-    private tryAppServiceTenantId: string = "6224bcc1-1690-4d04-b905-92265f948dad";
 
     constructor(private _functionsService: FunctionsService,
         private _userService: UserService,
         private _portalService: PortalService,
         private _broadcastService: IBroadcastService) {
 
-        this.noContainerFound = false;
-        this.noTenantsFound = false;
-        this.subscriptionPickerPlaceholder = 'Select Subscription';
-        this.serverFarmPickerPlaceholder = 'Select Server Farm (optional)';
-        this.geoRegions = ['West US']
-            .map(e => ({ displayLabel: e, value: e }))
-            .sort((a, b) => a.displayLabel.localeCompare(b.displayLabel));
 
 
         this._broadcastService.subscribe<FunctionInfo>(BroadcastEvent.FunctionDeleted, fi => {
@@ -89,54 +69,17 @@ export class DashboardComponent implements OnInit {
                 this._functionsService.setToken(token);
                 this._functionsService.getTemplates()
                     .subscribe(res => this.functionTemplates = res);
-                this.initializeUser();
+                this.initFunctions();
             });
         }
         else {
             this._functionsService.getTemplates()
                 .subscribe(res => this.functionTemplates = res);
-            this.initializeUser();
+            this.initFunctions();
         }
     }
 
-    private initializeUser() {
-        this._functionsService.initializeUser()
-            .subscribe(r => {
-                if (!r) {
-                    // No Container. Ask the user to pick.
-                    //get a list of subs and ask the user to chose.
-                    this._userService.getTenants()
-                        .subscribe(res => {
-                            this._broadcastService.clearBusyState();
-                            if (res.filter(e => e.TenantId.toLocaleLowerCase() !== this.tryAppServiceTenantId).length === 0) {
-                                // Try It Now
-                                this.noTenantsFound = true;
-                            } else {
-                                this.noContainerFound = true;
-                                // Normal Stuff
-                                this._functionsService.getSubscriptions()
-                                    .subscribe(res => {
-                                        this.subscriptions = res
-                                            .map(e => ({ displayLabel: e.displayName, value: e }))
-                                            .sort((a, b) => a.displayLabel.localeCompare(b.displayLabel));
-                                    });
-                                this._functionsService.getServerFarms()
-                                    .subscribe(res => {
-                                        this.serverFarms = res
-                                            .map(e => ({ displayLabel: e.serverFarmName, value: e }))
-                                            .sort((a, b) => a.displayLabel.localeCompare(b.displayLabel));
-                                    });
-                            }
-                        });
-
-                } else {
-                    this.initFunctions();
-                }
-            });
-    }
-
     initFunctions() {
-        this.noContainerFound = false;
         this._functionsService.redirectToIbizaIfNeeded();
         this._functionsService.getFunctions()
             .subscribe(res => {
@@ -150,39 +93,6 @@ export class DashboardComponent implements OnInit {
         this._functionsService.getHostSecrets();
     }
 
-    createFunctionsContainer() {
-        this._broadcastService.setBusyState();
-        var serverFarmId = this.selectedServerFarm ? this.selectedServerFarm.armId : null;
-        this._functionsService.createFunctionsContainer(this.selectedSubscription.subscriptionId, this.selectedGeoRegion, serverFarmId)
-            .subscribe(r => this.initFunctions());
-    }
-
-    onSubscriptionSelect(value: Subscription) {
-        this.selectedSubscription = value;
-        delete this.selectedServerFarm;
-        this.resetServerFarm = !this.resetServerFarm;
-        this._serverFarms = this.serverFarms.filter(e => e.value.subscriptionId.toLocaleLowerCase() === value.subscriptionId &&
-            this.selectedGeoRegion &&
-            e.value.geoRegion.toLocaleLowerCase() === this.selectedGeoRegion.toLocaleLowerCase());
-    }
-
-    onGeoRegionChange(value: string) {
-        this.selectedGeoRegion = value;
-        delete this.selectedServerFarm;
-        this.resetServerFarm = !this.resetServerFarm;
-        this._serverFarms = this.serverFarms.filter(e => e.value.subscriptionId.toLocaleLowerCase() === this.selectedSubscription.subscriptionId &&
-            e.value.geoRegion.toLocaleLowerCase() === value.toLocaleLowerCase());
-    }
-
-    createTrialFunctionsContainer() {
-        this._broadcastService.setBusyState();
-        this._functionsService.createTrialFunctionsContainer()
-            .subscribe(r => this.switchToTryAppServiceTenant());
-    }
-
-    switchToTryAppServiceTenant() {
-        window.location.href = `api/switchtenants/${this.tryAppServiceTenantId}${window.location.search}`;
-    }
 
     onAppSettingsClicked() {
         this.resetView();
