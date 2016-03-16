35a331270d4e94dffc56eba40024fc14179446dd
Ahmed ElSayed <ahmels@microsoft.com>
2016-03-16 12:05:11 -0700

    Fix 'take me to the metrics' link. Fixes #84

--- AzureFunctions.Client/app/components/function-monitor.component.ts
+++ AzureFunctions.Client/app/components/function-monitor.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input} from 'angular2/core';
+import {Component, Input, OnInit} from 'angular2/core';
 import {FunctionInfo} from '../models/function-info';
 
 @Component({
     selector: 'function-monitor',
     template:`<div class="wrapper">
-    <a [attr.href]="selectedFunction?.href" target="_blank">Take me to the metrics!<i class="fa fa-external-link"></i></a>
+    <a [attr.href]="dashboardUrl" target="_blank">Take me to the metrics! <i class="fa fa-external-link"></i></a>
     <h2>Integrated view coming soon!</h2>
     <div>
         <p>Per-function monitoring is not currently available in this view, but we're working on it. For now, you can view them through the SDK dashboard linked above, which is hosted by your function app</p>
@@ -12,7 +12,13 @@ import {FunctionInfo} from '../models/function-info';
     </div>
 </div>`,
 })
-export class FunctionMonitorComponent {
+export class FunctionMonitorComponent implements OnInit {
     @Input() selectedFunction: FunctionInfo;
+    public dashboardUrl: string;
     constructor() {}
+
+    ngOnInit(){
+        var scmUrl = this.selectedFunction.href.substring(0, this.selectedFunction.href.indexOf('/api/'));
+        this.dashboardUrl = `${scmUrl}/azurejobs`;
+    }
 }
