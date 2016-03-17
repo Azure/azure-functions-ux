2edeb59af62072903445aea9e7aa34acd3b97d39
Elliott Hamai <ellhamai@microsoft.com>
2016-03-17 15:59:25 -0700

    Adding Ibiza App blade support

---
+++ AzureFunctions.Client/app/models/portal.ts
@@ -0,0 +1,19 @@
+﻿export interface Event {
+    data: Data;
+}
+
+export interface Data{
+    signature: string;
+    kind: string;
+    data: string;
+}
+
+export class Verbs{
+    public static message = "message";
+    public static ready = "ready";
+    public static getAuthToken = "get-auth-token";
+    public static sendToken = "send-token";
+    public static openBlade = "open-blade";
+    public static sendResourceId = "send-resourceId";
+    public static sendAppSettingName = "send-appSettingName";
+}
