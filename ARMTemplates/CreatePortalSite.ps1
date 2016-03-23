c9f6b727d323cef32fb138519552e36fcbe1ece7
Ahmed ElSayed <ahmels@microsoft.com>
2016-03-22 17:14:08 -0700

    update ARM templates

--- ARMTemplates/CreatePortalSite.ps1
+++ ARMTemplates/CreatePortalSite.ps1
@@ -1,11 +1,41 @@
-# Change those two lines for other regions
-$name = "functions-bay"
-$location = "WestUS"
+$json = @"
+{
+    "value": [
+        {
+            "name": "functions-hk1",
+            "location": "EastAsia"
+        },
+        {
+            "name": "functions-bay",
+            "location": "WestUS"
+        },
+        {
+            "name": "functions-blu",
+            "location": "EastUS"
+        },
+        {
+            "name": "functions-db3",
+            "location": "NorthEurope"
+        }
+    ]
+}
+"@
 
-Select-AzureRmSubscription -SubscriptionName "Websites migration"
-$appLocation = "MSFT$location"
-$storageName = $name -replace "-", ""
-$Subscription = (Get-AzureRmContext).Subscription.SubscriptionId
-$farmId = "/subscriptions/$Subscription/resourceGroups/Default-Web-$appLocation/providers/Microsoft.Web/serverfarms/Default1"
-New-AzureRmResourceGroup -Name $name -Location $location -Force
-New-AzureRmResourceGroupDeployment -ResourceGroupName $name -appLocation $appLocation -TemplateFile FunctionsPortal.json -appName $name -appServicePlanId $farmId -storageAccountName $storageName -Force
+$values = $json | ConvertFrom-Json
+
+foreach ($e in $values.value)
+{
+    $location = $e.location
+    $name = $e.name
+    Select-AzureRmSubscription -SubscriptionName "Websites migration"
+    $appLocation = "MSFT$location"
+    $storageName = $name -replace "-", ""
+    If ($name -eq "functions-db3") 
+    {
+        $storageName += "1"
+    }
+    $Subscription = (Get-AzureRmContext).Subscription.SubscriptionId
+    $farmId = "/subscriptions/$Subscription/resourceGroups/Default-Web-$appLocation/providers/Microsoft.Web/serverfarms/Default1"
+    New-AzureRmResourceGroup -Name $name -Location $location -Force
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $name -appLocation $appLocation -TemplateFile FunctionsPortal.json -appName $name -appServicePlanId $farmId -storageAccountName $storageName -Force
+}
