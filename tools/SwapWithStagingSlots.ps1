ca1b57cb43b2ee2ae1cb8b15c38d36ffd74bafc7
davidebbo <david.ebbo@microsoft.com>
2016-03-25 09:34:15 -0700

    Added script to swap all sites. CDN purge not yet working

---
+++ tools/SwapWithStagingSlots.ps1
@@ -0,0 +1,26 @@
+﻿# make sure the script stops if one of the swaps failed
+$ErrorActionPreference = "Stop"
+
+Select-AzureRmSubscription -SubscriptionName "Websites migration"
+
+$ParametersObject = @{
+  targetSlot = "staging"
+}
+
+Write-Host swapping bay site ....
+Invoke-AzureRmResourceAction -ResourceGroupName functions-bay -ResourceType Microsoft.Web/sites -ResourceName functions-bay -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+
+Write-Host swapping blu site ....
+Invoke-AzureRmResourceAction -ResourceGroupName functions-blu -ResourceType Microsoft.Web/sites -ResourceName functions-blu -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+
+Write-Host swapping db3 site ....
+Invoke-AzureRmResourceAction -ResourceGroupName functions-db3 -ResourceType Microsoft.Web/sites -ResourceName functions-db3 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+
+Write-Host swapping hk1 site ....
+Invoke-AzureRmResourceAction -ResourceGroupName functions-hk1 -ResourceType Microsoft.Web/sites -ResourceName functions-hk1 -Action slotsswap -Parameters $ParametersObject -ApiVersion 2015-08-01 -Force
+
+Write-Host Done Swapping all sites
+
+
+Write-Host "Flushing the CDN. Note: this doesn't yet work!"
+#Invoke-AzureRmResourceAction -ResourceGroupName functions-bay -ResourceType Microsoft.Cdn/profiles/endpoints -ResourceName azure-functions/functions -Action purge -ApiVersion 2015-06-01 -Force
