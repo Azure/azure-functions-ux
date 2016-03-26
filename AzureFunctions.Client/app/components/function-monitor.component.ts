e7e9a24c95e4bb262fb693b500979eea736abf86
Matthew Henderson <mch8tf@virginia.edu>
2016-03-25 17:05:46 -0700

    updating function monitoring tab

--- AzureFunctions.Client/app/components/function-monitor.component.ts
+++ AzureFunctions.Client/app/components/function-monitor.component.ts
@@ -4,21 +4,29 @@ import {FunctionInfo} from '../models/function-info';
 @Component({
     selector: 'function-monitor',
     template:`<div class="wrapper">
-    <a [attr.href]="dashboardUrl" target="_blank">Take me to the metrics! <i class="fa fa-external-link"></i></a>
-    <h2>Integrated view coming soon!</h2>
-    <div>
-        <p>Per-function monitoring is not currently available in this view, but we're working on it. For now, you can view them through the SDK dashboard linked above, which is hosted by your function app</p>
-        <p>You can also see aggregate metrics for the entire function app using the "Monitoring" command in the top-right.</p>
+    <div class="text-center">
+        <p>Integrated view coming soon. Please use the links below:</p>
+    </div>
+    <div class="text-center">
+        <a [attr.href]="dashboardUrl" target="_blank">View the invocation log <i class="fa fa-external-link"></i></a>
+        <br/>
+        <a [attr.href]="pulseUrl" target="_blank">View the live event stream <i class="fa fa-external-link"></i></a>
+    </div>
+    <div class="text-center">
+        <p></p>
+        <img src="/images/monitoring-coming-soon.png" />
     </div>
 </div>`,
 })
 export class FunctionMonitorComponent implements OnInit {
     @Input() selectedFunction: FunctionInfo;
     public dashboardUrl: string;
+    public pulseUrl: string;
     constructor() {}
 
     ngOnInit(){
         var scmUrl = this.selectedFunction.href.substring(0, this.selectedFunction.href.indexOf('/api/'));
         this.dashboardUrl = `${scmUrl}/azurejobs/#/functions`;
+        this.pulseUrl = `${scmUrl}/azurejobs/#/functions`;
     }
 }
