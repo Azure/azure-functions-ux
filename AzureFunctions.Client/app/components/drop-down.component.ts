869358a7031e8438458b86a5cb753d54fd9ed36b
Alexey Rodionov <alrod@microsoft.com>
2016-03-30 18:55:00 -0700

    "Scenario" drop-down should only contain options that has at least one template, fixed #288

--- AzureFunctions.Client/app/components/drop-down.component.ts
+++ AzureFunctions.Client/app/components/drop-down.component.ts
@@ -3,7 +3,7 @@ import {DropDownElement} from '../models/drop-down-element';
 
 @Component({
     selector: 'drop-down',
-    inputs: ['options', 'placeholder', 'resetOnChange', 'disabled'],
+    inputs: ['options', 'placeholder', 'resetOnChange', 'disabled', 'selectedValue'],
     outputs: ['value'],
     templateUrl: 'templates/drop-down.component.html',
     styleUrls: ['styles/drop-down.style.css']
@@ -14,7 +14,7 @@ export class DropDownComponent<T> {
     public disabled: boolean;
     public value: EventEmitter<T>;
     private _options: DropDownElement<T>[];
-    public selectedValue: DropDownElement<T>;
+    public selectedElement: DropDownElement<T>;
 
     constructor() {
         this.value = new EventEmitter<T>();
@@ -36,17 +36,29 @@ export class DropDownComponent<T> {
         } else if (this._options.length > 0) {
             this.onSelect(this._options[0].id.toString());
         } else if (this._options.length === 0) {
-            delete this.selectedValue;
+            delete this.selectedElement;
         }
     }
 
-    set resetOnChange(vale) {
-        delete this.selectedValue;
+    set resetOnChange(value) {
+        delete this.selectedElement;
+    }
+
+    set selectedValue(value: T) {
+        if ((this.selectedElement.value !== value) && (value)) {
+            this.onSelectValue(value);
+        }
     }
 
     onSelect(id: string) {
         var element = this._options.find(e => e.id.toString() === id);
-        this.selectedValue = element;
+        this.selectedElement = element;
+        this.value.emit(element.value);
+    }
+
+    onSelectValue(value: T) {
+        var element = this._options.find(e => e.value === value);
+        this.selectedElement = element;
         this.value.emit(element.value);
     }
 }
