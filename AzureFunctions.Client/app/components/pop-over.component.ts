b2733d532047aab6cd279f8b91d5a447517ad3c9
Alexey Rodionov <alrod@microsoft.com>
2016-04-07 17:57:45 -0700

    Blur event shouldn't open a new tab, fixes #325

--- AzureFunctions.Client/app/components/pop-over.component.ts
+++ AzureFunctions.Client/app/components/pop-over.component.ts
@@ -19,14 +19,29 @@ export class PopOverComponent{
 
     onBlur(event: any) {
         this.show = false;
-
-        if (event.relatedTarget) {
+        if (event.relatedTarget && this.validURL(event.relatedTarget)) {
             window.open(
                 event.relatedTarget.toString(),
                 '_blank' // <- This is what makes it open in a new window.
             );
         }
     }
+
+    //http://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-an-url
+    private validURL(str): boolean {
+        var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+            '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locater
+
+        if (!pattern.test(str)) {
+            return false;
+        } else {
+            return true;
+		}
+	}
 
     handleClick() {
         this.clicked.emit(0);
@@ -36,4 +51,5 @@ export class PopOverComponent{
             }, this.hideAfter);
         }
     }
+
 }
