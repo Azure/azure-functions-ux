6349b6cb60f5012136ddb9a139e29b280dd7ef96
Ahmed ElSayed <ahmels@microsoft.com>
2016-05-24 13:47:24 -0700

    Adding error codes for ROBOCOPY

--- deploy.cmd
+++ deploy.cmd
@@ -93,7 +93,24 @@ echo Handling frontend Angular2 project.
 :: 4. KuduSync
 IF /I "%IN_PLACE_DEPLOYMENT%" NEQ "1" (
   call :ExecuteCmd ROBOCOPY "%DEPLOYMENT_SOURCE%\AzureFunctions.Client" "%DEPLOYMENT_TARGET%" /E /IS
-  IF !ERRORLEVEL! NEQ 0 goto error
+  :: http://ss64.com/nt/robocopy-exit.html
+  IF %ERRORLEVEL% EQU 16 echo ***FATAL ERROR*** & goto error
+  IF %ERRORLEVEL% EQU 15 echo OKCOPY + FAIL + MISMATCHES + XTRA & goto error
+  IF %ERRORLEVEL% EQU 14 echo FAIL + MISMATCHES + XTRA & goto error
+  IF %ERRORLEVEL% EQU 13 echo OKCOPY + FAIL + MISMATCHES & goto error
+  IF %ERRORLEVEL% EQU 12 echo FAIL + MISMATCHES& goto error
+  IF %ERRORLEVEL% EQU 11 echo OKCOPY + FAIL + XTRA & goto error
+  IF %ERRORLEVEL% EQU 10 echo FAIL + XTRA & goto error
+  IF %ERRORLEVEL% EQU 9 echo OKCOPY + FAIL & goto error
+  IF %ERRORLEVEL% EQU 8 echo FAIL & goto error
+  IF %ERRORLEVEL% EQU 7 echo OKCOPY + MISMATCHES + XTRA & goto error
+  IF %ERRORLEVEL% EQU 6 echo MISMATCHES + XTRA & goto error
+  IF %ERRORLEVEL% EQU 5 echo OKCOPY + MISMATCHES & goto error
+  IF %ERRORLEVEL% EQU 4 echo MISMATCHES & goto error
+  IF %ERRORLEVEL% EQU 3 echo OKCOPY + XTRA
+  IF %ERRORLEVEL% EQU 2 echo XTRA
+  IF %ERRORLEVEL% EQU 1 echo OKCOPY
+  IF %ERRORLEVEL% EQU 0 echo No Change
 )
 
 
