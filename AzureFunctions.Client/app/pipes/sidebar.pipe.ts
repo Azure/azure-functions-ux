6b9a77545668070b927be5505f678ce45cbc072a
Ahmed ElSayed <ahmels@microsoft.com>
2016-06-24 11:59:30 -0700

    Fix search filter on the sidebar

--- AzureFunctions.Client/app/pipes/sidebar.pipe.ts
+++ AzureFunctions.Client/app/pipes/sidebar.pipe.ts
@@ -7,9 +7,10 @@ import {FunctionInfo} from '../models/function-info';
 })
 @Injectable()
 export class SideBarFilterPipe implements PipeTransform {
-    transform(items: FunctionInfo[], args: string[]): any {
+    transform(items: FunctionInfo[], args: string[] | string): any {
+        var query = typeof args === 'string' ? args : args[0];
         if (args && args.length > 0 && args[0] && args[0].length > 0) {
-            return items.filter(item => !item.clientOnly && item.name.toLocaleLowerCase().indexOf(args[0].toLocaleLowerCase()) !== -1);
+            return items.filter(item => !item.clientOnly && item.name.toLocaleLowerCase().indexOf(query.toLocaleLowerCase()) !== -1);
         } else {
             return items;
         }
