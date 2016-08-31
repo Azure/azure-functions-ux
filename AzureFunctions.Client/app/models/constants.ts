41ffe4f9f96ce6c956aec110cf1b6f9396eec1c2
Alexey Rodionov <alrod@microsoft.com>
2016-08-31 15:20:25 -0700

    Update runtime and node versions

--- AzureFunctions.Client/app/models/constants.ts
+++ AzureFunctions.Client/app/models/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
-    public static runtimeVersion = "~0.4";
-    public static nodeVersion = "5.9.1";
+    public static runtimeVersion = "~0.5";
+    public static nodeVersion = "6.4.0";
     public static latest = "latest";
     public static runtimeVersionAppSettingName = "FUNCTIONS_EXTENSION_VERSION";
     public static nodeVersionAppSettingName = "WEBSITE_NODE_DEFAULT_VERSION";
