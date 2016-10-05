55f0414f5822204147b5bc6b186d547fb5e055b0
Ahmed ElSayed <ahmels@microsoft.com>
2016-10-04 17:06:29 -0700

    Switch between Manage page of disabled functions sometimes make one of them enabled. Fixes #584

--- AzureFunctions.Client/app/components/function-manage.component.ts
+++ AzureFunctions.Client/app/components/function-manage.component.ts
@@ -42,7 +42,6 @@ export class FunctionManageComponent {
             }];
         this.valueChange = new Subject<boolean>();
         this.valueChange
-            .distinctUntilChanged()
             .switchMap<FunctionInfo>((state, index) => {
                 this.selectedFunction.config.disabled = state;
                 return this._functionsService.updateFunction(this.selectedFunction);
