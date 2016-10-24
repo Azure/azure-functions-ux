3458010a7b97b6737736c4fd11a9a45cd5056f73
Alexey Rodionov <alrod@microsoft.com>
2016-10-24 09:13:38 -0700

    Update runtime version to "~0.7"

--- AzureFunctions/Controllers/AzureFunctionsController.cs
+++ AzureFunctions/Controllers/AzureFunctionsController.cs
@@ -57,7 +57,7 @@ namespace AzureFunctions.Controllers
         [HttpGet]
         public HttpResponseMessage GetLatestRuntime()
         {
-            return Request.CreateResponse(HttpStatusCode.OK, "~0.6");
+            return Request.CreateResponse(HttpStatusCode.OK, "~0.7");
         }
 
         [HttpGet]
