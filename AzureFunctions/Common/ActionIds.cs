9f141343001e33764549d4b431ebf53112e92c6b
Ahmed ElSayed <ahmels@microsoft.com>
2017-02-24 20:12:46 -0800

    add all actionids values

--- AzureFunctions/Common/ActionIds.cs
+++ AzureFunctions/Common/ActionIds.cs
@@ -10,14 +10,13 @@
         public static readonly string InvalidStorageConnectionString = $"/heal_actions/{nameof(InvalidStorageConnectionString)}";
         public static readonly string StorageAccountDoesNotExist = $"/heal_actions/{nameof(StorageAccountDoesNotExist)}";
         public static readonly string StorageAccountDoesntSupportQueues = $"/heal_actions/{nameof(StorageAccountDoesntSupportQueues)}";
-
-        public static string StorageAccountNullOrEmpty { get; internal set; }
-        public static string FunctionAppIsStopped { get; internal set; }
-        public static string AppIsMisconfiguredInAzure { get; internal set; }
-        public static string DefaultHostNameNotResolving { get; internal set; }
-        public static string KeysUnexpectedError { get; internal set; }
-        public static string ScaledOutStamp { get; internal set; }
-        public static string FunctionRuntimeCrashLoop { get; internal set; }
-        public static string FunctionRuntimeUnknownError { get; internal set; }
+        public static readonly string StorageAccountNullOrEmpty = $"/heal_actions/{nameof(StorageAccountNullOrEmpty)}";
+        public static readonly string FunctionAppIsStopped = $"/heal_actions/{nameof(FunctionAppIsStopped)}";
+        public static readonly string AppIsMisconfiguredInAzure = $"/heal_actions/{nameof(AppIsMisconfiguredInAzure)}";
+        public static readonly string DefaultHostNameNotResolving = $"/heal_actions/{nameof(DefaultHostNameNotResolving)}";
+        public static readonly string KeysUnexpectedError = $"/heal_actions/{nameof(KeysUnexpectedError)}";
+        public static readonly string ScaledOutStamp = $"/heal_actions/{nameof(ScaledOutStamp)}";
+        public static readonly string FunctionRuntimeCrashLoop = $"/heal_actions/{nameof(FunctionRuntimeCrashLoop)}";
+        public static readonly string FunctionRuntimeUnknownError = $"/heal_actions/{nameof(FunctionRuntimeUnknownError)}";
     }
 }
