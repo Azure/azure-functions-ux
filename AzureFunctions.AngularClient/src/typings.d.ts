365122fd62ac963b4ac657b5a4dac0925982f8ae
John Papa <john@Johns-MBP.lan>
2017-04-11 23:49:36 -0400

    Angular v4 and CLI v1 upgrade

--- AzureFunctions.AngularClient/src/typings.d.ts
+++ AzureFunctions.AngularClient/src/typings.d.ts
@@ -2,3 +2,9 @@
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
 
 declare var System: any;
+
+/* SystemJS module definition */
+declare var module: NodeModule;
+interface NodeModule {
+  id: string;
+}
