ddbbe2fe7a0a3205e0b48364c785dec9943c6e19
John Papa <john@Johns-MBP.lan>
2017-04-20 20:19:12 -0400

    put is baked into http. not an RxJS thing

--- AzureFunctions.AngularClient/src/app/shared/function-app.ts
+++ AzureFunctions.AngularClient/src/app/shared/function-app.ts
@@ -6,7 +6,6 @@ import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/operator/put';
 import 'rxjs/add/operator/retryWhen';
 import 'rxjs/add/operator/scan';
 import 'rxjs/add/observable/of';
