22a479316e2ecec393d2566cd855c8cb41e2d386
Alexey Rodionov <alrod@microsoft.com>
2017-05-12 18:26:29 -0700

    Request body won't sync up if run function then switch to another function. Fixes #1247

--- AzureFunctions.AngularClient/src/app/run-http/run-http.component.ts
+++ AzureFunctions.AngularClient/src/app/run-http/run-http.component.ts
@@ -24,7 +24,6 @@ export class RunHttpComponent {
     }
 
     set functionInfo(value: FunctionInfo) {
-        debugger;
         if (value.test_data) {
             try {
                 this.model = JSON.parse(value.test_data);
