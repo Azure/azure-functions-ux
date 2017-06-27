5329539a746dcb77c832bea874fde5cf3a5faa2b
Aniello <andimarc@microsoft.com>
2017-06-27 14:07:18 -0700

    Update BusyStateComponent to support multiple 'scoped' states in a single instance

--- AzureFunctions.AngularClient/src/app/busy-state/busy-state.component.ts
+++ AzureFunctions.AngularClient/src/app/busy-state/busy-state.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
+import { Guid } from './../shared/Utilities/Guid';
 
 @Component({
     selector: 'busy-state',
@@ -11,21 +13,60 @@ export class BusyStateComponent implements OnInit {
     @Input() name: string;
     isGlobal: boolean = false;
     @Input() message: string;
+    @Output() clear = new Subject<any>();
+
+    private busyStateMap: { [key: string]: boolean } = {};
+    private reservedKey: string = "-";
 
     ngOnInit() {
         this.isGlobal = this.name === 'global';
     }
 
     setBusyState() {
-        this.busy = true;
+        this.setScopedBusyState(this.reservedKey);
     }
 
-    clearBusyState() {
+    setScopedBusyState(key: string): string {
+        key = key || Guid.newGuid();
+        this.busyStateMap[key] = true;
+        this.busy = true;
+        return key;
+    }
+
+    clearBusyState(key?: string) {
+        key = key || this.reservedKey;
+        if (this.busyStateMap[key]) {
+            delete this.busyStateMap[key];
+        }
+        this.busy = !this.isEmptyMap(this.busyStateMap);
+    }
+
+    clearOverallBusyState() {
+        this.busyStateMap = {};
+        this.clear.next(1);
         this.busy = false;
+    }
+
+    getBusyState(): boolean {
+        return this.getScopedBusyState(this.reservedKey);
+    }
+
+    getScopedBusyState(key: string): boolean {
+        return !!key && !!this.busyStateMap[key];
     }
 
     get isBusy(): boolean {
         return this.busy;
+    }
+
+    // http://stackoverflow.com/a/20494546/3234163
+    isEmptyMap(map: any): boolean {
+        for (var key in map) {
+            if (map.hasOwnProperty(key)) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
