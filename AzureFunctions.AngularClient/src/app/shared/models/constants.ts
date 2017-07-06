8b0dafc1347872b9c4d98876681788580bc33f6a
shrirs@microsoft.com <shrirs@microsoft.com>
2017-07-05 20:36:32 -0700

    Service host url has port missing for OnPrem
    - Minor change to ensure the port number is part of the service host URL for OnPrem.

--- AzureFunctions.AngularClient/src/app/shared/models/constants.ts
+++ AzureFunctions.AngularClient/src/app/shared/models/constants.ts
@@ -1,4 +1,4 @@
-export class HttpMethods {
+﻿export class HttpMethods {
     public GET = "get";
     public POST = "post";
     public DELETE = "delete";
@@ -13,7 +13,7 @@ export class HttpMethods {
 
 export class Constants {
     public static serviceHost =
-        window.location.hostname === "localhost" || window.appsvc.env.runtimeType === "Standalone"
+        window.location.hostname === "localhost" || window.appsvc.env.runtimeType === "Standalone" || window.appsvc.env.runtimeType === "OnPrem"
         ? `https://${window.location.hostname}:${window.location.port}/`
         : `https://${window.location.hostname}/`;
 
