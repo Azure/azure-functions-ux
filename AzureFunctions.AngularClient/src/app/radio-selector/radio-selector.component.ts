66f1f4818f6a9fd69c711e5d845274e01841ae65
Aniello <andimarc@microsoft.com>
2017-07-17 15:42:44 -0700

    Updating RadioSelectorComponent to support binding to FormControl

--- AzureFunctions.AngularClient/src/app/radio-selector/radio-selector.component.ts
+++ AzureFunctions.AngularClient/src/app/radio-selector/radio-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnChanges, SimpleChanges } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
 import { SelectOption } from '../shared/models/select-option';
 
@@ -7,19 +8,70 @@ import { SelectOption } from '../shared/models/select-option';
     templateUrl: './radio-selector.component.html',
     styleUrls: ['./radio-selector.component.scss'],
 })
-export class RadioSelectorComponent<T> {
+export class RadioSelectorComponent<T> implements OnChanges {
+    public selectedValue: T = null;
+    @Input() control: FormControl;
+    @Input() group: FormGroup;
+    @Input() name: string;
     @Input() options: SelectOption<T>[];
     @Input() disabled: boolean;
+    @Input() highlightDirty: boolean;
     @Input() public defaultValue: T;
     @Output() public value: Subject<T>;
+
 
     constructor() {
         this.value = new EventEmitter<T>();
     }
 
+
+    private _setControl() {
+        if (this.group && this.name) {
+            this.control = <FormControl>this.group.controls[this.name];
+        }
+    }
+
+    private _setControlValue(value: T, markDirtyIfChanged: boolean) {
+        if (this.control) {
+            if (markDirtyIfChanged && this.control.value !== value) {
+                this.control.markAsDirty();
+            }
+            this.control.setValue(value);
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        //If control and defaultValue are modified at the same time, the value of defaultValue will be used.
+        //If only one input is modifed, the value of that input will be used.
+
+        let value = null;
+        let valueChanged = false;
+
+        if (changes['control']) {
+            value = this.control ? this.control.value : null;
+            valueChanged = true;
+        }
+        else if (changes['group'] || changes['name']) {
+            this._setControl();
+            value = this.control ? this.control.value : null;
+            valueChanged = true;
+        }
+
+        if (changes['defaultValue']) {
+            value = this.defaultValue;
+            valueChanged = true;
+        }
+
+        if (valueChanged) {
+            this.selectedValue = value;
+            this.value.next(this.selectedValue);
+        }
+    }
+
     select(option: SelectOption<T>) {
-        if (!this.disabled) {
-            this.defaultValue = option.value;
+        if (this.control ? !this.control.disabled : !this.disabled) {
+            this._setControlValue(option.value, true);
+            this.selectedValue = option.value;
             this.value.next(option.value);
         }
     }
