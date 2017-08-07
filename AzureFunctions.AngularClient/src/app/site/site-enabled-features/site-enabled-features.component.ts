633e506c99a4fdd98c9213ae20c64d97ee2f39ef
Allison Gruninger <agruning@purdue.edu>
2017-08-07 11:12:39 -0700

    start arrow and tab navigation for enabled features

--- AzureFunctions.AngularClient/src/app/site/site-enabled-features/site-enabled-features.component.ts
+++ AzureFunctions.AngularClient/src/app/site/site-enabled-features/site-enabled-features.component.ts
@@ -1,7 +1,9 @@
+import { ElementRef } from '@angular/core';
+import { Dom } from './../../shared/Utilities/dom';
 import { SiteDashboardComponent } from './../site-dashboard/site-dashboard.component';
-import { SiteTabIds } from './../../shared/models/constants';
+import { SiteTabIds, KeyCodes } from './../../shared/models/constants';
 import { Url } from './../../shared/Utilities/url';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/distinctUntilChanged';
@@ -46,6 +48,9 @@ export class SiteEnabledFeaturesComponent {
     private _site: ArmObj<Site>;
     private _siteSubject = new Subject<ArmObj<Site>>();
     private _descriptor: SiteDescriptor;
+    private _focusedFeatureIndex = -1;
+
+    @ViewChild('enabledFeatures') featureList: ElementRef;
 
     constructor(
         private _cacheService: CacheService,
@@ -128,8 +133,18 @@ export class SiteEnabledFeaturesComponent {
 
                 this._mergeFeaturesIntoF1(this.featureItems, latestFeatureItems);
                 this._saveFeatures(this.featureItems);
+
+                const features = this._getFeatures();
+
+                if (features.length > 0) {
+                    const feature = Dom.getTabbableControl(features[0].children[1]);
+                    feature.tabIndex = 0;
+                    this._focusedFeatureIndex = 0;
+                }
             });
     }
+
+
 
     set siteInput(site: ArmObj<Site>) {
         if (!site) {
@@ -490,5 +505,54 @@ export class SiteEnabledFeaturesComponent {
 
                 return items;
             });
+    }
+
+    private _getFeatures() {
+        return this.featureList.nativeElement.children;
+    }
+
+    private _clearFocusOnFeature(features: any, index: number) {
+        const oldFeature = Dom.getTabbableControl(features[index].children[1]);
+        Dom.clearFocus(oldFeature);
+    }
+
+    private _setFocusOnFeature(features: any, index: number) {
+        let finalIndex = -1;
+        let destFeature: HTMLElement;
+
+        if (index >= 0 && index < features.length) {
+            finalIndex = index;
+            destFeature = features[index].children[1];
+        } else if (index === -1) {
+            finalIndex = 0;
+            destFeature = features[0].children[1];
+        } else {
+            finalIndex = features.length - 1;
+            destFeature = features[finalIndex];
+        }
+
+        if (destFeature) {
+            const newFeature = Dom.getTabbableControl(destFeature);
+            Dom.setFocus(newFeature);
+        }
+
+        this._focusedFeatureIndex = finalIndex;
+    }
+
+
+    onKeyPress(event: KeyboardEvent) {
+        if (event.keyCode === KeyCodes.enter) {
+
+        } else if (event.keyCode === KeyCodes.arrowDown) {
+            const features = this._getFeatures();
+            this._clearFocusOnFeature(features, this._focusedFeatureIndex);
+            this._setFocusOnFeature(features, this._focusedFeatureIndex + 1);
+
+        } else if (event.keyCode === KeyCodes.arrowUp) {
+            const features = this._getFeatures();
+            this._clearFocusOnFeature(features, this._focusedFeatureIndex);
+            this._setFocusOnFeature(features, this._focusedFeatureIndex - 1);
+
+        }
     }
 }
