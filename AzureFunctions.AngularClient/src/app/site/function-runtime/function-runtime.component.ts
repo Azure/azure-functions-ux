cebac2e40fe511f4cfe6d0f29fb05d54dad84f85
Alexey Rodionov <alrod@microsoft.com>
2017-09-19 11:51:06 -0700

    Fix PR1

--- AzureFunctions.AngularClient/src/app/site/function-runtime/function-runtime.component.ts
+++ AzureFunctions.AngularClient/src/app/site/function-runtime/function-runtime.component.ts
@@ -347,20 +347,30 @@ export class FunctionRuntimeComponent implements OnDestroy {
         return this._updateContainerVersion(r.json(), version);
       })
       .mergeMap(r => {
-        return this.functionApp.getFunctionHostStatus().map((hostStatus: HostStatus) => {
-          debugger;
-          if (hostStatus.version === this.exactExtensionVersion) {
-            throw Error('Host version is not updated yet');
+        return this.functionApp.getFunctionHostStatus()
+        .map((hostStatus: HostStatus) => {
+          if (!hostStatus.version || hostStatus.version === this.exactExtensionVersion) {
+            throw Observable.throw('Host version is not updated yet');
           }
           return hostStatus;
         })
         .retryWhen(error => {
-          debugger;
-          return error.delay(5000);
+          return error.scan((errorCount: number, err: any) => {
+            if (errorCount >= 20) {
+                throw err;
+            } else {
+                return errorCount + 1;
+            }
+          }, 0).delay(3000);
         });
       })
+      .do(null, e => {
+        this._busyState.clearBusyState();
+        this._aiService.trackException(e, '/errors/rutime-update');
+        console.error(e);
+      })
       .subscribe((hostStatus: HostStatus) => {
-        debugger;
+
         this.exactExtensionVersion = hostStatus ? hostStatus.version : '';
         this.extensionVersion = version;
         this.setNeedUpdateExtensionVersion();
