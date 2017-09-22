71d19faca487c2a79858d9fb8aa7555dad55ca63
Aniello Scotto Di Marco <andimarc@microsoft.com>
2017-09-21 18:05:03 -0700

    Updating DropDownComponent to support option groups.

--- AzureFunctions.AngularClient/src/app/shared/models/drop-down-element.ts
+++ AzureFunctions.AngularClient/src/app/shared/models/drop-down-element.ts
@@ -5,6 +5,11 @@
     default?: boolean;
 }
 
+export interface DropDownGroupElement<T> {
+    displayLabel: string,
+    dropDownElements: DropDownElement<T>[]
+}
+
 export interface MultiDropDownElement<T> extends DropDownElement<T> {
     isSelected?: boolean;
     isFocused?: boolean;
