a078139d0ce25794a27f4081d8980d8c909704c6
travih <travih@microsoft.com>
2017-09-25 11:38:05 -0700

    Commit Comments

--- AzureFunctions.AngularClient/src/app/ibiza-feature/app-settings-shell/app-settings-shell.component.ts
+++ AzureFunctions.AngularClient/src/app/ibiza-feature/app-settings-shell/app-settings-shell.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit, Type } from '@angular/core';
+import { Component, Type, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { PortalResources } from 'app/shared/models/portal-resources';
 import { ActivatedRoute } from '@angular/router';
 import { SiteConfigComponent } from 'app/site/site-config/site-config.component';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: 'app-app-settings-shell',
     templateUrl: './app-settings-shell.component.html',
     styleUrls: ['./app-settings-shell.component.scss']
 })
-export class AppSettingsShellComponent implements OnInit {
+export class AppSettingsShellComponent implements OnDestroy {
     title: string;
 
     id: string;
@@ -24,6 +25,7 @@ export class AppSettingsShellComponent implements OnInit {
 
     iconUrl = '';
 
+    private routeParamsSubscription: Subscription;
     constructor(translateService: TranslateService, route: ActivatedRoute) {
         this.title = translateService.instant(PortalResources.tab_configuration);
         this.componentFactory = SiteConfigComponent;
@@ -33,7 +35,7 @@ export class AppSettingsShellComponent implements OnInit {
             resourceId: null
         };
 
-        route.params.subscribe(x => {
+        this.routeParamsSubscription = route.params.subscribe(x => {
             this.componentInput = {
                 viewInfoInput: {
                     resourceId: `/subscriptions/${x['subscriptionId']}/resourceGroups/${x[
@@ -44,5 +46,7 @@ export class AppSettingsShellComponent implements OnInit {
         });
     }
 
-    ngOnInit() {}
+    ngOnDestroy(): void {
+        this.routeParamsSubscription.unsubscribe();
+    }
 }
