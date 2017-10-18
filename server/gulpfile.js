5957a00448d3e48d31f18a5994276359c4d589dc
Travis Harris <travistylerharris@outlook.com>
2017-10-18 10:19:45 -0700

    PR comments

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -9,71 +9,79 @@ const path = require('path');
 const merge = require('gulp-merge-json');
 const del = require('del');
 
-gulp.task('build-resources', function (cb) {
+/********
+*   This is the task that is actually run in the cli, it will run the other tasks in the appropriate order
+*/
+gulp.task('build-resources', function(cb) {
     runSequence('resources-convert', 'resources-build', 'resources-combine', 'resources-clean', cb);
 });
 
-gulp.task('resources-clean', function () {
+/********
+*   In the process of building resources, intermediate folders are created for processing, this cleans them up at the end of the process
+*/
+gulp.task('resources-clean', function() {
     return del(['resources-convert', 'templateResoureces-convert', 'resources-build', 'templateresources-build']);
 });
 
-gulp.task('resources-convert', function () {
-    const s = gulp
+/********
+*   This task takes the Resource Resx files from both templates folder and Portal Resources Folder and converts them to json, it drops them into a intermediate 'convert' folder.
+*   Also it will change the file name format to Resources.<language code>.json
+*/
+gulp.task('resources-convert', function() {
+    const portalResourceStream = gulp
         .src(['../AzureFunctions/ResourcesPortal/**/Resources.resx'])
         .pipe(resx2())
         .pipe(
-        rename(function (p) {
-            const language = p.dirname.split(path.sep)[0];
-            if (!!language && language !== '.') {
-                p.basename = 'Resources.' + language;
-            }
-            p.dirname = '.';
-            p.extname = '.json';
-        })
+            rename(function(p) {
+                const language = p.dirname.split(path.sep)[0];
+                if (!!language && language !== '.') {
+                    p.basename = 'Resources.' + language;
+                }
+                p.dirname = '.';
+                p.extname = '.json';
+            })
         )
         .pipe(gulp.dest('resources-convert'));
 
-    const t = gulp
+    const templateResourceStream = gulp
         .src(['templates/**/Resources/**/Resources.resx'])
         .pipe(resx2())
         .pipe(
-        rename(function (p) {
-            const parts = p.dirname.split(path.sep);
-            const version = parts[0];
+            rename(function(p) {
+                const parts = p.dirname.split(path.sep);
+                const version = parts[0];
 
-            const language = parts.length > 2 ? parts[parts.length - 2] : null;
-            if (!!language && language !== '.') {
-                p.basename = 'Resources.' + language;
-            }
-            p.dirname = '.' + path.sep + version + path.sep;
-            p.extname = '.json';
-        })
+                const language = parts.length > 2 ? parts[parts.length - 2] : null;
+                if (!!language && language !== '.') {
+                    p.basename = 'Resources.' + language;
+                }
+                p.dirname = '.' + path.sep + version + path.sep;
+                p.extname = '.json';
+            })
         )
         .pipe(gulp.dest('templateResoureces-convert'));
-    return gulpMerge(s, t);
+    return gulpMerge(portalResourceStream, templateResourceStream);
 });
 
-gulp.task('resources-build', function () {
+
+/********
+*   This is the task takes the output of the  convert task and formats the json to be in the format that gets sent back to the client by the API, it's easier to do this here than at the end
+*/
+gulp.task('resources-build', function() {
     const streams = [];
     streams.push(
         gulp
             .src(['resources-convert/**/Resources.*.json'])
             .pipe(
-            jeditor(
-                function (json) {
+                jeditor(function(json) {
                     const enver = require(path.normalize('../server/resources-convert/Resources.json'));
-                    t = {
+                    const retVal = {
                         lang: json,
                         en: enver
                     };
 
-                    return t;
-                },
-                {
-                    indent_char: ' ',
-                    indent_size: 4
-                }
-            )
+                    return retVal;
+                })
             )
             .pipe(gulp.dest('resources-build'))
     );
@@ -82,44 +90,35 @@ gulp.task('resources-build', function () {
         gulp
             .src(['resources-convert/Resources.json'])
             .pipe(
-            jeditor(
-                function (json) {
-                    t = {
+                jeditor(function(json) {
+                    const retVal = {
                         en: json
                     };
 
-                    return t;
-                },
-                {
-                    indent_char: ' ',
-                    indent_size: 4
-                }
-            )
+                    return retVal;
+                })
             )
             .pipe(gulp.dest('resources-build'))
     );
 
+    //This fetches all version folders for templates and makes sure the appropriate action is done to each one
     const TemplateVersionDirectories = getSubDirectories('templateResoureces-convert');
     TemplateVersionDirectories.forEach(x => {
         streams.push(
             gulp
                 .src('templateResoureces-convert/' + x + '/Resources.*.json')
                 .pipe(
-                jeditor(
-                    function (json) {
-                        const enver = require(path.normalize('../server/templateResoureces-convert/' + x + '/Resources.json'));
-                        t = {
-                            lang: json,
-                            en: enver
-                        };
+                    jeditor(
+                        function(json) {
+                            const enver = require(path.normalize('../server/templateResoureces-convert/' + x + '/Resources.json'));
+                            const retVal = {
+                                lang: json,
+                                en: enver
+                            };
 
-                        return t;
-                    },
-                    {
-                        indent_char: ' ',
-                        indent_size: 4
-                    }
-                )
+                            return retVal;
+                        }
+                    )
                 )
                 .pipe(gulp.dest('templateresources-build/' + x))
         );
@@ -128,19 +127,15 @@ gulp.task('resources-build', function () {
             gulp
                 .src('templateResoureces-convert/' + x + '/Resources.json')
                 .pipe(
-                jeditor(
-                    function (json) {
-                        t = {
-                            en: json
-                        };
+                    jeditor(
+                        function(json) {
+                            const retVal = {
+                                en: json
+                            };
 
-                        return t;
-                    },
-                    {
-                        indent_char: ' ',
-                        indent_size: 4
-                    }
-                )
+                            return retVal;
+                        }
+                    )
                 )
                 .pipe(gulp.dest('templateresources-build/' + x))
         );
@@ -151,13 +146,17 @@ gulp.task('resources-build', function () {
 /*************
  * Resources Combining
  * https://stackoverflow.com/questions/46605923/gulp-merge-json-files-from-different-folders-while-keeping-folder-structure
+ * 
+ * This tasks goes through each template version folder and combines it with the corresponding portal resource file(matched by name) and deposits it into the /src/actions/resources folder for the API to consume.
+ * It also builds a version which contains no version, mostly for development purposes
+ * The end file name format is Resources.<language code>.<template version>.json for the template includes, for the default no template it'll be Resources.<language code>.json, also the english version will have no language code, it'll just be default
  */
 const files = [];
 const parentFolders = [];
 let streams = [];
 const baseNames = [];
 
-gulp.task('resources-combine', function () {
+gulp.task('resources-combine', function() {
     const TemplateVersionDirectories = getSubDirectories('templateresources-build');
     const s = [];
     TemplateVersionDirectories.forEach(x => {
@@ -176,9 +175,9 @@ gulp.task('resources-combine', function () {
                     .src(stream)
                     .pipe(merge({ fileName: fileName }))
                     .pipe(
-                    rename(function (p) {
-                        p.basename += '.' + x;
-                    })
+                        rename(function(p) {
+                            p.basename += '.' + x;
+                        })
                     )
                     .pipe(gulp.dest('src/actions/resources'))
             );
@@ -202,10 +201,10 @@ function getSubDirectories(folder) {
 function getFiles(folders) {
     let possibleDirectory;
 
-    folders.forEach(function (folder, index) {
+    folders.forEach(function(folder, index) {
         let tempFiles = fs.readdirSync('./' + folder);
 
-        tempFiles.forEach(function (fileOrDirectory) {
+        tempFiles.forEach(function(fileOrDirectory) {
             possibleDirectory = path.join(folder, fileOrDirectory);
             if (fs.lstatSync(possibleDirectory).isDirectory()) {
                 getFiles([possibleDirectory]);
@@ -221,7 +220,7 @@ function getFiles(folders) {
 }
 
 function makeStreams() {
-    files.forEach(function (file) {
+    files.forEach(function(file) {
         let thisParentFolders = path.dirname(file).substr(file.indexOf(path.sep));
 
         if (parentFolders.indexOf(thisParentFolders) === -1) {
@@ -229,12 +228,12 @@ function makeStreams() {
         }
     });
 
-    parentFolders.forEach(function (folder) {
+    parentFolders.forEach(function(folder) {
         let foldersFile = folder.substr(folder.indexOf(path.sep));
 
-        baseNames.forEach(function (baseName) {
+        baseNames.forEach(function(baseName) {
             streams.push(
-                files.filter(function (file) {
+                files.filter(function(file) {
                     return file.endsWith(path.join(foldersFile, baseName));
                 })
             );
--- server/src/actions/metadata.ts
+++ server/src/actions/metadata.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
-import * as fs from 'fs';
+import * as fs from 'async-file';
+import * as path from 'path';
 const bindingConfig = require('./binding-config.json');
 
 const _languageMap: { [key: string]: string } = {
@@ -14,7 +15,7 @@ export function getBindingConfig(_: Request, res: Response) {
     res.json(bindingConfig);
 }
 const defaultRuntimeVersion = '~1';
-export function getResources(req: Request, res: Response) {
+export async function getResources(req: Request, res: Response) {
     const runtime: string = req.query['runtime'] || defaultRuntimeVersion;
     const name: string = req.query['name'] || 'en';
 
@@ -32,19 +33,28 @@ export function getResources(req: Request, res: Response) {
     //this is the ideal item to return, it is the correct language and version asked for
     var versionFile = langCode === 'en' ? `Resources.${cleanRuntimeVersion}.json` : `Resources.${langCode}.${cleanRuntimeVersion}.json`;
     //This means the version asked for don't exist but the strings for hte default version will be returned
+    fs;
     var defaultVersionFile =
         langCode === 'en' ? `Resources.${cleanDefaultRuntimeVersion}.json` : `Resources.${langCode}.${cleanDefaultRuntimeVersion}.json`;
     //This is for development only so people can develop without having a templates folder laid out
-    var defaultFallbackFile =
-        langCode === 'en' ? `Resources.json` : `Resources.${langCode}.json`;
+    var defaultFallbackFile = langCode === 'en' ? `Resources.json` : `Resources.${langCode}.json`;
 
-    var folder = './resources/';
-    if (fs.existsSync(`${folder}${versionFile}`)) {
-        res.json(require(`${folder}${versionFile}`));
-    } else if (fs.existsSync(`${folder}${defaultVersionFile}`)) {
-        res.json(require(`${folder}${defaultVersionFile}`));
-    } else {
-        res.json(require(`${folder}${defaultFallbackFile}`));
+    var folder = path.join(__dirname, 'resources');
+    try {
+        let payload = await fs.readFile(path.join(folder, versionFile));
+        res.json(JSON.parse(payload));
+    } catch (e) {
+        try {
+            let payload = await fs.readFile(path.join(folder, defaultVersionFile));
+            res.json(JSON.parse(payload));
+        } catch (e) {
+            try {
+                let payload = await fs.readFile(path.join(folder, defaultFallbackFile));
+                res.json(JSON.parse(payload));
+            } catch (e) {
+                res.sendStatus(404);
+            }
+        }
     }
 }
 
