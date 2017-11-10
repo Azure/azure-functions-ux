73790934d56f6732e594e587abb7568e6e539387
asfarok <afarok.palash@gmail.com>
2017-11-10 11:26:12 -0800

    some minor css and standalone refactoring

--- AzureFunctions.AngularClient/src/app/top-bar-standalone-loginuser/top-bar-standalone-loginuser.component.ts
+++ AzureFunctions.AngularClient/src/app/top-bar-standalone-loginuser/top-bar-standalone-loginuser.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { User } from '../shared/models/user';
+import { ConfigService } from './../shared/services/config.service';
+
 
 @Component({
     selector: 'top-bar-standalone-loginuser',
@@ -8,6 +10,8 @@ import { User } from '../shared/models/user';
 })
 export class TopBarStandAloneLoginUserComponent {
     @Input() user: User;
-    constructor() {
+    public isStandalone: boolean;
+    constructor(private _configService: ConfigService) {
+        this.isStandalone = this._configService.isStandalone();
     }
 }
