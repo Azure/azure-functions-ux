8f5b67843beb6c3c3825a667cfa03b727532a6d2
Elliott Hamai <ellhamai@microsoft.com>
2018-01-02 10:36:08 -0800

    Closes #2026, #2162 - Clicking always on alert does nothing and refresh icon querying /apps

--- AzureFunctions.AngularClient/src/app/function-edit/function-edit.component.ts
+++ AzureFunctions.AngularClient/src/app/function-edit/function-edit.component.ts
@@ -198,7 +198,13 @@ export class FunctionEditComponent implements OnDestroy {
                         message: this._translateService.instant(PortalResources.topBar_alwaysOn),
                         iconClass: 'fa fa-exclamation-triangle warning',
                         learnMoreLink: 'https://go.microsoft.com/fwlink/?linkid=830855',
-                        clickCallback: null
+                        clickCallback: () =>{
+                            this._broadcastService.broadcastEvent<TreeUpdateEvent>(BroadcastEvent.TreeUpdate, {
+                                operation: 'navigate',
+                                resourceId: this.context.site.id,
+                                data: SiteTabIds.applicationSettings
+                            });
+                        }
                     });
                 }
             }
