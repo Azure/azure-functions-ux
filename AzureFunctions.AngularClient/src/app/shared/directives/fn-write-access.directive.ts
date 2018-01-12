0fb35d394fe5b30ec0297a435ccac26f25ba0774
Ahmed ElSayed <ahmels@microsoft.com>
2018-01-12 14:47:14 -0800

    Close subject and bind 'this' to getFunctionAppEditMode call in FnWriteAccessDirective

--- AzureFunctions.AngularClient/src/app/shared/directives/fn-write-access.directive.ts
+++ AzureFunctions.AngularClient/src/app/shared/directives/fn-write-access.directive.ts
@@ -1,7 +1,7 @@
 import { FunctionAppService } from 'app/shared/services/function-app.service';
 import { FunctionAppContext } from './../function-app-context';
 import { EditModeHelper } from './../Utilities/edit-mode.helper';
-import { Directive, Input, ElementRef } from '@angular/core';
+import { Directive, Input, ElementRef, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/switchMap';
@@ -10,19 +10,28 @@ import 'rxjs/add/operator/switchMap';
 @Directive({
     selector: '[fnWriteAccess]',
 })
-export class FnWriteAccessDirective {
+export class FnWriteAccessDirective implements OnDestroy {
 
     functionAppContextStream: Subject<FunctionAppContext>;
 
-    @Input('fnWriteAccess') set functionAppContext(value) {
-        this.functionAppContextStream.next(value);
+    @Input('fnWriteAccess')
+    set functionAppContext(value) {
+        if (this.functionAppContextStream && !this.functionAppContextStream.closed) {
+            this.functionAppContextStream.next(value);
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.functionAppContextStream.complete();
+        delete this.functionAppContextStream;
     }
 
     constructor(private elementRef: ElementRef, functionAppService: FunctionAppService) {
         this.functionAppContextStream = new Subject<FunctionAppContext>();
 
         this.functionAppContextStream
-            .switchMap(functionAppService.getFunctionAppEditMode)
+            .filter(c => !!c)
+            .switchMap(c => functionAppService.getFunctionAppEditMode(c))
             .map(result => result.isSuccessful ? EditModeHelper.isReadOnly(result.result) : false)
             .subscribe(isReadOnly => {
                 if (isReadOnly) {
