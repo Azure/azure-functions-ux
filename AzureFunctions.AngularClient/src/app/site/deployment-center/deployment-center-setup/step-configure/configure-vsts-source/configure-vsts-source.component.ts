247ffe751cce26ae6a5c850b625b4d04630e8c96
Travis Harris <travistylerharris@outlook.com>
2018-01-12 12:17:10 -0800

    Cleaning up vsts source

--- AzureFunctions.AngularClient/src/app/site/deployment-center/deployment-center-setup/step-configure/configure-vsts-source/configure-vsts-source.component.ts
+++ AzureFunctions.AngularClient/src/app/site/deployment-center/deployment-center-setup/step-configure/configure-vsts-source/configure-vsts-source.component.ts
@@ -9,6 +9,9 @@ import { Headers } from '@angular/http';
 import { UserService } from 'app/shared/services/user.service';
 import { Observable } from 'rxjs/Observable';
 import * as _ from 'lodash';
+import { Subject } from 'rxjs/Subject';
+import { VSORepo, VSOAccount } from 'app/site/deployment-center/Models/VSORepo';
+import { forkJoin } from 'rxjs/observable/forkJoin';
 export const PythonFramework = {
     Bottle: 'Bottle',
     Django: 'Django',
@@ -66,16 +69,18 @@ export class ConfigureVstsSourceComponent {
         }
     ];
     chosenBuildFramework: string;
-    private _resourceId: string;
-    //private _chosenRepository: string;
-    public AllGitRepoList: any[];
     public AccountList: DropDownElement<string>[];
     public ProjectList: DropDownElement<string>[];
     public RepositoryList: DropDownElement<string>[];
     public BranchList: DropDownElement<string>[];
     private token: string;
+    private _ngUnsubscribe = new Subject();
 
-    private vstsRepositories: VSTSRepository[];
+    private vstsRepositories: VSORepo[];
+
+    //Subscriptions
+    private _memberIdSubscription = new Subject();
+    private _branchSubscription = new Subject<string>();
     constructor(
         private _wizard: DeploymentCenterWizardService,
         _portalService: PortalService,
@@ -84,43 +89,50 @@ export class ConfigureVstsSourceComponent {
         _aiService: AiService,
         private _userService: UserService
     ) {
-        this._userService.getStartupInfo().subscribe(r => {
+        this._userService.getStartupInfo().takeUntil(this._ngUnsubscribe).subscribe(r => {
             this.token = r.token;
         });
-
-        this._wizard.resourceIdStream.subscribe(r => {
-            this._resourceId = r;
-        });
-
+        this.setupSubscriptions();
         this.populate();
-        this._wizard.wizardForm.controls.buildProvider.valueChanges.distinctUntilChanged().subscribe(r => {
+        this._wizard.wizardForm.controls.buildProvider.valueChanges.distinctUntilChanged().takeUntil(this._ngUnsubscribe).subscribe(r => {
             this.populate();
         });
     }
 
     private populate() {
-        this.GetMemberId()
+        this._memberIdSubscription.next();
+    }
+
+    setupSubscriptions() {
+        this._memberIdSubscription
+            .takeUntil(this._ngUnsubscribe)
+            .switchMap(() => this._cacheService.get('https://app.vssps.visualstudio.com/_apis/profile/profiles/me'))
+            .map(r => r.json())
+            .switchMap(r => this.FetchAccounts(r.id))
             .switchMap(r => {
-                return this.FetchAccounts(r.id);
-            })
-            .switchMap(r => {
-                let projectCalls: Observable<any[]>[] = [];
+                let projectCalls: Observable<VSORepo[]>[] = [];
                 r.forEach(account => {
-                    projectCalls.push(this.FetchProjectsForAccount(account.accountName));
+                    projectCalls.push(
+                        this._cacheService
+                            .get(
+                                `https://${account.accountName}.visualstudio.com/_apis/git/repositories?api-version=1.0`,
+                                true,
+                                this.getHeaders()
+                            )
+                            .map(res => res.json().value)
+                    );
                 });
-                return Observable.forkJoin(projectCalls);
+                return forkJoin(projectCalls);
             })
             .subscribe(r => {
                 this.vstsRepositories = [];
                 r.forEach(repoList => {
                     repoList.forEach(repo => {
-                        let url: string = repo.remoteUrl;
-
                         this.vstsRepositories.push({
                             name: repo.name,
-                            remoteUrl: url,
-                            account: url.split('.')[0].replace('https://', ''),
-                            projectName: repo.project.name,
+                            remoteUrl: repo.remoteUrl,
+                            account: repo.remoteUrl.split('.')[0].replace('https://', ''),
+                            project: repo.project,
                             id: repo.id
                         });
                     });
@@ -135,67 +147,19 @@ export class ConfigureVstsSourceComponent {
                     'value'
                 );
             });
-    }
-    private GetMemberId() {
-        return this._cacheService.get('https://app.vssps.visualstudio.com/_apis/profile/profiles/me').switchMap(r => {
-            return Observable.of(r.json());
-        });
-    }
 
-    private FetchAccounts(memberId: string): Observable<any[]> {
-        const accountsUrl = `https://app.vssps.visualstudio.com/_apis/Commerce/Subscription?memberId=${memberId}&includeMSAAccounts=true&queryOnlyOwnerAccounts=false&inlcudeDisabledAccounts=false&includeMSAAccounts=true&providerNamespaceId=VisualStudioOnline`;
-        return this._cacheService.get(accountsUrl, true, this.getHeaders()).switchMap(r => {
-            const accounts = r.json().value as any[];
-            if (this._wizard.wizardForm.controls.buildProvider.value === 'kudu') {
-                return Observable.of(accounts.filter(x => x.isAccountOwner));
-            } else {
-                return Observable.of(accounts);
-            }
-        });
-    }
-
-    private FetchProjectsForAccount(accountName: string): Observable<any[]> {
-        return this._cacheService
-            .get(`https://${accountName}.visualstudio.com/_apis/git/repositories?api-version=1.0`, true, this.getHeaders())
-            .switchMap(r => {
-                return Observable.of(r.json().value);
-            });
-    }
-
-    AccountChanged(accountName: string) {
-        this.ProjectList = _.uniqBy(
-            this.vstsRepositories.filter(r => r.account === accountName).map(repo => {
-                return {
-                    displayLabel: repo.projectName,
-                    value: repo.projectName
-                };
-            }),
-            'value'
-        );
-    }
-
-    ProjectChanged(projectName: string) {
-        this.RepositoryList = _.uniqBy(
-            this.vstsRepositories.filter(r => r.projectName === projectName).map(repo => {
-                return {
-                    displayLabel: repo.name,
-                    value: repo.remoteUrl
-                };
-            }),
-            'value'
-        );
-    }
-    RepoChanged(repoUri: string) {
-        const repoObj = _.first(this.vstsRepositories.filter(x => x.remoteUrl === repoUri));
-        const repoId = repoObj.id;
-        const account = repoObj.account;
-        this._wizard.wizardForm.controls.sourceSettings.value.repoUrl = repoUri;
-        this._cacheService
-            .get(
-                `https://${account}.visualstudio.com/DefaultCollection/_apis/git/repositories/${repoId}/refs/heads?api-version=1.0`,
-                true,
-                this.getHeaders()
-            )
+        this._branchSubscription
+            .takeUntil(this._ngUnsubscribe)
+            .switchMap(repoUri => {
+                const repoObj = _.first(this.vstsRepositories.filter(x => x.remoteUrl === repoUri));
+                const repoId = repoObj.id;
+                const account = repoObj.account;
+                return this._cacheService.get(
+                    `https://${account}.visualstudio.com/DefaultCollection/_apis/git/repositories/${repoId}/refs/heads?api-version=1.0`,
+                    true,
+                    this.getHeaders()
+                );
+            })
             .subscribe(r => {
                 const branchList = r.json().value;
                 this.BranchList = branchList.map(x => {
@@ -208,9 +172,51 @@ export class ConfigureVstsSourceComponent {
             });
     }
 
+    private FetchAccounts(memberId: string): Observable<VSOAccount[]> {
+        const accountsUrl = `https://app.vssps.visualstudio.com/_apis/Commerce/Subscription?memberId=${memberId}&includeMSAAccounts=true&queryOnlyOwnerAccounts=false&inlcudeDisabledAccounts=false&includeMSAAccounts=true&providerNamespaceId=VisualStudioOnline`;
+        return this._cacheService.get(accountsUrl, true, this.getHeaders()).switchMap(r => {
+            const accounts = r.json().value as VSOAccount[];
+            if (this._wizard.wizardForm.controls.buildProvider.value === 'kudu') {
+                return Observable.of(accounts.filter(x => x.isAccountOwner));
+            } else {
+                return Observable.of(accounts);
+            }
+        });
+    }
+
+    AccountChanged(accountName: string) {
+        this.ProjectList = _.uniqBy(
+            this.vstsRepositories.filter(r => r.account === accountName).map(repo => {
+                return {
+                    displayLabel: repo.project.name,
+                    value: repo.project.name
+                };
+            }),
+            'value'
+        );
+    }
+
+    ProjectChanged(projectName: string) {
+        this.RepositoryList = _.uniqBy(
+            this.vstsRepositories.filter(r => r.project.name === projectName).map(repo => {
+                return {
+                    displayLabel: repo.name,
+                    value: repo.remoteUrl
+                };
+            }),
+            'value'
+        );
+    }
+
+    RepoChanged(repoUri: string) {
+        this._wizard.wizardForm.controls.sourceSettings.value.repoUrl = repoUri;
+        this._branchSubscription.next(repoUri);
+    }
+
     BranchChanged(branch: string) {
         this._wizard.wizardForm.controls.sourceSettings.value.branch = branch;
     }
+
     private getHeaders(): Headers {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
@@ -223,6 +229,7 @@ export class ConfigureVstsSourceComponent {
     frameworkChanged(framework) {
         this.chosenBuildFramework = framework;
     }
+
     get showWorkingDirectory() {
         return true;
     }
