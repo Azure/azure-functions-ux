9d432ec4478e6cf11af9c69aa1594395e8870f14
Travis Harris <travistylerharris@outlook.com>
2018-01-13 00:37:35 -0800

    removing bad debug logging from server code

--- server/src/deployment-center/deployment-center.ts
+++ server/src/deployment-center/deployment-center.ts
@@ -7,7 +7,6 @@ import { setupDropboxAuthentication, getDropboxTokens } from './dropboxAuth';
 export function setupDeploymentCenter(app: Application) {
 	app.get('/api/SourceControlAuthenticationState', async (req, res) => {
 		const result = await Promise.all([ getBitbucketTokens(req), getGithubTokens(req), getOnedriveTokens(req), getDropboxTokens(req) ]);
-		console.log(result);
 		res.send({
 			github: result[1].authenticated,
 			onedrive: result[2].authenticated,
