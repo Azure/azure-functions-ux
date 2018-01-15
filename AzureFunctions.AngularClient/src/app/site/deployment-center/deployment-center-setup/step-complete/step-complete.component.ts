abd2e940a022d67ea144ce859aa8df6be03cd611
Travis Harris <travistylerharris@outlook.com>
2018-01-15 13:03:32 -0800

    adding some support for summary of Local Git Choice

--- AzureFunctions.AngularClient/src/app/site/deployment-center/deployment-center-setup/step-complete/step-complete.component.ts
+++ AzureFunctions.AngularClient/src/app/site/deployment-center/deployment-center-setup/step-complete/step-complete.component.ts
@@ -6,6 +6,10 @@ import { Observable } from 'rxjs/Observable';
 import { BroadcastService } from 'app/shared/services/broadcast.service';
 import { BroadcastEvent } from 'app/shared/models/broadcast-event';
 import { BusyStateScopeManager } from 'app/busy-state/busy-state-scope-manager';
+import { Subject } from 'rxjs/Subject';
+import { ArmObj } from 'app/shared/models/arm/arm-obj';
+import { Site } from 'app/shared/models/arm/site';
+import { PublishingCredentials } from 'app/shared/models/publishing-credentials';
 
 @Component({
     selector: 'app-step-complete',
@@ -15,22 +19,59 @@ import { BusyStateScopeManager } from 'app/busy-state/busy-state-scope-manager';
 export class StepCompleteComponent implements OnInit {
     resourceId: string;
     private _busyManager: BusyStateScopeManager;
-
+    private _ngUnsubscribe = new Subject();
+    private site: ArmObj<Site>;
+    private pubCreds: ArmObj<PublishingCredentials>;
+    private pubUserName: ArmObj<{
+        publishingUserName: string;
+    }>;
     constructor(
         public wizard: DeploymentCenterWizardService,
-        cacheService: CacheService,
+        private _cacheService: CacheService,
         private _armService: ArmService,
         private _broadcastService: BroadcastService
     ) {
         this._busyManager = new BusyStateScopeManager(_broadcastService, 'site-tabs');
 
-        this.wizard.resourceIdStream.subscribe(r => {
-            this.resourceId = r;
-        });
+        this.wizard.resourceIdStream
+            .takeUntil(this._ngUnsubscribe)
+            .switchMap(resourceId => {
+                return Observable.zip(
+                    this._cacheService.getArm(resourceId, false),
+                    this._cacheService.postArm(`${resourceId}/config/publishingcredentials/list`, false),
+                    this._cacheService.getArm(`/providers/Microsoft.Web/publishingUsers/web`, false),
+                    (site, pubCreds, publishingUser) => ({
+                        site: site.json(),
+                        pubCreds: pubCreds.json(),
+                        publishingUser: publishingUser.json()
+                    })
+                );
+            })
+            .subscribe(r => {
+                this.site = r.site;
+                this.pubCreds = r.pubCreds;
+                this.pubUserName = r.publishingUser;
+            });
+    }
+
+    get LocalGitCloneUri() {
+        const publishingUsername = this.pubUserName && this.pubUserName.properties.publishingUserName;
+        const scmUri = this.pubCreds && this.pubCreds.properties.scmUri.split('@')[1];
+        const siteName = this.site && this.site.name;
+        return `https://${publishingUsername}@${scmUri}:443/${siteName}.git`;
     }
 
     get summaryItems() {
-        return this.wizard.summaryItems;
+        let sumItems = this.wizard.summaryItems;
+        if (
+            this.wizard.wizardForm.controls &&
+            this.wizard.wizardForm.controls.sourceProvider &&
+            this.wizard.wizardForm.controls.sourceProvider.value === 'localgit'
+        ) {
+            sumItems.push({ name: 'Repository', value: this.LocalGitCloneUri });
+            sumItems.push({ name: 'Branch', value: 'master' });
+        }
+        return sumItems;
     }
     Save() {
         this._busyManager.setBusy();
