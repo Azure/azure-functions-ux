d47ef4f5712cb16eafb1db1fb17d98cc7171febb
Travis Harris <travih@microsoft.com>
2018-02-25 20:49:22 -0800

    Deployment slot fixes

--- AzureFunctions.AngularClient/src/app/site/deployment-center/deployment-center-setup/step-deployment-slot/step-deployment-slot.component.ts
+++ AzureFunctions.AngularClient/src/app/site/deployment-center/deployment-center-setup/step-deployment-slot/step-deployment-slot.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { DeploymentCenterStateManager } from '../wizard-logic/deployment-center-state-manager';
 import { DropDownElement } from '../../../../shared/models/drop-down-element';
+import { ArmService } from '../../../../shared/services/arm.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-step-deployment-slot',
   templateUrl: './step-deployment-slot.component.html',
   styleUrls: ['./step-deployment-slot.component.scss']
 })
-export class StepDeploymentSlotComponent implements OnInit {
+export class StepDeploymentSlotComponent implements OnDestroy {
 
   DeploymentSlotEnabledOptions =
     [{ displayLabel: 'No', value: false },
@@ -19,11 +21,31 @@ export class StepDeploymentSlotComponent implements OnInit {
   DeploymentSlotsLoading = false;
   ExistingDeploymentSlotsList: DropDownElement<string>[] = [];
   selectedDeploymentSlot = '';
-  constructor(
-    public wizard: DeploymentCenterStateManager
-  ) { }
 
-  ngOnInit() {
+  private _ngUnsubscribe = new Subject();
+  constructor(
+    public wizard: DeploymentCenterStateManager,
+    _armService: ArmService
+  ) {
+    this.wizard.resourceIdStream
+      .takeUntil(this._ngUnsubscribe)
+      .switchMap(r => _armService.get(`${r}/slots`))
+      .subscribe(r => {
+        const slots = r.json().value;
+        console.log(slots);
+        this.ExistingDeploymentSlotsList = slots.filter(slot => slot.name !== 'production').map(slot => {
+          const slotName: string = slot.name.split('/')[1];
+          return {
+            displayLabel: slotName,
+            value: slotName
+          };
+        });
+      });
+
+  }
+
+  ngOnDestroy() {
+    this._ngUnsubscribe.next();
   }
 
 }
