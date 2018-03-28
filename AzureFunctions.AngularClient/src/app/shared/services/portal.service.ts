745f699a1bd200d0f1145fd4c591347a89ca83fd
Travis Harris <travistylerharris@outlook.com>
2018-03-28 10:56:55 -0700

    Moving graph token out of initialization to be called on demand (#2487)
    * Moving graph token to be loaded on demand rather than at initialization time
    
    * adding first to subscription so unsubscribe happens and cleans up

--- AzureFunctions.AngularClient/src/app/shared/services/portal.service.ts
+++ AzureFunctions.AngularClient/src/app/shared/services/portal.service.ts
@@ -143,6 +143,33 @@ export class PortalService {
         this.postMessage(Verbs.openBladeCollectorInputs, JSON.stringify(payload));
         return this.operationStream
             .filter(o => o.operationId === operationId)
+            .first()
+            .switchMap((o: DataMessage<BladeResult>) => {
+                if (o.data.status === 'success') {
+                    return Observable.of(o.data);
+                } else if (o.data.status === 'cancelled') {
+                    return Observable.of(null);
+                } else {
+                    return Observable.throw(o.data);
+                }
+            });
+    }
+
+    getAdToken(tokenType: 'graph' | 'azureTfsApi'){
+        this.logAction('portal-service', `get-ad-token: ${tokenType}`, null);
+        const operationId = Guid.newGuid();
+
+        const payload = {
+            operationId: operationId,
+            data: {
+                tokenType: tokenType
+            }
+        }
+
+        this.postMessage('get-ad-token', JSON.stringify(payload));
+        return this.operationStream
+            .filter(o=> o.operationId === operationId)
+            .first()
             .switchMap((o: DataMessage<BladeResult>) => {
                 if (o.data.status === 'success') {
                     return Observable.of(o.data);
