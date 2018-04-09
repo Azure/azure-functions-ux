4ee785279016c8f60fcd4f40060047c682bd3b3c
Travis Harris <travistylerharris@outlook.com>
2018-04-09 10:28:42 -0700

    improving dropdown example with better validation

--- AzureFunctions.AngularClient/src/app/dev-guide/dropdown-example/dropdown-example.component.ts
+++ AzureFunctions.AngularClient/src/app/dev-guide/dropdown-example/dropdown-example.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { RequiredValidator } from '../../shared/validators/requiredValidator';
 import { TranslateService } from '@ngx-translate/core';
@@ -10,7 +10,7 @@ import { HighlightService } from '../highlight.service';
   templateUrl: './dropdown-example.component.html',
   styleUrls: ['./dropdown-example.component.scss']
 })
-export class DropdownExampleComponent implements OnInit {
+export class DropdownExampleComponent {
   highlighted = false;
   public FormGroupExample;
   public items = [
@@ -38,56 +38,60 @@ export class DropdownExampleComponent implements OnInit {
   ) {
     this.htmlCode = highlightService.highlightString(this.htmlCode, 'html');
     this.typescriptCode = highlightService.highlightString(this.typescriptCode, 'typescript');
-   }
 
-  ngOnInit() {
     const required = new RequiredValidator(this._translateService);
     this.FormGroupExample = this._fb.group({
       sourceProvider: [this.chosenValue, [required.validate.bind(required)]]
     });
-  }
-
+   }
 
   // tslint:disable-next-line:member-ordering
   public htmlCode = `
-<div [formGroup]="FormGroupExample" novalidate>
-  <ng-select class="custom-select" [items]="items" bindLabel="displayLabel" bindValue="value" placeholder="Select source" [(ngModel)]="chosenValue"
-    formControlName="sourceProvider">
-  </ng-select>
-</div>`;
+  <div [formGroup]="FormGroupExample" novalidate>
+    <ng-select class="custom-select" [items]="items" bindLabel="displayLabel" bindValue="value" placeholder="Select source" [(ngModel)]="chosenValue"
+      formControlName="sourceProvider">
+    </ng-select>
+    <!--  This invalidmessage directive injects the error message on a validation failure, input is control name -->
+    <div invalidmessage="sourceProvider"></div>
+    <h4>ngModel value: {{chosenValue}}</h4>
+    <h4>formValue value: {{FormGroupExample?.value?.sourceProvider}}</h4>
+  </div>`;
 
   // tslint:disable-next-line:member-ordering
   public typescriptCode = `
-  export class DropdownExampleComponent implements OnInit {
-    public FormGroupExample;
-    public items = [
-      {
-        displayLabel: 'Github',
-        value: 'github'
-      },
-      {
-        displayLabel: 'Visual Studio Online',
-        value: 'vso'
-      },
-      {
-        displayLabel: 'Bitbucket',
-        value: 'bitbucket'
-      },
-      {
-        displayLabel: 'FTP',
-        value: 'ftp'
-      }];
-    public chosenValue;
-    constructor(
-      private _fb: FormBuilder,
-      private _translateService: TranslateService
-    ) { }
-  
-    ngOnInit() {
-      const required = new RequiredValidator(this._translateService);
-      this.FormGroupExample = this._fb.group({
-        sourceProvider: [this.chosenValue, [required.validate.bind(required)]]
-      });
-    }
+export class DropdownExampleComponent {
+  highlighted = false;
+  public FormGroupExample;
+  public items = [
+    {
+      displayLabel: 'Github',
+      value: 'github'
+    },
+    {
+      displayLabel: 'Visual Studio Online',
+      value: 'vso'
+    },
+    {
+      displayLabel: 'Bitbucket',
+      value: 'bitbucket'
+    },
+    {
+      displayLabel: 'FTP',
+      value: 'ftp'
+    }];
+  public chosenValue;
+  constructor(
+    private _fb: FormBuilder,
+    private _translateService: TranslateService,
+    highlightService: HighlightService
+  ) {
+    this.htmlCode = highlightService.highlightString(this.htmlCode, 'html');
+    this.typescriptCode = highlightService.highlightString(this.typescriptCode, 'typescript');
+
+    const required = new RequiredValidator(this._translateService);
+    this.FormGroupExample = this._fb.group({
+      sourceProvider: [this.chosenValue, [required.validate.bind(required)]]
+    });
+   }
   }`;
 }
