723cca86126187cf572209e243079823babda327
Travis Harris <travistylerharris@outlook.com>
2018-04-17 11:00:57 -0700

    adding azure-cli script for swapping ux slots (#2568)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
-var shell = require('shelljs');
-var gulpMultiProcess = require('gulp-multi-process');
-var inlinesource = require('gulp-inline-source');
+const path = require('path')
+const shell = require('shelljs');
+const gulpMultiProcess = require('gulp-multi-process');
 
 gulp.task('run-dev',  function (cb) {
     return gulpMultiProcess(['run-angular', 'run-server'], cb);
@@ -26,9 +26,23 @@ gulp.task('run-server',  (cb) => {
     
 });
 
+gulp.task('swap-production-slots', () => {
+    
+    const configFile = path.join(__dirname, 'tools', 'production-slots.json');
+    const config = require(configFile);
+    const regions = config.regions
+    if(shell.exec(`az account set --subscription "${config.subscriptionName}"`) !== 0){
+        shell.echo("Subscription selection failed, stopping operation");
+        return;
+    }
 
-gulp.task('inline-coverage-source', function () {
-    return gulp.src('./client/coverage/*.html')
-        .pipe(inlinesource({attribute: false}))
-        .pipe(gulp.dest('./coverage/'));
+    regions.forEach(region => {
+        shell.echo(`swapping slot ${region}...`);
+        const cmd = `az webapp deployment slot swap --resource-group functions-${region} --name functions-${region} --slot staging`;
+        if(shell.exec(cmd) !== 0){
+            shell.echo("Failed to swap. Stopping operation.");
+            return;
+        };
+        
+    });
 });
