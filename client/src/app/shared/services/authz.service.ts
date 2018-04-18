8337e74c7d5e96d8cbcacf783833ea99090f47e6
Allison Gruninger <algrunin@Microsoft.com>
2018-04-18 16:01:40 -0700

    Fix slot locks

--- client/src/app/shared/services/authz.service.ts
+++ client/src/app/shared/services/authz.service.ts
@@ -45,7 +45,7 @@ export class AuthzService {
     hasReadOnlyLock(resourceId: string): Observable<boolean> {
         return this._getLocks(resourceId)
             .map(locks => {
-                return !!locks.find(l => l.properties.level === 'ReadOnly');
+                return !!locks.find(l => l.properties.level === 'ReadOnly' && l.id.toLocaleLowerCase() === resourceId.toLocaleLowerCase());
             })
             .catch(e => {
                 return Observable.of(false);
