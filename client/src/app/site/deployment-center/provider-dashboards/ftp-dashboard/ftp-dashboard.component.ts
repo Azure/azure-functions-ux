59fd10f661ff8fdc320cbee373ddf82cb5861d46
Travis Harris <travih@microsoft.com>
2018-05-09 11:03:22 -0700

    Fix tests and clean up unused code

--- client/src/app/site/deployment-center/provider-dashboards/ftp-dashboard/ftp-dashboard.component.ts
+++ client/src/app/site/deployment-center/provider-dashboards/ftp-dashboard/ftp-dashboard.component.ts
@@ -1,18 +1,13 @@
 import { Component, Input, OnInit, Injector, OnDestroy } from '@angular/core';
 import { Links } from '../../../../shared/models/constants';
-import { FormControl } from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
 import { SiteService } from '../../../../shared/services/site.service';
-import { CacheService } from '../../../../shared/services/cache.service';
 import { PublishingProfile } from '../../Models/publishing-profile';
 import { from } from 'rxjs/observable/from';
 import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
 import { ArmSiteDescriptor } from '../../../../shared/resourceDescriptors';
-import { TranslateService } from '@ngx-translate/core';
-import { PortalResources } from '../../../../shared/models/portal-resources';
 import { FeatureComponent } from '../../../../shared/components/feature-component';
 import { Observable } from 'rxjs/Observable';
-import { forkJoin } from 'rxjs/observable/forkJoin';
 @Component({
   selector: 'app-ftp-dashboard',
   templateUrl: './ftp-dashboard.component.html',
@@ -22,44 +17,26 @@ export class FtpDashboardComponent extends FeatureComponent<string> implements O
   public FwLinks = Links;
   @Input() resourceId;
 
-  public FTPAccessOptions =
-    [{ displayLabel: this._translateService.instant(PortalResources.FTPBoth), value: 'AllAllowed' },
-    { displayLabel: this._translateService.instant(PortalResources.FTPSOnly), value: 'FtpsOnly' },
-    { displayLabel: this._translateService.instant(PortalResources.FTPDisable), value: 'Disabled' }];
-
-  public ftpsEnabledControl = new FormControl('Disabled');
   public ftpsEndpoint = '';
 
   private _ngUnsubscribe$ = new Subject();
   private _blobUrl: string;
   public publishProfileLink: SafeUrl;
   public siteName: string;
-  public saving = false;
   constructor(
-    private _translateService: TranslateService,
     private _siteService: SiteService,
     private _domSanitizer: DomSanitizer,
-    private _cacheService: CacheService,
     injector: Injector) {
     super('FtpDashboardComponent', injector);
   }
 
   protected setup(inputEvents: Observable<string>) {
     return inputEvents
-      .switchMap(() => {
-        const getFtpsState$ = this._siteService.getSiteConfig(this.resourceId).do(siteConfig => {
-          if (siteConfig.isSuccessful) {
-            this.ftpsEnabledControl.reset();
-            this.ftpsEnabledControl.setValue(siteConfig.result.properties.ftpsState);
-          }
-        });
-        const getFtpsEndpoint$ = this._siteService.getPublishingProfile(this.resourceId)
-          .switchMap(r => from(PublishingProfile.parsePublishProfileXml(r.result)))
-          .filter(x => x.publishMethod === 'FTP')
-          .do(ftpProfile => {
-            this.ftpsEndpoint = ftpProfile.publishUrl.replace('ftp:/', 'ftps:/');
-          });
-        return forkJoin(getFtpsState$, getFtpsEndpoint$);
+      .switchMap(() => this._siteService.getPublishingProfile(this.resourceId))
+      .switchMap(r => from(PublishingProfile.parsePublishProfileXml(r.result)))
+      .filter(x => x.publishMethod === 'FTP')
+      .do(ftpProfile => {
+        this.ftpsEndpoint = ftpProfile.publishUrl.replace('ftp:/', 'ftps:/');
       });
   }
 
@@ -109,17 +86,5 @@ export class FtpDashboardComponent extends FeatureComponent<string> implements O
       windowUrl.revokeObjectURL(this._blobUrl);
       this._blobUrl = null;
     }
-  }
-  save() {
-    this.saving = true;
-    this._cacheService.patchArm(`${this.resourceId}/config/web`, null, {
-      properties: {
-        ftpsState: this.ftpsEnabledControl.value
-      }
-    }
-    ).subscribe(() => {
-      this.saving = false;
-      this.ftpsEnabledControl.markAsPristine();
-    });
   }
 }
