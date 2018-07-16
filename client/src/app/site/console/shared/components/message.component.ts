4954a45e475a735e770a378f6146b90cf6971eac
Krrish <krrishmittal15@gmail.com>
2018-07-16 12:18:49 -0700

    bash-prompt removed

--- client/src/app/site/console/shared/components/message.component.ts
+++ client/src/app/site/console/shared/components/message.component.ts
@@ -1,11 +1,7 @@
 import {Component, Input} from '@angular/core';
-import { slideUpDown } from '../animations/slideUp.animation';
 @Component({
     template: `<div class = "console-message-value">{{message}}</div><i *ngIf="loading" class="fa fa-spinner fa-spin fa-fw"></i>`,
-    styleUrls: ['./../../console.component.scss'],
-    animations: [
-        slideUpDown
-    ]
+    styleUrls: ['./../../console.component.scss']
 })
 export class MessageComponent {
     @Input() message: string;
