982681712cb90b89f30b024590250d0683409bb2
Krrish <krrishmittal15@gmail.com>
2018-08-10 16:12:39 -0700

    log-content-template-changed

--- client/src/app/log-streaming/log-content.component.ts
+++ client/src/app/log-streaming/log-content.component.ts
@@ -2,11 +2,28 @@ import { Component, Input } from '@angular/core';
 import { LogConsoleTypes } from '../shared/models/constants';
 
 @Component({
-    template: `<div class="log-content" [class.info-content]= "type === types.Info" [class.error-content]= "type === types.Error" [class.warning-content]= "type === types.Warning">{{logs}}</div>`,
+    template: `<div class="log-content" [ngClass]="getClassForLogType()">{{logs}}</div>`,
     styleUrls: ['log-streaming.component.scss']
 })
 export class LogContentComponent {
     public types = LogConsoleTypes;
     @Input() type: number;
     @Input() logs: string;
+
+    /**
+     * Return class according to the log type.
+     * If the log-type is normal, empty string is returned.
+     */
+    getClassForLogType(): string {
+        if (this.type === LogConsoleTypes.Error) {
+            return 'error-content';
+        }
+        if (this.type === LogConsoleTypes.Info) {
+            return 'info-content';
+        }
+        if (this.type === LogConsoleTypes.Warning) {
+            return 'warning-content';
+        }
+        return '';
+    }
 }
