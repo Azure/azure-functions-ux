40e1ee8c2120f59ca374e83d6f74ac3fa5815158
Allison Gruninger <agruning@purdue.edu>
2018-08-17 13:45:11 -0700

    update fwd link

--- client/src/app/shared/models/constants.ts
+++ client/src/app/shared/models/constants.ts
@@ -128,7 +128,7 @@ export class Links {
     public static vmSizeLearnMore = 'https://go.microsoft.com/fwlink/?linkid=873022';
     public static appServicePricing = 'https://go.microsoft.com/fwlink/?linkid=873021';
     public static funcConnStringsLearnMore = 'https://go.microsoft.com/fwlink/?linkid=875276';
-    public static extensionInstallHelpLink = 'https://github.com/Azure/azure-functions-host/wiki/Troubleshooting-extension-installation-(portal)';
+    public static extensionInstallHelpLink = 'https://go.microsoft.com/fwlink/?linkid=2010300';
 }
 
 export class Kinds {
