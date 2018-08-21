f21c424d837a731e6797e89f67730a94d853a365
Travis Harris <travisharris@Traviss-MBP.guest.corp.microsoft.com>
2018-08-21 09:40:57 -0700

    Fixes #2748 Repository Url should be a link

--- client/src/app/site/deployment-center/provider-dashboards/kudu-dashboard/kudu-dashboard.component.ts
+++ client/src/app/site/deployment-center/provider-dashboards/kudu-dashboard/kudu-dashboard.component.ts
@@ -314,6 +314,14 @@ export class KuduDashboardComponent implements OnChanges, OnDestroy {
         this.sidePanelOpened = true;
     }
 
+    repoLinkClick() {
+        if (!this.repo) {
+            return;
+        }
+        const win = window.open(this.repo, '_blank');
+        win.focus();
+    }
+
     get sourceLocation() {
         const scmType = this.deploymentObject && this.deploymentObject.siteConfig.properties.scmType;
         switch (scmType) {
