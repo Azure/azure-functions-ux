5c67cd3d6203b496ee351dea3a30fbc754b05ff3
Aniello <andimarc@microsoft.com>
2018-09-06 14:28:44 -0700

    Adding SwapDiffTableComponent

---
+++ client/src/app/site/deployment-slots/swap-slots/swap-diff-table/swap-diff-table.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input} from '@angular/core';
+import { SimpleSlotsDiff } from './../../../../shared/models/arm/slots-diff';
+
+@Component({
+    selector: 'swap-diff-table',
+    templateUrl: './swap-diff-table.component.html',
+    styleUrls: ['./../../common.scss', './../swap-slots.component.scss'],
+})
+export class SwapDiffTableComponent {
+    @Input() loading = true;
+    @Input() invalid = false;
+    @Input() loadedOrFailed = false;
+    @Input() diffs: SimpleSlotsDiff[] = null;
+    @Input() showToggle = false;
+    @Input() oldValueHeading: string;
+    @Input() newValueHeading: string;
+
+    previewSource = true;
+
+    constructor() { }
+}
