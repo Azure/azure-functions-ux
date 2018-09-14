a506dfc12b24ae37183de31f1d4d712e8652bb3e
Allison Gruninger <agruning@purdue.edu>
2018-09-14 15:32:22 -0700

    update markdown files on server, open deployment center

--- client/src/app/site/quickstart/step-create-function-instructions/step-create-function-instructions.component.ts
+++ client/src/app/site/quickstart/step-create-function-instructions/step-create-function-instructions.component.ts
@@ -3,6 +3,7 @@ import { QuickstartStateManager } from 'app/site/quickstart/wizard-logic/quickst
 import { BroadcastService } from 'app/shared/services/broadcast.service';
 import { BroadcastEvent } from 'app/shared/models/broadcast-event';
 import { SiteTabIds } from 'app/shared/models/constants';
+import { deploymentOptions } from './../wizard-logic/quickstart-models';
 
 @Component({
     selector: 'step-create-function-instructions',
@@ -12,19 +13,28 @@ import { SiteTabIds } from 'app/shared/models/constants';
 export class StepCreateFunctionInstructionsComponent {
 
     public instructions: string;
+    public deployment: deploymentOptions;
 
     constructor(
         private _wizardService: QuickstartStateManager,
         private _broadcastService: BroadcastService) {
 
         this.instructions = this._wizardService.instructions.value;
+        this.deployment = this._wizardService.deployment.value;
 
         this._wizardService.instructions.statusChanges.subscribe(() => {
             this.instructions = this._wizardService.instructions.value;
         });
+
+        this._wizardService.deployment.statusChanges.subscribe(() => {
+            this.deployment = this._wizardService.deployment.value;
+        });
     }
 
     public closeTab() {
+        if (this.deployment === 'deploymentCenter') {
+            this._broadcastService.broadcastEvent(BroadcastEvent.OpenTab, SiteTabIds.continuousDeployment);
+        }
         this._broadcastService.broadcastEvent(BroadcastEvent.CloseTab, SiteTabIds.quickstart);
     }
 }
