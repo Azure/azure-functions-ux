defde411072acbe9d35e2481167a4f34c64e05be
shimedh <shimedh@microsoft.com>
2018-10-23 08:50:37 -0700

    Managed service identity should open as context pane since leaf blade (#3362)
    (https://msazure.visualstudio.com/Antares/_workitems/edit/3122214)
    
    CORS should open as context pane since leaf blade
    (https://msazure.visualstudio.com/Antares/_workitems/edit/3122218)

--- client/src/app/site/site-manage/site-manage.component.ts
+++ client/src/app/site/site-manage/site-manage.component.ts
@@ -414,6 +414,7 @@ export class SiteManageComponent extends FeatureComponent<TreeViewInfo<SiteData>
           {
             detailBlade: 'MSIBlade',
             detailBladeInputs: { resourceUri: site.id },
+            openAsContextBlade: true,
           },
           this._portalService,
           null,
@@ -519,6 +520,7 @@ export class SiteManageComponent extends FeatureComponent<TreeViewInfo<SiteData>
         {
           detailBlade: 'ApiCors',
           detailBladeInputs: { resourceUri: site.id },
+          openAsContextBlade: true,
         },
         this._portalService
       ),
