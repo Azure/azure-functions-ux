8fd22550ad8c2a4c0a4ae96ac53729c6bcfd863a
Shibani Medhekar <shimedh@microsoft.com>
2018-11-12 23:16:10 -0800

    Fix build

--- client-react/src/polyfills/string.ts
+++ client-react/src/polyfills/string.ts
@@ -4,8 +4,8 @@ import 'core-js/es6/symbol';
 if (!String.prototype.format) {
   String.prototype.format = function() {
     const args = arguments;
-    return this.replace(/{(\d+)}/g, function(match, number) {
-      return typeof args[number] != 'undefined' ? args[number] : match;
+    return this.replace(/{(\d+)}/g, (match, number) => {
+      return typeof args[number] !== 'undefined' ? args[number] : match;
     });
   };
 }
