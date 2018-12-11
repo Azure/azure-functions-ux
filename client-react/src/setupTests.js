8cfbcb670b69ba4ad9ab94161415b2f41abcf8a7
Travis Harris <travih@microsoft.com>
2018-12-10 16:05:20 -0800

    fix unit test issues

--- client-react/src/setupTests.js
+++ client-react/src/setupTests.js
@@ -1,12 +1,17 @@
 import * as Enzyme from 'enzyme';
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new ReactSixteenAdapter() });
+Enzyme.configure({
+  adapter: new ReactSixteenAdapter(),
+});
 
 jest.doMock('react-i18next', () => ({
   // this mock makes sure any components using the translate HoC receive the t function as a prop
   translate: () => Component => {
-    Component.defaultProps = { ...Component.defaultProps, t: () => '' };
+    Component.defaultProps = {
+      ...Component.defaultProps,
+      t: () => '',
+    };
     return Component;
   },
 }));
