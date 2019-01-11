a44309bc8cdbd41d7eef55ebde8f1c14697c2936
Mitren Chinoy <mitren@gmail.com>
2019-01-11 09:48:58 -0800

    update comment

--- client/src/app/shared/Utilities/response-header.ts
+++ client/src/app/shared/Utilities/response-header.ts
@@ -1,5 +1,7 @@
 export class ResponseHeader {
-  // For certain APIs (github, docker, and ACR) the next
+  // For certain APIs (github, docker, and ACR) the next link is part of the response header
+  // in "link" property. This property contains an array of links along with their link relation (rel=)
+  // the links could be 'next', 'last', etc.
   public static getLinksFromLinkHeader(linksHeader: string[]): { [key: string]: string } {
     const links: { [key: string]: string } = {};
 
