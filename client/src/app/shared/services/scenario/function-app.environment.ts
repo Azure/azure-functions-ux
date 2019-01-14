0a44580c7e5b972c63b99ae807f9b1bd00f949c4
Travis Harris <travisharris@traviss-mbp.lan>
2019-01-13 21:14:01 -0800

    Add Azure Dev Ops Deployment to Windows Function Apps

--- client/src/app/shared/services/scenario/function-app.environment.ts
+++ client/src/app/shared/services/scenario/function-app.environment.ts
@@ -1,23 +1,18 @@
 import { ScenarioCheckInput } from './scenario.models';
-import { ScenarioIds } from '../../models/constants';
+// import { ScenarioIds } from '../../models/constants';
 import { Environment } from './scenario.models';
+import { Kinds } from 'app/shared/models/constants';
 
 export class FunctionAppEnvironment extends Environment {
   name = 'DynamicSite';
 
   constructor() {
     super();
-    this.scenarioChecks[ScenarioIds.vstsDeploymentHide] = {
-      id: ScenarioIds.vstsDeploymentHide,
-      runCheck: () => {
-        return { status: 'disabled' };
-      },
-    };
   }
 
   public isCurrentEnvironment(input?: ScenarioCheckInput): boolean {
     if (input && input.site) {
-      return input.site.kind.toLowerCase().includes('functionapp');
+      return input.site.kind.toLowerCase().includes(Kinds.functionApp);
     }
 
     return false;
