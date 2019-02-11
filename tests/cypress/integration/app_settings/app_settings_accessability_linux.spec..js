0aec783b77169d85a01cec559acfe1da444a69de
Travis Harris <travih@microsoft.com>
2019-02-10 20:26:38 -0800

    work on making a11y testing easier

--- tests/cypress/integration/app_settings/app_settings_accessability_linux.spec..js
+++ tests/cypress/integration/app_settings/app_settings_accessability_linux.spec..js
@@ -4,61 +4,34 @@ import { setupWindow } from '../../utilities/window';
 
 context('Windows App Settings Accessability', () => {
   beforeEach(() => {
-    startVisit().visit(
-      `/feature/subscriptions/1489df65-b065-4cc3-b5c2-4b37cc88b703/resourcegroups/test-ux/providers/microsoft.web/sites/test-linux-app-ux/settings?trustedAuthority=test`,
-      {
-        onBeforeLoad(win) {
-          setupWindow(win);
-          cy.spy(win.parent, 'postMessage').as('spyPostMessage');
-        },
-      }
-    );
+    startVisit()
+      .visit(
+        `/feature/subscriptions/1489df65-b065-4cc3-b5c2-4b37cc88b703/resourcegroups/test-ux/providers/microsoft.web/sites/test-linux-app-ux/settings?trustedAuthority=test`,
+        {
+          onBeforeLoad(win) {
+            setupWindow(win);
+            cy.spy(win.parent, 'postMessage').as('spyPostMessage');
+          },
+        }
+      )
+      .injectAxe();
   });
 
   it('General Settings Accessability', function() {
     cy.get('#app-settings-application-settings-tab')
       .window()
-      .then(win => {
-        var window = win;
-        var axe = require('axe-core');
-        window.eval(axe.source);
-        return window.axe.run();
-      })
-      .then(results => {
-        console.log(results);
-        expect(results.violations.filter(x => x.id !== 'page-has-heading-one').length).to.equal(0);
-      });
+      .checkA11y();
   });
 
   it('Application Settings Accessability', function() {
     cy.get('#app-settings-application-settings-tab')
       .click()
-      .window()
-      .then(win => {
-        var window = win;
-        var axe = require('axe-core');
-        window.eval(axe.source);
-        return window.axe.run();
-      })
-      .then(results => {
-        console.log(results);
-        expect(results.violations.filter(x => x.id !== 'page-has-heading-one').length).to.equal(0);
-      });
+      .checkA11y();
   });
 
   it('Path Mappings Accessability', function() {
     cy.get('#app-settings-path-mappings-tab')
       .click()
-      .window()
-      .then(win => {
-        var window = win;
-        var axe = require('axe-core');
-        window.eval(axe.source);
-        return window.axe.run();
-      })
-      .then(results => {
-        console.log(results);
-        expect(results.violations.filter(x => x.id !== 'page-has-heading-one').length).to.equal(0);
-      });
+      .checkA11y();
   });
 });
