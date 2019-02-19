2041fe1e6cdc17690a9e4b9ed8c184c7510c9835
Travis Harris <travisharris@traviss-mbp.lan>
2019-02-19 08:31:38 -0800

    remove dead code

--- client-react/src/theme/CustomOfficeFabric/AzurePortalCustomizations.ts
+++
@@ -1,7 +0,0 @@
-import { AzurePortalStyles } from './AzurePortal/AzurePortalStyles';
-import { ICustomizations } from 'office-ui-fabric-react';
-
-export const AzurePortalCustomizations: ICustomizations = {
-  settings: {},
-  scopedSettings: { ...AzurePortalStyles },
-};
