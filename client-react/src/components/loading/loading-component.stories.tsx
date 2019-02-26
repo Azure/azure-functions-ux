8337dd6562363837cbb6dd57badebda31e8c7984
Travis Harris <travih@microsoft.com>
2019-02-26 09:26:16 -0800

    Reinstate lazy loading and optimize monaco loading (#3681)
    * Fixes some casing that breaks build on linux
    
    * optimizing package bundling and monaco size

--- client-react/src/components/loading/loading-component.stories.tsx
+++ client-react/src/components/loading/loading-component.stories.tsx
@@ -1,15 +1,4 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import LoadingComponent from './loading-component';
-storiesOf('Loading Component', module)
-  .add('Loading with no errors', () => (
-    <LoadingComponent pastDelay={true} error={false} isLoading={true} timedOut={false} retry={action('retried')} />
-  ))
-  .add('Loading but not past delay', () => (
-    <LoadingComponent pastDelay={false} error={false} isLoading={true} timedOut={false} retry={action('retried')} />
-  ))
-  .add('Loading failed', () => (
-    <LoadingComponent pastDelay={false} error="error" isLoading={true} timedOut={false} retry={action('retried')} />
-  ))
-  .add('Timed out', () => <LoadingComponent pastDelay={false} error={false} isLoading={true} timedOut={true} retry={action('retried')} />);
+storiesOf('Loading Component', module).add('Loading with no errors', () => <LoadingComponent />);
