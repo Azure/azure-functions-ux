b7d4192e659b443d64e465ca700876e170213fba
Travis Harris <travih@microsoft.com>
2019-02-25 20:28:15 -0800

    optimizing package bundling and monaco size

--- client-react/src/components/loading/loading-component.tsx
+++ client-react/src/components/loading/loading-component.tsx
@@ -1,9 +1,6 @@
-import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { style } from 'typestyle/lib';
-import * as Loadable from 'react-loadable';
 
 const loadingCSS = style({
   position: 'fixed',
@@ -12,25 +9,12 @@ const loadingCSS = style({
   transform: 'translate(-50%, -50%)',
 });
 
-const LoadingComponent: React.FC<Loadable.LoadingComponentProps> = props => {
-  const { error, retry, pastDelay, timedOut } = props;
-  const { t } = useTranslation();
-  if (error || timedOut) {
-    return (
-      <div className={loadingCSS}>
-        Failed to load! <DefaultButton onClick={retry}>Retry</DefaultButton>
-      </div>
-    );
-  }
-  if (pastDelay) {
-    return (
-      <div className={loadingCSS}>
-        <Spinner size={SpinnerSize.large} label={t('loading')} ariaLive="assertive" />
-      </div>
-    );
-  }
-
-  return null;
+const LoadingComponent = () => {
+  return (
+    <div className={loadingCSS}>
+      <Spinner size={SpinnerSize.large} ariaLive="assertive" />
+    </div>
+  );
 };
 
 export default LoadingComponent;
