4e9909243fa5bbeb0683cf4ade90710354a7809c
Shibani Medhekar <shimedh@microsoft.com>
2019-03-20 16:04:52 -0700

    AB #4212252 Add GenericPlanSpec, onprem environment and QuotaService

---
+++ client-react/src/models/quotaSettings.ts
@@ -0,0 +1,26 @@
+export interface QuotaSettings {
+  properties: {
+    subscriptionId: string;
+    quotaSettings: QuotaSetting[];
+  };
+}
+
+export interface QuotaSetting {
+  key: string;
+  value: string;
+}
+
+export enum QuotaScope {
+  WebSpace,
+  Site,
+}
+
+export class QuotaNames {
+  public static readonly useCustomStorageForBackup = 'useCustomStorageForBackup';
+  public static readonly numberOfSlotsPerSite = 'NumberOfSlotsPerSite';
+  public static readonly numberOfBackups = 'NumberOfBackups';
+  public static readonly sslSupport = 'SslSupport';
+  public static readonly workerProcess64BitEnabled = 'WorkerProcess64BitEnabled';
+  public static readonly webSocketsEnabled = 'WebSocketsEnabled';
+  public static readonly alwaysOnEnabled = 'AlwaysOnEnabled';
+}
