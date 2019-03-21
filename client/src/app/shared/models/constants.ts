5cab6b3d81be048c2317ddd00565db9c6df6ce97
Allison Manifold <allisonm@microsoft.com>
2019-03-21 11:28:34 -0700

    update constant

--- client/src/app/shared/models/constants.ts
+++ client/src/app/shared/models/constants.ts
@@ -148,7 +148,7 @@ export class Links {
   public static containerPrivateRegistryLearnMore = 'https://go.microsoft.com/fwlink/?linkid=2041449';
   public static byosLearnMore = 'https://go.microsoft.com/fwlink/?linkid=2045372';
   public static deploymentCredentialsLearnMore = 'https://go.microsoft.com/fwlink/?linkid=2082375';
-  public static ipRestrictionsLearnMore: 'https://go.microsoft.com/fwlink/?linkid=854597';
+  public static ipRestrictionsLearnMore = 'https://go.microsoft.com/fwlink/?linkid=854597';
 }
 
 export class Kinds {
