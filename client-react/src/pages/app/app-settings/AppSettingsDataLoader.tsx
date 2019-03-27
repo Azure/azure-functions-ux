54f5ebceca8aab716be0e2326f00994970ab7b9f
Travis Harris <travih@microsoft.com>
2019-03-26 21:58:07 -0700

    readd telemetry for timing AppSettings loading (#3762)

--- client-react/src/pages/app/app-settings/AppSettingsDataLoader.tsx
+++ client-react/src/pages/app/app-settings/AppSettingsDataLoader.tsx
@@ -1,7 +1,7 @@
 import { FormikActions } from 'formik';
 import React, { useState, useEffect, useContext } from 'react';
 import { AppSettingsFormValues } from './AppSettings.types';
-import { convertStateToForm, convertFormToState, flattenVirtualApplicationsList,getCleanedConfigForSave } from './AppSettingsFormData';
+import { convertStateToForm, convertFormToState, flattenVirtualApplicationsList, getCleanedConfigForSave } from './AppSettingsFormData';
 import LoadingComponent from '../../../components/loading/loading-component';
 import {
   fetchApplicationSettingValues,
@@ -18,15 +18,15 @@ import { PortalContext } from '../../../PortalContext';
 import { useTranslation } from 'react-i18next';
 import { HttpResponseObject } from '../../../ArmHelper.types';
 import SiteService from '../../../ApiHelpers/SiteService';
+import LogService from '../../../utils/LogService';
+
 export interface AppSettingsDataLoaderProps {
-  children: (
-    props: {
-      initialFormValues: AppSettingsFormValues;
-      saving: boolean;
-      scaleUpPlan: () => void;
-      onSubmit: (values: AppSettingsFormValues, actions: FormikActions<AppSettingsFormValues>) => void;
-    }
-  ) => JSX.Element;
+  children: (props: {
+    initialFormValues: AppSettingsFormValues;
+    saving: boolean;
+    scaleUpPlan: () => void;
+    onSubmit: (values: AppSettingsFormValues, actions: FormikActions<AppSettingsFormValues>) => void;
+  }) => JSX.Element;
   resourceId: string;
 }
 
@@ -100,7 +100,7 @@ const AppSettingsDataLoader: React.FC<AppSettingsDataLoaderProps> = props => {
     } else {
       setCurrentAvailableStacks(windowsStacks.data);
     }
-
+    LogService.stopTrackPage('shell', { feature: 'AppSettings' });
     setInitialLoading(true);
   };
   const fillSlots = async () => {
