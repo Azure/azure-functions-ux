13a24b6b645f2879dbe3caf83d222eafb3b49b93
Travis Harris <travisharris@Traviss-MacBook-Pro.local>
2019-03-29 12:33:56 -0700

    Fixes AB#4254428 Add info box for documentation for github/bitbucket permissions

--- client/src/app/shared/models/portal-resources.ts
+++ client/src/app/shared/models/portal-resources.ts
@@ -1554,4 +1554,6 @@ export class PortalResources {
   public static editCertificateExlusionPaths = 'editCertificateExlusionPaths';
   public static noExclusionRulesDefined = 'noExclusionRulesDefined';
   public static newPath = 'newPath';
+  public static githubOrgMessage = 'githubOrgMessage';
+  public static bitbucketOrgMessage = 'bitbucketOrgMessage';
 }
