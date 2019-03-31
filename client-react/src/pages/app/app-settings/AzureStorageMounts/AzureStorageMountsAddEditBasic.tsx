fb8704f4229aab40d747985a875923003939e159
Travis Harris <Travih@microsoft.com>
2019-03-31 12:00:36 -0700

    Some fixes around mounted file storage UI

--- client-react/src/pages/app/app-settings/AzureStorageMounts/AzureStorageMountsAddEditBasic.tsx
+++ client-react/src/pages/app/app-settings/AzureStorageMounts/AzureStorageMountsAddEditBasic.tsx
@@ -21,50 +21,41 @@ const AzureStorageMountsAddEditBasic: React.FC<FormikProps<FormAzureStorageMount
   const setAccessKey = (accessKey: string) => {
     props.setValues({ ...props.values, accessKey });
   };
-  useEffect(
-    () => {
-      const storageAccountId = storageAccounts.value.find(x => x.name === props.values.accountName);
-      setAccountError('');
-      if (storageAccountId) {
-        setAccountSharesBlob([]);
-        setAccountSharesFiles([]);
-        setSharesLoading(true);
-        MakeArmCall({ resourceId: `${storageAccountId.id}/listKeys`, commandName: 'listStorageKeys', method: 'POST' })
-          .then(async ({ data }: any) => {
-            setAccessKey(data.keys[0].value);
-            const payload = {
-              accountName: props.values.accountName,
-              accessKey: data.keys[0].value,
-            };
-            try {
-              const blobCall = axios.post(
-                `https://functions.azure.com/api/getStorageContainers?accountName=${props.values.accountName}`,
-                payload
-              );
+  useEffect(() => {
+    const storageAccountId = storageAccounts.value.find(x => x.name === props.values.accountName);
+    setAccountError('');
+    if (storageAccountId) {
+      setAccountSharesBlob([]);
+      setAccountSharesFiles([]);
+      setSharesLoading(true);
+      MakeArmCall({ resourceId: `${storageAccountId.id}/listKeys`, commandName: 'listStorageKeys', method: 'POST' })
+        .then(async ({ data }: any) => {
+          setAccessKey(data.keys[0].value);
+          const payload = {
+            accountName: props.values.accountName,
+            accessKey: data.keys[0].value,
+          };
+          try {
+            const blobCall = axios.post(`/api/getStorageContainers?accountName=${props.values.accountName}`, payload);
 
-              const filesCall = axios.post(
-                `https://functions.azure.com/api/getStorageFileShares?accountName=${props.values.accountName}`,
-                payload
-              );
+            const filesCall = axios.post(`/api/getStorageFileShares?accountName=${props.values.accountName}`, payload);
 
-              const [blobs, files] = await Promise.all([blobCall, filesCall]);
-              setSharesLoading(false);
-              setAccountSharesFiles(files.data);
-              setAccountSharesBlob(blobs.data);
-              if (files.data.length === 0 && blobs.data.length === 0) {
-                setAccountError(t('noBlobsOrFilesShares'));
-              }
-            } catch (err) {
-              setAccountError(t('noWriteAccessStorageAccount'));
+            const [blobs, files] = await Promise.all([blobCall, filesCall]);
+            setSharesLoading(false);
+            setAccountSharesFiles(files.data);
+            setAccountSharesBlob(blobs.data);
+            if (files.data.length === 0 && blobs.data.length === 0) {
+              setAccountError(t('noBlobsOrFilesShares'));
             }
-          })
-          .catch(err => {
+          } catch (err) {
             setAccountError(t('noWriteAccessStorageAccount'));
-          });
-      }
-    },
-    [props.values.accountName]
-  );
+          }
+        })
+        .catch(err => {
+          setAccountError(t('noWriteAccessStorageAccount'));
+        });
+    }
+  }, [props.values.accountName]);
   const blobContainerOptions = accountSharesBlob.map((x: any) => ({ key: x.name, text: x.name }));
   const filesContainerOptions = accountSharesFiles.map((x: any) => ({ key: x.name, text: x.name }));
 
