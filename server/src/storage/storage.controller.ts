e4e0f3a45204762f2f46e0628d78b0d8c48e8d22
Travis Harris <Travih@microsoft.com>
2019-03-31 11:55:17 -0700

    rewrite storage pieces

--- server/src/storage/storage.controller.ts
+++ server/src/storage/storage.controller.ts
@@ -6,35 +6,26 @@ import { FileInterceptor } from '@nestjs/platform-express';
 export class StorageController {
   constructor(private storageService: StorageService) {}
 
-  @Post('getBlobSasUri')
-  getBlobSasUri(@Body('connectionString') connectionString, @Body('containerName') containerName) {
-    return this.storageService.getBlobSasUri(connectionString, containerName);
-  }
-
   @Post('getStorageContainers')
   getStorageContainers(@Body('accountName') accountName, @Body('accessKey') accessKey) {
+    if (!accountName) {
+      throw new HttpException('Header must contain Storage Connection String', 400);
+    }
+    if (!accessKey) {
+      throw new HttpException('Header must contain Storage Connection Container Name', 400);
+    }
+
     return this.storageService.getStorageContainers(accountName, accessKey);
   }
 
   @Post('getStorageFileShares')
   getStorageFileShares(@Body('accountName') accountName, @Body('accessKey') accessKey) {
-    return this.storageService.getFileShares(accountName, accessKey);
-  }
-
-  @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(
-    @UploadedFile() file,
-    @Headers('connectionstring') connectionString: string,
-    @Headers('containername') containername: string
-  ) {
-    if (!connectionString) {
+    if (!accountName) {
       throw new HttpException('Header must contain Storage Connection String', 400);
     }
-    if (!containername) {
+    if (!accessKey) {
       throw new HttpException('Header must contain Storage Connection Container Name', 400);
     }
-
-    return this.storageService.uploadFileToBlob(file, connectionString, containername);
+    return this.storageService.getFileShares(accountName, accessKey);
   }
 }
--- server/src/storage/storage.module.ts
+++ server/src/storage/storage.module.ts
@@ -1,37 +1,10 @@
-import { Module, HttpException, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { StorageController } from './storage.controller';
 import { StorageService } from './storage.service';
-import { MulterModule } from '@nestjs/platform-express';
-import { extname } from 'path';
-import { memoryStorage } from 'multer';
-import * as RateLimit from 'express-rate-limit';
-const storage = memoryStorage();
+import { SharedModule } from '../shared/shared.module';
 @Module({
-  imports: [
-    MulterModule.register({
-      storage,
-      fileFilter: (req, file, cb) => {
-        if (extname(file.originalname) !== '.zip') {
-          return cb(new HttpException('Only zip files allowed', 400), false);
-        }
-
-        cb(null, true);
-      },
-      limits: {
-        fileSize: 2 * 1024 * 1024, // 2MB
-        files: 1,
-      },
-    }),
-  ],
+  imports: [SharedModule],
   controllers: [StorageController],
   providers: [StorageService],
 })
-export class StorageModule {
-  configure(consumer: MiddlewareConsumer) {
-    const apiLimiter = new RateLimit({
-      windowMs: 60 * 1000, // 5 requests per 1 minute
-      max: 5,
-    });
-    consumer.apply(apiLimiter).forRoutes('api/upload');
-  }
-}
+export class StorageModule {}
