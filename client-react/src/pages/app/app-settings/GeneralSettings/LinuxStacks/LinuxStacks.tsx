04de602ee77025e16802125ba025abbedf9114ca
Travis Harris <travih@microsoft.com>
2019-05-15 13:44:37 -0700

    Fixes AB#4622800 [Configuration] Should separate stack from versions dropdown so that we can include minor versions (#3914)
    * Fixes AB#4622800 [Configuration] Should separate stack from versions dropdown so that we can include minor versions
    
    * PR comments

--- client-react/src/pages/app/app-settings/GeneralSettings/LinuxStacks/LinuxStacks.tsx
+++ client-react/src/pages/app/app-settings/GeneralSettings/LinuxStacks/LinuxStacks.tsx
@@ -1,7 +1,6 @@
 import { Field, FormikProps } from 'formik';
-import { DropdownMenuItemType, IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
-import React, { useContext } from 'react';
-
+import { IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
+import React, { useContext, useState, useEffect } from 'react';
 import Dropdown from '../../../../../components/form-controls/DropDown';
 import { AvailableStack } from '../../../../../models/available-stacks';
 import { ArmObj } from '../../../../../models/WebAppModels';
@@ -12,51 +11,169 @@ import { useTranslation } from 'react-i18next';
 import { ScenarioService } from '../../../../../utils/scenario-checker/scenario.service';
 import { ScenarioIds } from '../../../../../utils/scenario-checker/scenario-ids';
 import { Links } from '../../../../../utils/FwLinks';
+import DropdownNoFormik from '../../../../../components/form-controls/DropDownnoFormik';
 
 type PropsType = FormikProps<AppSettingsFormValues>;
-const parseLinuxBuiltInStacks = (builtInStacks: ArmObj<AvailableStack>[]) => {
-  const linuxFxVersionOptions: IDropdownOption[] = [];
 
+const getRuntimeStacks = (builtInStacks: ArmObj<AvailableStack>[]) => {
+  const stacks: IDropdownOption[] = [];
   builtInStacks.forEach(availableStackArm => {
     const availableStack: AvailableStack = availableStackArm.properties;
-    linuxFxVersionOptions.push({
+    stacks.push({
       key: availableStack.name,
       text: availableStack.display,
-      itemType: DropdownMenuItemType.Header,
     });
-    availableStack.majorVersions.forEach(majorVersion => {
-      linuxFxVersionOptions.push({
-        text: majorVersion.displayVersion,
-        key: majorVersion.runtimeVersion.toLowerCase(),
-      });
-    });
+  });
+  return stacks;
+};
 
-    linuxFxVersionOptions.push({ key: `${availableStack.name}-divider`, text: '-', itemType: DropdownMenuItemType.Divider });
+const getSelectedRuntimeStack = (builtInStacks: ArmObj<AvailableStack>[], version: string) => {
+  for (const s of builtInStacks) {
+    for (const majVer of s.properties.majorVersions) {
+      if (majVer.runtimeVersion.toLowerCase() === version) {
+        return s.name;
+      }
+      for (const minVer of majVer.minorVersions) {
+        if (minVer.runtimeVersion.toLowerCase() === version) {
+          return s.name;
+        }
+      }
+    }
+  }
+  return '';
+};
+const getMajorVersions = (builtInStacks: ArmObj<AvailableStack>[], stack: string) => {
+  const linuxFxVersionOptions: IDropdownOption[] = [];
+
+  const currentStack = builtInStacks.find(s => s.name === stack);
+  if (!currentStack) {
+    return [];
+  }
+  currentStack.properties.majorVersions.forEach(majorVersion => {
+    linuxFxVersionOptions.push({
+      text: majorVersion.displayVersion,
+      key: majorVersion.runtimeVersion.toLowerCase(),
+    });
   });
 
   return linuxFxVersionOptions;
 };
+const getSelectedMajorVersion = (builtInStacks: ArmObj<AvailableStack>[], version: string) => {
+  const stack = getSelectedRuntimeStack(builtInStacks, version);
+  const currentStack = builtInStacks.find(s => s.name === stack);
+  if (!currentStack) {
+    return '';
+  }
+  for (const majVer of currentStack.properties.majorVersions) {
+    if (majVer.runtimeVersion.toLowerCase() === version) {
+      return majVer.runtimeVersion.toLowerCase();
+    }
+    for (const minVer of majVer.minorVersions) {
+      if (minVer.runtimeVersion.toLowerCase() === version) {
+        return majVer.runtimeVersion.toLowerCase();
+      }
+    }
+  }
+  return '';
+};
+const getMinorVersions = (builtInStacks: ArmObj<AvailableStack>[], stack: string, majorVersion: string) => {
+  const linuxFxVersionOptions: IDropdownOption[] = [];
+  // included already handles the case that duplicate versions are included multiple times and needs to be filtered out
+  const includedAlready = new Set();
+  const currentStack = builtInStacks.find(s => s.name === stack);
+  if (!currentStack) {
+    return [];
+  }
+  const currentVersion = currentStack.properties.majorVersions.find(m => m.runtimeVersion.toLowerCase() === majorVersion);
+  if (!currentVersion) {
+    return [];
+  }
+  linuxFxVersionOptions.push({
+    text: currentVersion.displayVersion,
+    key: currentVersion.runtimeVersion.toLowerCase(),
+  });
+  includedAlready.add(currentVersion.runtimeVersion.toLowerCase());
+  currentVersion.minorVersions.forEach(minVer => {
+    const ver = minVer.runtimeVersion.toLowerCase();
+    if (!includedAlready.has(ver)) {
+      includedAlready.add(ver);
+      linuxFxVersionOptions.push({
+        text: minVer.displayVersion,
+        key: ver,
+      });
+    }
+  });
 
+  return linuxFxVersionOptions;
+};
 const LinuxStacks: React.FC<PropsType> = props => {
-  const { values } = props;
+  const { values, setFieldValue } = props;
   const { site } = values;
   const { app_write, editable } = useContext(PermissionsContext);
   const stacks = useContext(AvailableStacksContext);
-  const options = parseLinuxBuiltInStacks(stacks.value);
+  const runtimeOptions = getRuntimeStacks(stacks.value);
   const { t } = useTranslation();
 
+  const [runtimeStack, setRuntimeStack] = useState(getSelectedRuntimeStack(stacks.value, values.config.properties.linuxFxVersion));
+  const [majorVersion, setMajorVersion] = useState<string | null>(
+    getSelectedMajorVersion(stacks.value, values.config.properties.linuxFxVersion)
+  );
+  useEffect(() => {
+    setRuntimeStack(getSelectedRuntimeStack(stacks.value, values.config.properties.linuxFxVersion));
+    setMajorVersion(getSelectedMajorVersion(stacks.value, values.config.properties.linuxFxVersion));
+  }, [values.config.properties.linuxFxVersion]);
   const scenarioService = new ScenarioService(t);
   return (
     <>
       {scenarioService.checkScenario(ScenarioIds.linuxAppRuntime, { site }).status !== 'disabled' && (
-        <Field
-          name="config.properties.linuxFxVersion"
-          component={Dropdown}
-          disabled={!app_write || !editable}
-          label={t('stack')}
-          id="linux-fx-version-runtime"
-          options={options}
-        />
+        <>
+          <DropdownNoFormik
+            value={runtimeStack}
+            onChange={(e, newVal) => {
+              const majorVersions = getMajorVersions(stacks.value, newVal.key);
+              setRuntimeStack(newVal.key);
+              if (majorVersions.length > 0) {
+                const majVer = majorVersions[0];
+                setMajorVersion(majVer.key as string);
+                const minorVersions = getMinorVersions(stacks.value, newVal.key, majVer.key as string);
+                setMajorVersion(majVer.key as string);
+                if (minorVersions.length > 0) {
+                  setFieldValue('config.properties.linuxFxVersion', minorVersions[0].key);
+                }
+              }
+            }}
+            options={runtimeOptions}
+            disabled={!app_write || !editable}
+            label={t('stack')}
+            id="linux-fx-version-runtime"
+          />
+          {runtimeStack && (
+            <DropdownNoFormik
+              value={majorVersion || ''}
+              onChange={(e, newVal) => {
+                const minorVersions = getMinorVersions(stacks.value, runtimeStack, newVal.key);
+                setMajorVersion(newVal.key);
+                if (minorVersions.length > 0) {
+                  setFieldValue('config.properties.linuxFxVersion', minorVersions[0].key);
+                }
+              }}
+              options={getMajorVersions(stacks.value, runtimeStack)}
+              disabled={!app_write || !editable}
+              label={t('majorVersion')}
+              id="linux-fx-version-major-version"
+            />
+          )}
+          {majorVersion && (
+            <Field
+              name="config.properties.linuxFxVersion"
+              component={Dropdown}
+              disabled={!app_write || !editable}
+              label={t('minorVersion')}
+              id="linux-fx-version-minor-version"
+              options={getMinorVersions(stacks.value, runtimeStack, majorVersion)}
+            />
+          )}
+        </>
       )}
       <Field
         name="config.properties.appCommandLine"
