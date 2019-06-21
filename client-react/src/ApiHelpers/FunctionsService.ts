8919a6724efc1800c14f09efa60a849972eb90c3
Elliott Hamai <ellhamai@microsoft.com>
2019-06-21 15:14:21 -0700

    PR feedback

--- client-react/src/ApiHelpers/FunctionsService.ts
+++ client-react/src/ApiHelpers/FunctionsService.ts
@@ -3,13 +3,13 @@ import MakeArmCall from './ArmHelper';
 import { FunctionInfo } from '../models/functions/function-info';
 
 export default class FunctionsService {
-  public static fetchFunctions = (resourceId: string) => {
+  public static getFunctions = (resourceId: string) => {
     const id = `${resourceId}/functions`;
 
     return MakeArmCall<ArmArray<FunctionInfo>>({ resourceId: id, commandName: 'fetchFunctions' });
   };
 
-  public static fetchFunction = (resourceId: string) => {
+  public static getFunction = (resourceId: string) => {
     return MakeArmCall<ArmObj<FunctionInfo>>({ resourceId, commandName: 'fetchFunction' });
   };
 }
