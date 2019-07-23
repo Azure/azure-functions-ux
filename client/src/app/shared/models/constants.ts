c39098810f4b997adae278e883d4e601bd8d6402
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-07-23 16:26:50 -0700

    AB#4886227 Removing feature flag 'EnableLinuxElasticPremium'

--- client/src/app/shared/models/constants.ts
+++ client/src/app/shared/models/constants.ts
@@ -620,7 +620,6 @@ export class FeatureFlags {
   public static UseNewSlotsBlade = 'UseNewSlotsBlade';
   public static ShowLegacySlotsBlade = 'ShowLegacySlotsBlade';
   public static oldDeploymentCenter = 'oldvsts';
-  public static EnableLinuxElasticPremium = 'EnableLinuxElasticPremium';
   public static AllowFreeLinux = 'allowfreelinux';
   public static RemoveSlotsPreviewTag = 'RemoveSlotsPreviewTag';
 }
