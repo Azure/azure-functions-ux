25fa7632c115c8db866916aeda2f3286c1235bce
Allison Manifold <allisonm@microsoft.com>
2019-08-23 14:59:23 -0700

    Create Function: More Details Pivot (#4113)
    * create function
    
    * load required binding info
    
    * responsive binding and function creation
    
    * code clean-up
    
    * minor updates
    
    * pr updates
    
    * use context, centralize network call lcoation
    
    * one instance of functioncreatedata

--- client-react/src/models/functions/function-info.ts
+++ client-react/src/models/functions/function-info.ts
@@ -2,15 +2,15 @@ import { FunctionConfig } from './function-config';
 
 export interface FunctionInfo {
   name: string;
-  function_app_id: string;
-  script_root_path_href: string;
-  script_href: string;
-  config_href: string;
-  secrets_file_href: string;
-  href: string;
+  function_app_id?: string;
+  script_root_path_href?: string;
+  script_href?: string;
+  config_href?: string;
+  secrets_file_href?: string;
+  href?: string;
   config: FunctionConfig;
   files: { [key: string]: string };
   test_data: string;
-  invoke_url_template: string;
-  language: string;
+  invoke_url_template?: string;
+  language?: string;
 }
