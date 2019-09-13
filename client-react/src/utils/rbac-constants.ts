0b6002a6cc7d66fce9301f62b8c0199993cdd0e9
Mitren Chinoy <michinoy@microsoft.com>
2019-09-13 12:51:19 -0700

    PR comments.

--- client-react/src/utils/rbac-constants.ts
+++ client-react/src/utils/rbac-constants.ts
@@ -4,7 +4,4 @@ export default class RbacConstants {
   public static deleteScope = './delete';
   public static actionScope = './action';
   public static activeDirectoryWriteScope = 'Microsoft.Authorization/*/Write';
-  public static permissionsSuffix = '/providers/microsoft.authorization/permissions';
-  public static authSuffix = '/providers/Microsoft.Authorization/locks';
-  public static _wildCardEscapeSequence = '\\*';
 }
