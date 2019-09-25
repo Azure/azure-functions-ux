3334864a6d35325490b5b42b0cb016a03ad2fbf9
Krrish Mittal <krrishmittal15@gmail.com>
2019-09-24 18:26:22 -0700

    App Keys for Function Apps (#4151)
    * AppKeys Setup (Dataloader, Service, Main Component)
    
    * Command bar and banner added
    
    * add new keys, show all values bar added
    
    * table added for host and system keys
    
    * filter added
    
    * fetch keys api connected
    
    * add/edit panel UX completed
    
    * add/delete APIs connected
    
    * information banner replaced with messageBar
    
    * empty keyValue API setup
    
    * copy button style changed on portal
    
    * copy button hover fixed
    
    * renew key functionality added for app keys
    
    * app keys model changed
    
    * aria-label added
    
    * copy button re-sized
    
    * table and panel updated with new components
    
    * renew popup added
    
    * hide renew button on new app key create
    
    * resolved pr comments
    
    * property object fixed
    
    * resolved pr comments-2
    
    * pr comments - remove unncessary check

---
+++ client-react/src/pages/app/functions/app-keys/AppKeyAddEdit.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { AppKeysModel } from './AppKeys.types';
+import { useTranslation } from 'react-i18next';
+import TextFieldNoFormik from '../../../../components/form-controls/TextFieldNoFormik';
+import { addEditFormStyle } from '../../../../components/form-controls/formControl.override.styles';
+import ActionBar from '../../../../components/ActionBar';
+
+export interface AppKeyAddEditProps {
+  createAppKey: (item: AppKeysModel) => void;
+  closeBlade: () => void;
+  appKey: AppKeysModel;
+  otherAppKeys: AppKeysModel[];
+  resourceId: string;
+  panelItem: string;
+}
+
+const AppKeyAddEdit: React.FC<AppKeyAddEditProps> = props => {
+  const { appKey, otherAppKeys, createAppKey, closeBlade, panelItem } = props;
+  const [nameError, setNameError] = useState('');
+  const [currentAppKey, setCurrentAppKey] = useState(appKey);
+
+  const { t } = useTranslation();
+
+  const updateAppKeyName = (e: any, name: string) => {
+    const error = validateAppKeyName(name);
+    setNameError(error);
+    setCurrentAppKey({ ...currentAppKey, name });
+  };
+
+  const updateAppKeyValue = (e: any, value: string) => {
+    setCurrentAppKey({ ...currentAppKey, value });
+  };
+
+  const actionBarPrimaryButtonProps = {
+    id: 'save',
+    title: t('ok'),
+    onClick: () => createAppKey(currentAppKey),
+    disable: !!nameError,
+  };
+
+  const actionBarSecondaryButtonProps = {
+    id: 'cancel',
+    title: t('cancel'),
+    onClick: closeBlade,
+    disable: false,
+  };
+
+  const validateAppKeyName = (value: string) => {
+    if (!value) {
+      return t('appKeyPropIsRequired').format('name');
+    }
+    return otherAppKeys.filter(v => v.name.toLowerCase() === value.toLowerCase()).length >= 1 ? t('appKeyNamesUnique') : '';
+  };
+
+  return (
+    <>
+      <form className={addEditFormStyle}>
+        <TextFieldNoFormik
+          label={t('nameRes')}
+          id="app-key-edit-name"
+          widthOverride="100%"
+          value={currentAppKey.name}
+          errorMessage={nameError}
+          onChange={updateAppKeyName}
+          placeholder={t('enterAppKeyName')}
+          copyButton={true}
+          disabled={panelItem === 'edit' ? true : false}
+          autoFocus
+        />
+        <TextFieldNoFormik
+          label={t('value')}
+          id="app-key-edit-value"
+          widthOverride="100%"
+          value={currentAppKey.value}
+          onChange={updateAppKeyValue}
+          placeholder={t('defaultValueAppKey')}
+          copyButton={true}
+        />
+        <ActionBar id="app-key-edit-footer" primaryButton={actionBarPrimaryButtonProps} secondaryButton={actionBarSecondaryButtonProps} />
+      </form>
+    </>
+  );
+};
+
+export default AppKeyAddEdit;
---
+++ client-react/src/pages/app/functions/app-keys/AppKeysDataLoader.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect, useContext } from 'react';
+import AppKeysData from './AppKeys.data';
+import AppKeys from './AppKeys';
+import { AppKeysFormValues } from './AppKeys.types';
+import LoadingComponent from '../../../../components/loading/loading-component';
+import { PortalContext } from '../../../../PortalContext';
+
+const appKeysData = new AppKeysData();
+export const AppKeysContext = React.createContext(appKeysData);
+
+export interface AppKeysDataLoaderProps {
+  resourceId: string;
+}
+
+const AppKeysDataLoader: React.FC<AppKeysDataLoaderProps> = props => {
+  const { resourceId } = props;
+  const [initialValues, setInitialValues] = useState<AppKeysFormValues | null>(null);
+  const [refreshLoading, setRefeshLoading] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(true);
+  const portalContext = useContext(PortalContext);
+
+  const refreshData = () => {
+    setRefeshLoading(true);
+    fetchData();
+  };
+
+  const fetchData = async () => {
+    const site = await appKeysData.getSiteObject(resourceId);
+    const appKeys = await appKeysData.fetchKeys(resourceId);
+
+    if (appKeys.metadata.status === 409) {
+      // TODO: read only permissions given
+    }
+
+    setInitialValues(
+      appKeysData.convertStateToForm({
+        site: site.data,
+        keys: appKeys.metadata.success ? appKeys.data : null,
+      })
+    );
+    portalContext.loadComplete();
+    setInitialLoading(false);
+    setRefeshLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  if (initialLoading || !initialValues || refreshLoading) {
+    return <LoadingComponent />;
+  }
+
+  return (
+    <AppKeysContext.Provider value={appKeysData}>
+      <AppKeys resourceId={resourceId} initialValues={initialValues} refreshData={refreshData} />
+    </AppKeysContext.Provider>
+  );
+};
+
+export default AppKeysDataLoader;
