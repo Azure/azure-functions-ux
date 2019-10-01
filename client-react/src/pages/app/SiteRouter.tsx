be95c2c9968c6ee1eef797a9c023349cc462e2d1
Krrish Mittal <krrishmittal15@gmail.com>
2019-10-01 10:35:59 -0700

    SiteRouterContext added  (#4173)
    * AppServiceRouter renamed to SiteRouter
    
    * site router context created
    
    * fetch site added to site-context
    
    * fetchSite updated for appsettings
    
    * resolved pr comments

--- client-react/src/pages/app/SiteRouter.tsx
+++ client-react/src/pages/app/SiteRouter.tsx
@@ -1,15 +1,19 @@
-import React, { lazy, useContext } from 'react';
+import React, { lazy, useContext, createContext } from 'react';
 import { RouteComponentProps, Router } from '@reach/router';
 import { StartupInfoContext } from '../../StartupInfoContext';
 import { iconStyles } from '../../theme/iconStyles';
 import { ThemeContext } from '../../ThemeContext';
-export interface AppSeriviceRouterProps {
+import { SiteRouterData } from './SiteRouter.data';
+export interface SiteRouterProps {
   subscriptionId?: string;
   resourcegroup?: string;
   siteName?: string;
   slotName?: string;
   functionName?: string;
 }
+
+export const siteRouterData = new SiteRouterData();
+export const SiteRouterContext = createContext(siteRouterData);
 
 const AppSettingsLoadable: any = lazy(() => import(/* webpackChunkName:"appsettings" */ './app-settings/AppSettings'));
 const LogStreamLoadable: any = lazy(() => import(/* webpackChunkName:"logstream" */ './log-stream/LogStreamDataLoader'));
@@ -25,29 +29,31 @@ const FunctionCreateLoadable: any = lazy(() =>
 );
 const FunctionAppKeysLoadable: any = lazy(() => import(/* webpackChunkName:"functionappkeys" */ './functions/app-keys/AppKeysDataLoader'));
 
-const AppServiceRouter: React.FC<RouteComponentProps<AppSeriviceRouterProps>> = props => {
+const SiteRouter: React.FC<RouteComponentProps<SiteRouterProps>> = props => {
   const theme = useContext(ThemeContext);
 
   return (
     <main className={iconStyles(theme)}>
-      <StartupInfoContext.Consumer>
-        {value => {
-          return (
-            value.token && (
-              <Router>
-                <AppSettingsLoadable resourceId={value.resourceId} path="/settings" />
-                <LogStreamLoadable resourceId={value.resourceId} path="/log-stream" />
-                <ChangeAppPlanLoadable resourceId={value.resourceId} path="/changeappplan" />
-                <FunctionIntegrateLoadable resourceId={value.resourceId} path="/integrate" />
-                <FunctionBindingEditorLoadable resourceId={value.resourceId} path="/bindingeditor" />
-                <FunctionCreateLoadable resourceId={value.resourceId} path="/functioncreate" />
-                <FunctionAppKeysLoadable resourceId={value.resourceId} path="/appkeys" />
-              </Router>
-            )
-          );
-        }}
-      </StartupInfoContext.Consumer>
+      <SiteRouterContext.Provider value={siteRouterData}>
+        <StartupInfoContext.Consumer>
+          {value => {
+            return (
+              value.token && (
+                <Router>
+                  <AppSettingsLoadable resourceId={value.resourceId} path="/settings" />
+                  <LogStreamLoadable resourceId={value.resourceId} path="/log-stream" />
+                  <ChangeAppPlanLoadable resourceId={value.resourceId} path="/changeappplan" />
+                  <FunctionIntegrateLoadable resourceId={value.resourceId} path="/integrate" />
+                  <FunctionBindingEditorLoadable resourceId={value.resourceId} path="/bindingeditor" />
+                  <FunctionCreateLoadable resourceId={value.resourceId} path="/functioncreate" />
+                  <FunctionAppKeysLoadable resourceId={value.resourceId} path="/appkeys" />
+                </Router>
+              )
+            );
+          }}
+        </StartupInfoContext.Consumer>
+      </SiteRouterContext.Provider>
     </main>
   );
 };
-export default AppServiceRouter;
+export default SiteRouter;
