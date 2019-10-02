37d2df5a804c29885e8bc70fd268164529174ffc
Mitren Chinoy <nertim@users.noreply.github.com>
2019-10-02 16:58:56 -0700

    Update window.appsvc object in Angular and React for fusion (#4180)
    * update both client and client-react projects to have the exact same window.appsvc object.
    
    * pad each log item with identifying information.
    
    * address pr comments.
    
    * include hostname in data in the logs.
    
    * PR comments

--- client-react/src/portal-communicator.ts
+++ client-react/src/portal-communicator.ts
@@ -26,6 +26,7 @@ import {
   CheckLockRequest,
   CheckLockResponse,
   LockType,
+  PortalDebugInformation,
 } from './models/portal-models';
 import { ISubscription } from './models/subscription';
 import darkModeTheme from './theme/dark';
@@ -36,6 +37,7 @@ import { Dispatch, SetStateAction } from 'react';
 import { ThemeExtended } from './theme/SemanticColorsExtended';
 import LogService from './utils/LogService';
 import { LogCategories } from './utils/LogCategories';
+import { sendHttpRequest, getJsonHeaders } from './ApiHelpers/HttpClient';
 export default class PortalCommunicator {
   public static shellSrc: string;
   private static portalSignature = 'FxAppBlade';
@@ -90,12 +92,42 @@ export default class PortalCommunicator {
     PortalCommunicator.shellSrc = shellSrc;
     if (shellSrc) {
       const getStartupInfoObj = {
-        iframeHostName: null,
+        iframeHostName: '',
+        iframeAppName: '',
       };
-      // This is a required message. It tells the shell that your iframe is ready to receive messages.
-      PortalCommunicator.postMessage(Verbs.ready, null);
-      PortalCommunicator.postMessage(Verbs.initializationcomplete, null);
-      PortalCommunicator.postMessage(Verbs.getStartupInfo, this.packageData(getStartupInfoObj));
+
+      window.appsvc = {
+        version: '',
+        env: {
+          hostName: '',
+          appName: '',
+          azureResourceManagerEndpoint: '',
+          runtimeType: 'Azure',
+        },
+      };
+
+      this.getDebugInformation()
+        .then(response => {
+          if (response.metadata.success && response.data) {
+            getStartupInfoObj.iframeHostName = response.data.hostName;
+            getStartupInfoObj.iframeAppName = response.data.appName;
+            window.appsvc = {
+              version: response.data.version,
+              env: {
+                hostName: response.data.hostName,
+                appName: response.data.appName,
+                azureResourceManagerEndpoint: '',
+                runtimeType: 'Azure',
+              },
+            };
+          }
+        })
+        .finally(() => {
+          // This is a required message. It tells the shell that your iframe is ready to receive messages.
+          PortalCommunicator.postMessage(Verbs.ready, null);
+          PortalCommunicator.postMessage(Verbs.initializationcomplete, null);
+          PortalCommunicator.postMessage(Verbs.getStartupInfo, this.packageData(getStartupInfoObj));
+        });
     }
   }
 
@@ -391,6 +423,12 @@ export default class PortalCommunicator {
       this.setArmTokenInternal(startupInfo.token);
       this.i18n.changeLanguage(startupInfo.effectiveLocale);
       this.setStartupInfo(startupInfo);
+
+      if (window.appsvc) {
+        window.appsvc.env.azureResourceManagerEndpoint = startupInfo.armEndpoint;
+        window.appsvc.resourceId = startupInfo.resourceId;
+        window.appsvc.feature = startupInfo.featureInfo && startupInfo.featureInfo.feature;
+      }
     } else if (methodName === Verbs.sendToken2) {
       this.setArmTokenInternal(data.token);
     } else if (methodName === Verbs.sendNotificationStarted) {
@@ -402,15 +440,27 @@ export default class PortalCommunicator {
 
   private setArmTokenInternal = (token: string) => {
     this.setArmToken(token);
-    window.authToken = token;
+    if (window.appsvc && window.appsvc.env) {
+      window.appsvc.env.armToken = token;
+    }
   };
 
   private setArmEndpointInternal = (endpoint: string) => {
-    window.armEndpoint = endpoint;
+    if (window.appsvc && window.appsvc.env.azureResourceManagerEndpoint) {
+      window.appsvc.env.azureResourceManagerEndpoint = endpoint;
+    }
   };
 
   private packageData = (data: any) => {
     data.frameId = this.frameId;
     return JSON.stringify(data);
+  };
+
+  private getDebugInformation = () => {
+    return sendHttpRequest<PortalDebugInformation>({
+      url: '/api/debug',
+      method: 'GET',
+      headers: getJsonHeaders(),
+    });
   };
 }
--- client-react/src/react-app-env.d.ts
+++ client-react/src/react-app-env.d.ts
@@ -10,6 +10,23 @@ declare module 'office-ui-fabric-react/lib/TeachingBubble' {
   const TeachingBubbleContent: React.StatelessComponent<ITeachingBubbleProps>;
 }
 
+interface Environment {
+  hostName: string;
+  runtimeType: 'OnPrem' | 'Azure' | 'Standalone';
+  azureResourceManagerEndpoint?: string;
+  armToken?: string;
+  appName: string;
+}
+
+interface AppSvc {
+  env: Environment;
+  version: string;
+  resourceId?: string;
+  feature?: string;
+  cdn?: string;
+  cacheBreakQuery?: string;
+}
+
 declare global {
   interface Window {
     /**
@@ -18,8 +35,7 @@ declare global {
      * @param args The list of arguments format arguments. For example: "String with params {0} and {1}".format("val1", "val2");.
      * @return Formatted string.
      */
-    authToken?: string;
-    armEndpoint?: string;
+    appsvc?: AppSvc;
     updateAuthToken?: (type: string) => Promise<string>;
   }
 }
