87190cda2bfbee53bf354e0e8da06ac1f451da90
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-10-01 22:20:21 -0700

    [React] (fixup)

--- client-react/src/models/site/site.ts
+++ client-react/src/models/site/site.ts
@@ -5,6 +5,8 @@ import { SiteConfig } from './config';
 import { CloningInfo } from './cloning-info';
 import Url from '../../utils/url';
 
+const useApiVersion20181101 = Url.getParameterByName(null, 'useApiVersion20181101') === 'true';
+
 export type ContentAvailabilityState = 'Normal' | 'ReadOnly' | number;
 export type RuntimeAvailabilityState = 'Normal' | 'Degraded' | 'NotAvailable' | number;
 export type SiteAvailabilityState = 'Normal' | 'Limited' | 'DisasterRecoveryMode' | number;
@@ -81,90 +83,83 @@ export interface HostNameSslState {
 }
 
 export class SiteContants {
-  public static ContentAvailabilityStates =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          Normal: 'Normal' as ContentAvailabilityState,
-          ReadOnly: 'ReadOnly' as ContentAvailabilityState,
-        }
-      : {
-          Normal: 0 as ContentAvailabilityState,
-          ReadOnly: 1 as ContentAvailabilityState,
-        };
+  public static ContentAvailabilityStates = useApiVersion20181101
+    ? {
+        Normal: 'Normal' as ContentAvailabilityState,
+        ReadOnly: 'ReadOnly' as ContentAvailabilityState,
+      }
+    : {
+        Normal: 0 as ContentAvailabilityState,
+        ReadOnly: 1 as ContentAvailabilityState,
+      };
 
-  public static RuntimeAvailabilityStates =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          Normal: 'Normal' as RuntimeAvailabilityState,
-          Degraded: 'Degraded' as RuntimeAvailabilityState,
-          NotAvailable: 'NotAvailable' as RuntimeAvailabilityState,
-        }
-      : {
-          Normal: 0 as RuntimeAvailabilityState,
-          Degraded: 1 as RuntimeAvailabilityState,
-          NotAvailable: 2 as RuntimeAvailabilityState,
-        };
+  public static RuntimeAvailabilityStates = useApiVersion20181101
+    ? {
+        Normal: 'Normal' as RuntimeAvailabilityState,
+        Degraded: 'Degraded' as RuntimeAvailabilityState,
+        NotAvailable: 'NotAvailable' as RuntimeAvailabilityState,
+      }
+    : {
+        Normal: 0 as RuntimeAvailabilityState,
+        Degraded: 1 as RuntimeAvailabilityState,
+        NotAvailable: 2 as RuntimeAvailabilityState,
+      };
 
-  public static SiteAvailabilityStates =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          Normal: 'Normal' as SiteAvailabilityState,
-          Limited: 'Limited' as SiteAvailabilityState,
-          DisasterRecoveryMode: 'DisasterRecoveryMode' as SiteAvailabilityState,
-        }
-      : {
-          Normal: 0 as SiteAvailabilityState,
-          Limited: 1 as SiteAvailabilityState,
-          DisasterRecoveryMode: 2 as SiteAvailabilityState,
-        };
+  public static SiteAvailabilityStates = useApiVersion20181101
+    ? {
+        Normal: 'Normal' as SiteAvailabilityState,
+        Limited: 'Limited' as SiteAvailabilityState,
+        DisasterRecoveryMode: 'DisasterRecoveryMode' as SiteAvailabilityState,
+      }
+    : {
+        Normal: 0 as SiteAvailabilityState,
+        Limited: 1 as SiteAvailabilityState,
+        DisasterRecoveryMode: 2 as SiteAvailabilityState,
+      };
 
-  public static UsageStates =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          Normal: 'Normal' as UsageState,
-          Exceeded: 'Exceeded' as UsageState,
-        }
-      : {
-          Normal: 0 as UsageState,
-          Exceeded: 1 as UsageState,
-        };
+  public static UsageStates = useApiVersion20181101
+    ? {
+        Normal: 'Normal' as UsageState,
+        Exceeded: 'Exceeded' as UsageState,
+      }
+    : {
+        Normal: 0 as UsageState,
+        Exceeded: 1 as UsageState,
+      };
 
-  public static SslStates =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          Disabled: 'Disabled' as SslState,
-          SniEnabled: 'SniEnabled' as SslState,
-          IpBasedEnabled: 'IpBasedEnabled' as SslState,
-        }
-      : {
-          Disabled: 0 as SslState,
-          SniEnabled: 1 as SslState,
-          IpBasedEnabled: 2 as SslState,
-        };
+  public static SslStates = useApiVersion20181101
+    ? {
+        Disabled: 'Disabled' as SslState,
+        SniEnabled: 'SniEnabled' as SslState,
+        IpBasedEnabled: 'IpBasedEnabled' as SslState,
+      }
+    : {
+        Disabled: 0 as SslState,
+        SniEnabled: 1 as SslState,
+        IpBasedEnabled: 2 as SslState,
+      };
 
-  public static HostTypes =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          Standard: 'Standard' as HostType,
-          Repository: 'Repository' as HostType,
-        }
-      : {
-          Standard: 0 as HostType,
-          Repository: 1 as HostType,
-        };
+  public static HostTypes = useApiVersion20181101
+    ? {
+        Standard: 'Standard' as HostType,
+        Repository: 'Repository' as HostType,
+      }
+    : {
+        Standard: 0 as HostType,
+        Repository: 1 as HostType,
+      };
 
-  public static IpBasedSslStates =
-    Url.getParameterByName(null, 'useApiVersion20181101') === 'true'
-      ? {
-          NotConfigured: 'NotConfigured' as IpBasedSslState,
-          InProgress: 'InProgress' as IpBasedSslState,
-          Configured: 'Configured' as IpBasedSslState,
-          ConfigurationReverted: 'ConfigurationReverted' as IpBasedSslState,
-        }
-      : {
-          NotConfigured: 0 as IpBasedSslState,
-          InProgress: 1 as IpBasedSslState,
-          Configured: 2 as IpBasedSslState,
-          ConfigurationReverted: 3 as IpBasedSslState,
-        };
+  public static IpBasedSslStates = useApiVersion20181101
+    ? {
+        NotConfigured: 'NotConfigured' as IpBasedSslState,
+        InProgress: 'InProgress' as IpBasedSslState,
+        Configured: 'Configured' as IpBasedSslState,
+        ConfigurationReverted: 'ConfigurationReverted' as IpBasedSslState,
+      }
+    : {
+        NotConfigured: 0 as IpBasedSslState,
+        InProgress: 1 as IpBasedSslState,
+        Configured: 2 as IpBasedSslState,
+        ConfigurationReverted: 3 as IpBasedSslState,
+      };
 }
