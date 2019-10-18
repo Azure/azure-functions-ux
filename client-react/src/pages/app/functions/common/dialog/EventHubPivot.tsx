f11d0cf057b268113c01dd2b7a4f18782b971763
Allison Manifold <allisonm@microsoft.com>
2019-10-17 20:52:45 -0700

    update resources

--- client-react/src/pages/app/functions/common/dialog/EventHubPivot.tsx
+++ client-react/src/pages/app/functions/common/dialog/EventHubPivot.tsx
@@ -80,7 +80,7 @@ const EventHubPivot: React.SFC<NewConnectionDialogProps & CustomDropdownProps & 
 
   return (
     <form style={paddingSidesStyle}>
-      {namespaces && namespaces.length === 0 && <p>{'No Namespaces in this subscription'}</p>}
+      {namespaces && namespaces.length === 0 && <p>{t('eventHubPicker_noNamespaces')}</p>}
       <Dropdown
         label={t('eventHubPicker_namespace')}
         options={namespaceOptions}
@@ -98,7 +98,7 @@ const EventHubPivot: React.SFC<NewConnectionDialogProps & CustomDropdownProps & 
         }
       />
       {!eventHubs && <LoadingComponent />}
-      {eventHubs && eventHubs.length === 0 && <p>{'No Event Hubs in this Namespace'}</p>}
+      {eventHubs && eventHubs.length === 0 && <p>{t('eventHubPicker_noEventHubs')}</p>}
       <Dropdown
         label={t('eventHubPicker_eventHub')}
         options={eventHubOptions}
@@ -107,7 +107,7 @@ const EventHubPivot: React.SFC<NewConnectionDialogProps & CustomDropdownProps & 
       />
       {(!namespaceAuthRules || !eventHubAuthRules) && <LoadingComponent />}
       {namespaceAuthRules && namespaceAuthRules.length === 0 && (eventHubAuthRules && eventHubAuthRules.length === 0) && (
-        <p>{'No Policies in this Namespace and Event Hub'}</p>
+        <p>{t('eventHubPicker_noPolicies')}</p>
       )}
       <Dropdown
         label={t('eventHubPicker_policy')}
