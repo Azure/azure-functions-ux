f099998f2ab53097d46ab47655bf625b5d38810c
Shibani Medhekar <shimedh@microsoft.com>
2019-10-28 09:31:30 -0700

    AB#5635306 [Ibizafication][FunctionsEditor] Change getFunctionUrl Dialog to Callout

--- client-react/src/pages/app/functions/function-editor/FunctionEditorCommandBar.tsx
+++ client-react/src/pages/app/functions/function-editor/FunctionEditorCommandBar.tsx
@@ -1,27 +1,49 @@
 import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react/lib/CommandBar';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { CommandBarStyles } from '../../../../theme/CustomOfficeFabric/AzurePortal/CommandBar.styles';
 import { PortalContext } from '../../../../PortalContext';
 import { CustomCommandBarButton } from '../../../../components/CustomCommandBarButton';
+import FunctionEditorGetFunctionUrlCallout, { HostUrl } from './FunctionEditorGetFunctionUrlCallout';
+import { IDropdownOption } from 'office-ui-fabric-react/lib/Dropdown';
+import { IContextualMenuRenderItem } from 'office-ui-fabric-react';
 
 // Data for CommandBar
 interface FunctionEditorCommandBarProps {
   saveFunction: () => void;
   resetFunction: () => void;
   testFunction: () => void;
-  getFunctionUrl: () => void;
   showGetFunctionUrlCommand: boolean;
   dirty: boolean;
   disabled: boolean;
+  hostKeyDropdownOptions: IDropdownOption[];
+  hostKeyDropdownSelectedKey: string;
+  hostUrls: HostUrl[];
 }
 
 const FunctionEditorCommandBar: React.FC<FunctionEditorCommandBarProps> = props => {
-  const { saveFunction, resetFunction, testFunction, getFunctionUrl, showGetFunctionUrlCommand, dirty, disabled } = props;
+  const {
+    saveFunction,
+    resetFunction,
+    testFunction,
+    showGetFunctionUrlCommand,
+    dirty,
+    disabled,
+    hostUrls,
+    hostKeyDropdownOptions,
+    hostKeyDropdownSelectedKey,
+  } = props;
   const { t } = useTranslation();
   const portalCommunicator = useContext(PortalContext);
+  const [isDialogVisible, setIsDialogVisible] = useState<boolean>(false);
+  const onClickGetFunctionUrlCommand = () => {
+    setIsDialogVisible(true);
+  };
+
+  const getFunctionUrlButtonRef = useRef<IContextualMenuRenderItem | null>(null);
+
   const getItems = (): ICommandBarItemProps[] => {
-    const items = [
+    const items: ICommandBarItemProps[] = [
       {
         key: 'save',
         name: t('save'),
@@ -63,7 +85,8 @@ const FunctionEditorCommandBar: React.FC<FunctionEditorCommandBarProps> = props 
         },
         disabled: disabled,
         ariaLabel: t('functionEditorGetFunctionUrlAriaLabel'),
-        onClick: getFunctionUrl,
+        onClick: onClickGetFunctionUrlCommand,
+        componentRef: ref => (getFunctionUrlButtonRef.current = ref),
       });
     }
 
@@ -75,13 +98,26 @@ const FunctionEditorCommandBar: React.FC<FunctionEditorCommandBarProps> = props 
   }, [dirty]);
 
   return (
-    <CommandBar
-      items={getItems()}
-      aria-role="nav"
-      styles={CommandBarStyles}
-      ariaLabel={t('functionEditorCommandBarAriaLabel')}
-      buttonAs={CustomCommandBarButton}
-    />
+    <>
+      <CommandBar
+        items={getItems()}
+        aria-role="nav"
+        styles={CommandBarStyles}
+        ariaLabel={t('functionEditorCommandBarAriaLabel')}
+        buttonAs={CustomCommandBarButton}
+      />
+      {isDialogVisible && (
+        <>
+          <FunctionEditorGetFunctionUrlCallout
+            hostKeyDropdownOptions={hostKeyDropdownOptions}
+            hostKeyDropdownSelectedKey={hostKeyDropdownSelectedKey}
+            hostUrls={hostUrls}
+            setIsDialogVisible={setIsDialogVisible}
+            dialogTarget={getFunctionUrlButtonRef.current}
+          />
+        </>
+      )}
+    </>
   );
 };
 
---
+++ client-react/src/pages/app/functions/function-editor/FunctionEditorGetFunctionUrlCallout.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Dropdown as OfficeDropdown, IDropdownOption, Stack, Callout, DirectionalHint, Label } from 'office-ui-fabric-react';
+import { formLabelStyle } from '../../../../components/form-controls/formControl.override.styles';
+import TextFieldNoFormik from '../../../../components/form-controls/TextFieldNoFormik';
+import { style } from 'typestyle';
+
+export interface HostUrl {
+  key: string;
+  url: string;
+}
+
+interface FunctionEditorGetFunctionUrlCalloutProps {
+  hostKeyDropdownOptions: IDropdownOption[];
+  hostKeyDropdownSelectedKey: string;
+  hostUrls: HostUrl[];
+  setIsDialogVisible: (isVisible: boolean) => void;
+  dialogTarget: any;
+}
+
+const FunctionEditorGetFunctionUrlCallout: React.FC<FunctionEditorGetFunctionUrlCalloutProps> = props => {
+  const { hostKeyDropdownOptions, hostKeyDropdownSelectedKey, hostUrls, setIsDialogVisible, dialogTarget } = props;
+  const { t } = useTranslation();
+  const [url, setUrl] = useState<string>(() => {
+    for (const hostUrl of hostUrls) {
+      if (hostUrl.key === hostKeyDropdownSelectedKey) {
+        return hostUrl.url;
+      }
+    }
+
+    return '';
+  });
+
+  const onCloseDialog = () => {
+    setIsDialogVisible(false);
+  };
+
+  const onChangeHostKeyDropdown = (e: unknown, option: IDropdownOption) => {
+    setUrl(hostUrls[option.key]);
+  };
+
+  const calloutStyle = () =>
+    style({
+      padding: '20px',
+      minWidth: '600px',
+    });
+
+  return (
+    <Callout
+      role="alertdialog"
+      gapSpace={380}
+      target={dialogTarget}
+      onDismiss={onCloseDialog}
+      setInitialFocus={true}
+      directionalHint={DirectionalHint.rightBottomEdge}
+      isBeakVisible={true}>
+      <div className={calloutStyle()}>
+        <div className="ms-CalloutExample-header">
+          <h4 className="ms-CalloutExample-title">{t('keysDialog_getFunctionUrl')}</h4>
+        </div>
+        <Stack>
+          <Stack horizontal>
+            <Label className={`${formLabelStyle(false, false)}`}>{t('keysDialog_key')}</Label>
+            <OfficeDropdown
+              selectedKey={hostKeyDropdownSelectedKey}
+              options={hostKeyDropdownOptions}
+              onChange={onChangeHostKeyDropdown}
+              ariaLabel={t('functionAppDirectoryDropdownAriaLabel')}
+            />
+          </Stack>
+          <Stack horizontal>
+            <Label className={`${formLabelStyle(false, false)}`}>{t('keysDialog_url')}</Label>
+            <TextFieldNoFormik label={''} id="function-editor-function-url" value={url} disabled={true} copyButton={true} />
+          </Stack>
+        </Stack>
+      </div>
+    </Callout>
+  );
+};
+
+export default FunctionEditorGetFunctionUrlCallout;
