c82c27dcf5b76e79e34008f85aa22189e6d69b6d
Allison Manifold <allisonm@microsoft.com>
2019-10-29 13:50:55 -0700

    multiple bindings can fuel function creation

--- client-react/src/pages/app/functions/common/BindingFormBuilder.tsx
+++ client-react/src/pages/app/functions/common/BindingFormBuilder.tsx
@@ -39,8 +39,8 @@ export class BindingFormBuilder {
   }
 
   constructor(
-    private _bindingInfo: BindingInfo,
-    private _bindingMetadata: BindingConfigMetadata,
+    private _bindingInfoList: BindingInfo[],
+    private _bindingMetadataList: BindingConfigMetadata[],
     private _resourceId: string,
     private _t: i18next.TFunction,
     private _variables: { [key: string]: string }
@@ -49,23 +49,25 @@ export class BindingFormBuilder {
   public getInitialFormValues() {
     const initialFormValues: BindingEditorFormValues = {};
 
-    for (const setting of this._bindingMetadata.settings) {
-      let value = this._bindingInfo[setting.name];
+    let i = 0;
+    for (const bindingMetadata of this._bindingMetadataList) {
+      for (const setting of bindingMetadata.settings) {
+        let value = this._bindingInfoList[i][setting.name];
 
-      // If the stored value is empty, then make the assumption that everything is selected.
-      // That's how it works for HTTP, so for now let's assume that's how it works for all checkBoxLists
-      if (setting.value === BindingSettingValue.checkBoxList && !value) {
-        value = setting.enum ? setting.enum.map(e => e.value) : [];
+        // If the stored value is empty, then make the assumption that everything is selected.
+        // That's how it works for HTTP, so for now let's assume that's how it works for all checkBoxLists
+        if (setting.value === BindingSettingValue.checkBoxList && !value) {
+          value = setting.enum ? setting.enum.map(e => e.value) : [];
+        }
+
+        initialFormValues[setting.name] = value;
       }
 
-      initialFormValues[setting.name] = value;
+      // Bindings metadata uses 'trigger' as a direction, but functions.json does not
+      initialFormValues.direction = bindingMetadata.direction === BindingConfigDirection.trigger ? 'in' : bindingMetadata.direction;
+      initialFormValues.type = bindingMetadata.type;
+      i += 1;
     }
-
-    // Bindings metadata uses 'trigger' as a direction, but functions.json does not
-    initialFormValues.direction =
-      this._bindingMetadata.direction === BindingConfigDirection.trigger ? 'in' : this._bindingMetadata.direction;
-    initialFormValues.type = this._bindingMetadata.type;
-
     return initialFormValues;
   }
 
@@ -73,27 +75,31 @@ export class BindingFormBuilder {
     const fields: JSX.Element[] = [];
 
     let key = keyOffset;
-    for (const setting of this._bindingMetadata.settings) {
-      switch (setting.value) {
-        case BindingSettingValue.string:
-          if (setting.resource) {
-            fields.push(this._getResourceField(key, setting, formProps, isDisabled, this._resourceId));
-          } else {
-            fields.push(this._getTextField(key, setting, formProps, isDisabled));
-          }
-          break;
-        case BindingSettingValue.enum:
-          fields.push(this._getDropdown(key, setting, formProps, isDisabled));
-          break;
-        case BindingSettingValue.checkBoxList:
-          fields.push(this._getMultiSelectDropdown(key, setting, formProps, isDisabled));
-          break;
-        case BindingSettingValue.boolean:
-          fields.push(this._getBooleanToggle(key, setting, formProps, isDisabled));
-          break;
-      }
+    let i = 0;
+    for (const bindingMetadata of this._bindingMetadataList) {
+      for (const setting of bindingMetadata.settings) {
+        switch (setting.value) {
+          case BindingSettingValue.string:
+            if (setting.resource) {
+              fields.push(this._getResourceField(key, setting, formProps, isDisabled, this._resourceId));
+            } else {
+              fields.push(this._getTextField(key, setting, formProps, isDisabled));
+            }
+            break;
+          case BindingSettingValue.enum:
+            fields.push(this._getDropdown(key, setting, formProps, isDisabled));
+            break;
+          case BindingSettingValue.checkBoxList:
+            fields.push(this._getMultiSelectDropdown(key, setting, formProps, isDisabled, i));
+            break;
+          case BindingSettingValue.boolean:
+            fields.push(this._getBooleanToggle(key, setting, formProps, isDisabled));
+            break;
+        }
 
-      key = key + 1;
+        key = key + 1;
+      }
+      i = +1;
     }
 
     return fields;
@@ -193,9 +199,10 @@ export class BindingFormBuilder {
     key: number,
     setting: BindingConfigUIDefinition,
     formProps: FormikProps<BindingEditorFormValues>,
-    isDisabled: boolean
+    isDisabled: boolean,
+    i: number
   ) {
-    if (this._bindingInfo.type.toLowerCase() === 'httptrigger') {
+    if (this._bindingInfoList[i].type.toLowerCase() === 'httptrigger') {
       return (
         <FormControlWrapper
           label={BindingFormBuilder.getLocalizedString(setting.label, this._t, this._variables)}
