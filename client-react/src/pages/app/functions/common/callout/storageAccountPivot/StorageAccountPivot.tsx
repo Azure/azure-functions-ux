17ab50847b3deaadefdc010204f92b718878b665
Allison Manifold <allisonm@microsoft.com>
2019-10-29 16:05:22 -0700

    storage callout

---
+++ client-react/src/pages/app/functions/common/callout/storageAccountPivot/StorageAccountPivot.tsx
@@ -0,0 +1,102 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { NewConnectionCalloutProps } from '../Callout.properties';
+import { FieldProps, Formik, FormikProps } from 'formik';
+import LoadingComponent from '../../../../../../components/loading/loading-component';
+import { StorageAccountPivotContext } from './StorageAccountPivotDataLoader';
+import { ArmObj } from '../../../../../../models/arm-obj';
+import LogService from '../../../../../../utils/LogService';
+import { LogCategories } from '../../../../../../utils/LogCategories';
+import { IDropdownOption, Dropdown, DefaultButton } from 'office-ui-fabric-react';
+import { useTranslation } from 'react-i18next';
+import { paddingSidesStyle, paddingTopStyle } from '../Callout.styles';
+import { BindingEditorFormValues } from '../../BindingFormBuilder';
+import { StorageAccount } from '../../../../../../models/storage-account';
+
+interface DocumentDBPivotFormValues {
+  storageAccount: ArmObj<StorageAccount> | undefined;
+}
+
+const DocumentDBPivot: React.SFC<NewConnectionCalloutProps & FieldProps> = props => {
+  const provider = useContext(StorageAccountPivotContext);
+  const { t } = useTranslation();
+  const { resourceId } = props;
+  const [formValues, setFormValues] = useState<DocumentDBPivotFormValues>({ storageAccount: undefined });
+  const [storageAccounts, setStorageAccounts] = useState<ArmObj<StorageAccount>[] | undefined>(undefined);
+
+  useEffect(() => {
+    if (!storageAccounts) {
+      provider.fetchAzureStorageAccounts(resourceId).then(r => {
+        if (!r.metadata.success) {
+          LogService.trackEvent(
+            LogCategories.bindingResource,
+            'getDatabaseAccounts',
+            `Failed to get Database Accounts: ${r.metadata.error}`
+          );
+          return;
+        }
+        setStorageAccounts(r.data.value);
+      });
+    }
+  }, [formValues]);
+
+  if (!storageAccounts) {
+    return <LoadingComponent />;
+  }
+
+  const storageAccountOptions: IDropdownOption[] = [];
+  storageAccounts.forEach(account => storageAccountOptions.push({ text: account.name, key: account.id, data: account }));
+  if (!formValues.storageAccount && storageAccountOptions.length > 0) {
+    setFormValues({ ...formValues, storageAccount: storageAccounts[0] });
+  }
+
+  return (
+    <Formik
+      initialValues={formValues}
+      onSubmit={() =>
+        setStorageAccountConnection(formValues, props.setNewAppSettingName, props.setIsDialogVisible, props.form, props.field)
+      }>
+      {(formProps: FormikProps<DocumentDBPivotFormValues>) => {
+        return (
+          <form style={paddingSidesStyle}>
+            {!!storageAccounts && storageAccounts.length === 0 ? (
+              <p>{t('documentDBPivot_noDatabaseAccounts')}</p>
+            ) : (
+              <>
+                <Dropdown
+                  label={t('documentDBPivot_databaseAccount')}
+                  options={storageAccountOptions}
+                  selectedKey={formValues.storageAccount && formValues.storageAccount.id}
+                  onChange={(o, e) => {
+                    setFormValues({ storageAccount: e && e.data });
+                  }}
+                />
+              </>
+            )}
+            <footer style={paddingTopStyle}>
+              <DefaultButton disabled={!formValues.storageAccount} onClick={formProps.submitForm}>
+                {t('ok')}
+              </DefaultButton>
+            </footer>
+          </form>
+        );
+      }}
+    </Formik>
+  );
+};
+
+const setStorageAccountConnection = (
+  formValues: DocumentDBPivotFormValues,
+  setNewAppSettingName: (e: string) => void,
+  setIsDialogVisible: (d: boolean) => void,
+  formProps: FormikProps<BindingEditorFormValues>,
+  field: { name: string; value: any }
+) => {
+  if (formValues.storageAccount) {
+    const appSettingName = `${formValues.storageAccount.name}_STORAGE`;
+    formProps.setFieldValue(field.name, appSettingName);
+    setNewAppSettingName(appSettingName);
+    setIsDialogVisible(false);
+  }
+};
+
+export default DocumentDBPivot;
