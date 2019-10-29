b9e07d3dc50a0db4c2119665c34417ef69b89587
shimedh <shimedh@microsoft.com>
2019-10-29 13:02:13 -0700

    AB#5633015 FRee and Shared should not include mention of dedicated resources (#4254)

--- client-react/src/pages/app/spec-picker/specs/SharedPlanPriceSpec.ts
+++ client-react/src/pages/app/spec-picker/specs/SharedPlanPriceSpec.ts
@@ -24,13 +24,6 @@ export abstract class SharedPlanPriceSpec extends PriceSpec {
 
     this.hardwareItems = [
       {
-        id: 'pricing_includedHardware_azureComputeUnits',
-        iconUrl: 'image/app-service-plan.svg',
-        title: t('pricing_includedHardware_azureComputeUnits'),
-        description: t('pricing_computeDedicatedAcu'),
-        learnMoreUrl: CommonConstants.Links.azureComputeUnitLearnMore,
-      },
-      {
         id: 'memory',
         iconUrl: 'image/website-power.svg',
         title: t('memory'),
