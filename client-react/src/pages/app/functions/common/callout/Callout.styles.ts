124b7e89c590ed217f3ec8ebc29db908ba7cfe83
Allison Manifold <allisonm@microsoft.com>
2019-10-30 10:07:52 -0700

    update create styling

--- client-react/src/pages/app/functions/common/callout/Callout.styles.ts
+++ client-react/src/pages/app/functions/common/callout/Callout.styles.ts
@@ -6,3 +6,26 @@ export const paddingSidesStyle = {
   paddingLeft: '8px',
   paddingRight: '8px',
 };
+
+export const linkPaddingStyle = {
+  marginTop: '-10px',
+  paddingBottom: '10px',
+};
+
+export const calloutSyle3Fields = {
+  padding: '10px',
+  height: 300,
+  width: 400,
+};
+
+export const calloutSyle2Fields = {
+  padding: '10px',
+  height: 235,
+  width: 400,
+};
+
+export const calloutSyle1Field = {
+  padding: '10px',
+  height: 175,
+  width: 400,
+};
