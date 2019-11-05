a82e41fe9d1bd38ce48f26d8b04736642daade10
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-11-04 17:32:23 -0800

    AB#5680365 Disabling all controls in config blade while saving (#4274)
    AB#5680365 Disabling all controls in config blade while saving

--- client-react/src/pages/app/app-settings/ConnectionStrings/ConnectionStrings.tsx
+++ client-react/src/pages/app/app-settings/ConnectionStrings/ConnectionStrings.tsx
@@ -20,7 +20,8 @@ import Panel from '../../../../components/Panel/Panel';
 import { ThemeContext } from '../../../../ThemeContext';
 
 const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTranslation> = props => {
-  const permissionContext = useContext(PermissionsContext);
+  const { production_write, editable, saving } = useContext(PermissionsContext);
+  const disableAllControls = !editable || saving;
   const [showPanel, setShowPanel] = useState(false);
   const [panelItem, setPanelItem] = useState('add');
   const [currentConnectionString, setCurrentConnectionString] = useState<FormConnectionString | null>(null);
@@ -39,7 +40,7 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
       {
         key: 'app-settings-connection-strings-add',
         onClick: createNewItem,
-        disabled: !permissionContext.editable,
+        disabled: disableAllControls,
         iconProps: { iconName: 'Add' },
         name: t('newConnectionString'),
         ariaLabel: t('addNewConnectionString'),
@@ -53,7 +54,7 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
       {
         key: 'app-settings-connection-strings-bulk-edit',
         onClick: openBulkEdit,
-        disabled: !permissionContext.editable,
+        disabled: disableAllControls,
         iconProps: { iconName: 'Edit' },
         name: t('advancedEdit'),
       },
@@ -179,7 +180,7 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
           closeDelay={500}>
           <IconButton
             className={defaultCellStyle}
-            disabled={!permissionContext.editable}
+            disabled={disableAllControls}
             id={`app-settings-connection-strings-delete-${index}`}
             iconProps={{ iconName: 'Delete' }}
             ariaLabel={t('delete')}
@@ -197,7 +198,7 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
           closeDelay={500}>
           <IconButton
             className={defaultCellStyle}
-            disabled={!permissionContext.editable}
+            disabled={disableAllControls}
             id={`app-settings-connection-strings-edit-${index}`}
             iconProps={{ iconName: 'Edit' }}
             ariaLabel={t('edit')}
@@ -249,7 +250,11 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
         column.className = dirtyElementStyle(theme);
       }
       return (
-        <ActionButton id={`app-settings-connection-strings-name-${index}`} className={defaultCellStyle} onClick={() => onShowPanel(item)}>
+        <ActionButton
+          id={`app-settings-connection-strings-name-${index}`}
+          className={defaultCellStyle}
+          disabled={disableAllControls}
+          onClick={() => onShowPanel(item)}>
           <span aria-live="assertive" role="region">
             {item[column.fieldName!]}
           </span>
@@ -374,7 +379,7 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
           connectionString={currentConnectionString!}
           otherConnectionStrings={values.connectionStrings}
           updateConnectionString={onClosePanel}
-          disableSlotSetting={!permissionContext.production_write}
+          disableSlotSetting={!production_write}
           closeBlade={onCancel}
           site={values.site}
         />
@@ -385,7 +390,7 @@ const ConnectionStrings: React.FC<FormikProps<AppSettingsFormValues> & WithTrans
             updateAppSetting={saveBulkEdit}
             closeBlade={onCancel}
             connectionStrings={values.connectionStrings}
-            disableSlotSetting={!permissionContext.production_write}
+            disableSlotSetting={!production_write}
           />
         </Suspense>
       </Panel>
