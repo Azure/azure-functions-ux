127366e882912eea84ed5f227e81a4168e59e1c5
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-11-08 20:39:39 -0800

    AB#5525777 Adding DailyUsageQuota to Function Runtime Settings tab

--- client-react/src/pages/app/app-settings/Sections/FunctionRuntimeSettingsPivot.tsx
+++ client-react/src/pages/app/app-settings/Sections/FunctionRuntimeSettingsPivot.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
-import { AppSettingsFormValues } from '../AppSettings.types';
 import { FormikProps } from 'formik';
+import { useTranslation } from 'react-i18next';
+import { AppSettingsFormValues } from '../AppSettings.types';
+import { isEqual } from 'lodash-es';
+import DailyUsageQuota from '../FunctionRuntimeSettings/DailyUsageQuota';
+import { ScenarioIds } from '../../../../utils/scenario-checker/scenario-ids';
+import { ScenarioService } from '../../../../utils/scenario-checker/scenario.service';
+import { style } from 'typestyle';
+
+const tabContainerStyle = style({
+  marginTop: '15px',
+});
 
 const FunctionRuntimeSettingsPivot: React.FC<FormikProps<AppSettingsFormValues>> = props => {
-  return null;
+  const { t } = useTranslation();
+  const scenarioChecker = new ScenarioService(t);
+  const site = props.initialValues.site;
+
+  if (!site) {
+    return null;
+  }
+
+  return (
+    <>
+      <div id="function-runtime-settings" className={tabContainerStyle}>
+        {scenarioChecker.checkScenario(ScenarioIds.dailyUsageQuotaSupported, { site }).status === 'enabled' && (
+          <DailyUsageQuota {...props} />
+        )}
+      </div>
+    </>
+  );
+};
+
+const dailyMemoryTimeQuotaDirty = (values: AppSettingsFormValues, initialValues: AppSettingsFormValues) => {
+  return !isEqual(values.site.properties.dailyMemoryTimeQuota, initialValues.site.properties.dailyMemoryTimeQuota);
 };
 
 export const functionRuntimeSettingsDirty = (values: AppSettingsFormValues, initialValues: AppSettingsFormValues) => {
-  return false;
+  return dailyMemoryTimeQuotaDirty(values, initialValues);
 };
 
 export default FunctionRuntimeSettingsPivot;
