1db6f9d26bd56698286e27a0c7a8e545f3757041
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-11-13 17:23:36 -0800

    PR feedback - RuntimeVersion

--- client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeVersion.tsx
+++ client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeVersion.tsx
@@ -12,7 +12,6 @@ import { messageBannerStyle } from '../AppSettings.styles';
 import { RuntimeExtensionMajorVersions } from '../../../../models/functions/runtime-extension';
 import { findFormAppSettingValue } from '../AppSettingsFormData';
 import { CommonConstants } from '../../../../utils/CommonConstants';
-import { FunctionsRuntimeVersionHelper } from '../../../../utils/FunctionsRuntimeVersionHelper';
 
 const RuntimeVersion: React.FC<AppSettingsFormProps & WithTranslation> = props => {
   const { t, initialValues, asyncData } = props;
@@ -32,55 +31,100 @@ const RuntimeVersion: React.FC<AppSettingsFormProps & WithTranslation> = props =
     }
   };
 
-  const getInfoBox = () => {
-    const initialRuntimeVersion = findFormAppSettingValue(
-      initialValues.appSettings,
-      CommonConstants.AppSettingNames.functionsExtensionVersion
-    );
-    const initialRuntimeMajorVersion = FunctionsRuntimeVersionHelper.getFunctionsRuntimeMajorVersion(initialRuntimeVersion);
-    const exactRuntimeVersion = asyncData.functionsHostStatus.value && asyncData.functionsHostStatus.value.properties.version;
-
-    if (initialRuntimeMajorVersion !== RuntimeExtensionMajorVersions.custom) {
-      return { message: '' };
+  // Returns a warning/error message to be shown if one of the the following scenarios is true:
+  //  -FUNCTIONS_EXTENSION_VERSION is missing or set to empty.
+  //  -FUNCTIONS_EXTENSION_VERSION is set to 'latest' or 'beta'.
+  //  -FUNCTIONS_EXTENSION_VERSION is set to a valid exact version.
+  //  -FUNCTIONS_EXTENSION_VERSION is set to an invalid value.
+  const getVersionMessageBar = () => {
+    switch (initialVersionToLowerTrimmed) {
+      case RuntimeExtensionMajorVersions.v1:
+      case RuntimeExtensionMajorVersions.v2:
+      case RuntimeExtensionMajorVersions.v3:
+        // FUNCTIONS_EXTENSION_VERSION is set to a valid major version, so we don't need to show any warning/error
+        return { messageText: '' };
+      case '':
+        // FUNCTIONS_EXTENSION_VERSION is missing or set to empty, so show an error message.
+        return {
+          messageText: t('functionsRuntimeVersionMissingWarning'),
+          messageType: MessageBarType.error,
+        };
+      case 'latest':
+      case 'beta':
+        // FUNCTIONS_EXTENSION_VERSION is set to 'latest' or 'beta', so show a warning message.
+        // If we know the running version (from the host status call), include it in the warning message.
+        return {
+          messageText: !exactRuntimeVersion
+            ? t('functionsRuntimeVersionLatestOrBetaWarning').format(initialRuntimeVersion)
+            : t('functionsRuntimeVersionLatestOrBetaWithExactVersionWarning').format(initialRuntimeVersion, exactRuntimeVersion),
+          messageType: MessageBarType.warning,
+        };
+      default:
+        // FUNCTIONS_EXTENSION_VERSION is set to a non-empty value other than 'latest' or 'beta'.
+        // We need to check against the running version (from the host satus call) to determine whether the value is valid.
+        return getVersionMessageBarForCustom();
     }
+  };
 
-    if (!initialRuntimeVersion) {
-      return {
-        message: t('functionsRuntimeVersionMissingWarning'),
-        type: MessageBarType.error,
-      };
-    }
+  const getVersionMessageBarForCustom = () => {
+    // We need to compare the custom runtime version against the running version (from the host satus call) to determine whether the value is valid.
 
-    if (initialRuntimeVersion.toLowerCase() === 'latest' || initialRuntimeVersion.toLowerCase() === 'beta') {
-      return {
-        message: !exactRuntimeVersion
-          ? t('functionsRuntimeVersionLatestOrBetaWarning').format(initialRuntimeVersion)
-          : t('functionsRuntimeVersionLatestOrBetaWithExactVersionWarning').format(initialRuntimeVersion, exactRuntimeVersion),
-        type: MessageBarType.warning,
-      };
+    if (asyncData.functionsHostStatus.loadingState === 'loading') {
+      // The host status call hasn't completed yet, so we can't check whether the configured rutime version is valid.
+      // Don't show any error/warning, and wait for the call to complete.
+      return { messageText: '' };
     }
 
     if (!exactRuntimeVersion) {
-      return { message: '' };
-    }
-
-    if (
-      initialRuntimeVersion.toLowerCase() === exactRuntimeVersion.toLowerCase() ||
-      initialRuntimeVersion.toLowerCase() === exactRuntimeVersion.toLowerCase().replace(/.0$/, '-alpha')
-    ) {
+      // The host status call failed or it completed but returned a null/empty version.
+      // We can't tell whether the configured version is valid, but we show a warning.
       return {
-        message: t('functionsRuntimeVersionNeedsUpdateWarning').format(exactRuntimeVersion),
-        type: MessageBarType.warning,
+        messageText: t('functionsRuntimeVersionMissingWarning'),
+        messageType: MessageBarType.warning,
       };
     }
 
-    return {
-      message: t('functionsRuntimeVersionInvalidWarning').format(initialRuntimeVersion, exactRuntimeVersion),
-      type: MessageBarType.error,
-    };
+    // The host status call completed and we have a running version to validate against.
+    // Show the appropraite warning/error based on whether the configured and running versions match.
+    const comparison = compareConfiguredAndRunningVersions();
+    switch (comparison) {
+      case 'ExactMatch':
+        return {
+          message: t('functionsRuntimeVersionNeedsUpdateWarning').format(exactRuntimeVersion),
+          type: MessageBarType.warning,
+        };
+      case 'PartialMatch':
+        return {
+          message: t('functionsRuntimeVersionNeedsUpdateWarning').format(`${exactRuntimeVersion} (${initialRuntimeVersion})`),
+          type: MessageBarType.warning,
+        };
+      case 'NoMatch':
+        return {
+          messageText: t('functionsRuntimeVersionInvalidWarning').format(initialRuntimeVersion, exactRuntimeVersion),
+          messageType: MessageBarType.error,
+        };
+    }
   };
 
-  const { message, type: messageType } = getInfoBox() as { message: string; type?: MessageBarType };
+  const compareConfiguredAndRunningVersions = () => {
+    const exactVersionToLower = (exactRuntimeVersion || '').toLowerCase();
+
+    if (initialVersionToLowerTrimmed === exactVersionToLower) {
+      return 'ExactMatch';
+    }
+
+    // remove single leading '~', trailing '-alpha'
+    const initialVersionNormalized = (initialVersionToLowerTrimmed || '').toLowerCase().replace(/^~?|(-alpha)?$/g, '');
+
+    return !!initialVersionNormalized && exactVersionToLower.startsWith(initialVersionNormalized) ? 'PartialMatch' : 'NoMatch';
+  };
+
+  const exactRuntimeVersion = (asyncData.functionsHostStatus.value && asyncData.functionsHostStatus.value.properties.version) || '';
+  const initialRuntimeVersion =
+    findFormAppSettingValue(initialValues.appSettings, CommonConstants.AppSettingNames.functionsExtensionVersion) || '';
+  const initialVersionToLowerTrimmed = (initialRuntimeVersion || '').toLowerCase().replace(/^\s*|\s*$/g, '');
+
+  const { messageText, messageType } = getVersionMessageBar() as { messageText: string; messageType?: MessageBarType };
 
   return (
     <>
@@ -93,13 +137,13 @@ const RuntimeVersion: React.FC<AppSettingsFormProps & WithTranslation> = props =
           {t('readWritePermissionsRequired')}
         </MessageBar>
       ) : (
-        !!message && (
+        !!messageText && (
           <MessageBar
             id="function-app-settings-runtime-version-message"
             isMultiline={true}
             className={messageType ? messageBannerStyle(theme, messageType) : undefined}
             messageBarType={messageType}>
-            {message}
+            {messageText}
           </MessageBar>
         )
       )}
