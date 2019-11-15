b7769f10b48648ed45bc1072e610487c8c5a564e
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-11-15 10:46:30 -0800

    AB#5525777 Adding RuntimeVersion

--- client-react/src/pages/app/app-settings/Sections/FunctionRuntimeSettingsPivot.tsx
+++ client-react/src/pages/app/app-settings/Sections/FunctionRuntimeSettingsPivot.tsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import { FormikProps } from 'formik';
 import { useTranslation } from 'react-i18next';
-import { AppSettingsFormValues } from '../AppSettings.types';
 import { isEqual } from 'lodash-es';
+import { style } from 'typestyle';
+import { AppSettingsFormValues, AppSettingsFormProps } from '../AppSettings.types';
+import { findFormAppSettingValue } from '../AppSettingsFormData';
 import DailyUsageQuota from '../FunctionRuntimeSettings/DailyUsageQuota';
+import RuntimeVersion from '../FunctionRuntimeSettings/RuntimeVersion';
+import { CommonConstants } from '../../../../utils/CommonConstants';
 import { ScenarioIds } from '../../../../utils/scenario-checker/scenario-ids';
 import { ScenarioService } from '../../../../utils/scenario-checker/scenario.service';
-import { style } from 'typestyle';
 
 const tabContainerStyle = style({
   marginTop: '15px',
 });
 
-const FunctionRuntimeSettingsPivot: React.FC<FormikProps<AppSettingsFormValues>> = props => {
+const FunctionRuntimeSettingsPivot: React.FC<AppSettingsFormProps> = props => {
   const { t } = useTranslation();
   const scenarioChecker = new ScenarioService(t);
   const site = props.initialValues.site;
 
   return (
     <div id="function-runtime-settings" className={tabContainerStyle}>
+      {site.properties.state && site.properties.state.toLowerCase() === CommonConstants.SiteStates.running && <RuntimeVersion {...props} />}
+
       {scenarioChecker.checkScenario(ScenarioIds.dailyUsageQuotaSupported, { site }).status === 'enabled' && <DailyUsageQuota {...props} />}
     </div>
   );
+};
+
+const runtimeVersionDirty = (values: AppSettingsFormValues, initialValues: AppSettingsFormValues) => {
+  const currentValue = findFormAppSettingValue(values.appSettings, CommonConstants.AppSettingNames.functionsExtensionVersion) || '';
+  const initialValue = findFormAppSettingValue(initialValues.appSettings, CommonConstants.AppSettingNames.functionsExtensionVersion) || '';
+  return !isEqual(currentValue, initialValue);
 };
 
 const dailyMemoryTimeQuotaDirty = (values: AppSettingsFormValues, initialValues: AppSettingsFormValues) => {
@@ -29,7 +39,7 @@ const dailyMemoryTimeQuotaDirty = (values: AppSettingsFormValues, initialValues:
 };
 
 export const functionRuntimeSettingsDirty = (values: AppSettingsFormValues, initialValues: AppSettingsFormValues) => {
-  return dailyMemoryTimeQuotaDirty(values, initialValues);
+  return runtimeVersionDirty(values, initialValues) || dailyMemoryTimeQuotaDirty(values, initialValues);
 };
 
 export default FunctionRuntimeSettingsPivot;
