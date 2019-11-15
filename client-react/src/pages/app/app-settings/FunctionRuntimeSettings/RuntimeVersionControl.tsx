865afe521517bd0cf5712a831bab80e2c5c5c3f3
Aniello Scotto Di Marco <andimarc@microsoft.com>
2019-11-15 10:56:37 -0800

    PR feedback - RuntimeVersionControl

--- client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeVersionControl.tsx
+++ client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeVersionControl.tsx
@@ -19,44 +19,81 @@ const RuntimeVersionControl: React.FC<AppSettingsFormProps & WithTranslation> = 
   const disableAllControls = !app_write || !editable || saving;
 
   const getControl = () => {
-    let versionFilter: RuntimeExtensionMajorVersions | null = null;
-    let disable = false;
+    // If the app has functions, we need to prevent the user from changing the runtime version to a different major version.
+    let versionRestriction: RuntimeExtensionMajorVersions | null = null;
+    let [hasFunctions, failedToGetFunctions] = [false, false];
 
-    if (asyncData.functionsCount.loadingState === 'loading') {
-      versionFilter = null;
-      disable = true;
-    } else if (asyncData.functionsCount.loadingState === 'complete' && asyncData.functionsCount.value === 0) {
-      versionFilter = null;
-      disable = false;
-    } else if (initialRuntimeMajorVersion !== RuntimeExtensionMajorVersions.custom) {
-      versionFilter = initialRuntimeMajorVersion;
-      disable = false;
-    } else if (asyncData.functionsHostStatus.loadingState === 'loading') {
-      versionFilter = null;
-      disable = true;
-    } else if (asyncData.functionsHostStatus.loadingState === 'complete') {
-      versionFilter = FunctionsRuntimeVersionHelper.parseExactRuntimeVersion(asyncData.functionsHostStatus.value!.properties.version);
-      disable = false;
-    } else {
-      versionFilter = null;
-      disable = true;
+    // If we're waiting on an API call to determine the function count or current running version, disable the control.
+    let waitingOnApi = false;
+
+    // We only care about preventing the user from switching major versions if they have a runtime version is configured.
+    // If no runtime version is configured, the runtime will not be started, so there is no current running major version.
+    if (!!initialRuntimeVersion) {
+      switch (asyncData.functionsCount.loadingState) {
+        case 'loading':
+          // The functions call hasn't completed, so we don't know the functions count. Keep the control disabled until the call completes or fails.
+          waitingOnApi = true;
+          break;
+        case 'complete':
+          // The functions call completed successfully. If the function count > 0 , prevent the user from changing major versions.
+          hasFunctions = !!asyncData.functionsCount.value;
+          break;
+        case 'failed':
+          // The functions call failed, so we don't know the functions count. To be safe, prevent the user from changing major versions.
+          failedToGetFunctions = true;
+          break;
+      }
+    }
+
+    const versionRestrictionNeeded = hasFunctions || failedToGetFunctions;
+    if (!waitingOnApi && versionRestrictionNeeded) {
+      // We need to restict the user to the major version that's currently running.
+
+      if (initialRuntimeMajorVersion === RuntimeExtensionMajorVersions.custom) {
+        // The configured version is a custom value, so we need to restrict the user to the current running version and 'custom'.
+        // We need to check the host status call to get the exact running version.
+
+        if (asyncData.functionsHostStatus.loadingState === 'loading') {
+          // The host status call hasn't completed yet, so we don't know the exact running version.
+          // Keep the control disabled until the call completes or fails.
+          waitingOnApi = true;
+        } else {
+          // The host status call either completed or failed.
+
+          if (asyncData.functionsHostStatus.loadingState === 'complete') {
+            // Try to get the current running major version from the result of the host status call.
+            versionRestriction = FunctionsRuntimeVersionHelper.parseExactRuntimeVersion(
+              asyncData.functionsHostStatus.value!.properties.version
+            );
+          }
+
+          if (!versionRestriction) {
+            // We weren't able to determine the major version because the host status call failed or returned a null/invalid value.
+            // Try to get the intnded major version based off of the FUNCTIONS_EXTENSION_VERSION value configured
+            versionRestriction = FunctionsRuntimeVersionHelper.parseConfiguredRuntimeVersion(initialRuntimeVersion);
+          }
+        }
+      } else {
+        // The configured version is not a custom value, so we can infer the current running version from this value.
+        versionRestriction = initialRuntimeMajorVersion;
+      }
     }
 
     const options: IChoiceGroupOption[] = [
       {
         key: RuntimeExtensionMajorVersions.v1,
         text: t('~1'),
-        disabled: !!versionFilter && versionFilter !== RuntimeExtensionMajorVersions.v1,
+        disabled: !!versionRestriction && versionRestriction !== RuntimeExtensionMajorVersions.v1,
       },
       {
         key: RuntimeExtensionMajorVersions.v2,
         text: t('~2'),
-        disabled: !!versionFilter && versionFilter !== RuntimeExtensionMajorVersions.v2,
+        disabled: !!versionRestriction && versionRestriction !== RuntimeExtensionMajorVersions.v2,
       },
       {
         key: RuntimeExtensionMajorVersions.v3,
         text: t('version3Preview'),
-        disabled: !!versionFilter && versionFilter !== RuntimeExtensionMajorVersions.v3,
+        disabled: !!versionRestriction && versionRestriction !== RuntimeExtensionMajorVersions.v3,
       },
     ];
 
@@ -68,7 +105,19 @@ const RuntimeVersionControl: React.FC<AppSettingsFormProps & WithTranslation> = 
       });
     }
 
-    return { disableControl: disable, versionOptions: options };
+    const warning = hasFunctions
+      ? t('functionsRuntimeVersionExistingFunctionsWarning')
+      : failedToGetFunctions
+      ? t('functionsRuntimeVersionExistingFunctionsWarning')
+      : '';
+
+    return {
+      // disable the control if we're waiting on an API call or if we need to restrict the major version but can't determine the running version
+      disableControl: waitingOnApi || (versionRestrictionNeeded && !versionRestriction),
+      versionOptions: options,
+      existingFunctionsWarning: !waitingOnApi && warning,
+      waiting: waitingOnApi,
+    };
   };
 
   const getLatestCustomRuntimeVersion = () => {
@@ -108,21 +157,17 @@ const RuntimeVersionControl: React.FC<AppSettingsFormProps & WithTranslation> = 
   );
   const initialRuntimeMajorVersion = FunctionsRuntimeVersionHelper.getFunctionsRuntimeMajorVersion(initialRuntimeVersion);
 
-  const hasFunctions =
-    asyncData.functionsCount.loadingState === 'failed' ||
-    (asyncData.functionsCount.loadingState === 'complete' && asyncData.functionsCount.value !== 0);
-
-  const { disableControl, versionOptions } = getControl();
+  const { disableControl, versionOptions, existingFunctionsWarning } = getControl();
 
   return (
     <>
-      {!!hasFunctions && (
+      {!!existingFunctionsWarning && (
         <MessageBar
           id="function-app-settings-runtime-version-message"
           isMultiline={true}
           className={messageBannerStyle(theme, MessageBarType.warning)}
           messageBarType={MessageBarType.warning}>
-          {t('functionsRuntimeVersionExistingFunctionsWarning')}
+          {existingFunctionsWarning}
         </MessageBar>
       )}
       <RadioButtonNoFormik
