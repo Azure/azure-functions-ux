b78a0dd6da1a8e4f861f582258cbbd14d23a2442
Allison Manifold <allisonm@microsoft.com>
2019-12-11 11:31:21 -0800

    [Ibizafication][Function Create] Update Styling, Add Select Template Message (#4367)
    * update function styling
    
    * some more styling updates
    
    * more updates, include details message
    
    * pr updates

--- client-react/src/pages/app/functions/create/FunctionCreate.styles.ts
+++ client-react/src/pages/app/functions/create/FunctionCreate.styles.ts
@@ -13,10 +13,10 @@ export const detailsPaddingStyle = {
 
 export const filterBoxStyle = {
   root: {
-    marginLeft: '410px',
-    marginTop: '-10px',
+    marginLeft: '0px',
+    marginTop: '10px',
     height: '30px',
-    width: '300px',
+    width: '250px',
   },
 };
 
@@ -24,18 +24,22 @@ export const getCardStyle = (theme: ThemeExtended) => {
   return style({
     border: `solid 1px ${theme.semanticColors.cardBorderColor}`,
     borderRadius: '2px',
-    width: '300px',
-    height: '180px',
-    margin: '35px',
+    width: '250px',
+    height: '150px',
+    marginTop: '10px',
+    marginRight: '12px',
+    marginBottom: '5px',
     float: 'left',
     position: 'relative',
 
     $nest: {
       '&.selected': {
         borderColor: `${theme.semanticColors.inputBorder}`,
+        boxShadow: '0px 1px 4px rgba(0, 0, 0, 0.13)',
       },
       '&:hover': {
         cursor: 'pointer',
+        boxShadow: '0px 3px 8px rgba(0, 0, 0, 0.14)',
       },
       '&:focus': {
         outlineColor: `${theme.semanticColors.focusBorder}`,
@@ -47,32 +51,29 @@ export const getCardStyle = (theme: ThemeExtended) => {
   });
 };
 
-export const getHeaderStyle = (functionTemplate: FunctionTemplate) => {
+export const getHeaderStyle = () => {
   return style({
-    display: 'inline-flex',
-    width: '100%',
-    height: '54px',
-    lineHeight: '37px',
-    fontSize: '14px',
-    fontWeight: '600',
+    width: '24%',
+    float: 'left',
 
     $nest: {
       img: {
-        backgroundColor: `${_getTemplateColor(functionTemplate)}`,
-        height: '39px',
-        width: '39px',
-        padding: '5px',
-        marginRight: '15px',
+        height: '26px',
+        width: '26px',
+        position: 'absolute',
+        left: '26px',
+        top: '15px',
       },
     },
-  } as any);
+  });
 };
 
 export const getSrc = (functionTemplate: FunctionTemplate) => {
   if (functionTemplate.metadata && functionTemplate.metadata.categoryStyle) {
+    // TODO(allisonm): Reintroduce icons for blob, queue, and webhook (waiting on designer)
     switch (functionTemplate.metadata.categoryStyle) {
-      case 'blob':
-        return require('images/CreateCardIcons/blob.svg');
+      // case 'blob':
+      //   return require('images/CreateCardIcons/blob.svg');
       case 'cosmosDB':
         return require('images/CreateCardIcons/cosmosDB.svg');
       case 'eventGrid':
@@ -83,44 +84,47 @@ export const getSrc = (functionTemplate: FunctionTemplate) => {
         return require('images/CreateCardIcons/http.svg');
       case 'iot':
         return require('images/CreateCardIcons/iot.svg');
-      case 'queue':
-        return require('images/CreateCardIcons/queue.svg');
+      // case 'queue':
+      //   return require('images/CreateCardIcons/queue.svg');
       case 'serviceBus':
         return require('images/CreateCardIcons/serviceBus.svg');
       case 'timer':
         return require('images/CreateCardIcons/timer.svg');
-      case 'webhook':
-        return require('images/CreateCardIcons/webhook.svg');
+      // case 'webhook':
+      //   return require('images/CreateCardIcons/webhook.svg');
     }
   }
   return require('images/CreateCardIcons/other.svg');
 };
 
-export const getDescriptionStyle = (theme: ThemeExtended) => {
+export const getDescriptionStyle = () => {
   return style({
-    borderTop: `solid 1px ${theme.semanticColors.cardBorderColor}`,
-    marginTop: '-5px',
-    padding: '15px',
+    marginLeft: '24%',
+    paddingRight: '15px',
+    paddingTop: '15px',
   });
 };
 
-const _getTemplateColor = (template: FunctionTemplate) => {
-  return (
-    (template.metadata && template.metadata.categoryStyle && createCardColors[template.metadata.categoryStyle]) || createCardColors['other']
-  );
+export const getTitleStyle = () => {
+  return style({
+    top: '17px',
+    marginBottom: '8px',
+    fontWeight: 'bold',
+  });
 };
 
-// allisonm: Discuss with Byron regarding themeing these colors and light vs dark mode
-const createCardColors = {
-  blob: '#1E5890',
-  cosmosDB: '#379DA6',
-  eventGrid: '#719516',
-  eventHub: '#719516',
-  http: '#731DDA',
-  iot: '#990000',
-  other: '#000000',
-  queue: '#1E5890',
-  serviceBus: '#F67600',
-  timer: '#3C86FF',
-  webhook: '#731DDA',
+export const getInfoStyle = () => {
+  return style({
+    position: 'relative',
+    fontSize: '12px',
+  });
+};
+
+export const getSelectStyle = () => {
+  return style({
+    position: 'absolute',
+    fontSize: '12px',
+    color: '#015CDA',
+    bottom: '12px',
+  });
 };
