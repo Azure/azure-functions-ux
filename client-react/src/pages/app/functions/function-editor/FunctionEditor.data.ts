6194dc50b49f40d4584457ddd47063f08d6884d4
Krrish Mittal <krmitta@microsoft.com>
2020-01-02 09:57:25 -0800

    renamed class

--- client-react/src/pages/app/functions/function-editor/FunctionEditor.data.ts
+++ client-react/src/pages/app/functions/function-editor/FunctionEditor.data.ts
@@ -1,7 +1,7 @@
 import FunctionsService from '../../../../ApiHelpers/FunctionsService';
 import { FunctionInfo } from '../../../../models/functions/function-info';
 import { ArmObj } from '../../../../models/arm-obj';
-import { KeyValuePair } from './FunctionEditor.types';
+import { NameValuePair } from './FunctionEditor.types';
 
 export default class FunctionEditorData {
   public getFunctionInfo(resourceId: string) {
@@ -15,22 +15,22 @@ export default class FunctionEditorData {
   public getProcessedFunctionTestData(data: any) {
     const response = {
       method: '',
-      queries: [] as KeyValuePair[],
-      headers: [] as KeyValuePair[],
+      queries: [] as NameValuePair[],
+      headers: [] as NameValuePair[],
       body: '',
     };
     if (!!data.method) {
       response.method = data.method;
     }
     if (!!data.queryStringParams) {
-      const queries: KeyValuePair[] = [];
+      const queries: NameValuePair[] = [];
       for (const parameter of data.queryParameters) {
         queries.push({ name: parameter.name, value: parameter.value });
       }
       response.queries = queries;
     }
     if (!!data.headers) {
-      const headers: KeyValuePair[] = [];
+      const headers: NameValuePair[] = [];
       for (const parameter of data.headers) {
         headers.push({ name: parameter.name, value: parameter.value });
       }
