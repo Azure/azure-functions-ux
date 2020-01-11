c8eed79da02b77d339ac4539547832f456d3ea71
Krrish Mittal <krmitta@microsoft.com>
2020-01-10 18:30:03 -0800

    app files command bar added

--- client-react/src/pages/app/app-files/AppFiles.tsx
+++ client-react/src/pages/app/app-files/AppFiles.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ArmObj } from '../../../models/arm-obj';
 import { Site } from '../../../models/site/site';
+import AppFilesCommandBar from './AppFilesCommandBar';
 
 interface AppFilesProps {
   site: ArmObj<Site> | undefined;
 }
 
 const AppFiles: React.FC<AppFilesProps> = props => {
-  return <></>;
+  const [dirty /**setDirty**/] = useState(false);
+
+  const save = () => {};
+
+  const reset = () => {};
+
+  return (
+    <div>
+      <AppFilesCommandBar dirty={dirty} disabled={false} saveFile={save} resetFile={reset} />
+    </div>
+  );
 };
 
 export default AppFiles;
