abcec4223548003407bd0cd37d40eaaba1d82b74
Krrish Mittal <krmitta@microsoft.com>
2020-01-10 16:20:09 -0800

    app files data loader init

---
+++ client-react/src/pages/app/app-files/AppFilesDataLoader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface AppFilesDataLoaderProps {
+  resourceId: string;
+}
+
+const AppFilesDataLoader: React.FC<AppFilesDataLoaderProps> = props => {
+  return <></>;
+};
+
+export default AppFilesDataLoader;
