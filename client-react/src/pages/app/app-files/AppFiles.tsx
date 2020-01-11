a6071bc4589051f5c8e227231503b8196cc5a635
Krrish Mittal <krmitta@microsoft.com>
2020-01-10 18:15:28 -0800

    app files component added and site details fetched

---
+++ client-react/src/pages/app/app-files/AppFiles.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { ArmObj } from '../../../models/arm-obj';
+import { Site } from '../../../models/site/site';
+
+interface AppFilesProps {
+  site: ArmObj<Site> | undefined;
+}
+
+const AppFiles: React.FC<AppFilesProps> = props => {
+  return <></>;
+};
+
+export default AppFiles;
--- client-react/src/pages/app/app-files/AppFilesDataLoader.tsx
+++ client-react/src/pages/app/app-files/AppFilesDataLoader.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppFilesData from './AppFiles.data';
+import AppFiles from './AppFiles';
+import { ArmObj } from '../../../models/arm-obj';
+import { Site } from '../../../models/site/site';
+import SiteService from '../../../ApiHelpers/SiteService';
+import LoadingComponent from '../../../components/loading/loading-component';
 
 interface AppFilesDataLoaderProps {
   resourceId: string;
 }
 
+const appFilesData = new AppFilesData();
+export const AppFilesContext = React.createContext(appFilesData);
+
 const AppFilesDataLoader: React.FC<AppFilesDataLoaderProps> = props => {
-  return <></>;
+  const { resourceId } = props;
+  const [initialLoading, setInitialLoading] = useState(true);
+  const [site, setSite] = useState<ArmObj<Site> | undefined>(undefined);
+
+  const fetchData = async () => {
+    const siteResponse = await SiteService.fetchSite(resourceId);
+    if (siteResponse.metadata.success) {
+      setSite(siteResponse.data);
+    }
+    setInitialLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  if (initialLoading) {
+    return <LoadingComponent />;
+  }
+  return (
+    <AppFilesContext.Provider value={appFilesData}>
+      <AppFiles site={site} />
+    </AppFilesContext.Provider>
+  );
 };
 
 export default AppFilesDataLoader;
