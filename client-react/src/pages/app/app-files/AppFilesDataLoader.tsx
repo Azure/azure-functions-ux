ebe69679e8f4d3d4f84c91dee00208b487b58fb7
Krrish Mittal <krmitta@microsoft.com>
2020-01-10 19:28:33 -0800

    fill dropdown with the files

--- client-react/src/pages/app/app-files/AppFilesDataLoader.tsx
+++ client-react/src/pages/app/app-files/AppFilesDataLoader.tsx
@@ -5,6 +5,9 @@ import { ArmObj } from '../../../models/arm-obj';
 import { Site } from '../../../models/site/site';
 import SiteService from '../../../ApiHelpers/SiteService';
 import LoadingComponent from '../../../components/loading/loading-component';
+import { CommonConstants } from '../../../utils/CommonConstants';
+import FunctionsService from '../../../ApiHelpers/FunctionsService';
+import { VfsObject } from '../../../models/functions/vfs';
 
 interface AppFilesDataLoaderProps {
   resourceId: string;
@@ -17,11 +20,23 @@ const AppFilesDataLoader: React.FC<AppFilesDataLoaderProps> = props => {
   const { resourceId } = props;
   const [initialLoading, setInitialLoading] = useState(true);
   const [site, setSite] = useState<ArmObj<Site> | undefined>(undefined);
+  const [runtimeVersion, setRuntimeVersion] = useState<string | undefined>(undefined);
+  const [fileList, setFileList] = useState<VfsObject[] | undefined>(undefined);
 
   const fetchData = async () => {
-    const siteResponse = await SiteService.fetchSite(resourceId);
+    const [siteResponse, appSettingsResponse] = await Promise.all([
+      SiteService.fetchSite(resourceId),
+      SiteService.fetchApplicationSettings(resourceId),
+    ]);
     if (siteResponse.metadata.success) {
       setSite(siteResponse.data);
+    }
+    if (appSettingsResponse.metadata.success) {
+      const currentRuntimeVersion = appSettingsResponse.data.properties[CommonConstants.AppSettingNames.functionsExtensionVersion];
+      setRuntimeVersion(currentRuntimeVersion);
+      const fileListResponse = await FunctionsService.getFileContent(resourceId, undefined, currentRuntimeVersion);
+      console.log(runtimeVersion);
+      setFileList(fileListResponse.data as VfsObject[]);
     }
     setInitialLoading(false);
   };
@@ -31,12 +46,12 @@ const AppFilesDataLoader: React.FC<AppFilesDataLoaderProps> = props => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  if (initialLoading) {
+  if (initialLoading || !site) {
     return <LoadingComponent />;
   }
   return (
     <AppFilesContext.Provider value={appFilesData}>
-      <AppFiles site={site} />
+      <AppFiles site={site} fileList={fileList} />
     </AppFilesContext.Provider>
   );
 };
