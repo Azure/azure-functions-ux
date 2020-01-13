1c001b382af6918a89b7dd32fb1ca078a6ac2586
Krrish Mittal <krrishmittal15@gmail.com>
2020-01-13 14:55:49 -0800

    [Function App Editor] App-files file selector added (#4441)
    * file selector added
    
    * [Function App Files] App files monaco-editor added (#4442)
    
    * app files monaco editor added
    
    * [Function App Editor] Fill dropdown with the files (#4443)
    
    * fill dropdown with the files
    
    * run time version removed

---
+++ client-react/src/pages/app/app-files/AppFiles.styles.ts
@@ -0,0 +1,12 @@
+import { style } from 'typestyle';
+
+export const commandBarSticky = style({
+  position: 'sticky',
+  top: 0,
+  zIndex: 1,
+});
+
+export const editorStyle = style({
+  marginTop: '10px',
+  marginRight: '10px',
+});
