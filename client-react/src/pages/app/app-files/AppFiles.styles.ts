264ab06f7ba53f613623004ca311ea00be22fef2
Krrish Mittal <krrishmittal15@gmail.com>
2020-01-13 14:54:43 -0800

    [Function App Files] App files monaco-editor added (#4442)
    * app files monaco editor added
    
    * [Function App Editor] Fill dropdown with the files (#4443)
    
    * fill dropdown with the files
    
    * run time version removed

--- client-react/src/pages/app/app-files/AppFiles.styles.ts
+++ client-react/src/pages/app/app-files/AppFiles.styles.ts
@@ -5,3 +5,8 @@ export const commandBarSticky = style({
   top: 0,
   zIndex: 1,
 });
+
+export const editorStyle = style({
+  marginTop: '10px',
+  marginRight: '10px',
+});
--- client-react/src/pages/app/app-files/AppFiles.tsx
+++ client-react/src/pages/app/app-files/AppFiles.tsx
@@ -1,19 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ArmObj } from '../../../models/arm-obj';
 import { Site } from '../../../models/site/site';
 import AppFilesCommandBar from './AppFilesCommandBar';
-import { commandBarSticky } from './AppFiles.styles';
+import { commandBarSticky, editorStyle } from './AppFiles.styles';
 import FunctionEditorFileSelectorBar from '../functions/function-editor/FunctionEditorFileSelectorBar';
 import { IDropdownOption } from 'office-ui-fabric-react';
+import MonacoEditor from '../../../components/monaco-editor/monaco-editor';
+import { VfsObject } from '../../../models/functions/vfs';
+import LoadingComponent from '../../../components/loading/loading-component';
 
 interface AppFilesProps {
-  site: ArmObj<Site> | undefined;
+  site: ArmObj<Site>;
+  fileList?: VfsObject[];
 }
 
 const AppFiles: React.FC<AppFilesProps> = props => {
-  const { site } = props;
+  const { site, fileList } = props;
 
   const [dirty /**setDirty**/] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(true);
+  const [selectedFile, setSelectedFile] = useState<IDropdownOption | undefined>(undefined);
 
   const save = () => {};
 
@@ -21,16 +27,61 @@ const AppFiles: React.FC<AppFilesProps> = props => {
 
   const onFileSelectorChange = async (e: unknown, option: IDropdownOption) => {};
 
+  const onChange = (newValue, event) => {};
+
+  const getDropdownOptions = (): IDropdownOption[] => {
+    return !!fileList
+      ? fileList
+          .map(file => ({
+            key: file.name,
+            text: file.name,
+            isSelected: false,
+            data: file,
+          }))
+          .filter(file => file.data.mime !== 'inode/directory')
+          .sort((a, b) => a.key.localeCompare(b.key))
+      : [];
+  };
+
+  const fetchData = async () => {
+    const options = getDropdownOptions();
+    const file = options.length > 0 ? options[0] : undefined;
+    if (!!file) {
+      setSelectedFile(file);
+    }
+    setInitialLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className={commandBarSticky}>
       <AppFilesCommandBar dirty={dirty} disabled={false} saveFile={save} resetFile={reset} />
       <FunctionEditorFileSelectorBar
-        functionAppNameLabel={!!site ? site.name : undefined}
-        fileDropdownOptions={[]}
-        fileDropdownSelectedKey={''}
+        functionAppNameLabel={site.name}
+        fileDropdownOptions={getDropdownOptions()}
+        fileDropdownSelectedKey={!!selectedFile ? (selectedFile.key as string) : ''}
         disabled={false}
         onChangeDropdown={onFileSelectorChange}
       />
+      {initialLoading && <LoadingComponent />}
+      <div className={editorStyle}>
+        <MonacoEditor
+          value={''}
+          language={'json'}
+          onChange={onChange}
+          disabled={initialLoading}
+          options={{
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+            cursorBlinking: true,
+            renderWhitespace: 'all',
+          }}
+        />
+      </div>
     </div>
   );
 };
