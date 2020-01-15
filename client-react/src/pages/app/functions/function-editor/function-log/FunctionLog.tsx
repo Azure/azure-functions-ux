e7a7a79a6b5baea41c301459fc0558ebbfe91311
Krrish Mittal <krmitta@microsoft.com>
2020-01-15 13:03:14 -0800

    resources text used

--- client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.tsx
+++ client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { ReactComponent as DownChevron } from './../../../../../images/Common/down-chevron.svg';
 import { chevronIconStyle, logCommandBarStyle, logExpandButtonStyle } from './FunctionLog.styles';
+import { useTranslation } from 'react-i18next';
 interface FunctionLogProps {}
 
 const FunctionLog: React.FC<FunctionLogProps> = props => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className={logCommandBarStyle}>
         <span className={logExpandButtonStyle}>
           <DownChevron className={chevronIconStyle()} />
-          Logs
+          {t('logStreaming_logs')}
         </span>
       </div>
     </div>
