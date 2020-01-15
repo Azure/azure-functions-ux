3d555e15678c8ee8a4618e3160bedeb45eba9313
Krrish Mittal <krmitta@microsoft.com>
2020-01-15 14:55:47 -0800

    command bar buttons added

--- client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.styles.ts
+++ client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.styles.ts
@@ -13,6 +13,7 @@ export const logCommandBarStyle = style({
   display: 'flex',
   alignItems: 'center',
   marginLeft: '10px',
+  justifyContent: 'space-between',
 });
 
 export const logExpandButtonStyle = style({
@@ -23,4 +24,31 @@ export const logStreamStyle = style({
   height: '171px',
   backgroundColor: '#000000',
   overflow: 'auto',
+});
+
+export const logCommandBarButton = style({
+  marginTop: '5px',
+  paddingRight: '5px',
+});
+
+export const logCommandBarButtonListStyle = style({
+  float: 'right',
+});
+
+export const logCommandBarButtonLabelStyle = style({
+  marginRight: '16px',
+  cursor: 'pointer',
+});
+
+export const logCommandBarButtonStyle = style({
+  color: '#0078D4',
+  paddingRight: '5px',
+});
+
+export const logCommandBarSeparatorStyle = style({
+  marginLeft: '7px',
+  marginRight: '23px',
+  width: '1px',
+  border: '1px solid rgba(128, 128, 128, 0.7)',
+  height: '16px',
 });
--- client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.tsx
+++ client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as DownChevron } from './../../../../../images/Common/down-chevron.svg';
-import { chevronIconStyle, logCommandBarStyle, logExpandButtonStyle, logStreamStyle } from './FunctionLog.styles';
+import {
+  chevronIconStyle,
+  logCommandBarStyle,
+  logExpandButtonStyle,
+  logStreamStyle,
+  logCommandBarButtonListStyle,
+  logCommandBarButtonLabelStyle,
+  logCommandBarButtonStyle,
+  logCommandBarSeparatorStyle,
+} from './FunctionLog.styles';
 import { useTranslation } from 'react-i18next';
+import { Icon } from 'office-ui-fabric-react';
 interface FunctionLogProps {
   toggleExpand: () => void;
   isExpanded: boolean;
@@ -10,9 +20,24 @@ interface FunctionLogProps {
 const FunctionLog: React.FC<FunctionLogProps> = props => {
   const { t } = useTranslation();
   const { toggleExpand, isExpanded } = props;
+  const [connected, setConnected] = useState(true);
+  const [maximized, setMaximized] = useState(false);
+  const [started, setStarted] = useState(false);
 
   const onExpandClick = () => {
     toggleExpand();
+  };
+
+  const toggleConnection = () => {
+    setConnected(!connected);
+  };
+
+  const toggleStart = () => {
+    setStarted(!started);
+  };
+
+  const toggleMaximize = () => {
+    setMaximized(!maximized);
   };
 
   return (
@@ -22,6 +47,58 @@ const FunctionLog: React.FC<FunctionLogProps> = props => {
           <DownChevron className={chevronIconStyle(isExpanded)} />
           {t('logStreaming_logs')}
         </span>
+        {isExpanded && (
+          <span className={logCommandBarButtonListStyle}>
+            <span onClick={toggleConnection} className={logCommandBarButtonLabelStyle}>
+              {connected ? (
+                <>
+                  <Icon iconName="PlugDisconnected" className={logCommandBarButtonStyle} />
+                  {t('disconnect')}
+                </>
+              ) : (
+                <>
+                  <Icon iconName="PlugConnected" className={logCommandBarButtonStyle} />
+                  {t('connect')}
+                </>
+              )}
+            </span>
+            <span onClick={toggleStart} className={logCommandBarButtonLabelStyle}>
+              {started ? (
+                <>
+                  <Icon iconName="Stop" className={logCommandBarButtonStyle} />
+                  {t('stop')}
+                </>
+              ) : (
+                <>
+                  <Icon iconName="TriangleRight12" className={logCommandBarButtonStyle} />
+                  {t('start')}
+                </>
+              )}
+            </span>
+            <span className={logCommandBarSeparatorStyle} />
+            <span className={logCommandBarButtonLabelStyle}>
+              <Icon iconName="Copy" className={logCommandBarButtonStyle} />
+              {t('functionKeys_copy')}
+            </span>
+            <span className={logCommandBarButtonLabelStyle}>
+              <Icon iconName="CalculatorMultiply" className={logCommandBarButtonStyle} />
+              {t('logStreaming_clear')}
+            </span>
+            <span onClick={toggleMaximize} className={logCommandBarButtonLabelStyle}>
+              {maximized ? (
+                <>
+                  <Icon iconName="BackToWindow" className={logCommandBarButtonStyle} />
+                  {t('minimize')}
+                </>
+              ) : (
+                <>
+                  <Icon iconName="FullScreen" className={logCommandBarButtonStyle} />
+                  {t('maximize')}
+                </>
+              )}
+            </span>
+          </span>
+        )}
       </div>
       {isExpanded && <div className={logStreamStyle} />}
     </div>
