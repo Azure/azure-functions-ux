d1d72fc08cbd3cd622f3c29e22912d6f8dec45db
Mitren Chinoy <nertim@users.noreply.github.com>
2020-01-17 10:52:39 -0800

    Introduce the stacks api feature in functions server. (#4459)
    * initial thoughts on stacks api
    
    * changes so far
    
    * introduce stacks controller and copy over existing data from available stacks API.
    
    * some more changes.
    
    * address PR comments
    
    * move the api version and os check to validate methods.

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,10 @@ import { FunctionsModule } from './functions/functions.module';
 import { ProxyModule } from './proxy/proxy.module';
 import { StorageModule } from './storage/storage.module';
 import { DeploymentCenterModule } from './deployment-center/deployment-center.module';
+import { StacksModule } from './stacks/stacks.module';
 
 @Module({
   // HomeModule should always be last in this list because it includes the catch all route
-  imports: [SharedModule, ApiModule, FunctionsModule, ProxyModule, StorageModule, DeploymentCenterModule, HomeModule],
+  imports: [SharedModule, ApiModule, FunctionsModule, ProxyModule, StorageModule, DeploymentCenterModule, HomeModule, StacksModule],
 })
 export class AppModule {}
