2a7061a2699d876b74eedf3ddce7d3da0c17bccc
Krrish Mittal <krrishmittal15@gmail.com>
2020-01-22 15:59:23 -0800

    styles updated for function test (#4475)

--- client-react/src/pages/app/functions/function-editor/function-test/FunctionTestOutput.tsx
+++ client-react/src/pages/app/functions/function-editor/function-test/FunctionTestOutput.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { pivotItemWrapper, functionTestGroupStyle, responseCodeStyle, responseContentStyle } from './FunctionTest.styles';
+import {
+  pivotItemWrapper,
+  functionTestGroupStyle,
+  responseCodeStyle,
+  responseContentStyle,
+  testFormLabelStyle,
+} from './FunctionTest.styles';
 import { Label } from 'office-ui-fabric-react';
 import { ResponseContent } from '../FunctionEditor.types';
 import { HttpConstants } from '../../../../../utils/constants/HttpConstants';
@@ -17,18 +23,14 @@ const FunctionTestOutput: React.SFC<FunctionTestOutputProps> = props => {
   return (
     <div className={pivotItemWrapper}>
       <div className={functionTestGroupStyle}>
-        <Label>{t('httpRun_responseCode')}</Label>
+        <Label className={testFormLabelStyle}>{t('httpRun_responseCode')}</Label>
         <div className={responseCodeStyle}>
           {!!responseContent ? `${responseContent.code} ${HttpConstants.statusCodeToText(responseContent.code)}` : ''}
         </div>
       </div>
       <div className={functionTestGroupStyle}>
-        <Label>{t('httpRun_responseContent')}</Label>
+        <Label className={testFormLabelStyle}>{t('httpRun_responseContent')}</Label>
         <div className={responseContentStyle}>{!!responseContent && !!responseContent.text ? responseContent.text : ''}</div>
-      </div>
-      <div className={functionTestGroupStyle}>
-        <Label>{t('functionMonitor_invocationLog')}</Label>
-        {/* TODO (krmitta): Add Invocation Log for the Output */}
       </div>
     </div>
   );
