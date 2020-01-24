13b6f1d27b9822b646411b702f267e6e02e501f2
Krrish Mittal <krrishmittal15@gmail.com>
2020-01-23 17:38:32 -0800

    [Function Editor] Save input body on Run (#4479)
    * fix body not being saved
    
    * req body renamed

--- client-react/src/pages/app/functions/function-editor/function-test/FunctionTestInput.tsx
+++ client-react/src/pages/app/functions/function-editor/function-test/FunctionTestInput.tsx
@@ -30,6 +30,7 @@ import { BindingType } from '../../../../../models/functions/function-binding';
 export interface FunctionTestInputProps {
   functionInfo: ArmObj<FunctionInfo>;
   body: string;
+  onRequestBodyChange: (newValue, event) => void;
 }
 
 interface KeyValueComponent {
@@ -106,7 +107,7 @@ const KeyValueFieldArrayComponent: React.FC<KeyValueComponent> = props => {
 const FunctionTestInput: React.SFC<FormikProps<InputFormValues> & FunctionTestInputProps> = props => {
   const { t } = useTranslation();
 
-  const { values, functionInfo, body } = props;
+  const { values, functionInfo, body, onRequestBodyChange } = props;
 
   const getDropdownOptions = (): IDropdownOption[] => {
     const httpTrigger = functionInfo.properties.config.bindings.find(b => {
@@ -148,6 +149,7 @@ const FunctionTestInput: React.SFC<FormikProps<InputFormValues> & FunctionTestIn
           <MonacoEditor
             language="json"
             value={body}
+            onChange={onRequestBodyChange}
             height="300px"
             options={{
               minimap: { enabled: false },
