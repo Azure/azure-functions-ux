02a1d09afe3c4339b31623eaf29ced2b6a2561f1
Aniello Scotto Di Marco <andimarc@microsoft.com>
2020-01-24 16:45:24 -0800

    Don't include the 'ipSecurityRestrictions' or 'scmIpSecurityRestrictions' properties in the config/web payload when saving container config

--- client/src/app/site/container-settings/container-settings-manager.ts
+++ client/src/app/site/container-settings/container-settings-manager.ts
@@ -380,6 +380,13 @@ export class ContainerSettingsManager {
         siteConfig.properties.linuxFxVersion = null;
         siteConfig.properties.windowsFxVersion = null;
 
+        // The user may have VNET security restrictions enabled. If so, then including "ipSecurityRestrictions" or "scmIpSecurityRestrictions" in the payload for
+        // the config/web API means that the call will require joinViaServiceEndpoint/action permissions on the given subnet(s) referenced in the security restrictions.
+        // If the user doesn't have these permissions, the config/web API call will fail. (This is true even if these properties are just being round-tripped.)
+        // Since this UI doesn't allow modifying these properties, we can just remove them from the config object to avoid the unnecessary permissions requirement.
+        delete siteConfig.properties.ipSecurityRestrictions;
+        delete siteConfig.properties.scmIpSecurityRestrictions;
+
         if (os === 'linux') {
           siteConfig.properties.linuxFxVersion = formData.siteConfig.fxVersion;
         } else {
