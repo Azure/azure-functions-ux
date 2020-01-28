f66c658fa859d2ece4d76b65e08e9b08bfb7da9a
Krrish Mittal <krmitta@microsoft.com>
2020-01-27 17:14:37 -0800

    lowercased

--- client-react/src/pages/app/functions/function-editor/FunctionEditor.tsx
+++ client-react/src/pages/app/functions/function-editor/FunctionEditor.tsx
@@ -181,7 +181,7 @@ export const FunctionEditor: React.SFC<FunctionEditorProps> = props => {
   const fetchData = async () => {
     const options = getDropdownOptions();
     let file = options.length > 0 ? options[0] : undefined;
-    const scriptFilename = getScriptFile();
+    const scriptFilename = getScriptFile().toLowerCase();
     if (!!scriptFilename) {
       const scriptFileOption = options.filter(option => option.text === scriptFilename);
       if (scriptFileOption.length === 1) {
