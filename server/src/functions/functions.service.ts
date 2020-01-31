1765480beb0c273ea5301e892ea493e69b7b6370
Aniello Scotto Di Marco <andimarc@microsoft.com>
2020-01-31 09:56:58 -0800

    Revert "Quickstart language support added (Re-added :P) (#4517)"
    This reverts commit 463a3fde2360fd169bc85e32fc40dca5c031127d.

--- server/src/functions/functions.service.ts
+++ server/src/functions/functions.service.ts
@@ -1,8 +1,6 @@
 import { Injectable, HttpException, OnModuleInit } from '@nestjs/common';
 import { join, normalize } from 'path';
 import { readdir, exists, readFile } from 'async-file';
-import * as fs from 'fs';
-import { Constants } from 'src/constants';
 
 @Injectable()
 export class FunctionsService implements OnModuleInit {
@@ -24,31 +22,12 @@ export class FunctionsService implements OnModuleInit {
     return this.bindingsMap[runtimeVersion] || this.bindingsMap.default;
   }
 
-  async getQuickStart(fileName: string, language: string) {
-    const langLower = language.toLowerCase();
-    let langCode = 'en';
-    if (langLower !== 'en') {
-      if (!!Constants.quickstartLanguageMap[langLower]) {
-        langCode = Constants.quickstartLanguageMap[langLower].toLowerCase();
-      } else {
-        langCode = langLower;
-      }
+  async getQuickStart(fileName: string) {
+    const fileNameLower = fileName.toLowerCase();
+    if (!this.quickStartMap[fileNameLower]) {
+      throw new HttpException(`${fileName} does not exist`, 404);
     }
-    let fileNameLower = `${fileName.toLowerCase()}${langCode !== 'en' ? `_${langCode}` : ''}`;
-
-    if (!!this.quickStartMap[fileNameLower]) {
-      return this.quickStartMap[fileNameLower];
-    }
-    /**
-     * Check for the 'en' quickstart file if the specified langauge file is not available
-     */
-    if (langCode !== 'en') {
-      fileNameLower = fileName.toLowerCase();
-      if (!!this.quickStartMap[fileNameLower]) {
-        return this.quickStartMap[fileNameLower];
-      }
-    }
-    throw new HttpException(`${fileName} does not exist`, 404);
+    return this.quickStartMap[fileNameLower];
   }
 
   private async loadTemplateFiles() {
@@ -85,22 +64,11 @@ export class FunctionsService implements OnModuleInit {
     if (!(await exists(quickStartDir))) {
       return;
     }
-    this.readQuickstartDirectory(quickStartDir);
-  }
-
-  private async readQuickstartDirectory(dirPath: string) {
-    const dirFiles = await readdir(dirPath);
+    const dirFiles = await readdir(quickStartDir);
     const loading = dirFiles.map(async file => {
-      const filePath = join(dirPath, file);
-      if (fs.existsSync(filePath)) {
-        if (fs.lstatSync(filePath).isDirectory()) {
-          this.readQuickstartDirectory(filePath);
-        } else {
-          const contents = await readFile(filePath, { encoding: 'utf8' });
-          const fileName = file.replace('.md', '');
-          this.quickStartMap[fileName.toLowerCase()] = contents;
-        }
-      }
+      const contents = await readFile(join(quickStartDir, file), { encoding: 'utf8' });
+      const fileName = file.replace('.md', '');
+      this.quickStartMap[fileName.toLowerCase()] = contents;
     });
     await loading;
   }
