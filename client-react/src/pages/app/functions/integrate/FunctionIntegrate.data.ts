f5a2c6d9b19fe677d8634a587f4302e87772657e
Reid Fortier <refortie@microsoft.com>
2020-01-30 16:54:30 -0800

    Further cleanup

--- client-react/src/pages/app/functions/integrate/FunctionIntegrate.data.ts
+++ client-react/src/pages/app/functions/integrate/FunctionIntegrate.data.ts
@@ -3,7 +3,6 @@ import { HttpResponseObject } from '../../../../ArmHelper.types';
 import { ArmObj } from '../../../../models/arm-obj';
 import { Binding } from '../../../../models/functions/binding';
 import { FunctionInfo } from '../../../../models/functions/function-info';
-import SiteService from '../../../../ApiHelpers/SiteService';
 
 export default class FunctionIntegrateData {
   public getBindings(functionAppResourceId: string): Promise<HttpResponseObject<ArmObj<Binding[]>>> {
@@ -12,10 +11,6 @@ export default class FunctionIntegrateData {
 
   public getBinding(functionAppResourceId: string, bindingId: string): Promise<HttpResponseObject<ArmObj<Binding>>> {
     return FunctionsService.getBinding(functionAppResourceId, bindingId);
-  }
-
-  public getFunctionAppApplicationSettings(functionAppResourceId: string): Promise<HttpResponseObject<ArmObj<{ [key: string]: string }>>> {
-    return SiteService.fetchApplicationSettings(functionAppResourceId);
   }
 
   public getFunction(functionResourceId: string): Promise<HttpResponseObject<ArmObj<FunctionInfo>>> {
