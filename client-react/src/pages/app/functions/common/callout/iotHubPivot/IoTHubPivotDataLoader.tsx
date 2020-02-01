fc3b941f6f93bd62ea96dc5530324fadf1027329
Allison Manifold <allisonm@microsoft.com>
2020-01-31 16:12:11 -0800

    [Ibizafication] [Function Create] Dropdowns Should not use side panels (#4524)
    * props going through to service bus
    
    * update dropdowns
    
    * add comment on drop down

--- client-react/src/pages/app/functions/common/callout/iotHubPivot/IoTHubPivotDataLoader.tsx
+++ client-react/src/pages/app/functions/common/callout/iotHubPivot/IoTHubPivotDataLoader.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { NewConnectionCalloutProps } from '../Callout.properties';
 import IotHubPivot from './IoTHubPivot';
 import IotHubPivotData from './IoTHubPivot.data';
+import { CustomDropdownProps } from '../../../../../../components/form-controls/DropDown';
+import { FieldProps } from 'formik';
+import { IDropdownProps } from 'office-ui-fabric-react';
 
 const iotHubPivotData = new IotHubPivotData();
 export const IoTHubPivotContext = React.createContext(iotHubPivotData);
 
-const IotHubPivotDataLoader: React.SFC<NewConnectionCalloutProps> = props => {
+const IotHubPivotDataLoader: React.SFC<NewConnectionCalloutProps & CustomDropdownProps & FieldProps & IDropdownProps> = props => {
   return (
     <IoTHubPivotContext.Provider value={iotHubPivotData}>
       <IotHubPivot {...props} />
