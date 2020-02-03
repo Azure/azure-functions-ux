faef79ac3b9a4e16fbb9a3b2c0ffbd70aa596f3c
Reid Fortier <refortie@microsoft.com>
2020-02-03 14:36:57 -0800

    Updated the Fabric version and added in a cron error message override

--- client-react/src/components/form-controls/TextField.tsx
+++ client-react/src/components/form-controls/TextField.tsx
@@ -5,6 +5,7 @@ import { FieldProps } from 'formik';
 import { Subject } from 'rxjs';
 import get from 'lodash-es/get';
 import TextFieldNoFormik from './TextFieldNoFormik';
+
 interface EventMsg {
   e: any;
   value: string;
@@ -18,9 +19,11 @@ interface CustomTextFieldProps {
   learnMoreLink?: string;
   dirty?: boolean;
 }
+
 class TextField extends React.Component<FieldProps & ITextFieldProps & CustomTextFieldProps, any> {
   private inputDebouncer = new Subject<EventMsg>();
   private readonly DEBOUNCE_TIME = 300;
+
   public componentWillMount() {
     const { field, form } = this.props;
     this.inputDebouncer.pipe(debounceTime(this.DEBOUNCE_TIME)).subscribe(({ e, value }) => {
@@ -28,13 +31,19 @@ class TextField extends React.Component<FieldProps & ITextFieldProps & CustomTex
       field.onChange(e);
     });
   }
+
   public componentWillUnmount() {
     this.inputDebouncer.unsubscribe();
   }
+
   public render() {
     const { field, form, ...rest } = this.props;
     const errorMessage = get(form.errors, field.name, '') as string;
-    return <TextFieldNoFormik value={field.value} onBlur={field.onBlur} errorMessage={errorMessage} onChange={this.onChange} {...rest} />;
+    const finalErrorMessage = this.cronErrorMessage(errorMessage);
+
+    return (
+      <TextFieldNoFormik value={field.value} onBlur={field.onBlur} errorMessage={finalErrorMessage} onChange={this.onChange} {...rest} />
+    );
   }
 
   private onChange = (e: any, value: string) => {
@@ -43,5 +52,23 @@ class TextField extends React.Component<FieldProps & ITextFieldProps & CustomTex
       value,
     });
   };
+
+  // TODO (refortie): Temporary hard-coding of the documentation link.
+  // Remove this once we get the API update so that errors have learn more
+  private cronErrorMessage(errorMessage: string): string | JSX.Element {
+    if (errorMessage.includes('Invalid Cron Expression')) {
+      return (
+        <>
+          Invalid Cron Expression. Please consult the{' '}
+          <a target="_blank" href="https://azure.microsoft.com/en-us/documentation/articles/functions-bindings-timer/">
+            documentation
+          </a>{' '}
+          to learn more.
+        </>
+      );
+    }
+    return errorMessage;
+  }
 }
+
 export default TextField;
