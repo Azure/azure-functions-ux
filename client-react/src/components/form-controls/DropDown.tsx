8808356ee46aa1fd8b2ecf86e5402a0e372fec98
Reid Fortier <refortie@microsoft.com>
2020-02-12 13:32:55 -0800

    Fill out the default values when creating a binding

--- client-react/src/components/form-controls/DropDown.tsx
+++ client-react/src/components/form-controls/DropDown.tsx
@@ -20,14 +20,16 @@ const Dropdown = (props: FieldProps & IDropdownProps & CustomDropdownProps) => {
   const onChange = (e: unknown, option: IDropdownOption) => {
     form.setFieldValue(field.name, option.key);
   };
+
   const errorMessage = get(form.errors, field.name, '') as string;
+
   return (
     <DropdownNoFormik
       value={field.value === undefined ? 'null' : field.value}
       onBlur={field.onBlur}
       errorMessage={errorMessage}
       onChange={onChange}
-      // Overridding default dropdown to panel transfer due to many of our dropsdown existing in panels
+      // Overriding default dropdown to panel transfer due to many of our dropdown existing in panels
       // https://github.com/OfficeDev/office-ui-fabric-react/commit/1aa8ab4e9e16ecc17d8e90c1374c0958eba77ee3#diff-406409baf14f369160f322b075e148d4
       responsiveMode={ResponsiveMode.large}
       {...props}
