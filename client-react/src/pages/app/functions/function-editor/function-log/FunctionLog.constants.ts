ae65a6b1c79ce007782419656fb11e6b2e552d6e
Allison Manifold <allisonm@microsoft.com>
2020-02-19 12:44:45 -0800

    [Ibizafication][Function Logs] Update function log filters and fix scroll race condition (#4609)

--- client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.constants.ts
+++ client-react/src/pages/app/functions/function-editor/function-log/FunctionLog.constants.ts
@@ -1,31 +1,41 @@
 import { QPSchemaDocumentStreamInfo, TelemetryTypesEnum } from '../../../../../QuickPulseQuery/QuickPulseSchema';
 
-export const defaultDocumentStreams: QPSchemaDocumentStreamInfo[] = [
-  {
-    Id: 'all-types-default',
-    DocumentFilterGroups: [
-      {
-        TelemetryType: TelemetryTypesEnum.Request,
-        Filters: { Filters: [] },
-      },
-      {
-        TelemetryType: TelemetryTypesEnum.Dependency,
-        Filters: { Filters: [] },
-      },
-      {
-        TelemetryType: TelemetryTypesEnum.Exception,
-        Filters: { Filters: [] },
-      },
-      {
-        TelemetryType: TelemetryTypesEnum.Event,
-        Filters: { Filters: [] },
-      },
-      {
-        TelemetryType: TelemetryTypesEnum.Trace,
-        Filters: { Filters: [] },
-      },
-    ],
-  },
-];
+export function getDefaultDocumentStreams(functionName: string): QPSchemaDocumentStreamInfo[] {
+  return [
+    {
+      Id: 'all-types-default',
+      DocumentFilterGroups: [
+        {
+          TelemetryType: TelemetryTypesEnum.Request,
+          Filters: { Filters: [] },
+        },
+        {
+          TelemetryType: TelemetryTypesEnum.Dependency,
+          Filters: { Filters: [] },
+        },
+        {
+          TelemetryType: TelemetryTypesEnum.Exception,
+          Filters: { Filters: [] },
+        },
+        {
+          TelemetryType: TelemetryTypesEnum.Event,
+          Filters: { Filters: [] },
+        },
+        {
+          TelemetryType: TelemetryTypesEnum.Trace,
+          Filters: {
+            Filters: [
+              {
+                FieldName: 'CustomDimensions.Category',
+                Predicate: 'Equal',
+                Comparand: `Function.${functionName}`,
+              },
+            ],
+          },
+        },
+      ],
+    },
+  ];
+}
 
 export const defaultClient = 'v2';
