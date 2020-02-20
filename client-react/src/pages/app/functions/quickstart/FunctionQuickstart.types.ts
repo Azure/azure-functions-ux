8245ac5f60c5e9750a686ca6b0a0dbdb7398630e
Krrish Mittal <krrishmittal15@gmail.com>
2020-02-19 18:23:23 -0800

    set default selected file on quickstart (#4614)
    * set default selected file on quickstart
    
    * resolved pr comments

---
+++ client-react/src/pages/app/functions/quickstart/FunctionQuickstart.types.ts
@@ -0,0 +1,6 @@
+export enum QuickstartOptions {
+  visualStudio = 'vsDirectPublish',
+  visualStudioCode = 'vsCodeDirectPublish',
+  coreTools = 'coretoolsDirectPublish',
+  maven = 'mavenDirectPublish',
+}
