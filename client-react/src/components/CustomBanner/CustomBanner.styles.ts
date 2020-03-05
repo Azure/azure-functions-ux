666dcaeba4ff6eadeb837a088030f45d0ade184e
Krrish Mittal <krmitta@microsoft.com>
2020-03-05 15:09:43 -0800

    banner code refactored

--- client-react/src/components/CustomBanner/CustomBanner.styles.ts
+++ client-react/src/components/CustomBanner/CustomBanner.styles.ts
@@ -1,18 +1,44 @@
-export const messageBannerStyles = {
-  root: {
-    height: '33px',
-  },
-  content: {
-    height: '33px',
-    paddingLeft: '15px',
-  },
-  text: {
-    marginTop: '9px',
-  },
-  iconContainer: {
-    display: 'contents' as 'contents',
-  },
-  icon: {
-    marginTop: '9px',
-  },
+import { style } from 'typestyle';
+import { ThemeExtended } from '../../theme/SemanticColorsExtended';
+import { MessageBarType } from 'office-ui-fabric-react';
+
+export const messageBannerStyles = isCustomIcon => {
+  const styles = {
+    root: {
+      height: '33px',
+    },
+    content: {
+      height: '33px',
+      paddingLeft: '15px',
+    },
+    text: {
+      marginTop: '9px',
+    },
+    iconContainer: {
+      display: isCustomIcon ? ('none' as 'none') : ('contents' as 'contents'),
+    },
+    icon: {},
+  };
+  if (!isCustomIcon) {
+    styles.icon = {
+      marginTop: '9px',
+    };
+  }
+  return styles;
+};
+
+export const messageBannerTextStyle = style({
+  marginLeft: '24px',
+});
+
+export const messageBannerIconStyle = style({
+  position: 'absolute',
+  height: '16px',
+  width: '16px',
+});
+
+export const messageBannerClass = (theme: ThemeExtended, type: MessageBarType) => {
+  return style({
+    backgroundColor: type === MessageBarType.info ? theme.semanticColors.infoBackground : undefined,
+  });
 };
--- client-react/src/components/CustomBanner/CustomBanner.tsx
+++ client-react/src/components/CustomBanner/CustomBanner.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MessageBar, MessageBarType } from 'office-ui-fabric-react';
-import { messageBannerStyles } from './CustomBanner.styles';
+import { messageBannerStyles, messageBannerTextStyle, messageBannerIconStyle, messageBannerClass } from './CustomBanner.styles';
+import { ThemeContext } from '../../ThemeContext';
 
 interface CustomBannerProps {
   message: string;
   type: MessageBarType;
+  id?: string;
+  icon?: JSX.Element;
+  className?: string;
 }
 
 const CustomBanner: React.FC<CustomBannerProps> = props => {
-  const { message, type } = props;
+  const { message, type, id, icon, className: customClassName } = props;
+
+  const theme = useContext(ThemeContext);
+
+  let className = messageBannerClass(theme, type);
+
+  if (!!customClassName) {
+    className = Object.assign(className, customClassName);
+  }
 
   return (
     <div>
-      <MessageBar id="app-stopped-banner" isMultiline={true} messageBarType={type} styles={messageBannerStyles}>
-        {message}
+      <MessageBar
+        id={`${id}-custom-banner`}
+        isMultiline={true}
+        messageBarType={type}
+        styles={messageBannerStyles(!!icon)}
+        className={className}>
+        {!!icon ? (
+          <>
+            <span className={messageBannerIconStyle}>{icon}</span>
+            <span className={messageBannerTextStyle}>{message}</span>
+          </>
+        ) : (
+          message
+        )}
       </MessageBar>
     </div>
   );
