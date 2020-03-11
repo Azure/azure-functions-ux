c351b1ca19a2a91319e5af865cd6838357aa53b5
Allison Manifold <allisonm@microsoft.com>
2020-03-10 20:47:05 -0700

    make function log consumable by monitor

--- client-react/src/pages/app/functions/monitor/FunctionMonitor.tsx
+++ client-react/src/pages/app/functions/monitor/FunctionMonitor.tsx
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Pivot, PivotItem } from 'office-ui-fabric-react';
+import { paddingStyle } from './FunctionMonitor.styles';
+import FunctionLog from '../function-log/FunctionLog';
 
 interface FunctionMonitorProps {
   resourceId: string;
+  resetAppInsightsToken: () => void;
+  appInsightsToken?: string;
+}
+
+enum PivotState {
+  invocation = 'invocation',
+  logs = 'logs',
 }
 
 const FunctionMonitor: React.FC<FunctionMonitorProps> = props => {
+  const { resourceId, resetAppInsightsToken, appInsightsToken } = props;
+  const { t } = useTranslation();
+  const [pivotStateKey, setPivotStateKey] = useState<PivotState>(PivotState.invocation);
+  const functionName = resourceId.split('/functions/').pop() || '';
+
+  const onPivotItemClicked = (item?: PivotItem) => {
+    if (!!item) {
+      setPivotStateKey(item.props.itemKey as PivotState);
+    }
+  };
+
+  const getPivotTabId = (itemKey: string) => {
+    switch (itemKey) {
+      case PivotState.invocation:
+        return 'function-monitor-invocation-tab';
+      case PivotState.logs:
+        return 'function-monitor-logs-tab';
+    }
+    return '';
+  };
+
   return (
-    <div>
-      <h2>{'Hello there'}</h2>
+    <div style={paddingStyle}>
+      <Pivot getTabId={getPivotTabId} selectedKey={pivotStateKey} onLinkClick={onPivotItemClicked}>
+        <PivotItem itemKey={PivotState.invocation} headerText={t('functionMonitor_invocation')} />
+        <PivotItem itemKey={PivotState.logs} headerText={t('functionMonitor_logs')}>
+          <FunctionLog
+            isExpanded={true}
+            resetAppInsightsToken={resetAppInsightsToken}
+            appInsightsToken={appInsightsToken}
+            functionName={functionName}
+            forceMaximized={true}
+            hideChevron={true}
+          />
+        </PivotItem>
+      </Pivot>
     </div>
   );
 };
