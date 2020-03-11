441555072878034349a14bede840448f46c04bf6
Aniello Scotto Di Marco <andimarc@microsoft.com>
2020-03-11 06:51:13 -0700

    Hiding TIP controls for Dynamic Function Apps

--- client/src/app/shared/models/constants.ts
+++ client/src/app/shared/models/constants.ts
@@ -328,6 +328,7 @@ export class ScenarioIds {
   public static readonly containerSettings = 'containerSettings';
   public static readonly isPublishProfileBasedDeploymentEnabled = 'isPublishProfileBasedDeploymentEnabled';
   public static readonly enableGitHubAction = 'enableGitHubAction';
+  public static readonly tipSupported = 'tipSupported';
 }
 
 export class NationalCloudArmUris {
