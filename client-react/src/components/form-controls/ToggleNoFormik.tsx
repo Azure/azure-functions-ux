e83aa2049b046ae8ad15627009749fb8a50c05f3
Reid Fortier <refortie@microsoft.com>
2020-03-11 12:35:55 -0700

    Bring toggle in line with rest of components

---
+++ client-react/src/components/form-controls/ToggleNoFormik.tsx
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import { Toggle as OfficeToggle, IToggleProps } from 'office-ui-fabric-react/lib/Toggle';
+import { ThemeContext } from '../../ThemeContext';
+import ReactiveFormControl from './ReactiveFormControl';
+
+interface CustomToggleProps {
+  id: string;
+  infoBubbleMessage?: string;
+  label?: string;
+  errorMessage?: string;
+  checked?: boolean;
+  onChange: (_e, checked: boolean) => void;
+}
+
+const ToggleNoFormik = (props: IToggleProps & CustomToggleProps) => {
+  const { checked, onChange, errorMessage, label } = props;
+  const theme = useContext(ThemeContext);
+
+  return (
+    <ReactiveFormControl {...props}>
+      <OfficeToggle
+        aria-labelledby={`${props.id}-label`}
+        ariaLabel={label}
+        onChange={onChange}
+        errorMessage={errorMessage}
+        styles={{
+          thumb: {
+            backgroundColor: checked ? theme.semanticColors.buttonBackground : theme.semanticColors.primaryButtonBackground,
+          },
+        }}
+        {...props}
+      />
+    </ReactiveFormControl>
+  );
+};
+
+export default ToggleNoFormik;
