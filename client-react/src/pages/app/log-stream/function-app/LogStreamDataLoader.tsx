6e63ac8fb5d0b2bacf768732e8b9986f70a67bf8
Allison Manifold <allisonm@microsoft.com>
2020-04-02 17:05:21 -0700

    log stream for function apps

--- client-react/src/pages/app/log-stream/function-app/LogStreamDataLoader.tsx
+++ client-react/src/pages/app/log-stream/function-app/LogStreamDataLoader.tsx
@@ -1,4 +1,15 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { StartupInfoContext } from '../../../../StartupInfoContext';
+import { ArmSiteDescriptor } from '../../../../utils/resourceDescriptors';
+import AppInsightsService from '../../../../ApiHelpers/AppInsightsService';
+import { ArmObj } from '../../../../models/arm-obj';
+import { AppInsightsComponent } from '../../../../models/app-insights';
+import LogService from '../../../../utils/LogService';
+import { LogCategories } from '../../../../utils/LogCategories';
+import LoadingComponent from '../../../../components/Loading/LoadingComponent';
+import AppInsightsSetup from '../../functions/function/monitor/AppInsightsSetup';
+import FunctionLogAppInsightsDataLoader from '../../functions/function/function-log/FunctionLogAppInsightsDataLoader';
+import { paddingStyle } from './LogStream.styles';
 
 export interface LogStreamDataLoaderProps {
   resourceId: string;
@@ -6,8 +17,78 @@ export interface LogStreamDataLoaderProps {
 
 const LogStreamDataLoader: React.FC<LogStreamDataLoaderProps> = props => {
   const { resourceId } = props;
+  const [appInsightsComponent, setAppInsightsComponent] = useState<ArmObj<AppInsightsComponent> | undefined | null>(undefined);
+  const startupInfoContext = useContext(StartupInfoContext);
+  const armSiteDescriptor = new ArmSiteDescriptor(resourceId);
+  const siteResourceId = armSiteDescriptor.getTrimmedResourceId();
 
-  return <h2>{`Log Stream for function app ${resourceId}`}</h2>;
+  const fetchComponent = async (force?: boolean) => {
+    const appInsightsResourceIdResponse = await AppInsightsService.getAppInsightsResourceId(
+      siteResourceId,
+      startupInfoContext.subscriptions
+    );
+    if (appInsightsResourceIdResponse.metadata.success) {
+      const aiResourceId = appInsightsResourceIdResponse.data;
+      if (!!aiResourceId) {
+        const appInsightsResponse = await AppInsightsService.getAppInsights(aiResourceId);
+        if (appInsightsResponse.metadata.success) {
+          setAppInsightsComponent(appInsightsResponse.data);
+        } else {
+          LogService.error(
+            LogCategories.functionLog,
+            'getAppInsights',
+            `Failed to get app insights: ${appInsightsResponse.metadata.error}`
+          );
+        }
+      }
+    } else {
+      setAppInsightsComponent(null);
+      LogService.error(
+        LogCategories.functionLog,
+        'getAppInsightsResourceId',
+        `Failed to get app insights resource Id: ${appInsightsResourceIdResponse.metadata.error}`
+      );
+    }
+  };
+
+  const resetAppInsightsComponent = () => {
+    setAppInsightsComponent(undefined);
+  };
+
+  useEffect(() => {
+    fetchComponent();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    if (appInsightsComponent === undefined) {
+      fetchComponent(true);
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [appInsightsComponent]);
+
+  if (appInsightsComponent === undefined) {
+    return <LoadingComponent />;
+  }
+
+  if (appInsightsComponent === null) {
+    return <AppInsightsSetup siteId={siteResourceId} fetchNewAppInsightsComponent={resetAppInsightsComponent} />;
+  }
+
+  return (
+    <div style={paddingStyle}>
+      <FunctionLogAppInsightsDataLoader
+        resourceId={resourceId}
+        isExpanded={true}
+        forceMaximized={true}
+        isResizable={false}
+        hideChevron={true}
+        isFunctionApp={true}
+      />
+    </div>
+  );
 };
 
 export default LogStreamDataLoader;
