cf6f6813d832fce581413645d4d5b0117f74d770
Allison Manifold <allisonm@microsoft.com>
2020-04-02 19:04:51 -0700

    fetch site in router

--- client-react/src/pages/app/log-stream/LogStreamRouter.tsx
+++ client-react/src/pages/app/log-stream/LogStreamRouter.tsx
@@ -1,20 +1,38 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { ArmObj } from '../../../models/arm-obj';
+import { Site } from '../../../models/site/site';
+import { SiteRouterContext } from '../SiteRouter';
 import { useTranslation } from 'react-i18next';
+import LogService from '../../../utils/LogService';
+import { LogCategories } from '../../../utils/LogCategories';
 import LoadingComponent from '../../../components/Loading/LoadingComponent';
 import { ScenarioService } from '../../../utils/scenario-checker/scenario.service';
 import { ScenarioIds } from '../../../utils/scenario-checker/scenario-ids';
 import FunctionAppDataLoader from '../functions/log-stream/LogStreamDataLoader';
 import WebAppDataLoader from './LogStreamDataLoader';
-import { SiteStateContext } from '../../../SiteState';
 
 export interface LogStreamRouterProps {
   resourceId: string;
 }
 const LogStreamRouter: React.FC<LogStreamRouterProps> = props => {
   const { resourceId } = props;
-  const siteStateContext = useContext(SiteStateContext);
-  const site = siteStateContext.getSite();
+  const [site, setSite] = useState<ArmObj<Site> | undefined>(undefined);
+  const siteContext = useContext(SiteRouterContext);
   const { t } = useTranslation();
+
+  const fetchSite = async () => {
+    const siteResponse = await siteContext.fetchSite(resourceId);
+    if (siteResponse.metadata.success) {
+      setSite(siteResponse.data);
+    } else {
+      LogService.error(LogCategories.logStreamLoad, 'fetchSite', `Failed to fetch site: ${siteResponse.metadata.error}`);
+    }
+  };
+
+  useEffect(() => {
+    fetchSite();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   if (!site) {
     return <LoadingComponent />;
