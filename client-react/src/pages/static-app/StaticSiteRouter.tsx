021c8c687548c96ccb755a65de2713adfeea0991
Krrish Mittal <takyyon@88LCKT2.redmond.corp.microsoft.com>
2020-04-10 11:57:48 -0700

    data loader added

--- client-react/src/pages/static-app/StaticSiteRouter.tsx
+++ client-react/src/pages/static-app/StaticSiteRouter.tsx
@@ -1,8 +1,10 @@
 import { RouteComponentProps, Router } from '@reach/router';
-import React, { useContext } from 'react';
+import React, { useContext, lazy } from 'react';
 import { ThemeContext } from '../../ThemeContext';
 import { iconStyles } from '../../theme/iconStyles';
 import { StartupInfoContext } from '../../StartupInfoContext';
+
+const ConfigurationLoadable: any = lazy(() => import(/* webpackChunkName: "configuration" */ './configuration/ConfigurationDataLoader'));
 
 export interface StaticSiteRouterProps {
   subscriptionId?: string;
@@ -17,7 +19,13 @@ const StaticSiteRouter: React.FC<RouteComponentProps<StaticSiteRouterProps>> = p
     <main className={iconStyles(theme)}>
       <StartupInfoContext.Consumer>
         {value => {
-          return value.token && <Router>{`StaticSite Router`}</Router>;
+          return (
+            value.token && (
+              <Router>
+                <ConfigurationLoadable resourceId={value.resourceId} path="/configuration" />
+              </Router>
+            )
+          );
         }}
       </StartupInfoContext.Consumer>
     </main>
---
+++ client-react/src/pages/static-app/configuration/ConfigurationDataLoader.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface ConfigurationDataLoaderProps {
+  resourceId: string;
+}
+
+const ConfigurationDataLoader: React.FC<ConfigurationDataLoaderProps> = props => {
+  return <div>{`StaticSite Router`}</div>;
+};
+
+export default ConfigurationDataLoader;
