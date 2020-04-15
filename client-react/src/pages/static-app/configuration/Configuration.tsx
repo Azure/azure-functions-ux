4c876438247dc5d145bad24742819433bd17c9e2
Krrish Mittal <krrishmittal15@gmail.com>
2020-04-15 12:13:28 -0700

    delete environment variable function added (#4813)
    * delete en-variable function added
    
    * bulk add/edit added (#4814)
    
    Co-authored-by: Krrish Mittal <takyyon@WIN-5KMQ9EN96RP.redmond.corp.microsoft.com>
    
    Co-authored-by: Krrish Mittal <takyyon@WIN-5KMQ9EN96RP.redmond.corp.microsoft.com>

--- client-react/src/pages/static-app/configuration/Configuration.tsx
+++ client-react/src/pages/static-app/configuration/Configuration.tsx
@@ -30,6 +30,7 @@ import { sortBy } from 'lodash-es';
 import { KeyValue } from '../../../models/portal-models';
 import ConfigurationData from './Configuration.data';
 import ConfirmDialog from '../../../components/ConfirmDialog/ConfirmDialog';
+import ConfigurationAdvancedAddEdit from './ConfigurationAdvancedAddEdit';
 
 interface ConfigurationProps {
   staticSite: ArmObj<StaticSite>;
@@ -81,7 +82,8 @@ const Configuration: React.FC<ConfigurationProps> = props => {
   };
 
   const openBulkEdit = () => {
-    // TODO (krmitta): Add logic here
+    setShowPanel(true);
+    setPanelType(PanelType.bulk);
   };
 
   const toggleFilter = () => {
@@ -143,7 +145,7 @@ const Configuration: React.FC<ConfigurationProps> = props => {
             iconProps={{ iconName: 'Delete' }}
             ariaLabel={t('delete')}
             onClick={() => {
-              // TODO (krmitta): Add delete function
+              deleteEnvironmentVariable(index);
             }}
           />
         </TooltipHost>
@@ -294,8 +296,6 @@ const Configuration: React.FC<ConfigurationProps> = props => {
 
   const updateEnvironmentVariable = (updatedEnvironmentVariables: EnvironmentVariable[]) => {
     const sortedUpdatedEnvironmentVariables = sort(updatedEnvironmentVariables);
-    const dirtyState = getDirtyState(sortedUpdatedEnvironmentVariables);
-    setIsDirty(dirtyState);
     setEnvironmentVariables([...sortedUpdatedEnvironmentVariables]);
   };
 
@@ -332,7 +332,6 @@ const Configuration: React.FC<ConfigurationProps> = props => {
 
   const initEnvironmentVariables = () => {
     setEnvironmentVariables(getInitialEnvironmentVariables());
-    setIsDirty(false);
   };
 
   const discard = () => {
@@ -358,6 +357,19 @@ const Configuration: React.FC<ConfigurationProps> = props => {
     hideOnChangeConfirmDialog();
   };
 
+  const deleteEnvironmentVariable = (index: number) => {
+    if (index < environmentVariables.length) {
+      const deleteItem = environmentVariables[index];
+      setEnvironmentVariables([...environmentVariables.filter(environmentVariable => environmentVariable !== deleteItem)]);
+    }
+  };
+
+  useEffect(() => {
+    const dirtyState = getDirtyState(environmentVariables);
+    setIsDirty(dirtyState);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [environmentVariables]);
   useEffect(() => {
     initEnvironmentVariables();
 
@@ -444,6 +456,13 @@ const Configuration: React.FC<ConfigurationProps> = props => {
           headerText={t('staticSite_addEditEnvironmentVariable')}>
           <ConfigurationAddEdit
             currentEnvironmentVariableIndex={currentEnvironmentVariableIndex!}
+            environmentVariables={environmentVariables}
+            cancel={onCancel}
+            updateEnvironmentVariable={updateEnvironmentVariable}
+          />
+        </Panel>
+        <Panel isOpen={showPanel && panelType === PanelType.bulk} onDismiss={onCancel}>
+          <ConfigurationAdvancedAddEdit
             environmentVariables={environmentVariables}
             cancel={onCancel}
             updateEnvironmentVariable={updateEnvironmentVariable}
---
+++ client-react/src/pages/static-app/configuration/ConfigurationAdvancedAddEdit.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState, useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { EnvironmentVariable } from './Configuration.types';
+import ActionBar from '../../../components/ActionBar';
+import MonacoEditor, { getMonacoEditorTheme } from '../../../components/monaco-editor/monaco-editor';
+import { PortalTheme } from '../../../models/portal-models';
+import { StartupInfoContext } from '../../../StartupInfoContext';
+
+interface ConfigurationAdvancedAddEditProps {
+  cancel: () => void;
+  updateEnvironmentVariable: (environmentVariables: EnvironmentVariable[]) => void;
+  environmentVariables: EnvironmentVariable[];
+}
+
+const ConfigurationAdvancedAddEdit: React.FC<ConfigurationAdvancedAddEditProps> = props => {
+  const { environmentVariables, updateEnvironmentVariable, cancel } = props;
+
+  const [environmentVariablesJSON, setEnvironmentVariablesJSON] = useState('');
+
+  const { t } = useTranslation();
+
+  const startUpInfoContext = useContext(StartupInfoContext);
+
+  const save = () => {
+    const environmentVariablesArray = JSON.parse(environmentVariablesJSON);
+    updateEnvironmentVariable(environmentVariablesArray);
+    cancel();
+  };
+
+  const onChange = (newValue: string, event: any) => {
+    setEnvironmentVariablesJSON(newValue);
+    // TODO (krmitta): Add validations
+  };
+
+  const actionBarPrimaryButtonProps = {
+    id: 'save',
+    title: t('ok'),
+    onClick: save,
+    disable: false,
+  };
+
+  const actionBarSecondaryButtonProps = {
+    id: 'cancel',
+    title: t('cancel'),
+    onClick: cancel,
+    disable: false,
+  };
+
+  useEffect(() => {
+    setEnvironmentVariablesJSON(JSON.stringify(environmentVariables, null, 2));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [environmentVariables]);
+  return (
+    <>
+      <form>
+        <MonacoEditor
+          value={environmentVariablesJSON}
+          language="json"
+          onChange={onChange}
+          options={{
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+          }}
+          height="calc(100vh - 140px)"
+          theme={getMonacoEditorTheme(startUpInfoContext.theme as PortalTheme)}
+        />
+        <ActionBar
+          id="environment-variable-edit-footer"
+          primaryButton={actionBarPrimaryButtonProps}
+          secondaryButton={actionBarSecondaryButtonProps}
+        />
+      </form>
+    </>
+  );
+};
+
+export default ConfigurationAdvancedAddEdit;
