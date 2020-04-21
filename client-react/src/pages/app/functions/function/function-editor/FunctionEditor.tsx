062427a529845c1e178141da688adfa3811fc081
Krrish Mittal <takyyon@pvttest.guest.corp.microsoft.com>
2020-04-21 16:14:29 -0700

    expand log panel

--- client-react/src/pages/app/functions/function/function-editor/FunctionEditor.tsx
+++ client-react/src/pages/app/functions/function/function-editor/FunctionEditor.tsx
@@ -180,6 +180,9 @@ export const FunctionEditor: React.SFC<FunctionEditorProps> = props => {
     const updatedData = JSON.stringify(data);
     const tempFunctionInfo = functionInfo;
     tempFunctionInfo.properties.test_data = updatedData;
+    if (!logPanelExpanded) {
+      setLogPanelExpanded(true);
+    }
     props.run(tempFunctionInfo, values.xFunctionKey);
   };
 
