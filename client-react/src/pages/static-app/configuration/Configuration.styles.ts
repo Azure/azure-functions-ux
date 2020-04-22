ccef35d0e9df047117686861508e8f871224b9ae
Krrish Mittal <takyyon@MININT-T7N0J7N.redmond.corp.microsoft.com>
2020-04-22 15:53:13 -0700

    copy column removed

--- client-react/src/pages/static-app/configuration/Configuration.styles.ts
+++ client-react/src/pages/static-app/configuration/Configuration.styles.ts
@@ -21,8 +21,3 @@ export const environmentSelectorStackStyle = (theme: ThemeExtended) =>
 export const environmentSelectorLabelStyle = style({
   paddingRight: '10px',
 });
-
-export const copyButtonIconStyle = style({
-  paddingLeft: '5px',
-  paddingTop: '2px',
-});
