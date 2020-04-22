31d65c58260d996f2eb26893cd25eb1b33279ac7
Krrish Mittal <takyyon@pvttest.guest.corp.microsoft.com>
2020-04-21 21:33:41 -0700

    add editor and remove tests

--- client-react/src/pages/static-app/configuration/ConfigurationAdvancedAddEdit.tsx
+++ client-react/src/pages/static-app/configuration/ConfigurationAdvancedAddEdit.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { EnvironmentVariable } from './Configuration.types';
 import ActionBar from '../../../components/ActionBar';
 import { ConfigurationUtils } from './Configuration.utils';
 import { MessageBarType } from 'office-ui-fabric-react';
+import MonacoEditor, { getMonacoEditorTheme } from '../../../components/monaco-editor/monaco-editor';
+import { EditorLanguage } from '../../../utils/EditorManager';
+import { StartupInfoContext } from '../../../StartupInfoContext';
+import { PortalTheme } from '../../../models/portal-models';
 
 interface ConfigurationAdvancedAddEditProps {
   cancel: () => void;
@@ -14,10 +18,12 @@ interface ConfigurationAdvancedAddEditProps {
 const ConfigurationAdvancedAddEdit: React.FC<ConfigurationAdvancedAddEditProps> = props => {
   const { environmentVariables, updateEnvironmentVariable, cancel } = props;
 
-  const [environmentVariablesJSON, setEnvironmentVariablesJSON] = useState('');
+  const [environmentVariablesJSON, setEnvironmentVariablesJSON] = useState('[]');
   const [errorMessage, setErrorMessage] = useState('');
 
   const { t } = useTranslation();
+
+  const startUpContext = useContext(StartupInfoContext);
 
   const save = () => {
     validate(environmentVariablesJSON);
@@ -49,6 +55,11 @@ const ConfigurationAdvancedAddEdit: React.FC<ConfigurationAdvancedAddEditProps> 
     return errorMessage ? { message: errorMessage, level: MessageBarType.error } : undefined;
   };
 
+  const onChange = (newValue, e) => {
+    setEnvironmentVariablesJSON(newValue);
+    validate(newValue);
+  };
+
   useEffect(() => {
     setEnvironmentVariablesJSON(JSON.stringify(environmentVariables, null, 2));
 
@@ -57,6 +68,17 @@ const ConfigurationAdvancedAddEdit: React.FC<ConfigurationAdvancedAddEditProps> 
   return (
     <>
       <form>
+        <MonacoEditor
+          value={environmentVariablesJSON}
+          language={EditorLanguage.json}
+          onChange={onChange}
+          options={{
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+          }}
+          height="calc(100vh - 140px)"
+          theme={getMonacoEditorTheme(startUpContext.theme as PortalTheme)}
+        />
         <ActionBar
           id="environment-variable-edit-footer"
           primaryButton={actionBarPrimaryButtonProps}
