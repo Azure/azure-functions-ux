3f95b65998276fb59e3bd1fc9e8e884a3939f0db
Krrish Mittal <takyyon@yoprajap-lap2.fareast.corp.microsoft.com>
2020-05-19 21:26:53 -0700

    app insights warning banner added for keyvault reference

--- client-react/src/utils/CommonConstants.ts
+++ client-react/src/utils/CommonConstants.ts
@@ -187,4 +187,6 @@ export class CommonConstants {
     usNat: 'https://management.azure.eaglex.ic.gov',
     usSec: 'https://management.azure.microsoft.scloud',
   };
+
+  public static isKeyVaultReference = (value: string) => value.toLocaleLowerCase().startsWith('@microsoft.keyvault(');
 }
