aa6260e1806763a24ae5c1e66ed3c85ac9aaedf2
Reid Fortier <refortie@microsoft.com>
2020-05-20 14:13:05 -0700

    Remove extra comments that somehow got added twice

--- client-react/src/models/functions/function-binding.ts
+++ client-react/src/models/functions/function-binding.ts
@@ -2,10 +2,6 @@
 // For some reason binding direction is only in/out when
 // stored in Function config.  Binding direction downloaded from templates
 // also have Trigger as a possible direction.
-// Should only be used for FunctionInfo binding direction.
-// For some reason binding direction is only in/out when
-// stored in Function config.  Binding direction downloaded from templates
-// also have Trigger as a possible direction.
 export enum BindingDirection {
   in = 'in',
   out = 'out',
