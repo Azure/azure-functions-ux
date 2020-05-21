781e37109b1c81f18da84655f69327e1374c59b2
refortie <37600290+refortie@users.noreply.github.com>
2020-05-21 11:23:47 -0700

    [Ibizafication][Integrate] Obey rules on bindings (#4943)
    * Rules WiP
    
    * Don't use rules on HTTP
    
    * Fix conditional
    
    * revert validate type change
    
    * PR updates

--- client-react/src/pages/app/functions/function/integrate/BindingPanel/BindingCreator.tsx
+++ client-react/src/pages/app/functions/function/integrate/BindingPanel/BindingCreator.tsx
@@ -147,7 +147,7 @@ const bindingTypeSpecificFields = (
 
   const builder = new BindingFormBuilder([formProps.values], [binding], functionAppId, t);
 
-  return builder.getFields(formProps, false);
+  return builder.getFields(formProps, false, true);
 };
 
 const getDefaultValues = (bindingType: BindingType, filteredBindings: Binding[]): KeyValue<string> => {
--- client-react/src/pages/app/functions/function/integrate/FunctionIntegrateConstants.ts
+++ client-react/src/pages/app/functions/function/integrate/FunctionIntegrateConstants.ts
@@ -1,5 +1,8 @@
 export class FunctionIntegrateConstants {
   public static readonly eventGridType: string = 'eventGridTrigger';
+  public static readonly httpType: string = 'httpTrigger';
 
   public static readonly builtInBindingTypes: string[] = ['httpTrigger', 'timerTrigger', 'http'];
+
+  public static readonly rulePrefix: string = 'rule-';
 }
