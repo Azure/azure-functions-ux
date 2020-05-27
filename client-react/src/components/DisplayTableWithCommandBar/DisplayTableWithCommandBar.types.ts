feb4f61ca1bdd6680b3458e9fdceaa36f07a0110
Krrish Mittal <takyyon@DIN80011421.fareast.corp.microsoft.com>
2020-05-26 23:18:24 -0700

    remove extraa file

--- client-react/src/components/DisplayTableWithCommandBar/DisplayTableWithCommandBar.types.ts
+++
@@ -1,3 +0,0 @@
-export const getSortedItems = (items: any[], columnKey: string, isSortedDescending?: boolean) => [
-  ...items.slice(0).sort((a: any, b: any) => ((isSortedDescending ? a[columnKey] < b[columnKey] : a[columnKey] > b[columnKey]) ? 1 : -1)),
-];
