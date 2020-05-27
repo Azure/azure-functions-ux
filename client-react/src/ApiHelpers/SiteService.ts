e8bebd4d27f240ac8ac31c05f4f9049d1ed6bd0a
Allison Manifold <allisonm@microsoft.com>
2020-05-27 14:12:50 -0700

    update synctrigger call

--- client-react/src/ApiHelpers/SiteService.ts
+++ client-react/src/ApiHelpers/SiteService.ts
@@ -182,11 +182,7 @@ export default class SiteService {
   };
 
   public static fireSyncTrigger = (site: ArmObj<Site>, token: string) => {
-    if (isLinuxDynamic(site)) {
-      return MakeArmCall<any>({ resourceId: `${site.id}/hostruntime/admin/host/synctriggers`, commandName: 'syncTrigger', method: 'POST' });
-    } else {
-      return sendHttpRequest({ url: Url.getSyncTriggerUrl(site), method: 'POST', headers: { Authorization: `Bearer ${token}` } });
-    }
+    return MakeArmCall<any>({ resourceId: `${site.id}/host/default/sync`, commandName: 'syncTrigger', method: 'POST' });
   };
 
   public static getPublishProfile = (resourceId: string) => {
