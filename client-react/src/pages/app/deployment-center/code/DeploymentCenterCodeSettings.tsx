568c4b00331cd5b106c15aa0555490ea3cb3fe0f
Kartikeya Kandula <t-kakan@microsoft.com>
2020-06-01 15:20:34 -0400

    Only show github component if github selected from source dropdown

--- client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeSettings.tsx
+++ client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeSettings.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { DeploymentCenterFieldProps, AppTypes } from '../DeploymentCenter.types';
 import DeploymentCenterCodeSource from './DeploymentCenterCodeSource';
 import DeploymentCenterGitHubDataLoader from '../github-provider/DeploymentCenterGitHubDataLoader';
+import { ScmTypes } from '../../../../models/site/config';
 
 const DeploymentCenterCodeSettings: React.FC<DeploymentCenterFieldProps> = props => {
   const { formProps } = props;
+  const isGitHubSource = formProps && formProps.values.scmType === ScmTypes.GitHub;
   return (
     <>
       <DeploymentCenterCodeSource />
-      <DeploymentCenterGitHubDataLoader formProps={formProps} appType={AppTypes.Code} />
+      {isGitHubSource && <DeploymentCenterGitHubDataLoader formProps={formProps} appType={AppTypes.Code} />}
     </>
   );
 };
