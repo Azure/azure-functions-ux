53ea1ea8b983430133b5b2370499bee6aa0fa5e6
Kartikeya Kandula <kandula.kartikeya@gmail.com>
2020-06-04 10:32:26 -0400

    Added log errors for Github calls, created function for getting github token, moved getting data to DeploymentCenterGitHubDataLoader

--- client-react/src/pages/app/deployment-center/github-provider/DeploymentCenterGitHubAccount.tsx
+++ client-react/src/pages/app/deployment-center/github-provider/DeploymentCenterGitHubAccount.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { DeploymentCenterGitHubProviderProps } from '../DeploymentCenter.types';
 import { PrimaryButton, Label, Link, IDropdownOption } from 'office-ui-fabric-react';
@@ -6,72 +6,26 @@ import ReactiveFormControl from '../../../../components/form-controls/ReactiveFo
 import { additionalTextFieldControl } from '../DeploymentCenter.styles';
 import Dropdown from '../../../../components/form-controls/DropDown';
 import { Field } from 'formik';
-import DeploymentCenterData from '../DeploymentCenter.data';
 
 const DeploymentCenterGitHubAccount: React.FC<DeploymentCenterGitHubProviderProps> = props => {
-  const { gitHubUser, gitHubAccountStatusMessage, authorizeGitHubAccount } = props;
-  const deploymentCenterData = new DeploymentCenterData();
+  const {
+    gitHubUser,
+    gitHubAccountStatusMessage,
+    authorizeGitHubAccount,
+    fetchOrganizationOptions,
+    fetchRepositoryOptions,
+    fetchBranchOptions: fetchBatchOptions,
+    organizationOptions,
+    repositoryOptions,
+    branchOptions,
+  } = props;
   const { t } = useTranslation();
 
-  const [organizationOptions, setOrganizationOptions] = useState<IDropdownOption[]>([]);
-  const [repositoryOptions, setRepositoryOptions] = useState<IDropdownOption[]>([]);
-  const [branchOptions, setBranchOptions] = useState<IDropdownOption[]>([]);
-
-  const fetchOrganizationOptions = async () => {
-    const newOrganizationOptions: IDropdownOption[] = [];
-
-    const armToken = window.appsvc && window.appsvc.env.armToken ? `bearer ${window.appsvc.env.armToken}` : '';
-    if (gitHubUser) {
-      const gitHubOrganizationsResponse = await deploymentCenterData.getGitHubOrganizations(armToken);
-
-      gitHubOrganizationsResponse.data.forEach(org => {
-        newOrganizationOptions.push({ key: org.url, text: org.login });
-      });
-
-      newOrganizationOptions.push({ key: gitHubUser.repos_url, text: gitHubUser.login });
-    }
-
-    setOrganizationOptions(newOrganizationOptions);
-  };
-
-  const fetchRepositoryOptions = async (repositories_url: string) => {
-    const newRepositoryOptions: IDropdownOption[] = [];
-
-    const armToken = window.appsvc && window.appsvc.env.armToken ? `bearer ${window.appsvc.env.armToken}` : '';
-
-    const gitHubRepositoriesResponse = await (repositories_url.toLocaleLowerCase().indexOf('github.com/users/') > -1
-      ? deploymentCenterData.getGitHubUserRepositories(armToken)
-      : deploymentCenterData.getGitHubOrgRepositories(repositories_url, armToken));
-
-    gitHubRepositoriesResponse.data.forEach(repository => {
-      newRepositoryOptions.push({ key: repository.url, text: repository.name });
-    });
-
-    setRepositoryOptions(newRepositoryOptions);
-  };
-
-  const fetchBatchOptions = async (repository_url: string) => {
-    const newBranchOptions: IDropdownOption[] = [];
-
-    const armToken = window.appsvc && window.appsvc.env.armToken ? `bearer ${window.appsvc.env.armToken}` : '';
-
-    const gitHubBranchesResponse = await deploymentCenterData.getGitHubBranches(repository_url, armToken);
-
-    gitHubBranchesResponse.data.forEach(branch => {
-      newBranchOptions.push({ key: branch.name, text: branch.name });
-    });
-
-    setBranchOptions(newBranchOptions);
-  };
-
   const onOrganizationChange = (event: React.FormEvent<HTMLDivElement>, option: IDropdownOption) => {
-    setBranchOptions([]);
-    setRepositoryOptions([]);
     fetchRepositoryOptions(option.key.toString());
   };
 
   const onRepositoryChange = (event: React.FormEvent<HTMLDivElement>, option: IDropdownOption) => {
-    setBranchOptions([]);
     fetchBatchOptions(option.key.toString());
   };
 
@@ -79,7 +33,6 @@ const DeploymentCenterGitHubAccount: React.FC<DeploymentCenterGitHubProviderProp
 
   useEffect(() => {
     fetchOrganizationOptions();
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gitHubUser]);
 
--- client-react/src/pages/app/deployment-center/github-provider/DeploymentCenterGitHubDataLoader.tsx
+++ client-react/src/pages/app/deployment-center/github-provider/DeploymentCenterGitHubDataLoader.tsx
@@ -5,6 +5,10 @@ import { useTranslation } from 'react-i18next';
 import DeploymentCenterData from '../DeploymentCenter.data';
 import GitHubService from '../../../../ApiHelpers/GitHubService';
 import { DeploymentCenterFieldProps } from '../DeploymentCenter.types';
+import { IDropdownOption } from 'office-ui-fabric-react';
+import LogService from '../../../../utils/LogService';
+import { LogCategories } from '../../../../utils/LogCategories';
+import { getErrorMessage } from '../../../../ApiHelpers/ArmHelper';
 
 interface authorizationResult {
   timerId: NodeJS.Timeout;
@@ -20,6 +24,91 @@ const DeploymentCenterGitHubDataLoader: React.FC<DeploymentCenterFieldProps> = p
   const [gitHubAccountStatusMessage, setGitHubAccountStatusMessage] = useState<string | undefined>(
     t('deploymentCenterOAuthFetchingUserInformation')
   );
+
+  const [organizationOptions, setOrganizationOptions] = useState<IDropdownOption[]>([]);
+  const [repositoryOptions, setRepositoryOptions] = useState<IDropdownOption[]>([]);
+  const [branchOptions, setBranchOptions] = useState<IDropdownOption[]>([]);
+
+  const getArmToken = () => {
+    return window.appsvc && window.appsvc.env.armToken ? `bearer ${window.appsvc.env.armToken}` : '';
+  };
+
+  const fetchOrganizationOptions = async () => {
+    setOrganizationOptions([]);
+    setRepositoryOptions([]);
+    setBranchOptions([]);
+    const newOrganizationOptions: IDropdownOption[] = [];
+
+    const armToken = getArmToken();
+    if (gitHubUser) {
+      const gitHubOrganizationsResponse = await deploymentCenterData.getGitHubOrganizations(armToken);
+
+      if (gitHubOrganizationsResponse.metadata.success) {
+        gitHubOrganizationsResponse.data.forEach(org => {
+          newOrganizationOptions.push({ key: org.url, text: org.login });
+        });
+      } else {
+        LogService.error(
+          LogCategories.deploymentCenter,
+          'DeploymentCenterGitHubDataLoader',
+          `Failed to fetch GitHub organizations with error: ${getErrorMessage(gitHubOrganizationsResponse.metadata.error)}`
+        );
+      }
+
+      newOrganizationOptions.push({ key: gitHubUser.repos_url, text: gitHubUser.login });
+    }
+
+    setOrganizationOptions(newOrganizationOptions);
+  };
+
+  const fetchRepositoryOptions = async (repositories_url: string) => {
+    setRepositoryOptions([]);
+    setBranchOptions([]);
+    const newRepositoryOptions: IDropdownOption[] = [];
+
+    const armToken = getArmToken();
+
+    const gitHubRepositoriesResponse = await (repositories_url.toLocaleLowerCase().indexOf('github.com/users/') > -1
+      ? deploymentCenterData.getGitHubUserRepositories(armToken)
+      : deploymentCenterData.getGitHubOrgRepositories(repositories_url, armToken));
+
+    if (gitHubRepositoriesResponse.metadata.success) {
+      gitHubRepositoriesResponse.data.forEach(repository => {
+        newRepositoryOptions.push({ key: repository.url, text: repository.name });
+      });
+    } else {
+      LogService.error(
+        LogCategories.deploymentCenter,
+        'DeploymentCenterGitHubDataLoader',
+        `Failed to fetch GitHub repositories with error: ${getErrorMessage(gitHubRepositoriesResponse.metadata.error)}`
+      );
+    }
+
+    setRepositoryOptions(newRepositoryOptions);
+  };
+
+  const fetchBranchOptions = async (repository_url: string) => {
+    setBranchOptions([]);
+    const newBranchOptions: IDropdownOption[] = [];
+
+    const armToken = getArmToken();
+
+    const gitHubBranchesResponse = await deploymentCenterData.getGitHubBranches(repository_url, armToken);
+
+    if (gitHubBranchesResponse.metadata.success) {
+      gitHubBranchesResponse.data.forEach(branch => {
+        newBranchOptions.push({ key: branch.name, text: branch.name });
+      });
+    } else {
+      LogService.error(
+        LogCategories.deploymentCenter,
+        'DeploymentCenterGitHubDataLoader',
+        `Failed to fetch GitHub branches with error: ${getErrorMessage(gitHubBranchesResponse.metadata.error)}`
+      );
+    }
+
+    setBranchOptions(newBranchOptions);
+  };
 
   const authorizeGitHubAccount = async () => {
     const oauthWindow = window.open(GitHubService.authorizeUrl, 'appservice-deploymentcenter-provider-auth', 'width=800, height=600');
@@ -54,7 +143,7 @@ const DeploymentCenterGitHubDataLoader: React.FC<DeploymentCenterFieldProps> = p
       oauthWindow && oauthWindow.close();
 
       if (authorizationResult.redirectUrl) {
-        const armToken = window.appsvc && window.appsvc.env.armToken ? `bearer ${window.appsvc.env.armToken}` : '';
+        const armToken = getArmToken();
         return deploymentCenterData.storeGitHubToken(authorizationResult.redirectUrl, armToken).then(() => fetchData());
       } else {
         return fetchData();
@@ -66,7 +155,7 @@ const DeploymentCenterGitHubDataLoader: React.FC<DeploymentCenterFieldProps> = p
   // repos, orgs, branches, workflow file, etc.
 
   const fetchData = async () => {
-    const armToken = window.appsvc && window.appsvc.env.armToken ? `bearer ${window.appsvc.env.armToken}` : '';
+    const armToken = getArmToken();
     const getGitHubUserRequest = deploymentCenterData.getGitHubUser(armToken);
 
     const [gitHubUserResponse] = await Promise.all([getGitHubUserRequest]);
@@ -85,12 +174,24 @@ const DeploymentCenterGitHubDataLoader: React.FC<DeploymentCenterFieldProps> = p
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    fetchOrganizationOptions();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gitHubUser]);
+
   return (
     <DeploymentCenterGitHubProvider
       formProps={formProps}
       gitHubUser={gitHubUser}
       gitHubAccountStatusMessage={gitHubAccountStatusMessage}
       authorizeGitHubAccount={authorizeGitHubAccount}
+      fetchOrganizationOptions={fetchOrganizationOptions}
+      fetchRepositoryOptions={fetchRepositoryOptions}
+      fetchBranchOptions={fetchBranchOptions}
+      organizationOptions={organizationOptions}
+      repositoryOptions={repositoryOptions}
+      branchOptions={branchOptions}
     />
   );
 };
