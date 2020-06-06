1fd747611423714fa658ffe5b4b3b4b1d4e13d58
Allison Manifold <allisonm@microsoft.com>
2020-06-06 16:56:13 -0700

    add Tomcat 8.5

--- server/src/stacks/webapp/2020-06-01/stacks/javaContainers.ts
+++ server/src/stacks/webapp/2020-06-01/stacks/javaContainers.ts
@@ -107,8 +107,78 @@ export const javaContainersStack: WebAppStack = {
     },
     {
       displayText: 'Tomcat 8.5',
-      value: 'Tomcat 8.5',
-      minorVersions: [],
+      value: 'TOMCAT',
+      minorVersions: [
+        {
+          displayText: 'Tomcat 8.5 (Auto-update)',
+          value: '8.5',
+          containerSettings: {
+            javaLinuxSupport: {
+              java11Runtime: 'TOMCAT|8.5-java11',
+              java8Runtime: 'TOMCAT|8.5-jre8',
+            },
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.6',
+          value: '8.5.6',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.47',
+          value: '8.5.47',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.42',
+          value: '8.5.47',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.41',
+          value: '8.5.47',
+          containerSettings: {
+            javaLinuxSupport: {
+              java11Runtime: 'TOMCAT|8.5.41-java11',
+              java8Runtime: 'TOMCAT|8.5.41-java8', // Note (allisonm): For some reason this is java8 not jre8
+            },
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.37',
+          value: '8.5.37',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.34',
+          value: '8.5.34',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.31',
+          value: '8.5.31',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+        {
+          displayText: 'Tomcat 8.5.20',
+          value: '8.5.20',
+          containerSettings: {
+            javaLinuxSupport: {},
+          },
+        },
+      ],
     },
     {
       displayText: 'Tomcat 8.0',
