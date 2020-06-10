c819897372a3bafd0d81c89a1910735dbf789b77
Reid Fortier <refortie@microsoft.com>
2020-06-10 13:13:21 -0700

    Generate a unique app setting connection name

--- client-react/src/pages/app/functions/common/callout/Callout.properties.ts
+++ client-react/src/pages/app/functions/common/callout/Callout.properties.ts
@@ -2,6 +2,7 @@ import { IDropdownOption } from 'office-ui-fabric-react';
 
 export interface NewConnectionCalloutProps {
   resourceId: string;
+  appSettingKeys: string[];
   setNewAppSetting: React.Dispatch<React.SetStateAction<{ key: string; value: string }>>;
   setSelectedItem: React.Dispatch<React.SetStateAction<IDropdownOption | undefined>>;
   setIsDialogVisible: React.Dispatch<React.SetStateAction<boolean>>;
