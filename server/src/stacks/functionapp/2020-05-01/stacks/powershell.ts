f524707894f750a485c5b59b9374c2add68029dd
Shibani Medhekar <shibanimedhekar@MININT-VLD9T6P.redmond.corp.microsoft.com>
2020-06-15 19:20:13 -0700

    Updating FunctionApp stack to add Powershell 7 support

--- server/src/stacks/functionapp/2020-05-01/stacks/powershell.ts
+++ server/src/stacks/functionapp/2020-05-01/stacks/powershell.ts
@@ -7,7 +7,30 @@ export const powershellStack: FunctionAppStack = {
   versions: [
     {
       sortOrder: 0,
-      displayText: '6',
+      displayText: '7.0',
+      value: '7',
+      isDefault: true,
+      supportedPlatforms: [
+        {
+          sortOrder: 0,
+          os: 'windows',
+          isPreview: false,
+          isDeprecated: false,
+          isHidden: false,
+          applicationInsightsEnabled: true,
+          runtimeVersion: '~7',
+          appSettingsDictionary: {
+            FUNCTIONS_WORKER_RUNTIME: 'powershell',
+          },
+          siteConfigPropertiesDictionary: {
+            PowerShellVersion: '~7',
+          },
+        },
+      ],
+    },
+    {
+      sortOrder: 1,
+      displayText: '6.2',
       value: '6',
       isDefault: true,
       supportedPlatforms: [
