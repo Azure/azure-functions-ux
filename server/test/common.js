13af27d65d5da47db775e231ca4dd467b79e6bc1
Allison Manifold <allisonm@microsoft.com>
2020-06-17 17:37:55 -0700

    [Stacks][2020-06-01] Add web app tests (#5087)
    * add first stack server tests
    
    * flush out stack test
    
    * clean-up
    
    * move to version folder
    
    * test yaml file
    
    * nohup
    
    * background task
    
    * no rescursive
    
    * just test with mocha
    
    * try ci
    
    * node10?
    
    * test host is running
    
    * localhost
    
    * start
    
    * server dies
    
    * run all tests

---
+++ server/test/common.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+const localhost = 'https://localhost:44300';
+
+describe('Basic server tests', () => {
+  // Test simple success test
+  describe('test expect', () => {
+    it('should not fail', done => {
+      expect(5).to.equal(5);
+      done();
+    });
+  });
+
+  // Test the server is running
+  describe('/GET localhost', () => {
+    it('should ensure the host is running', done => {
+      chai
+        .request(localhost)
+        .get('/')
+        .then(res => {
+          expect(res).to.have.status(200);
+          done();
+        })
+        .catch(err => {
+          done(err);
+        });
+    });
+  });
+});
