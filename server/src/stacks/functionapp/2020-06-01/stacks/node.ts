4ca2b5f0bcad335a90f598641a1c9d3ea45c4d12
Allison Manifold <allisonm@microsoft.com>
2020-06-22 12:39:27 -0700

    add node major versions

--- server/src/stacks/functionapp/2020-06-01/stacks/node.ts
+++ server/src/stacks/functionapp/2020-06-01/stacks/node.ts
@@ -1,7 +1,23 @@
 import { FunctionAppStack } from '../stack.model';
 
 export const nodeStack: FunctionAppStack = {
-  displayText: 'Node',
+  displayText: 'Node.js',
   value: 'node',
-  majorVersions: [],
+  majorVersions: [
+    {
+      displayText: 'Node.js 12',
+      value: '12',
+      minorVersions: [],
+    },
+    {
+      displayText: 'Node.js 10',
+      value: '10',
+      minorVersions: [],
+    },
+    {
+      displayText: 'Node.js 8',
+      value: '8',
+      minorVersions: [],
+    },
+  ],
 };
