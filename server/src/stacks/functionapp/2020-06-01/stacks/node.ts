52d4dc34a025e2301830e1459dd692397906f954
Allison Manifold <allisonm@microsoft.com>
2020-06-22 12:53:31 -0700

    update node 12 lts minor version

--- server/src/stacks/functionapp/2020-06-01/stacks/node.ts
+++ server/src/stacks/functionapp/2020-06-01/stacks/node.ts
@@ -1,13 +1,63 @@
 import { FunctionAppStack } from '../stack.model';
 
+const node12EOL = new Date(2022, 4, 1);
+const node10EOL = new Date(2021, 4, 1);
+const node8EOL = new Date(2019, 12, 31);
+
 export const nodeStack: FunctionAppStack = {
   displayText: 'Node.js',
   value: 'node',
+  preferredOs: 'windows',
   majorVersions: [
     {
       displayText: 'Node.js 12',
       value: '12',
-      minorVersions: [],
+      minorVersions: [
+        {
+          displayText: 'Node.js 12 LTS',
+          value: '12 LTS',
+          stackSettings: {
+            windowsRuntimeSettings: {
+              runtimeVersion: '~12',
+              remoteDebuggingSupported: false,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              gitHubActionSettings: {
+                isSupported: true,
+                supportedVersion: '12.x',
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'node',
+                WEBSITE_NODE_DEFAULT_VERSION: '~12',
+              },
+              siteConfigPropertiesDictionary: {},
+              supportedFunctionsExtensionVersions: ['~3'],
+              endOfLifeDate: node12EOL,
+            },
+            linuxRuntimeSettings: {
+              runtimeVersion: 'Node|12',
+              remoteDebuggingSupported: false,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              gitHubActionSettings: {
+                isSupported: true,
+                supportedVersion: '10.x',
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'node',
+              },
+              siteConfigPropertiesDictionary: {
+                Use32BitWorkerProcess: false,
+                linuxFxVersion: 'Node|12',
+              },
+              supportedFunctionsExtensionVersions: ['~3'],
+              endOfLifeDate: node12EOL,
+            },
+          },
+        },
+      ],
     },
     {
       displayText: 'Node.js 10',
