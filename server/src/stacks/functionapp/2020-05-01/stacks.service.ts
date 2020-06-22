5bf33afd823021006ae5e9a431ad44491f4129b3
shimedh <shimedh@microsoft.com>
2020-06-22 11:00:12 -0700

    Update FunctionApp stacks to include Java 11 (#5103)
    Co-authored-by: Shibani Medhekar <shibanimedhekar@RobbiepSurfaceL.redmond.corp.microsoft.com>
    Co-authored-by: Allison Manifold <allisonm@microsoft.com>

--- server/src/stacks/functionapp/2020-05-01/stacks.service.ts
+++ server/src/stacks/functionapp/2020-05-01/stacks.service.ts
@@ -4,11 +4,12 @@ import { dotnetCoreStack } from './stacks/dotnetCore';
 import { nodeStack } from './stacks/node';
 import { pythonStack } from './stacks/python';
 import { java8Stack } from './stacks/java8';
+import { java11Stack } from './stacks/java11';
 import { powershellStack } from './stacks/powershell';
 
 @Injectable()
 export class FunctionAppStacksService20200501 {
   getStacks(): FunctionAppStack[] {
-    return [dotnetCoreStack, nodeStack, pythonStack, java8Stack, powershellStack];
+    return [dotnetCoreStack, nodeStack, pythonStack, java11Stack, java8Stack, powershellStack];
   }
 }
