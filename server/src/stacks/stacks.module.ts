f3986d0d1fcfe2542463b59bd04e3f81459a4162
Allison Manifold <allisonm@microsoft.com>
2020-06-23 15:10:19 -0700

    [Stacks][Function App][2020-06-01] Expose 2020-06-01 Function App Stack Version (#5117)
    * [Stacks][Function App][2020-06-01] Add Model, .NET Core Stack, Node Sack, Python Stack (#5104)
    
    * add model and dotnetCore
    
    * add supportedPlatforms
    
    * supportedFunctionsExtensionVersions
    
    * update values
    
    * update runtimeVersion windows
    
    * add node stack base
    
    * add node major versions
    
    * update node 12 lts minor version
    
    * add node 10
    
    * add node 8
    
    * update value
    
    * add python file
    
    * add python linux values
    
    * [Stacks][Function App][2020-06-01] Add .NET Core 2.2 (#5111)
    
    * update model
    
    * update model positioning
    
    * [Stacks][Function App][2020-06-01] Add Java Stack (#5108)
    
    * add java base file
    
    * add java 11 info
    
    * add java 8
    
    * add windows siteconfigproperties
    
    * Stacks][Function App][2020-06-01] Add PowerShell Stack (#5112)
    
    * [Stacks][Function App][2020-06-01] Add Stacks Service, Update Controller, Add Tests (#5115)
    
    * add service
    
    * expose service to controller
    
    * rearrange properties
    
    * add tests
    
    * add node 6 for v1
    
    * add dotnet framework stack for v1
    
    * update powershell text
    
    * removing isdeprecated and EOL dates

--- server/src/stacks/stacks.module.ts
+++ server/src/stacks/stacks.module.ts
@@ -4,10 +4,11 @@ import { StacksController } from './stacks.controller';
 import { WebAppStacksService20200501 } from './webapp/2020-05-01/stacks.service';
 import { FunctionAppStacksService20200501 } from './functionapp/2020-05-01/stacks.service';
 import { WebAppStacksService20200601 } from './webapp/2020-06-01/stacks.service';
+import { FunctionAppStacksService20200601 } from './functionapp/2020-06-01/stacks.service';
 
 @Module({
   imports: [SharedModule],
   controllers: [StacksController],
-  providers: [WebAppStacksService20200501, FunctionAppStacksService20200501, WebAppStacksService20200601],
+  providers: [WebAppStacksService20200501, FunctionAppStacksService20200501, WebAppStacksService20200601, FunctionAppStacksService20200601],
 })
 export class StacksModule {}
