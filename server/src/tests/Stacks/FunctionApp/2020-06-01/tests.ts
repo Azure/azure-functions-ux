5ac0aadf3646151ed9f6131ec3b400b6bfdee45f
Allison Manifold <allisonm@microsoft.com>
2020-06-22 21:37:25 -0700

    add tests

---
+++ server/src/tests/Stacks/FunctionApp/2020-06-01/tests.ts
@@ -0,0 +1,67 @@
+import { FunctionAppStacksService20200601 } from '../../../../stacks/functionapp/2020-06-01/stacks.service';
+import {
+  validateAllStackLength,
+  validateDotnetStack,
+  validateNodeStack,
+  validatePythonStack,
+  validateJavaStack,
+  validatePowershellStack,
+} from './validations';
+
+const functionAppStacksService = new FunctionAppStacksService20200601();
+
+describe('FunctionApp Stacks Test 2020-05-01', () => {
+  // Test length of all stacks
+  describe('Test all stack length', () => {
+    it('should validate all stacks are returned', done => {
+      const stacks = functionAppStacksService.getStacks();
+      validateAllStackLength(stacks);
+      done();
+    });
+  });
+
+  // Test .NET Core stack
+  describe('Test the .NET Core stack', () => {
+    it('should validate the .NET Core stack', done => {
+      const stacks = functionAppStacksService.getStacks();
+      validateDotnetStack(stacks);
+      done();
+    });
+  });
+
+  // Test Node stack
+  describe('Test the Node.js stack', () => {
+    it('should validate the Node.js stack', done => {
+      const stacks = functionAppStacksService.getStacks();
+      validateNodeStack(stacks);
+      done();
+    });
+  });
+
+  // Test Python stack
+  describe('Test the Python stack', () => {
+    it('should validate the Python stack', done => {
+      const stacks = functionAppStacksService.getStacks();
+      validatePythonStack(stacks);
+      done();
+    });
+  });
+
+  // Test Java stack
+  describe('Test the Java stack', () => {
+    it('should validate the Java stack', done => {
+      const stacks = functionAppStacksService.getStacks();
+      validateJavaStack(stacks);
+      done();
+    });
+  });
+
+  // Test PowerShell Core stack
+  describe('Test the PowerShell Core stack', () => {
+    it('should validate the PowerShell Core stack', done => {
+      const stacks = functionAppStacksService.getStacks();
+      validatePowershellStack(stacks);
+      done();
+    });
+  });
+});
