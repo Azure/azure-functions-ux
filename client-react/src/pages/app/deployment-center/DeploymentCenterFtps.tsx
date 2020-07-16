752edfad393df653194bce524e435085e87f8082
Kartikeya Kandula <kandula.kartikeya@gmail.com>
2020-07-16 00:47:43 -0400

    refresh on ftps settings updated to hide content while refreshing and only show progress bar

--- client-react/src/pages/app/deployment-center/DeploymentCenterFtps.tsx
+++ client-react/src/pages/app/deployment-center/DeploymentCenterFtps.tsx
@@ -71,134 +71,137 @@ const DeploymentCenterFtps: React.FC<
 
   return (
     <>
-      {isLoading && getProgressIndicator()}
-      <div className={deploymentCenterContent}>
-        {deploymentCenterContext && !deploymentCenterContext.hasWritePermission && (
-          <CustomBanner message={t('deploymentCenterFtpsWritePermissionRequired')} type={MessageBarType.blocked} />
-        )}
+      {isLoading ? (
+        getProgressIndicator()
+      ) : (
+        <div className={deploymentCenterContent}>
+          {deploymentCenterContext && !deploymentCenterContext.hasWritePermission && (
+            <CustomBanner message={t('deploymentCenterFtpsWritePermissionRequired')} type={MessageBarType.blocked} />
+          )}
 
-        <p>{t('deploymentCenterFtpsDescription')}</p>
-        <TextFieldNoFormik
-          id="deployment-center-ftps-endpoint"
-          label={t('deploymentCenterFtpsEndpointLabel')}
-          widthOverride="100%"
-          value={ftpsEndpoint}
-          copyButton={true}
-          disabled={true}
-        />
+          <p>{t('deploymentCenterFtpsDescription')}</p>
+          <TextFieldNoFormik
+            id="deployment-center-ftps-endpoint"
+            label={t('deploymentCenterFtpsEndpointLabel')}
+            widthOverride="100%"
+            value={ftpsEndpoint}
+            copyButton={true}
+            disabled={true}
+          />
 
-        <h3>{t('deploymentCenterFtpsApplicationScopeTitle')}</h3>
-        <p>{t('deploymentCenterFtpsApplicationScopeDescription')}</p>
+          <h3>{t('deploymentCenterFtpsApplicationScopeTitle')}</h3>
+          <p>{t('deploymentCenterFtpsApplicationScopeDescription')}</p>
 
-        <TextFieldNoFormik
-          id="deployment-center-ftps-application-username"
-          label={t('deploymentCenterFtpsUsernameLabel')}
-          widthOverride="100%"
-          value={publishingProfile && publishingProfile.userName}
-          copyButton={true}
-          disabled={true}
-        />
+          <TextFieldNoFormik
+            id="deployment-center-ftps-application-username"
+            label={t('deploymentCenterFtpsUsernameLabel')}
+            widthOverride="100%"
+            value={publishingProfile && publishingProfile.userName}
+            copyButton={true}
+            disabled={true}
+          />
 
-        <TextFieldNoFormik
-          id="deployment-center-ftps-application-password"
-          label={t('deploymentCenterFtpsPasswordLabel')}
-          widthOverride="100%"
-          value={publishingProfile && publishingProfile.userPWD}
-          copyButton={true}
-          disabled={true}
-          type={applicationPasswordType}
-          additionalControls={[
-            <ActionButton
-              id="deployment-center-ftps-application-password-visibility-toggle"
-              key="deployment-center-ftps-application-password-visibility-toggle"
-              className={additionalTextFieldControl}
-              ariaLabel={
-                applicationPasswordType === 'password' ? t('showApplicationPasswordAriaLabel') : t('hideApplicationPasswordAriaLabel')
-              }
-              onClick={toggleShowApplicationPassword}
-              iconProps={{ iconName: applicationPasswordType === 'password' ? 'RedEye' : 'Hide' }}>
-              {applicationPasswordType === 'password' ? t('show') : t('hide')}
-            </ActionButton>,
-            <ActionButton
-              id="deployment-center-ftps-application-password-reset"
-              key="deployment-center-ftps-application-password-reset"
-              className={additionalTextFieldControl}
-              ariaLabel={t('resetPublishProfileAriaLabel')}
-              onClick={toggleResetCalloutVisibility}
-              iconProps={{ iconName: 'refresh' }}>
-              {t('reset')}
-            </ActionButton>,
-          ]}
-        />
+          <TextFieldNoFormik
+            id="deployment-center-ftps-application-password"
+            label={t('deploymentCenterFtpsPasswordLabel')}
+            widthOverride="100%"
+            value={publishingProfile && publishingProfile.userPWD}
+            copyButton={true}
+            disabled={true}
+            type={applicationPasswordType}
+            additionalControls={[
+              <ActionButton
+                id="deployment-center-ftps-application-password-visibility-toggle"
+                key="deployment-center-ftps-application-password-visibility-toggle"
+                className={additionalTextFieldControl}
+                ariaLabel={
+                  applicationPasswordType === 'password' ? t('showApplicationPasswordAriaLabel') : t('hideApplicationPasswordAriaLabel')
+                }
+                onClick={toggleShowApplicationPassword}
+                iconProps={{ iconName: applicationPasswordType === 'password' ? 'RedEye' : 'Hide' }}>
+                {applicationPasswordType === 'password' ? t('show') : t('hide')}
+              </ActionButton>,
+              <ActionButton
+                id="deployment-center-ftps-application-password-reset"
+                key="deployment-center-ftps-application-password-reset"
+                className={additionalTextFieldControl}
+                ariaLabel={t('resetPublishProfileAriaLabel')}
+                onClick={toggleResetCalloutVisibility}
+                iconProps={{ iconName: 'refresh' }}>
+                {t('reset')}
+              </ActionButton>,
+            ]}
+          />
 
-        <CustomFocusTrapCallout
-          target="#deployment-center-ftps-application-password-reset"
-          onDismissFunction={toggleResetCalloutVisibility}
-          setInitialFocus={true}
-          hidden={isResetCalloutHidden}
-          title={t('resetPublishProfileConfirmationTitle')}
-          description={t('resetPublishProfileConfirmationDescription')}
-          primaryButtonTitle={t('reset')}
-          primaryButtonFunction={resetApplicationPasswordFromCallout}
-          defaultButtonTitle={t('cancel')}
-          defaultButtonFunction={toggleResetCalloutVisibility}
-        />
+          <CustomFocusTrapCallout
+            target="#deployment-center-ftps-application-password-reset"
+            onDismissFunction={toggleResetCalloutVisibility}
+            setInitialFocus={true}
+            hidden={isResetCalloutHidden}
+            title={t('resetPublishProfileConfirmationTitle')}
+            description={t('resetPublishProfileConfirmationDescription')}
+            primaryButtonTitle={t('reset')}
+            primaryButtonFunction={resetApplicationPasswordFromCallout}
+            defaultButtonTitle={t('cancel')}
+            defaultButtonFunction={toggleResetCalloutVisibility}
+          />
 
-        <h3>{t('deploymentCenterFtpsUserScopeTitle')}</h3>
-        <p>{t('deploymentCenterFtpsUserScopeDescription').format(sampleWebProviderDomainUsername)}</p>
+          <h3>{t('deploymentCenterFtpsUserScopeTitle')}</h3>
+          <p>{t('deploymentCenterFtpsUserScopeDescription').format(sampleWebProviderDomainUsername)}</p>
 
-        <Field
-          id="deployment-center-ftps-provider-username"
-          name="publishingUsername"
-          component={TextField}
-          widthOverride="60%"
-          label={t('deploymentCenterFtpsUsernameLabel')}
-        />
+          <Field
+            id="deployment-center-ftps-provider-username"
+            name="publishingUsername"
+            component={TextField}
+            widthOverride="60%"
+            label={t('deploymentCenterFtpsUsernameLabel')}
+          />
 
-        <Field
-          id="deployment-center-ftps-provider-password"
-          name="publishingPassword"
-          component={TextField}
-          widthOverride="60%"
-          label={t('deploymentCenterFtpsPasswordLabel')}
-          type={providerPasswordType}
-          additionalControls={[
-            <ActionButton
-              id="deployment-center-ftps-provider-password-visibility-toggle"
-              key="deployment-center-ftps-provider-password-visibility-toggle"
-              className={additionalTextFieldControl}
-              ariaLabel={providerPasswordType === 'password' ? t('showProviderPasswordAriaLabel') : t('hideProviderPasswordAriaLabel')}
-              onClick={toggleShowProviderPassword}
-              iconProps={{ iconName: providerPasswordType === 'password' ? 'RedEye' : 'Hide' }}>
-              {providerPasswordType === 'password' ? t('show') : t('hide')}
-            </ActionButton>,
-          ]}
-        />
+          <Field
+            id="deployment-center-ftps-provider-password"
+            name="publishingPassword"
+            component={TextField}
+            widthOverride="60%"
+            label={t('deploymentCenterFtpsPasswordLabel')}
+            type={providerPasswordType}
+            additionalControls={[
+              <ActionButton
+                id="deployment-center-ftps-provider-password-visibility-toggle"
+                key="deployment-center-ftps-provider-password-visibility-toggle"
+                className={additionalTextFieldControl}
+                ariaLabel={providerPasswordType === 'password' ? t('showProviderPasswordAriaLabel') : t('hideProviderPasswordAriaLabel')}
+                onClick={toggleShowProviderPassword}
+                iconProps={{ iconName: providerPasswordType === 'password' ? 'RedEye' : 'Hide' }}>
+                {providerPasswordType === 'password' ? t('show') : t('hide')}
+              </ActionButton>,
+            ]}
+          />
 
-        <Field
-          id="deployment-center-ftps-provider-confirm-password"
-          name="publishingConfirmPassword"
-          component={TextField}
-          widthOverride="60%"
-          label={t('deploymentCenterFtpsConfirmPasswordLabel')}
-          type={providerConfirmPasswordType}
-          additionalControls={[
-            <ActionButton
-              id="deployment-center-ftps-provider-confirm-password-visibility-toggle"
-              key="deployment-center-ftps-provider-confirm-password-visibility-toggle"
-              className={additionalTextFieldControl}
-              ariaLabel={
-                providerConfirmPasswordType === 'password'
-                  ? t('showProviderConfirmPasswordAriaLabel')
-                  : t('hideProviderConfirmPasswordAriaLabel')
-              }
-              onClick={toggleShowProviderConfirmPassword}
-              iconProps={{ iconName: providerConfirmPasswordType === 'password' ? 'RedEye' : 'Hide' }}>
-              {providerConfirmPasswordType === 'password' ? t('show') : t('hide')}
-            </ActionButton>,
-          ]}
-        />
-      </div>
+          <Field
+            id="deployment-center-ftps-provider-confirm-password"
+            name="publishingConfirmPassword"
+            component={TextField}
+            widthOverride="60%"
+            label={t('deploymentCenterFtpsConfirmPasswordLabel')}
+            type={providerConfirmPasswordType}
+            additionalControls={[
+              <ActionButton
+                id="deployment-center-ftps-provider-confirm-password-visibility-toggle"
+                key="deployment-center-ftps-provider-confirm-password-visibility-toggle"
+                className={additionalTextFieldControl}
+                ariaLabel={
+                  providerConfirmPasswordType === 'password'
+                    ? t('showProviderConfirmPasswordAriaLabel')
+                    : t('hideProviderConfirmPasswordAriaLabel')
+                }
+                onClick={toggleShowProviderConfirmPassword}
+                iconProps={{ iconName: providerConfirmPasswordType === 'password' ? 'RedEye' : 'Hide' }}>
+                {providerConfirmPasswordType === 'password' ? t('show') : t('hide')}
+              </ActionButton>,
+            ]}
+          />
+        </div>
+      )}
     </>
   );
 };
