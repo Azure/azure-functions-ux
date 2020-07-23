21dc2e95825b1f9f19310b862a5549ba74c9843d
Krrish Mittal <takyyon@DESKTOP-9975RNV.corp.microsoft.com>
2020-07-23 12:13:38 -0700

    webappStackSettings component created

--- client-react/src/pages/app/app-settings/GeneralSettings/Stacks.tsx
+++ client-react/src/pages/app/app-settings/GeneralSettings/Stacks.tsx
@@ -1,14 +1,11 @@
 import { FormikProps } from 'formik';
 import React from 'react';
-
 import { ScenarioIds } from '../../../../utils/scenario-checker/scenario-ids';
 import { ScenarioService } from '../../../../utils/scenario-checker/scenario.service';
 import { AppSettingsFormValues } from '../AppSettings.types';
-import { settingsWrapper } from '../AppSettingsForm';
-import LinuxStacks from './LinuxStacks/LinuxStacks';
-import WindowsStacks from './WindowsStacks/WindowsStacks';
 import { useTranslation } from 'react-i18next';
 import FunctionAppStackSettings from './stacks/function-app/FunctionAppStackSettings';
+import WebAppStackSettings from './stacks/web-app/WebAppStackSettings';
 
 const Stacks: React.SFC<FormikProps<AppSettingsFormValues>> = props => {
   const { values } = props;
@@ -18,28 +15,8 @@ const Stacks: React.SFC<FormikProps<AppSettingsFormValues>> = props => {
 
   if (scenarioService.checkScenario(ScenarioIds.functionAppRuntimeStack, { site }).status === 'enabled') {
     return <FunctionAppStackSettings {...props} />;
-  } else if (scenarioService.checkScenario(ScenarioIds.linuxAppStack, { site }).status === 'enabled') {
-    return (
-      <>
-        <h3>{t('stackSettings')}</h3>
-        <div className={settingsWrapper}>
-          <LinuxStacks {...props} />
-        </div>
-      </>
-    );
-  } else if (scenarioService.checkScenario(ScenarioIds.xenonAppRuntimeStack, { site }).status === 'disabled') {
-    return null;
   } else {
-    // NOTE(michinoy): Always default to the windows based runtime stack.
-
-    return (
-      <>
-        <h3>{t('stackSettings')}</h3>
-        <div className={settingsWrapper}>
-          <WindowsStacks {...props} />
-        </div>
-      </>
-    );
+    return <WebAppStackSettings {...props} />;
   }
 };
 
