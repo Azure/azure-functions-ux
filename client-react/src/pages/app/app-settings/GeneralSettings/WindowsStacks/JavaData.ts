51abce171a9295962f428c765d4d14de01646e82
Krrish Mittal <takyyon@DESKTOP-9975RNV.corp.microsoft.com>
2020-07-28 11:23:23 -0700

    consider version if windows present

--- client-react/src/pages/app/app-settings/GeneralSettings/WindowsStacks/JavaData.ts
+++ client-react/src/pages/app/app-settings/GeneralSettings/WindowsStacks/JavaData.ts
@@ -40,10 +40,22 @@ export const getJavaMinorVersionObject = (javaStack: WebAppStack, selectedJavaVe
 };
 
 export const getJavaMajorVersionAsDropdownOptions = (javaStack: WebAppStack, osType?: AppStackOs): IDropdownOption[] => {
-  return javaStack.majorVersions.map(x => ({
-    key: x.value,
-    text: x.displayText,
-  }));
+  const options: IDropdownOption[] = [];
+  javaStack.majorVersions.forEach(javaStackMajorVersion => {
+    let windowsRuntimeCount = 0;
+    javaStackMajorVersion.minorVersions.forEach(javaStackMinorVersion => {
+      if (javaStackMinorVersion.stackSettings.windowsRuntimeSettings) {
+        windowsRuntimeCount += 1;
+      }
+    });
+    if (windowsRuntimeCount > 0) {
+      options.push({
+        key: javaStackMajorVersion.value,
+        text: javaStackMajorVersion.displayText,
+      });
+    }
+  });
+  return options;
 };
 
 export const getJavaMinorVersionAsDropdownOptions = (
@@ -52,27 +64,44 @@ export const getJavaMinorVersionAsDropdownOptions = (
   t: i18next.TFunction
 ): IDropdownOption[] => {
   const currentJavaMajorVersionDetails = javaStack.majorVersions.find(x => x.value === currentJavaMajorVersion);
-  return (
-    (!!currentJavaMajorVersionDetails &&
-      currentJavaMajorVersionDetails.minorVersions.map(x => ({
-        key: x.stackSettings.windowsRuntimeSettings ? x.stackSettings.windowsRuntimeSettings.runtimeVersion : x.value,
-        text: `${
-          x.stackSettings.windowsRuntimeSettings && x.stackSettings.windowsRuntimeSettings.isAutoUpdate
-            ? t('stackVersionAutoUpdate').format(x.displayText)
-            : x.displayText
-        }`,
-      }))) ||
-    []
-  );
+  const options: IDropdownOption[] = [];
+  if (!!currentJavaMajorVersionDetails) {
+    currentJavaMajorVersionDetails.minorVersions.forEach(minorVersion => {
+      if (minorVersion.stackSettings.windowsRuntimeSettings) {
+        options.push({
+          key: minorVersion.stackSettings.windowsRuntimeSettings.runtimeVersion
+            ? minorVersion.stackSettings.windowsRuntimeSettings.runtimeVersion
+            : minorVersion.value,
+          text: `${
+            minorVersion.stackSettings.windowsRuntimeSettings.isAutoUpdate
+              ? t('stackVersionAutoUpdate').format(minorVersion.displayText)
+              : minorVersion.displayText
+          }`,
+        });
+      }
+    });
+  }
+  return options;
 };
 
-export const getJavaContainersOptions = (javaContainers: WebAppStack): IDropdownOption[] =>
-  javaContainers.majorVersions.map(x => {
-    return {
-      key: getJavaContainerKey(x),
-      text: x.displayText,
-    };
+export const getJavaContainersOptions = (javaContainers: WebAppStack): IDropdownOption[] => {
+  const options: IDropdownOption[] = [];
+  javaContainers.majorVersions.forEach(javaContainerMajorVersion => {
+    let windowsContainerCount = 0;
+    javaContainerMajorVersion.minorVersions.forEach(javaContainerMinorVersion => {
+      if (javaContainerMinorVersion.stackSettings.windowsContainerSettings) {
+        windowsContainerCount += 1;
+      }
+    });
+    if (windowsContainerCount > 0) {
+      options.push({
+        key: getJavaContainerKey(javaContainerMajorVersion),
+        text: javaContainerMajorVersion.displayText,
+      });
+    }
   });
+  return options;
+};
 
 export const getFrameworkVersionOptions = (
   javaContainers: WebAppStack,
@@ -95,10 +124,12 @@ export const getFrameworkVersionOptions = (
 };
 
 const getJavaContainerKey = (javaContainerMajorVersion: AppStackMajorVersion<any>) => {
-  return javaContainerMajorVersion.minorVersions.length > 0 &&
-    javaContainerMajorVersion.minorVersions[0].stackSettings.windowsContainerSettings
-    ? javaContainerMajorVersion.minorVersions[0].stackSettings.windowsContainerSettings.javaContainer
-    : javaContainerMajorVersion.value;
+  for (let i = 0; i < javaContainerMajorVersion.minorVersions.length; ++i) {
+    if (javaContainerMajorVersion.minorVersions[i].stackSettings.windowsContainerSettings) {
+      return javaContainerMajorVersion.minorVersions[i].stackSettings.windowsContainerSettings.javaContainer;
+    }
+  }
+  return javaContainerMajorVersion.value;
 };
 
 const getLatestNonPreviewJavaMajorMinorVersion = (javaStack: WebAppStack) => {
