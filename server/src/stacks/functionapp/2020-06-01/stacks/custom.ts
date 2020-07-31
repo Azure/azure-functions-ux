b37988087691782b4555649210834af0a57f8485
Pragna Gopa <pgopa@microsoft.com>
2020-07-31 14:32:24 -0700

    [Stacks][Custom] Add custom to Functions Stacks (#5201)
    * Update tests
    
    * Addressing CR
    
    * Set preview flag
    
    * set linuxFxVersion to emtpy
    
    * Fix tests
    
    Co-authored-by: Allison Manifold <allisonm@microsoft.com>

---
+++ server/src/stacks/functionapp/2020-06-01/stacks/custom.ts
@@ -0,0 +1,58 @@
+import { FunctionAppStack } from '../stack.model';
+
+export const customStack: FunctionAppStack = {
+  displayText: 'Custom',
+  value: 'custom',
+  preferredOs: 'windows',
+  majorVersions: [
+    {
+      displayText: 'Custom',
+      value: 'custom',
+      minorVersions: [
+        {
+          displayText: 'Custom',
+          value: 'custom',
+          stackSettings: {
+            windowsRuntimeSettings: {
+              runtimeVersion: 'custom',
+              isPreview: true,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              remoteDebuggingSupported: false,
+              gitHubActionSettings: {
+                isSupported: false
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'custom',
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: true,
+              },
+              supportedFunctionsExtensionVersions: ['~3', '~2'],
+            },
+            linuxRuntimeSettings: {
+              runtimeVersion: '',
+              isPreview: true,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              remoteDebuggingSupported: false,
+              gitHubActionSettings: {
+                isSupported: false,
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'custom'
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: false,
+                linuxFxVersion: '',
+              },
+              supportedFunctionsExtensionVersions: ['~3', '~2'],
+            },
+          },
+        },
+      ],
+    }
+  ],
+};
