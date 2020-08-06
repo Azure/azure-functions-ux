5b4f9c3219d5fa58b1a7ad9f8ea88f7724c92df8
Stephanie Pellegrino <stpelleg@microsoft.com>
2020-08-05 17:24:52 -0700

    PR updates associated with storage mounts dirty state in column

--- client-react/src/pages/app/app-settings/AzureStorageMounts/AzureStorageMounts.tsx
+++ client-react/src/pages/app/app-settings/AzureStorageMounts/AzureStorageMounts.tsx
@@ -1,7 +1,6 @@
 import { FormikProps } from 'formik';
 import { DetailsListLayoutMode, SelectionMode, IColumn } from 'office-ui-fabric-react/lib/DetailsList';
 import React, { useContext, useState } from 'react';
-import { withTranslation, WithTranslation } from 'react-i18next';
 import { defaultCellStyle } from '../../../../components/DisplayTableWithEmptyMessage/DisplayTableWithEmptyMessage';
 import { AppSettingsFormValues, FormAzureStorageMounts } from '../AppSettings.types';
 import IconButton from '../../../../components/IconButton/IconButton';
@@ -15,18 +14,23 @@ import CustomPanel from '../../../../components/CustomPanel/CustomPanel';
 import CustomBanner from '../../../../components/CustomBanner/CustomBanner';
 import { ThemeContext } from '../../../../ThemeContext';
 import { dirtyElementStyle } from '../AppSettings.styles';
+import { ArmObj } from '../../../../models/arm-obj';
+import { useTranslation } from 'react-i18next';
+import { AppKind } from '../../../../utils/AppKind';
+import { Site } from '../../../../models/site/site';
 
-const AzureStorageMounts: React.FC<FormikProps<AppSettingsFormValues> & WithTranslation> = props => {
+const AzureStorageMounts: React.FC<FormikProps<AppSettingsFormValues>> = props => {
   const { app_write, editable, saving } = useContext(PermissionsContext);
-  const disableAllControls = !app_write || !editable || saving;
   const theme = useContext(ThemeContext);
+  const disableAllControls = !app_write || !editable || saving;
 
   const [showPanel, setShowPanel] = useState(false);
   const [currentAzureStorageMount, setCurrentAzureStorageMount] = useState<FormAzureStorageMounts | null>(null);
   const [currentItemIndex, setCurrentItemIndex] = useState<number | undefined>(undefined);
   const [createNewItem, setCreateNewItem] = useState(false);
 
-  const { values, t } = props;
+  const { values } = props;
+  const { t } = useTranslation();
 
   const getCommandBarItems = (): ICommandBarItemProps[] => {
     return [
@@ -52,14 +56,14 @@ const AzureStorageMounts: React.FC<FormikProps<AppSettingsFormValues> & WithTran
     setShowPanel(true);
     setCurrentAzureStorageMount(blankAzureStorageMount);
     setCreateNewItem(true);
-    setCurrentItemIndex(-1);
+    setCurrentItemIndex(undefined);
   };
 
   const onClosePanel = (item: FormAzureStorageMounts): void => {
     const azureStorageMountsItem = values.azureStorageMounts || [];
     const azureStorageMounts = [...azureStorageMountsItem];
-    if (!createNewItem) {
-      azureStorageMounts[currentItemIndex!] = item;
+    if (!createNewItem && currentItemIndex) {
+      azureStorageMounts[currentItemIndex] = item;
     } else {
       azureStorageMounts.push(item);
     }
@@ -107,8 +111,7 @@ const AzureStorageMounts: React.FC<FormikProps<AppSettingsFormValues> & WithTran
   const isAzureStorageMountDirty = (index: number): boolean => {
     const initialstorageMounts = props.initialValues.azureStorageMounts || [];
     const currentRow = values.azureStorageMounts[index] || null;
-    const initialstorageMountIndex = initialstorageMounts.findIndex(x => isAzureStorageMountEqual(x, currentRow));
-    return initialstorageMountIndex < 0;
+    return !!currentRow && initialstorageMounts.findIndex(x => isAzureStorageMountEqual(x, currentRow)) < 0;
   };
 
   const onRenderItemColumn = (item: FormAzureStorageMounts, index: number, column: IColumn) => {
@@ -249,37 +252,41 @@ const AzureStorageMounts: React.FC<FormikProps<AppSettingsFormValues> & WithTran
     ];
   };
 
-  if (!app_write) {
-    return <CustomBanner message={t('applicationSettingsNoPermission')} type={MessageBarType.warning} />;
-  }
+  const isXenonApp = (obj: ArmObj<Site>) => {
+    return AppKind.hasKinds(obj, ['xenon']);
+  };
 
-  return (
-    <>
-      <DisplayTableWithCommandBar
-        commandBarItems={getCommandBarItems()}
-        items={values.azureStorageMounts || []}
-        columns={getColumns()}
-        isHeaderVisible={true}
-        layoutMode={DetailsListLayoutMode.justified}
-        selectionMode={SelectionMode.none}
-        selectionPreservedOnEmptyClick={true}
-        emptyMessage={t('emptyAzureStorageMount')}
-      />
-      <CustomPanel
-        type={PanelType.medium}
-        isOpen={showPanel}
-        onDismiss={onCancel}
-        headerText={createNewItem ? t('newAzureStorageMount') : t('editAzureStorageMount')}>
-        <AzureStorageMountsAddEdit
-          azureStorageMount={currentAzureStorageMount!}
-          otherAzureStorageMounts={values.azureStorageMounts}
-          updateAzureStorageMount={onClosePanel.bind(onRenderItemColumn)}
-          closeBlade={onCancel.bind(onRenderItemColumn)}
-          enableValidation={!values.site.kind || !values.site.kind.includes('xenon')}
+  const getTable = () => {
+    return (
+      <>
+        <DisplayTableWithCommandBar
+          commandBarItems={getCommandBarItems()}
+          items={values.azureStorageMounts || []}
+          columns={getColumns()}
+          isHeaderVisible={true}
+          layoutMode={DetailsListLayoutMode.justified}
+          selectionMode={SelectionMode.none}
+          selectionPreservedOnEmptyClick={true}
+          emptyMessage={t('emptyAzureStorageMount')}
         />
-      </CustomPanel>
-    </>
-  );
+        <CustomPanel
+          type={PanelType.medium}
+          isOpen={showPanel}
+          onDismiss={onCancel}
+          headerText={createNewItem ? t('newAzureStorageMount') : t('editAzureStorageMount')}>
+          <AzureStorageMountsAddEdit
+            azureStorageMount={currentAzureStorageMount!}
+            otherAzureStorageMounts={values.azureStorageMounts}
+            updateAzureStorageMount={item => onClosePanel(item)}
+            closeBlade={onCancel}
+            enableValidation={!isXenonApp(values.site)}
+          />
+        </CustomPanel>
+      </>
+    );
+  };
+
+  return !app_write ? <CustomBanner message={t('applicationSettingsNoPermission')} type={MessageBarType.warning} /> : getTable();
 };
 
-export default withTranslation('translation')(AzureStorageMounts);
+export default AzureStorageMounts;
