fad0a28d402bff00eb185f88e7613018b688d52a
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2020-08-11 11:06:27 -0700

    Implement Scaffolding for Bitbucket Service  (#5229)
    * Implement Scaffolding for Bitbucket Service #7957842
    
    * Scaffolding for Bitbucket Service
    
    * removing async from calls

---
+++ client-react/src/ApiHelpers/BitbucketService.ts
@@ -0,0 +1,29 @@
+import Url from '../utils/url';
+import { HttpResponseObject } from '../ArmHelper.types';
+import { ProviderToken } from '../models/provider';
+import { BitbucketUser, BitbucketRepository, BitbucketBranch } from '../models/bitbucket';
+
+export default class BitbucketService {
+  public static authorizeUrl = `${Url.serviceHost}auth/bitbucket/authorize`;
+
+  public static getUser = (bitbucketToken: string): Promise<HttpResponseObject<BitbucketUser>> => {
+    throw Error('Not implemented');
+  };
+
+  public static getToken = (redirectUrl: string): Promise<HttpResponseObject<ProviderToken>> => {
+    throw Error('Not implemented');
+  };
+
+  public static getRepositories = (bitbucketToken: string, logger?: (page, response) => void): Promise<BitbucketRepository[]> => {
+    throw Error('Not implemented');
+  };
+
+  public static getBranches = (
+    org: string,
+    repo: string,
+    bitbucketToken: string,
+    logger?: (page, response) => void
+  ): Promise<BitbucketBranch[]> => {
+    throw Error('Not implemented');
+  };
+}
