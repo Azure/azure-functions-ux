2e1b294f6d3341cbc5e6d6790479cec67317fd41
Mitren Chinoy <michinoy@microsoft.com>
2020-08-16 16:20:19 -0700

    Refactor Deployment center

---
+++ client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeDataLoader.tsx
@@ -0,0 +1,98 @@
+import React, { useEffect, useContext, useState } from 'react';
+import { SiteStateContext } from '../../../../SiteState';
+import { DeploymentCenterContext } from '../DeploymentCenterContext';
+import { ArmArray } from '../../../../models/arm-obj';
+import {
+  DeploymentProperties,
+  DeploymentCenterFormData,
+  DeploymentCenterCodeFormData,
+  DeploymentCenterYupValidationSchemaType,
+  DeploymentCenterDataLoaderProps,
+} from '../DeploymentCenter.types';
+import DeploymentCenterData from '../DeploymentCenter.data';
+import { DeploymentCenterCodeFormBuilder } from '../code/DeploymentCenterCodeFormBuilder';
+import { useTranslation } from 'react-i18next';
+import { DeploymentCenterPublishingContext } from '../DeploymentCenterPublishingContext';
+import { getErrorMessage } from '../../../../ApiHelpers/ArmHelper';
+import DeploymentCenterCodeForm from './DeploymentCenterCodeForm';
+
+const DeploymentCenterCodeDataLoader: React.FC<DeploymentCenterDataLoaderProps> = props => {
+  const { resourceId } = props;
+  const { t } = useTranslation();
+
+  const siteStateContext = useContext(SiteStateContext);
+  const deploymentCenterContext = useContext(DeploymentCenterContext);
+  const deploymentCenterPublishingContext = useContext(DeploymentCenterPublishingContext);
+
+  const deploymentCenterData = new DeploymentCenterData();
+  const deploymentCenterCodeFormBuilder = new DeploymentCenterCodeFormBuilder(t);
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [deployments, setDeployments] = useState<ArmArray<DeploymentProperties> | undefined>(undefined);
+  const [deploymentsError, setDeploymentsError] = useState<string | undefined>(undefined);
+  const [codeFormData, setCodeFormData] = useState<DeploymentCenterFormData<DeploymentCenterCodeFormData> | undefined>(undefined);
+  const [codeFormValidationSchema, setCodeFormValidationSchema] = useState<
+    DeploymentCenterYupValidationSchemaType<DeploymentCenterCodeFormData> | undefined
+  >(undefined);
+
+  const fetchData = async () => {
+    const deploymentsResponse = await deploymentCenterData.getSiteDeployments(resourceId);
+
+    if (deploymentsResponse.metadata.success) {
+      setDeployments(deploymentsResponse.data);
+    } else {
+      const errorMessage = getErrorMessage(deploymentsResponse.metadata.error);
+      setDeploymentsError(
+        errorMessage ? t('deploymentCenterCodeDeploymentsFailedWithError').format(errorMessage) : t('deploymentCenterCodeDeploymentsFailed')
+      );
+    }
+
+    setIsLoading(false);
+  };
+
+  const generateForm = () => {
+    if (deploymentCenterContext.siteConfig) {
+      deploymentCenterCodeFormBuilder.setSiteConfig(deploymentCenterContext.siteConfig);
+    }
+
+    if (deploymentCenterContext.configMetadata) {
+      deploymentCenterCodeFormBuilder.setConfigMetadata(deploymentCenterContext.configMetadata);
+    }
+
+    if (deploymentCenterContext.applicationSettings) {
+      deploymentCenterCodeFormBuilder.setApplicationSettings(deploymentCenterContext.applicationSettings);
+    }
+
+    if (deploymentCenterPublishingContext.publishingUser) {
+      deploymentCenterCodeFormBuilder.setPublishingUser(deploymentCenterPublishingContext.publishingUser);
+    }
+
+    setCodeFormData(deploymentCenterCodeFormBuilder.generateFormData());
+    setCodeFormValidationSchema(deploymentCenterCodeFormBuilder.generateYupValidationSchema());
+  };
+
+  const refresh = () => {
+    setIsLoading(true);
+    fetchData();
+    deploymentCenterContext.refresh();
+  };
+
+  useEffect(() => {
+    fetchData();
+    generateForm();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [siteStateContext, deploymentCenterContext]);
+
+  return (
+    <DeploymentCenterCodeForm
+      deployments={deployments}
+      deploymentsError={deploymentsError}
+      formData={codeFormData}
+      formValidationSchema={codeFormValidationSchema}
+      isLoading={isLoading}
+      refresh={refresh}
+    />
+  );
+};
+
+export default DeploymentCenterCodeDataLoader;
