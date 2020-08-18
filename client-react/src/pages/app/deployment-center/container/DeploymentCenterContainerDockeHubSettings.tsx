af48fe1b49d1a12160224d679556dc12a5ff4d19
Mitren Chinoy <nertim@users.noreply.github.com>
2020-08-18 13:21:41 -0700

    Implement scaffolding of container type forms for single container option. (#5257)

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerDockeHubSettings.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerDockeHubSettings.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import { Field } from 'formik';
+import TextField from '../../../../components/form-controls/TextField';
+import { useTranslation } from 'react-i18next';
+import { IChoiceGroupOptionProps } from 'office-ui-fabric-react';
+import { ContainerDockerAccessTypes, DeploymentCenterFieldProps, DeploymentCenterContainerFormData } from '../DeploymentCenter.types';
+import Dropdown from '../../../../components/form-controls/DropDown';
 
-const DeploymentCenterContainerDockerHubSettings: React.FC<{}> = props => {
-  return <p>Docker Hub view</p>;
+const DeploymentCenterContainerDockerHubSettings: React.FC<DeploymentCenterFieldProps<DeploymentCenterContainerFormData>> = props => {
+  const { formProps } = props;
+  const { t } = useTranslation();
+
+  const accessTypes: IChoiceGroupOptionProps[] = [
+    {
+      key: ContainerDockerAccessTypes.public,
+      text: t('containerRepositoryPublic'),
+    },
+    {
+      key: ContainerDockerAccessTypes.private,
+      text: t('containerRepositoryPrivate'),
+    },
+  ];
+
+  return (
+    <>
+      <Field
+        id="container-dockerHub-imageAndTag"
+        name="imageAndTag"
+        component={TextField}
+        label={t('containerImageAndTag')}
+        placeholder={t('containerImageAndTagPlaceholder')}
+      />
+
+      <Field
+        id="container-dockerHub-accessType"
+        name="dockerAccessType"
+        component={Dropdown}
+        options={accessTypes}
+        label={t('containerRepositoryAccess')}
+      />
+
+      {formProps && formProps.values.dockerAccessType === ContainerDockerAccessTypes.private && (
+        <>
+          <Field id="container-dockerHub-username" name="username" component={TextField} label={t('containerLogin')} />
+
+          <Field id="container-dockerHub-password" name="password" component={TextField} label={t('containerPassword')} />
+        </>
+      )}
+
+      <Field id="container-dockerHub-startUpFile" name="command" component={TextField} label={t('containerStartupFile')} />
+    </>
+  );
 };
 
 export default DeploymentCenterContainerDockerHubSettings;
