4bcba70503e44e2b3740c823bbf74ca546d5a230
Krrish Mittal <krrishmittal15@gmail.com>
2020-08-19 14:39:12 -0700

    update headers (#5262)
    Co-authored-by: Krrish Mittal <takyyon@MININT-8UC1I0L.northamerica.corp.microsoft.com>

--- client/src/app/shared/services/function-app.service.ts
+++ client/src/app/shared/services/function-app.service.ts
@@ -161,7 +161,9 @@ export class FunctionAppService {
 
   saveApiProxy(context: FunctionAppContext, jsonString: string, runtimeVersion): Result<Response> {
     const headers = this._armService.getHeaders();
-    headers['Cache-Control'] = 'no-cache';
+    headers.delete('If-None-Match');
+    headers.append('Cache-Control', 'no-cache');
+    headers.append('If-Match', '*');
     const resourceId = `${context.site.id}${context.urlTemplates.getProxiesVfsUrl(runtimeVersion, 'proxies.json')}`;
     const url = this._armService.getArmUrl(resourceId, this._armService.antaresApiVersion20181101);
 
