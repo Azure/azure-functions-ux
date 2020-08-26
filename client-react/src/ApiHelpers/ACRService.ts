ecaf1849f599d31e89e8530fea21689309ad366f
Mitren Chinoy <nertim@users.noreply.github.com>
2020-08-26 16:45:19 -0700

    Wireup the ACR calls to the ACR dropdowns (#5282)
    * wireup the ACR calls to the ACR dropdowns
    
    * address PR comments

---
+++ client-react/src/ApiHelpers/ACRService.ts
@@ -0,0 +1,151 @@
+import MakeArmCall from './ArmHelper';
+import { ArmArray, ArmObj } from '../models/arm-obj';
+import { ACRRegistry, ACRWebhookPayload, ACRCredential, ACRRepositories, ACRTags } from '../models/acr';
+import { CommonConstants } from '../utils/CommonConstants';
+import { HttpResponseObject } from '../ArmHelper.types';
+import { sendHttpRequest } from './HttpClient';
+import Url from '../utils/url';
+
+export default class ACRService {
+  public static getRegistries(subscriptionId: string) {
+    return MakeArmCall<ArmArray<ACRRegistry>>({
+      resourceId: `/subscriptions/${subscriptionId}/providers/Microsoft.ContainerRegistry/registries`,
+      commandName: 'getRegistries',
+      method: 'GET',
+      apiVersion: CommonConstants.ApiVersions.acrApiVersion,
+    });
+  }
+
+  public static listCredentials(resourceId: string) {
+    return MakeArmCall<ACRCredential>({
+      resourceId: `${resourceId}/listCredentials`,
+      commandName: 'getCredentials',
+      method: 'POST',
+      apiVersion: CommonConstants.ApiVersions.acrApiVersion,
+    });
+  }
+
+  public static updateAcrWebhook(resourceId: string, name: string, location: string, properties: ACRWebhookPayload) {
+    const body = {
+      name,
+      location,
+      properties,
+      id: '',
+    };
+
+    return MakeArmCall<ArmObj<ACRWebhookPayload>>({
+      resourceId,
+      body,
+      commandName: 'updateAcrWebhook',
+      method: 'PUT',
+      apiVersion: CommonConstants.ApiVersions.acrApiVersion,
+    });
+  }
+
+  public static deleteAcrWebhook(resourceId: string) {
+    return MakeArmCall<void>({
+      resourceId,
+      commandName: 'deleteAcrWebhook',
+      method: 'DELETE',
+      apiVersion: CommonConstants.ApiVersions.acrApiVersion,
+    });
+  }
+
+  public static getRepositories(loginServer: string, username: string, password: string, logger?: (page, error) => void) {
+    const encoded = btoa(`${username}:${password}`);
+
+    const headers = {
+      Authorization: `Basic ${encoded}`,
+      'Content-Type': 'application/json',
+    };
+
+    const url = `https://${loginServer}/v2/_catalog`;
+
+    return ACRService._dispatchPageableRequest<ACRRepositories>(loginServer, url, headers, logger);
+  }
+
+  public static getTags(loginServer: string, repository: string, username: string, password: string, logger?: (page, error) => void) {
+    const encoded = btoa(`${username}:${password}`);
+
+    const headers = {
+      Authorization: `Basic ${encoded}`,
+      'Content-Type': 'application/json',
+    };
+
+    const url = `https://${loginServer}/v2/${repository}/tags/list`;
+
+    return this._dispatchPageableRequest<ACRTags>(loginServer, url, headers, logger);
+  }
+
+  private static async _dispatchPageableRequest<T>(
+    loginServer: string,
+    originalUrl: string,
+    headers: { [key: string]: string },
+    logger?: (page, error) => void
+  ): Promise<T[]> {
+    const items: T[] = [];
+    let nextLink = originalUrl;
+    let page = 1;
+    do {
+      const passThroughUrl = `/api/passthrough?q=${nextLink}`;
+      const data = ACRService._generatePassthroughObject(nextLink, headers);
+
+      const response = await sendHttpRequest<T>({
+        data,
+        url: `${Url.serviceHost}${passThroughUrl}`,
+        method: 'POST',
+      });
+
+      if (response.metadata.success) {
+        nextLink = ACRService._getNextLink(loginServer, response);
+        items.push(response.data);
+      } else {
+        nextLink = '';
+        if (logger) {
+          logger(page, response.metadata.error);
+        }
+      }
+
+      page = page + 1;
+    } while (nextLink);
+
+    return items;
+  }
+
+  private static _generatePassthroughObject(url: string, headers: { [key: string]: string }) {
+    return {
+      method: 'GET',
+      url: url,
+      body: null,
+      headers: headers,
+    };
+  }
+
+  public static _getNextLink(loginServer: string, response: HttpResponseObject<unknown>): string {
+    if (response && response.metadata.success) {
+      const linkHeader = response.metadata.headers['link'];
+      const links = ACRService._getLinksFromLinkHeader(linkHeader);
+      if (links && links.next) {
+        return `https://${loginServer}${links.next}`;
+      }
+    }
+
+    return '';
+  }
+
+  // For certain APIs (github, docker, and ACR) the next link is part of the response header
+  // in "link" property. This property contains an array of links along with their link relation (rel=)
+  // the links could be 'next', 'last', etc.
+  public static _getLinksFromLinkHeader(linkHeader: string): { [key: string]: string } {
+    const links: { [key: string]: string } = {};
+
+    if (linkHeader) {
+      const section = linkHeader.split(';');
+      const url = section[0].replace(/<(.*)>/, '$1').trim();
+      const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      links[name] = url;
+    }
+
+    return links;
+  }
+}
--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerAcrDataLoader.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerAcrDataLoader.tsx
@@ -1,16 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext, useRef } from 'react';
 import { DeploymentCenterFieldProps, DeploymentCenterContainerFormData } from '../DeploymentCenter.types';
 import DeploymentCenterContainerAcrSettings from './DeploymentCenterContainerAcrSettings';
-import { SiteStateContext } from '../../../../SiteState';
 import { DeploymentCenterContext } from '../DeploymentCenterContext';
 import { IDropdownOption, MessageBarType } from 'office-ui-fabric-react';
+import DeploymentCenterData from '../DeploymentCenter.data';
+import { getErrorMessage } from '../../../../ApiHelpers/ArmHelper';
+import { ACRCredential } from '../../../../models/acr';
 
 const DeploymentCenterContainerAcrDataLoader: React.FC<DeploymentCenterFieldProps<DeploymentCenterContainerFormData>> = props => {
+  const deploymentCenterData = new DeploymentCenterData();
+  const deploymentCenterContext = useContext(DeploymentCenterContext);
+
   const [acrRegistryOptions, setAcrRegistryOptions] = useState<IDropdownOption[]>([]);
   const [acrImageOptions, setAcrImageOptions] = useState<IDropdownOption[]>([]);
   const [acrTagOptions, setAcrTagOptions] = useState<IDropdownOption[]>([]);
   const [acrStatusMessage, setAcrStatusMessage] = useState<string | undefined>(undefined);
   const [acrStatusMessageType, setAcrStatusMessageType] = useState<MessageBarType | undefined>(undefined);
+  const registryCredentials = useRef<{ [key: string]: ACRCredential }>({});
 
   const fetchData = () => {
     setAcrRegistryOptions([]);
@@ -18,26 +24,107 @@ const DeploymentCenterContainerAcrDataLoader: React.FC<DeploymentCenterFieldProp
     setAcrTagOptions([]);
     setAcrStatusMessage(undefined);
     setAcrStatusMessageType(undefined);
+    fetchRegistries();
   };
 
-  const fetchImages = (registry: string) => {
-    throw Error('Not Implemented');
+  const fetchRegistries = async () => {
+    if (deploymentCenterContext.siteDescriptor) {
+      const registriesResponse = await deploymentCenterData.getAcrRegistries(deploymentCenterContext.siteDescriptor.subscription);
+      if (registriesResponse.metadata.success && registriesResponse.data) {
+        const dropdownOptions = registriesResponse.data.value
+          .filter(registry => registry.properties.adminUserEnabled)
+          .map(registry => ({
+            key: `${registry.properties.loginServer}:${registry.id}`,
+            text: registry.name,
+          }));
+
+        setAcrRegistryOptions(dropdownOptions);
+      } else {
+        const errorMessage = getErrorMessage(registriesResponse.metadata.error);
+        if (errorMessage) {
+          setAcrStatusMessage(errorMessage);
+          setAcrStatusMessageType(MessageBarType.error);
+        }
+      }
+    }
   };
 
-  const fetchTags = (registry: string, image: string) => {
-    throw Error('Not Implemented');
+  const fetchRepositories = async (registrySelected: string) => {
+    setAcrTagOptions([]);
+    setAcrStatusMessage(undefined);
+    setAcrStatusMessageType(undefined);
+
+    const [loginServer, resourceId] = registrySelected.split(':');
+
+    if (!registryCredentials.current[loginServer]) {
+      const credentialsResponse = await deploymentCenterData.listAcrCredentials(resourceId);
+
+      if (credentialsResponse.metadata.success && credentialsResponse.data.passwords && credentialsResponse.data.passwords.length > 0) {
+        registryCredentials.current[loginServer] = credentialsResponse.data;
+      } else {
+        const errorMessage = getErrorMessage(credentialsResponse.metadata.error);
+        if (errorMessage) {
+          setAcrStatusMessage(errorMessage);
+          setAcrStatusMessageType(MessageBarType.error);
+        }
+      }
+    }
+
+    const credentials = registryCredentials.current[loginServer];
+
+    if (credentials) {
+      const username = credentials.username;
+      const password = credentials.passwords[0].value;
+
+      const repositoriesResponse = await deploymentCenterData.getAcrRepositories(loginServer, username, password);
+
+      const repositoryOptions: IDropdownOption[] = [];
+      repositoriesResponse.forEach(response => {
+        const dropdownOptions =
+          response && response.repositories && response.repositories.length > 0
+            ? response.repositories.map(repository => ({ key: repository, text: repository }))
+            : [];
+        repositoryOptions.push(...dropdownOptions);
+      });
+
+      setAcrImageOptions(repositoryOptions);
+    }
+  };
+
+  const fetchTags = async (registrySelected: string, imageSelected: string) => {
+    setAcrStatusMessage(undefined);
+    setAcrStatusMessageType(undefined);
+
+    const loginServer = registrySelected.split(':')[0];
+    const credentials = registryCredentials.current[loginServer];
+
+    if (credentials) {
+      const username = credentials.username;
+      const password = credentials.passwords[0].value;
+
+      const tagsResponse = await deploymentCenterData.getAcrTags(loginServer, imageSelected, username, password);
+
+      const tagOptions: IDropdownOption[] = [];
+      tagsResponse.forEach(response => {
+        const dropdownOptions =
+          response && response.tags && response.tags.length > 0 ? response.tags.map(tag => ({ key: tag, text: tag })) : [];
+        tagOptions.push(...dropdownOptions);
+      });
+
+      setAcrTagOptions(tagOptions);
+    }
   };
 
   useEffect(() => {
     fetchData();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [SiteStateContext, DeploymentCenterContext]);
+  }, [deploymentCenterContext]);
 
   return (
     <DeploymentCenterContainerAcrSettings
       {...props}
-      fetchImages={fetchImages}
+      fetchImages={fetchRepositories}
       fetchTags={fetchTags}
       acrRegistryOptions={acrRegistryOptions}
       acrImageOptions={acrImageOptions}
