c7cd2d52cfabc27c1c51e47b8b0e69dc211bb84f
Mitren Chinoy <michinoy@microsoft.com>
2020-08-28 10:55:45 -0700

    address pr comments

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerContinuousDeploymentSettings.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerContinuousDeploymentSettings.tsx
@@ -22,6 +22,30 @@ const DeploymentCenterContainerContinuousDeploymentSettings: React.FC<
     setWebhookFieldType(!webhookFieldType ? 'password' : undefined);
   };
 
+  const additionalWebhookControls = () => {
+    return [
+      <ActionButton
+        id="continuous-deployment-webhook-visibility-toggle"
+        key="continuous-deployment-webhook-visibility-toggle"
+        className={additionalTextFieldControl}
+        onClick={toggleShowWebhook}
+        iconProps={{ iconName: webhookFieldType === 'password' ? 'RedEye' : 'Hide' }}>
+        {webhookFieldType === 'password' ? t('show') : t('hide')}
+      </ActionButton>,
+    ];
+  };
+
+  const continuousDeploymentOptions = [
+    {
+      key: ContinuousDeploymentOption.on,
+      text: t('on'),
+    },
+    {
+      key: ContinuousDeploymentOption.off,
+      text: t('off'),
+    },
+  ];
+
   useEffect(() => {
     if (
       deploymentCenterPublishingContext &&
@@ -36,21 +60,11 @@ const DeploymentCenterContainerContinuousDeploymentSettings: React.FC<
   return (
     <>
       <Field
-        id="continuous-deployment-enabled"
+        id="continuous-deployment-option"
         label={t('continuousDeployment')}
-        name="continuousDeployment"
-        defaultSelectedKey={props.formProps.values.continuousDeploymentOption}
+        name="continuousDeploymentOption"
         component={RadioButton}
-        options={[
-          {
-            key: ContinuousDeploymentOption.on,
-            text: t('on'),
-          },
-          {
-            key: ContinuousDeploymentOption.off,
-            text: t('off'),
-          },
-        ]}
+        options={continuousDeploymentOptions}
       />
 
       <TextFieldNoFormik
@@ -60,16 +74,7 @@ const DeploymentCenterContainerContinuousDeploymentSettings: React.FC<
         copyButton={true}
         disabled={true}
         type={webhookFieldType}
-        additionalControls={[
-          <ActionButton
-            id="continuous-deployment-webhook-visibility-toggle"
-            key="continuous-deployment-webhook-visibility-toggle"
-            className={additionalTextFieldControl}
-            onClick={toggleShowWebhook}
-            iconProps={{ iconName: webhookFieldType === 'password' ? 'RedEye' : 'Hide' }}>
-            {webhookFieldType === 'password' ? t('show') : t('hide')}
-          </ActionButton>,
-        ]}
+        additionalControls={additionalWebhookControls()}
       />
     </>
   );
