236e7d515e0ee723bcea5d99e3664b47e397b9e3
Allison Manifold <allisonm@microsoft.com>
2020-09-09 17:34:40 -0700

    [Stacks][WebApp] Add JBoss EAP Preview (#5331)
    * add jboss info except runtimeversion
    
    * add runtime, update preview title, update tests
    
    * update runtime
    
    * update sort order

--- server/src/stacks/webapp/2020-05-01/stacks/config/linux/java8.ts
+++ server/src/stacks/webapp/2020-05-01/stacks/config/linux/java8.ts
@@ -79,6 +79,23 @@ export const java8LinuxConfigStack: WebAppConfigStack = {
         isHidden: false,
       },
       {
+        displayVersion: 'JBoss EAP 7.2 - Preview',
+        runtimeVersion: 'JBOSSEAP|7.2-java8',
+        isDefault: false,
+        minorVersions: [
+          {
+            displayVersion: '7.2',
+            runtimeVersion: 'JBOSSEAP|7.2-java8',
+            isDefault: true,
+            isRemoteDebuggingEnabled: false,
+          },
+        ],
+        applicationInsights: true,
+        isPreview: true,
+        isDeprecated: false,
+        isHidden: false,
+      },
+      {
         displayVersion: 'WildFly 14 - Preview',
         runtimeVersion: 'WILDFLY|14-jre8',
         isDefault: true,
