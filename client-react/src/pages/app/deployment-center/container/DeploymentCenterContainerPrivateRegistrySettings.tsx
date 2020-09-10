db6aa8eac3a75e343ca9b678c3ea918c555a99e6
Mitren Chinoy <nertim@users.noreply.github.com>
2020-09-10 13:44:30 -0700

    Always get the serverurl, login, password, image, and tag for github action setup. (#5341)

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerPrivateRegistrySettings.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerPrivateRegistrySettings.tsx
@@ -1,29 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Field } from 'formik';
 import TextField from '../../../../components/form-controls/TextField';
 import { useTranslation } from 'react-i18next';
-import { DeploymentCenterFieldProps, DeploymentCenterContainerFormData } from '../DeploymentCenter.types';
-import { ScmType } from '../../../../models/site/config';
-import { isWorkflowOptionExistingOrAvailable } from '../utility/GitHubActionUtility';
 
-const DeploymentCenterContainerPrivateRegistrySettings: React.FC<DeploymentCenterFieldProps<DeploymentCenterContainerFormData>> = props => {
-  const { formProps } = props;
+const DeploymentCenterContainerPrivateRegistrySettings: React.FC<{}> = props => {
   const { t } = useTranslation();
-
-  const [isUsingExistingOrAvailableWorkflowConfig, setIsUsingExistingOrAvailableWorkflowConfig] = useState(false);
-  const [isGitHubAction, setIsGitHubAction] = useState(false);
-
-  useEffect(() => {
-    setIsUsingExistingOrAvailableWorkflowConfig(isWorkflowOptionExistingOrAvailable(formProps.values.workflowOption));
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formProps.values.workflowOption]);
-
-  useEffect(() => {
-    setIsGitHubAction(formProps.values.scmType === ScmType.GitHubAction);
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [formProps.values.scmType]);
 
   // NOTE(michinoy): In case of GitHub Action, we will always need to get the user credentials for their private
   // registry. This is because the workflow would need to use those credentials to push the images and app service
@@ -51,12 +32,8 @@ const DeploymentCenterContainerPrivateRegistrySettings: React.FC<DeploymentCente
         id="container-privateRegistry-imageAndTag"
         name="privateRegistryImageAndTag"
         component={TextField}
-        label={isGitHubAction && !isUsingExistingOrAvailableWorkflowConfig ? t('containerImage') : t('containerImageAndTag')}
-        placeholder={
-          isGitHubAction && !isUsingExistingOrAvailableWorkflowConfig
-            ? t('containerImagePlaceholder')
-            : t('containerImageAndTagPlaceholder')
-        }
+        label={t('containerImageAndTag')}
+        placeholder={t('containerImageAndTagPlaceholder')}
       />
 
       <Field id="container-privateRegistry-startUpFile" name="command" component={TextField} label={t('containerStartupFile')} />
