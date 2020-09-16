1e191aa4f2c6177cfbe194dac3c71670712f59d5
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2020-09-16 13:09:33 -0700

    [Deployment Center V2] Dirty state on tabs (#5379)
    * Dirty state on tabs
    
    * pr updates

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerPivot.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerPivot.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
-import { Pivot, PivotItem } from 'office-ui-fabric-react';
+import React, { useContext } from 'react';
+import { Pivot, PivotItem, IPivotItemProps } from 'office-ui-fabric-react';
 import DeploymentCenterContainerSettings from './DeploymentCenterContainerSettings';
 import DeploymentCenterFtps from '../DeploymentCenterFtps';
 import { useTranslation } from 'react-i18next';
 import { DeploymentCenterContainerPivotProps } from '../DeploymentCenter.types';
 import DeploymentCenterContainerLogs from './DeploymentCenterContainerLogs';
+import { ScmType } from '../../../../models/site/config';
+import { DeploymentCenterContext } from '../DeploymentCenterContext';
+import { DeploymentCenterPublishingContext } from '../DeploymentCenterPublishingContext';
+import { ThemeContext } from '../../../../ThemeContext';
+import CustomTabRenderer from '../../app-settings/Sections/CustomTabRenderer';
 
 const DeploymentCenterContainerPivot: React.FC<DeploymentCenterContainerPivotProps> = props => {
   const { logs, formProps, isLoading } = props;
   const { t } = useTranslation();
+
+  const deploymentCenterContext = useContext(DeploymentCenterContext);
+  const deploymentCenterPublishingContext = useContext(DeploymentCenterPublishingContext);
+  const theme = useContext(ThemeContext);
+
+  const isSettingsDirty = (): boolean => {
+    return !!deploymentCenterContext.siteConfig && deploymentCenterContext.siteConfig.properties.scmType === ScmType.None;
+  };
+
+  const isFtpsDirty = (): boolean => {
+    const currentUser = deploymentCenterPublishingContext.publishingUser;
+
+    return (
+      !!currentUser &&
+      (currentUser.properties.publishingUserName !== formProps.values.publishingUsername ||
+        (!!formProps.values.publishingPassword && currentUser.properties.publishingPassword !== formProps.values.publishingPassword))
+    );
+  };
 
   return (
     <Pivot>
@@ -16,11 +39,21 @@ const DeploymentCenterContainerPivot: React.FC<DeploymentCenterContainerPivotPro
         <DeploymentCenterContainerLogs logs={logs} isLoading={isLoading} />
       </PivotItem>
 
-      <PivotItem headerText={t('deploymentCenterPivotItemSettingsHeaderText')} ariaLabel={t('deploymentCenterPivotItemSettingsAriaLabel')}>
+      <PivotItem
+        headerText={t('deploymentCenterPivotItemSettingsHeaderText')}
+        ariaLabel={t('deploymentCenterPivotItemSettingsAriaLabel')}
+        onRenderItemLink={(link: IPivotItemProps, defaultRenderer: (link: IPivotItemProps) => JSX.Element) =>
+          CustomTabRenderer(link, defaultRenderer, theme, isSettingsDirty, t('modifiedTag'))
+        }>
         <DeploymentCenterContainerSettings formProps={formProps} />
       </PivotItem>
 
-      <PivotItem headerText={t('deploymentCenterPivotItemFtpsHeaderText')} ariaLabel={t('deploymentCenterPivotItemFtpsAriaLabel')}>
+      <PivotItem
+        headerText={t('deploymentCenterPivotItemFtpsHeaderText')}
+        ariaLabel={t('deploymentCenterPivotItemFtpsAriaLabel')}
+        onRenderItemLink={(link: IPivotItemProps, defaultRenderer: (link: IPivotItemProps) => JSX.Element) =>
+          CustomTabRenderer(link, defaultRenderer, theme, isFtpsDirty, t('modifiedTag'))
+        }>
         <DeploymentCenterFtps formProps={formProps} isLoading={isLoading} />
       </PivotItem>
     </Pivot>
