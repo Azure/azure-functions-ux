78893530c0a2053d0c3c78695cccb79e83159a46
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2020-09-17 11:27:24 -0700

    updating container settings dirty tab (#5398)

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerPivot.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerPivot.tsx
@@ -3,7 +3,7 @@ import { Pivot, PivotItem, IPivotItemProps } from 'office-ui-fabric-react';
 import DeploymentCenterContainerSettings from './DeploymentCenterContainerSettings';
 import DeploymentCenterFtps from '../DeploymentCenterFtps';
 import { useTranslation } from 'react-i18next';
-import { DeploymentCenterContainerPivotProps } from '../DeploymentCenter.types';
+import { DeploymentCenterContainerPivotProps, ContainerRegistrySources } from '../DeploymentCenter.types';
 import DeploymentCenterContainerLogs from './DeploymentCenterContainerLogs';
 import { ScmType } from '../../../../models/site/config';
 import { DeploymentCenterContext } from '../DeploymentCenterContext';
@@ -20,7 +20,50 @@ const DeploymentCenterContainerPivot: React.FC<DeploymentCenterContainerPivotPro
   const theme = useContext(ThemeContext);
 
   const isSettingsDirty = (): boolean => {
-    return !!deploymentCenterContext.siteConfig && deploymentCenterContext.siteConfig.properties.scmType === ScmType.None;
+    return (
+      (isContainerGeneralSettingsDirty() ||
+        (formProps.values.registrySource === ContainerRegistrySources.privateRegistry && isPrivateRegistrySettingsDirty()) ||
+        (formProps.values.registrySource === ContainerRegistrySources.docker && isDockerSettingsDirty()) ||
+        (formProps.values.registrySource === ContainerRegistrySources.acr && isAcrSettingsDirty())) &&
+      (!!deploymentCenterContext.siteConfig && deploymentCenterContext.siteConfig.properties.scmType === ScmType.None)
+    );
+  };
+
+  const isPrivateRegistrySettingsDirty = (): boolean => {
+    return (
+      formProps.values.privateRegistryServerUrl !== formProps.initialValues.privateRegistryServerUrl ||
+      formProps.values.privateRegistryUsername !== formProps.initialValues.privateRegistryUsername ||
+      formProps.values.privateRegistryPassword !== formProps.initialValues.privateRegistryPassword ||
+      formProps.values.privateRegistryImageAndTag !== formProps.initialValues.privateRegistryImageAndTag ||
+      formProps.values.privateRegistryComposeYml !== formProps.initialValues.privateRegistryComposeYml
+    );
+  };
+
+  const isDockerSettingsDirty = (): boolean => {
+    return (
+      formProps.values.dockerHubAccessType !== formProps.initialValues.dockerHubAccessType ||
+      formProps.values.dockerHubImageAndTag !== formProps.initialValues.dockerHubImageAndTag ||
+      formProps.values.dockerHubComposeYml !== formProps.initialValues.dockerHubComposeYml
+    );
+  };
+
+  const isAcrSettingsDirty = (): boolean => {
+    return (
+      formProps.values.acrLoginServer !== formProps.initialValues.acrLoginServer ||
+      formProps.values.acrImage !== formProps.initialValues.acrImage ||
+      formProps.values.acrTag !== formProps.initialValues.acrTag ||
+      formProps.values.acrComposeYml !== formProps.initialValues.acrComposeYml
+    );
+  };
+
+  const isContainerGeneralSettingsDirty = (): boolean => {
+    return (
+      formProps.values.scmType !== formProps.initialValues.scmType ||
+      formProps.values.option !== formProps.initialValues.option ||
+      formProps.values.registrySource !== formProps.initialValues.registrySource ||
+      formProps.values.continuousDeploymentOption !== formProps.initialValues.continuousDeploymentOption ||
+      formProps.values.command !== formProps.initialValues.command
+    );
   };
 
   const isFtpsDirty = (): boolean => {
