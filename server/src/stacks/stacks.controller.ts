f7a988a3bd1c8d6f7fa453db91281ef4589d6f05
Allison Manifold <allisonm@microsoft.com>
2020-09-18 14:30:53 -0700

    add validations in seperate file

--- server/src/stacks/stacks.controller.ts
+++ server/src/stacks/stacks.controller.ts
@@ -1,55 +1,25 @@
-import { Controller, Query, HttpException, Post, Get } from '@nestjs/common';
+import { Controller, Query, Post, Get } from '@nestjs/common';
 import { Versions } from './versions';
 import { StacksService20200501 } from './2020-05-01/service/StackService';
 import { StacksService20200601 } from './2020-06-01/service/StackService';
 import { AppStackOs } from './2020-06-01/models/AppStackModel';
 import { FunctionAppStackValue } from './2020-06-01/models/FunctionAppStackModel';
 import { WebAppStackValue } from './2020-06-01/models/WebAppStackModel';
+import {
+  validateApiVersion,
+  validateOs,
+  validateRemoveHiddenStacks,
+  validateFunctionAppStack,
+  validateRemoveDeprecatedStacks,
+  validateRemovePreviewStacks,
+  validateWebAppStack,
+} from './validations';
 
 @Controller('stacks')
 export class StacksController {
   constructor(private _stackService20200501: StacksService20200501, private _stackService20200601: StacksService20200601) {}
 
-  @Post('webAppCreateStacks')
-  webAppCreateStacks(@Query('api-version') apiVersion: string) {
-    this._validateApiVersion(apiVersion, [Versions.version20200501]);
-
-    if (apiVersion === Versions.version20200501) {
-      return this._stackService20200501.getWebAppCreateStacks();
-    }
-  }
-
-  @Post('webAppConfigStacks')
-  webAppConfigStacks(@Query('api-version') apiVersion: string, @Query('os') os?: 'linux' | 'windows') {
-    this._validateApiVersion(apiVersion, [Versions.version20200501]);
-    this._validateOs(os);
-
-    if (apiVersion === Versions.version20200501) {
-      return this._stackService20200501.getWebAppConfigStacks(os);
-    }
-  }
-
-  @Post('webAppGitHubActionStacks')
-  webAppGitHubActionStacks(@Query('api-version') apiVersion: string, @Query('os') os?: 'linux' | 'windows') {
-    this._validateApiVersion(apiVersion, [Versions.version20200501]);
-    this._validateOs(os);
-
-    if (apiVersion === Versions.version20200501) {
-      return this._stackService20200501.getWebAppGitHubActionStacks(os);
-    }
-  }
-
-  @Post('functionAppStacks')
-  functionAppStacksPost(@Query('api-version') apiVersion: string, @Query('removeHiddenStacks') removeHiddenStacks?: string) {
-    this._validateApiVersion(apiVersion, [Versions.version20200501]);
-    this._validateRemoveHiddenStacks(removeHiddenStacks);
-    const removeHidden = removeHiddenStacks && removeHiddenStacks.toLowerCase() === 'true';
-
-    if (apiVersion === Versions.version20200501) {
-      return this._stackService20200501.getFunctionAppStacks(removeHidden);
-    }
-  }
-
+  // 2020-06-01 ------------------------------------------------------------------------------------------------------------ //
   @Get('functionAppStacks')
   functionAppStacks(
     @Query('api-version') apiVersion: string,
@@ -59,12 +29,12 @@ export class StacksController {
     @Query('removeDeprecatedStacks') removeDeprecatedStacks?: string,
     @Query('removePreviewStacks') removePreviewStacks?: string
   ) {
-    this._validateApiVersion(apiVersion, [Versions.version20200601]);
-    this._validateOs(os);
-    this._validateFunctionAppStack(stack);
-    this._validateRemoveHiddenStacks(removeHiddenStacks);
-    this._validateRemoveDeprecatedStacks(removeDeprecatedStacks);
-    this._validateRemovePreviewStacks(removePreviewStacks);
+    validateApiVersion(apiVersion, [Versions.version20200601]);
+    validateOs(os);
+    validateFunctionAppStack(stack);
+    validateRemoveHiddenStacks(removeHiddenStacks);
+    validateRemoveDeprecatedStacks(removeDeprecatedStacks);
+    validateRemovePreviewStacks(removePreviewStacks);
 
     const removeHidden = removeHiddenStacks && removeHiddenStacks.toLowerCase() === 'true';
     const removeDeprecated = removeDeprecatedStacks && removeDeprecatedStacks.toLowerCase() === 'true';
@@ -84,12 +54,12 @@ export class StacksController {
     @Query('removeDeprecatedStacks') removeDeprecatedStacks?: string,
     @Query('removePreviewStacks') removePreviewStacks?: string
   ) {
-    this._validateApiVersion(apiVersion, [Versions.version20200601]);
-    this._validateOs(os);
-    this._validateWebAppStack(stack);
-    this._validateRemoveHiddenStacks(removeHiddenStacks);
-    this._validateRemoveDeprecatedStacks(removeDeprecatedStacks);
-    this._validateRemovePreviewStacks(removePreviewStacks);
+    validateApiVersion(apiVersion, [Versions.version20200601]);
+    validateOs(os);
+    validateWebAppStack(stack);
+    validateRemoveHiddenStacks(removeHiddenStacks);
+    validateRemoveDeprecatedStacks(removeDeprecatedStacks);
+    validateRemovePreviewStacks(removePreviewStacks);
 
     const removeHidden = removeHiddenStacks && removeHiddenStacks.toLowerCase() === 'true';
     const removeDeprecated = removeDeprecatedStacks && removeDeprecatedStacks.toLowerCase() === 'true';
@@ -99,61 +69,47 @@ export class StacksController {
       return this._stackService20200601.getWebAppStacks(os, stack, removeHidden, removeDeprecated, removePreview);
     }
   }
+  // ------------------------------------------------------------------------------------------------------------------ //
 
-  private _validateOs(os?: AppStackOs) {
-    if (os && os.toLowerCase() !== 'linux' && os.toLowerCase() !== 'windows') {
-      throw new HttpException(`Incorrect os '${os}' provided. Allowed os values are 'linux' or 'windows'.`, 400);
+  // 2020-05-01 (please use 2020-06-01 instead) ----------------------------------------------------------------------- //
+  @Post('webAppCreateStacks')
+  webAppCreateStacks(@Query('api-version') apiVersion: string) {
+    validateApiVersion(apiVersion, [Versions.version20200501]);
+
+    if (apiVersion === Versions.version20200501) {
+      return this._stackService20200501.getWebAppCreateStacks();
     }
   }
 
-  private _validateApiVersion(apiVersion: string, acceptedVersions: string[]) {
-    if (!apiVersion) {
-      throw new HttpException(`Missing 'api-version' query parameter. Allowed versions are: ${acceptedVersions.join(', ')}.`, 400);
-    }
+  @Post('webAppConfigStacks')
+  webAppConfigStacks(@Query('api-version') apiVersion: string, @Query('os') os?: 'linux' | 'windows') {
+    validateApiVersion(apiVersion, [Versions.version20200501]);
+    validateOs(os);
 
-    if (!acceptedVersions.includes(apiVersion)) {
-      throw new HttpException(`Incorrect api-version '${apiVersion}' provided. Allowed versions are: ${acceptedVersions.join(', ')}.`, 400);
+    if (apiVersion === Versions.version20200501) {
+      return this._stackService20200501.getWebAppConfigStacks(os);
     }
   }
 
-  private _validateFunctionAppStack(stack?: FunctionAppStackValue) {
-    const stackValues: FunctionAppStackValue[] = ['dotnetCore', 'dotnetFramework', 'java', 'node', 'powershell', 'python', 'custom'];
-    if (stack && !stackValues.includes(stack)) {
-      throw new HttpException(`Incorrect stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+  @Post('webAppGitHubActionStacks')
+  webAppGitHubActionStacks(@Query('api-version') apiVersion: string, @Query('os') os?: 'linux' | 'windows') {
+    validateApiVersion(apiVersion, [Versions.version20200501]);
+    validateOs(os);
+
+    if (apiVersion === Versions.version20200501) {
+      return this._stackService20200501.getWebAppGitHubActionStacks(os);
     }
   }
 
-  private _validateWebAppStack(stack?: WebAppStackValue) {
-    const stackValues: WebAppStackValue[] = ['aspnet', 'dotnetcore', 'java', 'javacontainers', 'node', 'php', 'python', 'ruby'];
-    if (stack && !stackValues.includes(stack)) {
-      throw new HttpException(`Incorrect stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
-    }
-  }
+  @Post('functionAppStacks')
+  functionAppStacksPost(@Query('api-version') apiVersion: string, @Query('removeHiddenStacks') removeHiddenStacks?: string) {
+    validateApiVersion(apiVersion, [Versions.version20200501]);
+    validateRemoveHiddenStacks(removeHiddenStacks);
+    const removeHidden = removeHiddenStacks && removeHiddenStacks.toLowerCase() === 'true';
 
-  private _validateRemoveHiddenStacks(removeHiddenStacks?: string) {
-    if (removeHiddenStacks && removeHiddenStacks.toLowerCase() !== 'true' && removeHiddenStacks.toLowerCase() !== 'false') {
-      throw new HttpException(
-        `Incorrect removeHiddenStacks '${removeHiddenStacks}' provided. Allowed removeHiddenStacks values are 'true' or 'false'.`,
-        400
-      );
+    if (apiVersion === Versions.version20200501) {
+      return this._stackService20200501.getFunctionAppStacks(removeHidden);
     }
   }
-
-  private _validateRemoveDeprecatedStacks(removeDeprecatedStacks?: string) {
-    if (removeDeprecatedStacks && removeDeprecatedStacks.toLowerCase() !== 'true' && removeDeprecatedStacks.toLowerCase() !== 'false') {
-      throw new HttpException(
-        `Incorrect removeDeprecatedStacks '${removeDeprecatedStacks}' provided. Allowed removeDeprecatedStacks values are 'true' or 'false'.`,
-        400
-      );
-    }
-  }
-
-  private _validateRemovePreviewStacks(removePreviewStacks?: string) {
-    if (removePreviewStacks && removePreviewStacks.toLowerCase() !== 'true' && removePreviewStacks.toLowerCase() !== 'false') {
-      throw new HttpException(
-        `Incorrect removePreviewStacks '${removePreviewStacks}' provided. Allowed removePreviewStacks values are 'true' or 'false'.`,
-        400
-      );
-    }
-  }
+  // ---------------------------------------------------------------------------------------------------------------------- //
 }
