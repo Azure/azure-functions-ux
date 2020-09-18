4babf96a1ec24d4808486e4b83f95ba6e25eff73
Allison Manifold <ammanifold@gmail.com>
2020-09-17 19:17:53 -0700

    Revert "[Stacks][Web App] Combine Java SE into one stack (#5410)"
    This reverts commit 21bf28d6b7994eaeb1f008158eeda6fea29d1f3d.

--- server/src/stacks/webapp/2020-06-01/stacks/javaContainers.ts
+++ server/src/stacks/webapp/2020-06-01/stacks/javaContainers.ts
@@ -9,18 +9,11 @@ export const javaContainersStack: WebAppStack<JavaContainers> = {
       value: 'javase',
       minorVersions: [
         {
-          displayText: 'Java SE',
-          value: 'SE',
+          displayText: 'Java SE 11',
+          value: '11',
           stackSettings: {
-            windowsContainerSettings: {
-              javaContainer: 'JAVA',
-              // note (allisonm): for Java SE the java container version doesn't matter
-              javaContainerVersion: 'SE',
-              isAutoUpdate: true,
-            },
             linuxContainerSettings: {
               java11Runtime: 'JAVA|11-java11',
-              java8Runtime: 'JAVA|8-jre8',
               isAutoUpdate: true,
             },
           },
@@ -42,6 +35,21 @@ export const javaContainersStack: WebAppStack<JavaContainers> = {
             linuxContainerSettings: {
               // Note (allisonm): This doesn't have suffix of -java11 since setting to 11.0.5 prevents auto-updates
               java11Runtime: 'JAVA|11.0.5',
+            },
+          },
+        },
+        {
+          displayText: 'Java SE 8',
+          value: '8',
+          stackSettings: {
+            windowsContainerSettings: {
+              javaContainer: 'JAVA',
+              javaContainerVersion: '8',
+              isAutoUpdate: true,
+            },
+            linuxContainerSettings: {
+              java8Runtime: 'JAVA|8-jre8',
+              isAutoUpdate: true,
             },
           },
         },
