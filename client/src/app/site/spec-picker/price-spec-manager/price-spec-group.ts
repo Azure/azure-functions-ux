09df18203546a47e63034807ce77523833d4701e
shimedh <shimedh@microsoft.com>
2020-09-22 10:01:03 -0700

    AB#7062491 Remove Standard from recommended SKU (#5418)
    * AB#7062491 Remove Standard from recommended SKU
    
    * adding some more changes
    
    * some more changes
    
    * Some more fixes
    
    Co-authored-by: Shibani Medhekar <shibanimedhekar@amW10.europe.corp.microsoft.com>

--- client/src/app/site/spec-picker/price-spec-manager/price-spec-group.ts
+++ client/src/app/site/spec-picker/price-spec-manager/price-spec-group.ts
@@ -1,4 +1,4 @@
-import { Links, FeatureFlags } from 'app/shared/models/constants';
+import { Links, FeatureFlags, LogCategories } from 'app/shared/models/constants';
 import { PriceSpec, PriceSpecInput } from './price-spec';
 import { FreePlanPriceSpec } from './free-plan-price-spec';
 import { SharedPlanPriceSpec } from './shared-plan-price-spec';
@@ -27,6 +27,8 @@ import { GenericPlanPriceSpec } from './generic-plan-price-spec';
 import { PricingTier } from 'app/shared/models/arm/pricingtier';
 import { ArmArrayResult } from 'app/shared/models/arm/arm-obj';
 import { Url } from 'app/shared/Utilities/url';
+import { FlightingUtil } from '../../../shared/Utilities/flighting-utility';
+import { LogService } from '../../../shared/services/log.service';
 
 export enum BannerMessageLevel {
   ERROR = 'error',
@@ -78,10 +80,12 @@ export abstract class PriceSpecGroup {
   isExpanded = false;
 
   protected ts: TranslateService;
+  protected logService: LogService;
 
   constructor(protected injector: Injector, protected specManager: PlanPriceSpecManager) {
     this.ts = injector.get(TranslateService);
     this.emptyInfoLinkText = this.ts.instant(PortalResources.clickToLearnMore);
+    this.logService = injector.get(LogService);
   }
 
   abstract initialize(input: PriceSpecInput);
@@ -263,9 +267,18 @@ export class ProdSpecGroup extends PriceSpecGroup {
       }
     }
 
+    const isPartOfPv2Experiment = FlightingUtil.checkSubscriptionInFlight(input.subscriptionId, FlightingUtil.Features.Pv2Experimentation);
+    const isLinux = (input.specPickerInput.data && input.specPickerInput.data.isLinux) || ArmUtil.isLinuxApp(input.plan);
+
     // NOTE(michinoy): The OS type determines whether standard small plan is recommended or additional pricing tier.
-    if ((input.specPickerInput.data && input.specPickerInput.data.isLinux) || ArmUtil.isLinuxApp(input.plan)) {
+    // NOTE(shimedh): If subscription is part of PV2 experiment flighting we always add standard small plan in additional pricing tier irrespective of OS.
+    if (isPartOfPv2Experiment || isLinux) {
       this.additionalSpecs.unshift(new StandardSmallPlanPriceSpec(this.injector));
+      this.logService.debug(LogCategories.specPickerPv2Experiment, {
+        subscriptionId: input.subscriptionId,
+        isLinux: isLinux,
+        isPartOfPv2Experiment: isPartOfPv2Experiment,
+      });
     } else {
       this.recommendedSpecs.unshift(new StandardSmallPlanPriceSpec(this.injector));
     }
