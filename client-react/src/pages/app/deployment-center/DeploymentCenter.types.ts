98e20a364bce1ecef98b0c322d092d6f897d01b5
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2020-10-06 13:11:43 -0700

    [Deployment Center V2] Update Configured view runtime stack and version (#5471)
    * Configured view runtime stack and version
    
    * ===
    
    * toLowerCase fixes

--- client-react/src/pages/app/deployment-center/DeploymentCenter.types.ts
+++ client-react/src/pages/app/deployment-center/DeploymentCenter.types.ts
@@ -61,6 +61,41 @@ export enum WorkflowFileDeleteOptions {
   Delete = 'Delete',
 }
 
+export enum RuntimeStackOptions {
+  Python = 'python',
+  DotNetCore = 'dotnetcore',
+  Ruby = 'ruby',
+  Java11 = 'java-11',
+  Java8 = 'java-8',
+  JBossEAP = 'jbosseap',
+  Node = 'node',
+  PHP = 'php',
+  AspDotNet = 'asp.net',
+}
+
+export enum RuntimeStackDisplayNames {
+  Python = 'Python',
+  DotNetCore = '.NET Core',
+  Ruby = 'Ruby',
+  Java11 = 'Java 11',
+  Java8 = 'Java 8',
+  Node = 'Node',
+  PHP = 'PHP',
+  AspDotNet = 'ASP.NET',
+}
+
+export enum RuntimeVersionOptions {
+  Tomcat = 'tomcat',
+  javaSE = 'java',
+  JBossEAP = 'jbosseap',
+}
+
+export enum RuntimeVersionDisplayNames {
+  JBossEAP = 'JBoss EAP',
+  Tomcat = 'Tomcat',
+  JavaSE = 'Java SE',
+}
+
 export interface DeploymentCenterDataLoaderProps {
   resourceId: string;
 }
