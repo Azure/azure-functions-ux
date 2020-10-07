bb5c2b4a527c12831a4cddc1ca2b0fe734439452
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2020-10-06 23:41:07 -0700

    [Dropbox] Dropbox folder and provider information (#5474)
    * dropbox folders
    
    * hasMore update
    
    * retriggering checks

--- client-react/src/pages/app/deployment-center/dropbox-provider/DeploymentCenterDropboxDataLoader.tsx
+++ client-react/src/pages/app/deployment-center/dropbox-provider/DeploymentCenterDropboxDataLoader.tsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import { DropboxUser } from '../../../../models/dropbox';
+import { DropboxFolder, DropboxUser } from '../../../../models/dropbox';
 import { DeploymentCenterFieldProps } from '../DeploymentCenter.types';
 import { IDropdownOption } from 'office-ui-fabric-react';
 import DeploymentCenterDropboxProvider from './DeploymentCenterDropboxProvider';
+import DeploymentCenterData from '../DeploymentCenter.data';
+import { DeploymentCenterContext } from '../DeploymentCenterContext';
 
 const DeploymentCenterDropboxDataLoader: React.FC<DeploymentCenterFieldProps> = props => {
   const { t } = useTranslation();
@@ -16,17 +18,44 @@ const DeploymentCenterDropboxDataLoader: React.FC<DeploymentCenterFieldProps> = 
   const [loadingFolders, setLoadingFolders] = useState(false);
   const [folderOptions, setFolderOptions] = useState<IDropdownOption[]>([]);
 
+  const deploymentCenterData = new DeploymentCenterData();
+
+  const deploymentCenterContext = useContext(DeploymentCenterContext);
+
   const fetchData = async () => {
-    setDropboxUser(undefined);
+    const dropboxUserResponse = await deploymentCenterData.getDropboxUser(deploymentCenterContext.dropboxToken);
+
     setDropboxAccountStatusMessage(undefined);
-    fetchFolderOptions();
-    throw Error('Not implemented');
+
+    if (
+      dropboxUserResponse.metadata.success &&
+      dropboxUserResponse.data &&
+      dropboxUserResponse.data.name &&
+      dropboxUserResponse.data.name.display_name
+    ) {
+      // NOTE(stpelleg): if unsuccessful, assume the user needs to authorize.
+      setDropboxUser(dropboxUserResponse.data);
+      formProps.setFieldValue('dropboxUser', dropboxUserResponse.data);
+    }
   };
 
   const fetchFolderOptions = async () => {
     setLoadingFolders(true);
     setFolderOptions([]);
-    throw Error('Not implemented');
+    const folderNames: DropboxFolder[] = [];
+
+    if (dropboxUser) {
+      const dropboxFolderResponse = await deploymentCenterData.getDropboxFolders(deploymentCenterContext.dropboxToken);
+
+      if (dropboxFolderResponse) {
+        dropboxFolderResponse.forEach(item => {
+          folderNames.push(item);
+        });
+      }
+    }
+
+    setFolderOptions(folderNames.map(folder => ({ key: folder.name, text: folder.name })));
+    setLoadingFolders(false);
   };
 
   const authorizeDropboxAccount = () => {
@@ -37,6 +66,22 @@ const DeploymentCenterDropboxDataLoader: React.FC<DeploymentCenterFieldProps> = 
     fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    if (!formProps.values.dropboxUser) {
+      fetchData();
+    } else {
+      setDropboxUser(formProps.values.dropboxUser);
+      setDropboxAccountStatusMessage(undefined);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [deploymentCenterContext.dropboxToken]);
+
+  useEffect(() => {
+    fetchFolderOptions();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dropboxUser]);
 
   return (
     <DeploymentCenterDropboxProvider
