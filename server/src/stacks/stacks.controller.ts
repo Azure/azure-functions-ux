1a893fd1ce8400f9eaea5faff88d06a017833d7e
Allison Manifold <allisonm@microsoft.com>
2020-10-15 15:35:31 -0700

    expose api on controller

--- server/src/stacks/stacks.controller.ts
+++ server/src/stacks/stacks.controller.ts
@@ -2,9 +2,12 @@ import { Controller, Query, Post, Get } from '@nestjs/common';
 import { Versions } from './versions';
 import { StacksService20200501 } from './2020-05-01/service/StackService';
 import { StacksService20200601 } from './2020-06-01/service/StackService';
+import { StacksService20201001 } from './2020-10-01/service/StackService';
 import { AppStackOs } from './2020-06-01/models/AppStackModel';
-import { FunctionAppStackValue } from './2020-06-01/models/FunctionAppStackModel';
-import { WebAppStackValue } from './2020-06-01/models/WebAppStackModel';
+import { FunctionAppStackValue as FunctionAppStack20200601Value } from './2020-06-01/models/FunctionAppStackModel';
+import { WebAppStackValue as WebAppStack20200601Value } from './2020-06-01/models/WebAppStackModel';
+import { FunctionAppStackValue as FunctionAppStack20201001Value } from './2020-10-01/models/FunctionAppStackModel';
+import { WebAppStackValue as WebAppStack20201001Value } from './2020-10-01/models/WebAppStackModel';
 import {
   validateApiVersion,
   validateOs,
@@ -17,20 +20,24 @@ import {
 
 @Controller('stacks')
 export class StacksController {
-  constructor(private _stackService20200501: StacksService20200501, private _stackService20200601: StacksService20200601) {}
+  constructor(
+    private _stackService20200501: StacksService20200501,
+    private _stackService20200601: StacksService20200601,
+    private _stackService20201001: StacksService20201001
+  ) {}
 
   @Get('functionAppStacks')
   functionAppStacks(
     @Query('api-version') apiVersion: string,
     @Query('os') os?: AppStackOs,
-    @Query('stack') stack?: FunctionAppStackValue,
+    @Query('stack') stack?: string,
     @Query('removeHiddenStacks') removeHiddenStacks?: string,
     @Query('removeDeprecatedStacks') removeDeprecatedStacks?: string,
     @Query('removePreviewStacks') removePreviewStacks?: string
   ) {
-    validateApiVersion(apiVersion, [Versions.version20200601]);
+    validateApiVersion(apiVersion, [Versions.version20200601, Versions.version20201001]);
     validateOs(os);
-    validateFunctionAppStack(stack);
+    validateFunctionAppStack(apiVersion, stack);
     validateRemoveHiddenStacks(removeHiddenStacks);
     validateRemoveDeprecatedStacks(removeDeprecatedStacks);
     validateRemovePreviewStacks(removePreviewStacks);
@@ -40,7 +47,22 @@ export class StacksController {
     const removePreview = removePreviewStacks && removePreviewStacks.toLowerCase() === 'true';
 
     if (apiVersion === Versions.version20200601) {
-      return this._stackService20200601.getFunctionAppStacks(os, stack, removeHidden, removeDeprecated, removePreview);
+      return this._stackService20200601.getFunctionAppStacks(
+        os,
+        stack as FunctionAppStack20200601Value,
+        removeHidden,
+        removeDeprecated,
+        removePreview
+      );
+    }
+    if (apiVersion === Versions.version20201001) {
+      return this._stackService20201001.getFunctionAppStacks(
+        os,
+        stack as FunctionAppStack20201001Value,
+        removeHidden,
+        removeDeprecated,
+        removePreview
+      );
     }
   }
 
@@ -48,14 +70,14 @@ export class StacksController {
   webAppStacks(
     @Query('api-version') apiVersion: string,
     @Query('os') os?: AppStackOs,
-    @Query('stack') stack?: WebAppStackValue,
+    @Query('stack') stack?: string,
     @Query('removeHiddenStacks') removeHiddenStacks?: string,
     @Query('removeDeprecatedStacks') removeDeprecatedStacks?: string,
     @Query('removePreviewStacks') removePreviewStacks?: string
   ) {
-    validateApiVersion(apiVersion, [Versions.version20200601]);
+    validateApiVersion(apiVersion, [Versions.version20200601, Versions.version20201001]);
     validateOs(os);
-    validateWebAppStack(stack);
+    validateWebAppStack(apiVersion, stack);
     validateRemoveHiddenStacks(removeHiddenStacks);
     validateRemoveDeprecatedStacks(removeDeprecatedStacks);
     validateRemovePreviewStacks(removePreviewStacks);
@@ -65,7 +87,22 @@ export class StacksController {
     const removePreview = removePreviewStacks && removePreviewStacks.toLowerCase() === 'true';
 
     if (apiVersion === Versions.version20200601) {
-      return this._stackService20200601.getWebAppStacks(os, stack, removeHidden, removeDeprecated, removePreview);
+      return this._stackService20200601.getWebAppStacks(
+        os,
+        stack as WebAppStack20200601Value,
+        removeHidden,
+        removeDeprecated,
+        removePreview
+      );
+    }
+    if (apiVersion === Versions.version20201001) {
+      return this._stackService20201001.getWebAppStacks(
+        os,
+        stack as WebAppStack20201001Value,
+        removeHidden,
+        removeDeprecated,
+        removePreview
+      );
     }
   }
 
--- server/src/stacks/validations.ts
+++ server/src/stacks/validations.ts
@@ -1,7 +1,10 @@
 import { HttpException } from '@nestjs/common';
 import { AppStackOs } from './2020-06-01/models/AppStackModel';
-import { FunctionAppStackValue } from './2020-06-01/models/FunctionAppStackModel';
-import { WebAppStackValue } from './2020-06-01/models/WebAppStackModel';
+import { FunctionAppStackValue as FunctionAppStackValue20200601 } from './2020-06-01/models/FunctionAppStackModel';
+import { WebAppStackValue as WebAppStackValue20200601 } from './2020-06-01/models/WebAppStackModel';
+import { FunctionAppStackValue as FunctionAppStackValue20201001 } from './2020-10-01/models/FunctionAppStackModel';
+import { WebAppStackValue as WebAppStackValue20201001 } from './2020-10-01/models/WebAppStackModel';
+import { Versions } from './versions';
 
 export function validateOs(os?: AppStackOs) {
   if (os && os !== 'linux' && os !== 'windows') {
@@ -19,17 +22,39 @@ export function validateApiVersion(apiVersion: string, acceptedVersions: string[
   }
 }
 
-export function validateFunctionAppStack(stack?: FunctionAppStackValue) {
-  const stackValues: FunctionAppStackValue[] = ['dotnetCore', 'dotnetFramework', 'java', 'node', 'powershell', 'python', 'custom'];
-  if (stack && !stackValues.includes(stack)) {
-    throw new HttpException(`Incorrect function app stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+export function validateFunctionAppStack(apiVersion: string, stack?: string) {
+  if (apiVersion === Versions.version20200601) {
+    const stackValues: FunctionAppStackValue20200601[] = [
+      'dotnetCore',
+      'dotnetFramework',
+      'java',
+      'node',
+      'powershell',
+      'python',
+      'custom',
+    ];
+    if (stack && !(stackValues as string[]).includes(stack)) {
+      throw new HttpException(`Incorrect function app stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+    }
+  } else if (apiVersion === Versions.version20201001) {
+    const stackValues: FunctionAppStackValue20201001[] = ['dotnet', 'java', 'node', 'powershell', 'python', 'custom'];
+    if (stack && !(stackValues as string[]).includes(stack)) {
+      throw new HttpException(`Incorrect function app stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+    }
   }
 }
 
-export function validateWebAppStack(stack?: WebAppStackValue) {
-  const stackValues: WebAppStackValue[] = ['aspnet', 'dotnetcore', 'java', 'javacontainers', 'node', 'php', 'python', 'ruby'];
-  if (stack && !stackValues.includes(stack)) {
-    throw new HttpException(`Incorrect web app stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+export function validateWebAppStack(apiVersion: string, stack?: string) {
+  if (apiVersion === Versions.version20200601) {
+    const stackValues: WebAppStackValue20200601[] = ['aspnet', 'dotnetcore', 'java', 'javacontainers', 'node', 'php', 'python', 'ruby'];
+    if (stack && !(stackValues as string[]).includes(stack)) {
+      throw new HttpException(`Incorrect web app stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+    }
+  } else if (apiVersion === Versions.version20201001) {
+    const stackValues: WebAppStackValue20201001[] = ['dotnet', 'java', 'javacontainers', 'node', 'php', 'python', 'ruby'];
+    if (stack && !(stackValues as string[]).includes(stack)) {
+      throw new HttpException(`Incorrect web app stack '${stack}' provided. Allowed stack values are ${stackValues.join(', ')}.`, 400);
+    }
   }
 }
 
