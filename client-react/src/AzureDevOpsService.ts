f34d3f5b3f3c48db7f365b0fa1c9ed0becddf179
Mitren Chinoy <michinoy@microsoft.com>
2020-11-23 17:16:42 -0800

    Clean up existing APIs and add some new ones.

--- client-react/src/AzureDevOpsService.ts
+++ client-react/src/AzureDevOpsService.ts
@@ -1,10 +1,14 @@
 import { uniqBy } from 'lodash-es';
 import { sendHttpRequest } from './ApiHelpers/HttpClient';
+import { HttpResponseObject } from './ArmHelper.types';
 import {
   AuthenticatedUser,
   AuthenticatedUserContext,
   AzureDevOpsUrl,
   DevOpsAccount,
+  DevOpsBranches,
+  DevOpsBuildDefinition,
+  DevOpsRepositories,
   TargetAzDevDeployment,
 } from './pages/app/deployment-center/DeploymentCenter.types';
 import { DeploymentCenterConstants } from './pages/app/deployment-center/DeploymentCenterConstants';
@@ -15,14 +19,14 @@ import LogService from './utils/LogService';
 import Url from './utils/url';
 
 export default class AzureDevOpsService {
-  private static readonly targetAzDevDeployment = (
+  private readonly _targetAzDevDeployment = (
     Url.getFeatureValue(CommonConstants.FeatureFlags.targetAzDevDeployment) || TargetAzDevDeployment.SU2
   ).toLowerCase();
-  private static accountsList: DevOpsAccount[] = [];
-  private static authenticatedUser: AuthenticatedUser;
+  private _accountsList: DevOpsAccount[] = [];
+  private _authenticatedUser: AuthenticatedUser;
 
-  public static getAzureDevOpsUrl = (): AzureDevOpsUrl => {
-    switch (AzureDevOpsService.targetAzDevDeployment) {
+  public getAzureDevOpsUrl = (): AzureDevOpsUrl => {
+    switch (this._targetAzDevDeployment) {
       case TargetAzDevDeployment.Devfabric:
         return {
           Tfs: DeploymentCenterConstants.AzDevDevFabricTfsUri,
@@ -56,24 +60,14 @@ export default class AzureDevOpsService {
     }
   };
 
-  public static getBuildDef(account: string, projectUrl: string, buildId: string) {
-    const url = `${projectUrl}/_apis/build/Definitions/${buildId}?api-version=2.0`;
-    return this.getAccounts().then(result => {
-      const currentAccount = this.accountsList.find(x => x.AccountName.toLowerCase() === account.toLowerCase());
-      const forceMsaPassthrough = currentAccount ? currentAccount.ForceMsaPassThrough : false;
-      return sendHttpRequest<any>({ url, method: 'GET', headers: this.getAzDevDirectHeaders(forceMsaPassthrough) });
-    });
-  }
-
-  public static async getAccounts() {
-    if (this.accountsList.length > 0) {
-      return this.accountsList;
+  public async getAccounts() {
+    if (this._accountsList.length > 0) {
+      return this._accountsList;
     }
+
     const authenticatedUserContext = await this.getUserContext();
     if (authenticatedUserContext) {
-      const url = `${AzureDevOpsService.getAzureDevOpsUrl().Sps}_apis/accounts?memeberId=${
-        this.authenticatedUser.descriptor
-      }?api-version=5.0-preview.1`;
+      const url = `${this.getAzureDevOpsUrl().Sps}_apis/accounts?memeberId=${this._authenticatedUser.descriptor}?api-version=5.0-preview.1`;
 
       const [accounts1Response, accounts2Response] = await Promise.all([
         sendHttpRequest<any>({ url, method: 'GET', headers: this.getAzDevDirectHeaders(false) }),
@@ -84,7 +78,7 @@ export default class AzureDevOpsService {
         const accountList1: DevOpsAccount[] = accounts1Response.data.map(account => ({ ...account, ForceMsaPassThrough: false }));
         const accountList2: DevOpsAccount[] = accounts2Response.data.map(account => ({ ...account, ForceMsaPassThrough: true }));
         const mixedAccountList = uniqBy(accountList1.concat(accountList2), 'AccountId');
-        this.accountsList = mixedAccountList;
+        this._accountsList = mixedAccountList;
         return mixedAccountList;
       } else {
         if (accounts1Response.metadata.error) {
@@ -106,10 +100,10 @@ export default class AzureDevOpsService {
     }
   }
 
-  public static async getUserContext() {
-    const url = `${AzureDevOpsService.getAzureDevOpsUrl().Sps}_apis/connectionData`;
-    if (this.authenticatedUser) {
-      return Promise.resolve(this.authenticatedUser);
+  public async getUserContext() {
+    const url = `${this.getAzureDevOpsUrl().Sps}_apis/connectionData`;
+    if (this._authenticatedUser) {
+      return Promise.resolve(this._authenticatedUser);
     }
     const authenticatedUserResponse = await sendHttpRequest<AuthenticatedUserContext>({
       url,
@@ -117,7 +111,7 @@ export default class AzureDevOpsService {
       headers: this.getAzDevDirectHeaders(false),
     });
     if (authenticatedUserResponse.metadata.success) {
-      return Promise.resolve((this.authenticatedUser = authenticatedUserResponse.data.authenticatedUser));
+      return Promise.resolve((this._authenticatedUser = authenticatedUserResponse.data.authenticatedUser));
     } else {
       LogService.error(
         LogCategories.deploymentCenter,
@@ -128,7 +122,7 @@ export default class AzureDevOpsService {
     }
   }
 
-  public static getAzDevDirectHeaders(appendMsaPassthroughHeader: boolean = true) {
+  public getAzDevDirectHeaders(appendMsaPassthroughHeader: boolean = true) {
     return {
       'content-type': 'application/json',
       accept: 'application/json',
@@ -137,8 +131,35 @@ export default class AzureDevOpsService {
     };
   }
 
-  private static _getAuthTokenForMakingAzDevRequestBasedOnDeployment(): string {
-    if (AzureDevOpsService.targetAzDevDeployment === TargetAzDevDeployment.Devfabric) {
+  public getBuildDef(account: string, projectUrl: string, buildId: string): Promise<HttpResponseObject<DevOpsBuildDefinition>> {
+    const url = `${projectUrl}/_apis/build/Definitions/${buildId}?api-version=2.0`;
+
+    // NOTE(michinoy): Need to figure out
+    return this._sendRequest<DevOpsBuildDefinition>(url, account);
+  }
+
+  public getRepositoriesForAccount(account: string): Promise<HttpResponseObject<DevOpsRepositories>> {
+    const url = `${this.getAzureDevOpsUrl().Tfs}${account}/_apis/git/repositories`;
+
+    return this._sendRequest<DevOpsRepositories>(url, account);
+  }
+
+  public getBranchesForRepo(account: string, repositoryId: string): Promise<HttpResponseObject<DevOpsBranches>> {
+    const url = `${this.getAzureDevOpsUrl().Tfs}${account}/_apis/git/repositories/${repositoryId}/refs?api-version=5.0`;
+
+    return this._sendRequest<DevOpsBranches>(url, account);
+  }
+
+  private _sendRequest<T>(url: string, account: string): Promise<HttpResponseObject<T>> {
+    return this.getAccounts().then(result => {
+      const currentAccount = this._accountsList.find(x => x.AccountName.toLowerCase() === account.toLowerCase());
+      const forceMsaPassthrough = currentAccount ? currentAccount.ForceMsaPassThrough : false;
+      return sendHttpRequest<T>({ url, method: 'GET', headers: this.getAzDevDirectHeaders(forceMsaPassthrough) });
+    });
+  }
+
+  private _getAuthTokenForMakingAzDevRequestBasedOnDeployment(): string {
+    if (this._targetAzDevDeployment === TargetAzDevDeployment.Devfabric) {
       const authTokenOverride = Url.getFeatureValue(CommonConstants.FeatureFlags.authTokenOverride);
       if (!authTokenOverride) {
         throw new Error('You are using Devfabric environment but PAT is not provided');
