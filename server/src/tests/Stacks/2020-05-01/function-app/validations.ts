56a86712d6348867351e1906260ae82c708a8c14
Hanzhang Zeng <48038149+Hazhzeng@users.noreply.github.com>
2020-11-25 10:29:55 -0800

    [Stacks][Function App] Enable function app Python 3.9 in preview and hidden (#5576)

--- server/src/tests/Stacks/2020-05-01/function-app/validations.ts
+++ server/src/tests/Stacks/2020-05-01/function-app/validations.ts
@@ -44,7 +44,7 @@ export function validatePythonStack(stacks) {
   expect(pythonStack.displayText).to.equal('Python');
   expect(pythonStack.value).to.equal('python');
   expect(pythonStack.sortOrder).to.equal(2);
-  expect(pythonStack.versions.length).to.equal(3);
+  expect(pythonStack.versions.length).to.equal(4);
   expect(pythonStack).to.deep.equal(hardCodedPythonStack);
 }
 
