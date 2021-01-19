05474ea5e12fe27efbdeb9ac5513c33529b957e1
Krrish Mittal <takyyon@MININT-3BKVT16.northamerica.corp.microsoft.com>
2021-01-19 13:05:52 -0800

    dotnetcore and aspnet removed

--- client-react/src/pages/app/app-settings/GeneralSettings/WindowsStacks/WindowsStacks.tsx
+++ client-react/src/pages/app/app-settings/GeneralSettings/WindowsStacks/WindowsStacks.tsx
@@ -28,28 +28,21 @@ const WindowsStacks: React.FC<StackProps> = props => {
     return supportedStacks
       .filter(stack => {
         const stackValue = stack.value.toLocaleLowerCase();
-        // NOTE(krmitta): General Settings for Windows web app stacks only supports dotnetcore, dotnet, python, php and java for now.
+        // NOTE(krmitta): General Settings for Windows web app stacks only supports dotnet, python, php and java for now.
         // I will be adding the node support at a later time.
         return (
           stackValue === RuntimeStacks.java ||
           stackValue === RuntimeStacks.php ||
           stackValue === RuntimeStacks.python ||
-          stackValue === RuntimeStacks.dotnetcore ||
-          stackValue === RuntimeStacks.aspnet
+          stackValue === RuntimeStacks.dotnet
         );
       })
       .map(stack => {
-        const stackKey = stack.value.toLowerCase();
         return {
-          key: stackKey === RuntimeStacks.aspnet ? RuntimeStacks.dotnet : stackKey,
+          key: stack.value.toLowerCase(),
           text: stack.displayText,
         };
       });
-  };
-
-  const isDotnetStack = () => {
-    // NOTE (krmitta): Dotnet and Aspnet will be merged into one starting 2020-10-01
-    return values.currentlySelectedStack === RuntimeStacks.aspnet || values.currentlySelectedStack === RuntimeStacks.dotnet;
   };
 
   return (
@@ -64,7 +57,7 @@ const WindowsStacks: React.FC<StackProps> = props => {
         label={t('stack')}
         id="app-settings-stack-dropdown"
       />
-      {isDotnetStack() || showNonJavaAnyway ? <DotNetStack {...props} /> : null}
+      {values.currentlySelectedStack === RuntimeStacks.dotnet || showNonJavaAnyway ? <DotNetStack {...props} /> : null}
       {values.currentlySelectedStack === RuntimeStacks.php || showNonJavaAnyway ? <PhpStack {...props} /> : null}
       {values.currentlySelectedStack === RuntimeStacks.python || showNonJavaAnyway ? <PythonStack {...props} /> : null}
       {javaSelected ? <JavaStack {...props} /> : null}
