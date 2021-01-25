17f60fa181e4278077c537baae6cca1fb95b2f14
Mitren Chinoy <michinoy@microsoft.com>
2021-01-25 11:31:05 -0800

    Use the log method on component failure.

--- client-react/src/components/ErrorLogger.tsx
+++ client-react/src/components/ErrorLogger.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { LogEntryLevel } from '../models/portal-models';
 import PortalCommunicator from '../portal-communicator';
 import { PortalContext } from '../PortalContext';
 
@@ -9,7 +8,16 @@ export default class ErrorLogger extends React.Component<{}, {}> {
   public context!: PortalCommunicator;
 
   public componentDidCatch(error, info) {
-    this.context.logMessageDeprecated(LogEntryLevel.Error, error, info);
+    this.context.log({
+      action: 'componentDidCatch',
+      actionModifier: 'error',
+      resourceId: '',
+      logLevel: 'error',
+      data: {
+        error,
+        info,
+      },
+    });
   }
 
   public render() {
