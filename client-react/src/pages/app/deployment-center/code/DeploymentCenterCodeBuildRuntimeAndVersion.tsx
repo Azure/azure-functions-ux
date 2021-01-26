99f96558fff704a5a88a5a7e684229e04c2ea5eb
Mitren Chinoy <michinoy@microsoft.com>
2021-01-26 11:03:59 -0800

    more changes.

--- client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeBuildRuntimeAndVersion.tsx
+++ client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeBuildRuntimeAndVersion.tsx
@@ -169,9 +169,17 @@ const DeploymentCenterCodeBuildRuntimeAndVersion: React.FC<DeploymentCenterField
   const getRuntimeStackRecommendedVersion = (stackValue: string, runtimeVersionValue: string): string => {
     // NOTE(michinoy): As all the dotnet stacks are under one 'dotnet' section now, we need to do this manual mapping to get the correct
     // recommended version for github action.
-    const versionValue = runtimeVersionValue.toLocaleLowerCase();
-    const version =
-      stackValue === RuntimeStacks.dotnet && (versionValue === 'v5.0' || versionValue === 'dotnetcore|5.0') ? '5' : runtimeVersionValue;
+    let version = runtimeVersionValue;
+    if (stackValue === RuntimeStacks.dotnet) {
+      const versionValue = runtimeVersionValue.toLocaleLowerCase();
+      if (versionValue === 'v5.0' || versionValue === 'dotnetcore|5.0') {
+        version = '5';
+      } else if (versionValue === '3.1' || versionValue === 'dotnetcore|3.1') {
+        version = '3.1';
+      } else if (versionValue === '2.1' || versionValue === 'dotnetcore|2.1') {
+        version = '2.1';
+      }
+    }
 
     const key = generateGitHubActionRuntimeVersionMappingKey(siteStateContext.isLinuxApp, stackValue, version);
 
