62b24485bd7be1e289410995e3ead60ec393f185
Stephanie Pellegrino <stpelleg@microsoft.com>
2021-02-17 12:39:09 -0800

    logs timer

--- client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeLogsTimer.tsx
+++ client-react/src/pages/app/deployment-center/code/DeploymentCenterCodeLogsTimer.tsx
@@ -1,12 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { DeploymentCenterCodeLogsTimerProps } from '../DeploymentCenter.types';
-import { logsTimerStyle } from '../DeploymentCenter.styles';
 import { useTranslation } from 'react-i18next';
+import { CommandBar, ICommandBarItemProps } from 'office-ui-fabric-react';
+import { CommandBarStyles } from '../../../../theme/CustomOfficeFabric/AzurePortal/CommandBar.styles';
+import { CustomCommandBarButton } from '../../../../components/CustomCommandBarButton';
+import { getTelemetryInfo } from '../utility/DeploymentCenterUtility';
+import { PortalContext } from '../../../../PortalContext';
 
 const DeploymentCenterCodeLogsTimer: React.FC<DeploymentCenterCodeLogsTimerProps> = props => {
   const refreshMilliseconds = 30000;
   const [timeLeft, setTimeLeft] = useState(refreshMilliseconds / 1000);
   const { t } = useTranslation();
+
+  const portalContext = useContext(PortalContext);
 
   useEffect(() => {
     setInterval(() => {
@@ -24,7 +30,31 @@ const DeploymentCenterCodeLogsTimer: React.FC<DeploymentCenterCodeLogsTimerProps
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return <div className={logsTimerStyle}>{t('logsRefreshTimerMessage').format(timeLeft)}</div>;
+  const commandBarItems: ICommandBarItemProps[] = [
+    {
+      key: 'refresh',
+      name: t('refresh'),
+      iconProps: {
+        iconName: 'Refresh',
+      },
+      ariaLabel: t('deploymentCenterRefreshCommandAriaLabel'),
+      onClick: () => {
+        portalContext.log(getTelemetryInfo('verbose', 'refreshButton', 'clicked'));
+        props.refreshLogs();
+      },
+    },
+  ];
+
+  //return <div className={logsTimerStyle}>{t('logsRefreshTimerMessage').format(timeLeft)}</div>;
+  return (
+    <CommandBar
+      items={commandBarItems}
+      role="nav"
+      styles={CommandBarStyles}
+      ariaLabel={t('managePublishProfileCommandBarAriaLabel')}
+      buttonAs={CustomCommandBarButton}
+    />
+  );
 };
 
 export default DeploymentCenterCodeLogsTimer;
