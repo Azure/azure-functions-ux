5b42158d9fa785f40d2ddee52a5b768d63e8d01b
Francisco Gomez Gamino <frangom@microsoft.com>
2021-04-08 16:54:40 -0700

    [Stacks][Function App] Add PowerShell 7 on Linux Stacks API (hidden / preview)

--- server/src/stacks/2020-06-01/stacks/function-app-stacks/Powershell.ts
+++ server/src/stacks/2020-06-01/stacks/function-app-stacks/Powershell.ts
@@ -33,6 +33,24 @@ export const powershellStack: FunctionAppStack = {
               },
               supportedFunctionsExtensionVersions: ['~3'],
             },
+            linuxRuntimeSettings: {
+              runtimeVersion: 'PowerShell|7',
+              remoteDebuggingSupported: false,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              gitHubActionSettings: {
+                isSupported: true
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'powershell',
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: false,
+                linuxFxVersion: 'PowerShell|7',
+              },
+              supportedFunctionsExtensionVersions: ['~3'],
+            },
           },
         },
       ],
