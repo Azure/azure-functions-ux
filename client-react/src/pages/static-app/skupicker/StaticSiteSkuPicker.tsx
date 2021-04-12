78d1c7c2fe8431b085f1e1b2865a82debbfb0e64
Stephanie Pellegrino <stpelleg@microsoft.com>
2021-04-12 00:27:51 -0700

    organize

--- client-react/src/pages/static-app/skupicker/StaticSiteSkuPicker.tsx
+++ client-react/src/pages/static-app/skupicker/StaticSiteSkuPicker.tsx
@@ -1,8 +1,22 @@
+import { DefaultButton, IChoiceGroupOption } from 'office-ui-fabric-react';
 import React, { useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import RadioButtonNoFormik from '../../../components/form-controls/RadioButtonNoFormik';
+import { PortalContext } from '../../../PortalContext';
 import { ThemeContext } from '../../../ThemeContext';
-import { gridContainerStyle, gridHeaderItemStyle, gridItemStyle } from './StaticSiteSkuPicker.styles';
+import {
+  gridBottomSelectedItemStyle,
+  gridContainerStyle,
+  planFeatureItemStyle,
+  gridSelectedItemStyle,
+  gridUnselectedItemStyle,
+  radioButtonStyle,
+  skuTitleStyle,
+  skuDescriptionStyle,
+  planFeaturesTitleStyle,
+  skuTitleSelectedStyle,
+  skuTitleUnselectedStyle,
+} from './StaticSiteSkuPicker.styles';
 
 export interface StaticSiteSkuPickerProps {
   isStaticSiteCreate: boolean;
@@ -10,85 +24,230 @@ export interface StaticSiteSkuPickerProps {
   resourceId: string;
 }
 
-export enum StaticSiteSku {
-  Free = 'Free',
-  Standard = 'Standard',
-}
-
 const StaticSiteSkuPicker: React.FC<StaticSiteSkuPickerProps> = props => {
   const { isStaticSiteCreate, currentSku } = props;
   const { t } = useTranslation();
 
   const theme = useContext(ThemeContext);
+  const portalContext = useContext(PortalContext);
 
-  const [selectedSku, setSelectedSku] = useState<StaticSiteSku>(StaticSiteSku.Free);
+  const [selectedSku, setSelectedSku] = useState<string>('Free');
+  const [freeColumnClassName, setFreeColumnClassName] = useState<string>(gridSelectedItemStyle(theme));
+  const [standardColumnClassName, setStandardColumnClassName] = useState<string>(gridUnselectedItemStyle(theme));
 
   useEffect(() => {
-    setSelectedSku(currentSku === StaticSiteSku.Standard ? StaticSiteSku.Standard : StaticSiteSku.Free);
+    if (currentSku) {
+      setSelectedSku(currentSku);
+    }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  useEffect(() => {
+    const isStandardSelected = selectedSku === 'Standard';
+    setFreeColumnClassName(isStandardSelected ? gridUnselectedItemStyle(theme) : gridSelectedItemStyle(theme));
+    setStandardColumnClassName(isStandardSelected ? gridSelectedItemStyle(theme) : gridUnselectedItemStyle(theme));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedSku]);
+
+  const getHeaderRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeaturesTitleStyle(theme)}>{t('Plan/Features')}</div>
+        {getFreeSkuTitleSection()}
+        {getStandardSkuTitleSection()}
+      </>
+    );
+  };
+
+  const getFreeSkuTitleSection = (): JSX.Element => {
+    return (
+      <>
+        <div className={selectedSku === 'Free' ? skuTitleSelectedStyle(theme) : skuTitleUnselectedStyle(theme)}>
+          <div className={radioButtonStyle}>
+            <RadioButtonNoFormik
+              id="static-site-sku"
+              selectedKey={selectedSku}
+              options={[
+                {
+                  key: 'Free',
+                  text: '',
+                },
+              ]}
+              onChange={(e: any, configOptions: IChoiceGroupOption) => {
+                setSelectedSku(configOptions.key);
+              }}
+            />
+          </div>
+          <div className={skuTitleStyle}>{t('staticSiteFree')}</div>
+          <div className={skuDescriptionStyle}>{t('staticSiteFreeDescription')}</div>
+        </div>
+      </>
+    );
+  };
+
+  const getStandardSkuTitleSection = (): JSX.Element => {
+    return (
+      <>
+        <div className={selectedSku === 'Standard' ? skuTitleSelectedStyle(theme) : skuTitleUnselectedStyle(theme)}>
+          <div className={radioButtonStyle}>
+            <RadioButtonNoFormik
+              id="static-site-sku"
+              selectedKey={selectedSku}
+              options={[
+                {
+                  key: 'Standard',
+                  text: '',
+                },
+              ]}
+              onChange={(e: any, configOptions: IChoiceGroupOption) => {
+                setSelectedSku(configOptions.key);
+              }}
+            />
+          </div>
+          <div className={skuTitleStyle}>{t('staticSiteStandard')}</div>
+          <div className={skuDescriptionStyle}>{t('staticSiteStandardDescription')}</div>
+        </div>
+      </>
+    );
+  };
+
+  const getPriceRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('Price')}</div>
+        <div className={freeColumnClassName}>{t('staticSiteFree')}</div>
+        <div className={standardColumnClassName}>{t('staticSiteStandardPrice')}</div>
+      </>
+    );
+  };
+
+  const getIncludedBandwidthRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteIncludedBandwidth')}</div>
+        <div className={freeColumnClassName}>{t('staticSiteIncludedBandwidthAmount')}</div>
+        <div className={standardColumnClassName}>{t('staticSiteIncludedBandwidthAmount')}</div>
+      </>
+    );
+  };
+
+  const getBandwidthOverageRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteBandwidthOverage')}</div>
+        <div className={freeColumnClassName}>{t(' - ')}</div>
+        <div className={standardColumnClassName}>{t('staticSiteStandardBandwidthOverageAmount')}</div>
+      </>
+    );
+  };
+
+  const getCustomDomainsRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteCustomDomains')}</div>
+        <div className={freeColumnClassName}>{t('staticSiteFreeCustomDomainAmount')}</div>
+        <div className={standardColumnClassName}>{t('staticSiteStandardCustomDomainAmount')}</div>
+      </>
+    );
+  };
+
+  const getSslCertificatesRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteSslCertificates')}</div>
+        <div className={freeColumnClassName}>{t('staticSiteFree')}</div>
+        <div className={standardColumnClassName}>{t('staticSiteFree')}</div>
+      </>
+    );
+  };
+
+  const getCustomAuthenticationRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteCustomAuthentication')}</div>
+        <div className={freeColumnClassName}>{t(' - ')}</div>
+        <div className={standardColumnClassName}>{t('Yes')}</div>
+      </>
+    );
+  };
+
+  const getPrivateLinkRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSitePrivateLink')}</div>
+        <div className={freeColumnClassName}>{t(' - ')}</div>
+        <div className={standardColumnClassName}>{t('Yes')}</div>
+      </>
+    );
+  };
+
+  const getStorageRow = (): JSX.Element => {
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteStorage')}</div>
+        <div className={freeColumnClassName}>{t('staticSiteFreeStorageAmount')}</div>
+        <div className={standardColumnClassName}>{t('staticSiteStandardStorageAmount')}</div>
+      </>
+    );
+  };
+
+  const getAzureFunctionsRow = (): JSX.Element => {
+    const isFreeSelected: boolean = selectedSku === 'Free';
+    return (
+      <>
+        <div className={planFeatureItemStyle(theme)}>{t('staticSiteAzureFunctions')}</div>
+        <div className={isFreeSelected ? gridBottomSelectedItemStyle(theme) : freeColumnClassName}>
+          {t('staticSiteFreeAzureFunctionsAmount')}
+        </div>
+        <div className={!isFreeSelected ? gridBottomSelectedItemStyle(theme) : standardColumnClassName}>
+          {t('staticSiteStandardAzureFunctionsAmount')}
+        </div>
+      </>
+    );
+  };
+
+  const gridRows: JSX.Element[] = [
+    getHeaderRow(),
+    getPriceRow(),
+    getIncludedBandwidthRow(),
+    getBandwidthOverageRow(),
+    getCustomDomainsRow(),
+    getSslCertificatesRow(),
+    getCustomAuthenticationRow(),
+    getPrivateLinkRow(),
+    getStorageRow(),
+    getAzureFunctionsRow(),
+  ];
 
   return (
     <>
       {isStaticSiteCreate && <h3>{t('staticSitePlanComparison')}</h3>}
 
-      <div className={gridContainerStyle}>
-        <div className={gridHeaderItemStyle(theme)}>{t('Plan/Features')}</div>
-        <div className={gridHeaderItemStyle(theme)}>
-          {t('Free')}
-          <RadioButtonNoFormik
-            id="static-site-sku"
-            selectedKey={selectedSku}
-            options={[
-              {
-                key: StaticSiteSku.Free,
-                text: t('Free'),
-              },
-            ]}
-          />
-        </div>
-        <div className={gridHeaderItemStyle(theme)}>
-          {t('Standard')}
-          <RadioButtonNoFormik
-            id="static-site-sku"
-            selectedKey={selectedSku}
-            options={[
-              {
-                key: StaticSiteSku.Standard,
-                text: t('Standard'),
-              },
-            ]}
-          />
-        </div>
-        <div className={gridHeaderItemStyle(theme)}>{t('Price')}</div>
-        <div className={gridItemStyle(theme)}>{t('Free')}</div>
-        <div className={gridItemStyle(theme)}>{t('$9/month')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteIncludedBandwidth')}</div>
-        <div className={gridItemStyle(theme)}>{t('100GB per subscription')}</div>
-        <div className={gridItemStyle(theme)}>{t('100GB per subscription')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteBandwidthCoverage')}</div>
-        <div className={gridItemStyle(theme)}>{t(' - ')}</div>
-        <div className={gridItemStyle(theme)}>{t('$0.02 perGB per subscription')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteCustomDomains')}</div>
-        <div className={gridItemStyle(theme)}>{t('2 per app')}</div>
-        <div className={gridItemStyle(theme)}>{t('5 per app')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteSslCertificates')}</div>
-        <div className={gridItemStyle(theme)}>{t('Free')}</div>
-        <div className={gridItemStyle(theme)}>{t('Free')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteCustomAuthentication')}</div>
-        <div className={gridItemStyle(theme)}>{t(' - ')}</div>
-        <div className={gridItemStyle(theme)}>{t('Yes')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSitePrivateLink')}</div>
-        <div className={gridItemStyle(theme)}>{t(' - ')}</div>
-        <div className={gridItemStyle(theme)}>{t('Yes')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteStorage')}</div>
-        <div className={gridItemStyle(theme)}>{t('0.25 GB')}</div>
-        <div className={gridItemStyle(theme)}>{t('2GB')}</div>
-        <div className={gridHeaderItemStyle(theme)}>{t('staticSiteAzureFunctions')}</div>
-        <div className={gridItemStyle(theme)}>{t('Managed')}</div>
-        <div className={gridItemStyle(theme)}>{t('Managed or bring your own')}</div>
-      </div>
+      <div className={gridContainerStyle}>{gridRows}</div>
+
+      {isStaticSiteCreate && (
+        <DefaultButton
+          text={t('staticSiteApply')}
+          ariaLabel={t('staticSiteApply')}
+          onClick={() => {
+            portalContext.closeSelf(currentSku);
+          }}
+          disabled={currentSku === selectedSku}
+        />
+      )}
+
+      {!isStaticSiteCreate && (
+        <DefaultButton
+          text={t('Save')}
+          ariaLabel={t('Save')}
+          onClick={() => {
+            //TODO (stpelleg): billing meter implementation
+          }}
+          disabled={currentSku === selectedSku}
+        />
+      )}
     </>
   );
 };
