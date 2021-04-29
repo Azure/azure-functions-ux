abb7a76bf9923e58b04bd07698efeff95387c500
Aniello Scotto Di Marco <andimarc@microsoft.com>
2021-04-29 09:26:51 -0700

    update stacks calls to use Fusion API for USNat and USSec (#5929)

--- client-react/src/ApiHelpers/RuntimeStackService.ts
+++ client-react/src/ApiHelpers/RuntimeStackService.ts
@@ -4,10 +4,17 @@ import { WebAppStack } from '../models/stacks/web-app-stacks';
 import { FunctionAppStack } from '../models/stacks/function-app-stacks';
 import { AppStackOs } from '../models/stacks/app-stacks';
 import MakeArmCall from './ArmHelper';
-import { ArmArray } from '../models/arm-obj';
+import { ArmArray, ArmObj } from '../models/arm-obj';
+import { sendHttpRequest } from './HttpClient';
+import { HttpResponseObject } from '../ArmHelper.types';
+import { NationalCloudEnvironment } from '../utils/scenario-checker/national-cloud.environment';
 
 export default class RuntimeStackService {
   public static getWebAppConfigurationStacks = (stacksOs: AppStackOs) => {
+    if (RuntimeStackService._useFusionApi()) {
+      return RuntimeStackService._getWebAppConfigurationStacksNonArm(stacksOs);
+    }
+
     const queryParams = [
       `stackOsType=${stacksOs}`,
       `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
@@ -23,6 +30,10 @@ export default class RuntimeStackService {
   };
 
   public static getFunctionAppConfigurationStacks = (stacksOs: AppStackOs) => {
+    if (RuntimeStackService._useFusionApi()) {
+      return RuntimeStackService._getFunctionAppConfigurationStacksNonArm(stacksOs);
+    }
+
     const queryParams = [
       `stackOsType=${stacksOs}`,
       `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
@@ -38,6 +49,10 @@ export default class RuntimeStackService {
   };
 
   public static getWebAppGitHubActionStacks = async (stacksOs: AppStackOs) => {
+    if (RuntimeStackService._useFusionApi()) {
+      return RuntimeStackService._getWebAppGitHubActionStacksNonArm(stacksOs);
+    }
+
     const queryParams = [
       `stackOsType=${stacksOs}`,
       `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
@@ -55,6 +70,10 @@ export default class RuntimeStackService {
   };
 
   public static getFunctionAppGitHubActionStacks = async (stacksOs: AppStackOs) => {
+    if (RuntimeStackService._useFusionApi()) {
+      return RuntimeStackService._getFunctionAppGitHubActionStacksNonArm(stacksOs);
+    }
+
     const queryParams = [
       `stackOsType=${stacksOs}`,
       `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
@@ -75,4 +94,87 @@ export default class RuntimeStackService {
     const flagValue = Url.getFeatureValue(CommonConstants.FeatureFlags.showHiddenStacks);
     return flagValue && flagValue.toLocaleLowerCase() === 'true';
   };
+
+  private static _useFusionApi() {
+    return NationalCloudEnvironment.isUSNat() || NationalCloudEnvironment.isUSSec();
+  }
+
+  private static _getWebAppConfigurationStacksNonArm = (stacksOs: AppStackOs) => {
+    const queryParams = [
+      `os=${stacksOs}`,
+      `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
+      `api-version=${CommonConstants.ApiVersions.stacksApiVersion20201001}`,
+    ];
+
+    const url = `${Url.serviceHost}stacks/webAppStacks?${queryParams.join('&')}`;
+    return RuntimeStackService._getStacksResponse<WebAppStack>(url);
+  };
+
+  private static _getFunctionAppConfigurationStacksNonArm = (stacksOs: AppStackOs) => {
+    const queryParams = [
+      `os=${stacksOs}`,
+      `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
+      `api-version=${CommonConstants.ApiVersions.stacksApiVersion20201001}`,
+    ];
+
+    const url = `${Url.serviceHost}stacks/functionAppStacks?${queryParams.join('&')}`;
+    return RuntimeStackService._getStacksResponse<FunctionAppStack>(url);
+  };
+
+  private static _getWebAppGitHubActionStacksNonArm = async (stacksOs: AppStackOs) => {
+    const queryParams = [
+      `os=${stacksOs}`,
+      `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
+      `removeDeprecatedStacks=${true}`,
+      `removeNonGitHubActionStacks=${true}`,
+      `api-version=${CommonConstants.ApiVersions.stacksApiVersion20201001}`,
+    ];
+
+    const url = `${Url.serviceHost}stacks/webAppStacks?${queryParams.join('&')}`;
+    return RuntimeStackService._getStacksResponse<WebAppStack>(url);
+  };
+
+  private static _getFunctionAppGitHubActionStacksNonArm = async (stacksOs: AppStackOs) => {
+    const queryParams = [
+      `os=${stacksOs}`,
+      `removeHiddenStacks=${!RuntimeStackService._isShowHiddenStackFlagPassed()}`,
+      `removeDeprecatedStacks=${true}`,
+      `removeNonGitHubActionStacks=${true}`,
+      `api-version=${CommonConstants.ApiVersions.stacksApiVersion20201001}`,
+    ];
+
+    const url = `${Url.serviceHost}stacks/functionAppStacks?${queryParams.join('&')}`;
+    return RuntimeStackService._getStacksResponse<FunctionAppStack>(url);
+  };
+
+  private static _getStacksResponse = async <T>(url: string) => {
+    const stacksResponse = await sendHttpRequest<T[]>({
+      url,
+      method: 'GET',
+    });
+
+    const success = stacksResponse.metadata.success && !!stacksResponse.data;
+    const mappedResult: HttpResponseObject<ArmArray<T>> = {
+      ...stacksResponse,
+      metadata: {
+        ...stacksResponse.metadata,
+        success,
+      },
+      data: RuntimeStackService._convertToArmArrayResult(success ? stacksResponse.data : []),
+    };
+
+    return mappedResult;
+  };
+
+  private static _convertToArmArrayResult<T>(data: T[]): ArmArray<T> {
+    const armObjectArray: ArmObj<T>[] = data.map(stack => {
+      return {
+        id: '',
+        location: '',
+        name: '',
+        properties: stack,
+      };
+    });
+    return { value: armObjectArray };
+  }
 }
