82796b8e66d013e6163322e325184b6dd898134e
liuqidake <qiliuchina@yahoo.com>
2021-05-26 13:25:45 -0700

    use siteStateContext

--- client-react/src/pages/app/app-settings/Sections/PathMappingsPivot.tsx
+++ client-react/src/pages/app/app-settings/Sections/PathMappingsPivot.tsx
@@ -6,13 +6,14 @@ import HandlerMappings from '../HandlerMappings/HandlerMappings';
 import VirtualApplications from '../VirtualApplications/VirtualApplications';
 import { isEqual } from 'lodash-es';
 import AzureStorageMounts from '../AzureStorageMounts/AzureStorageMounts';
-import { PermissionsContext, SiteContext } from '../Contexts';
+import { PermissionsContext } from '../Contexts';
 import { MessageBarType, Link } from 'office-ui-fabric-react';
 import { learnMoreLinkStyle } from '../../../../components/form-controls/formControl.override.styles';
 import { Links } from '../../../../utils/FwLinks';
 import CustomBanner from '../../../../components/CustomBanner/CustomBanner';
 import { ScenarioService } from '../../../../utils/scenario-checker/scenario.service';
 import { ScenarioIds } from '../../../../utils/scenario-checker/scenario-ids';
+import { SiteStateContext } from '../../../../SiteState';
 
 interface PathMappingsPivotProps {
   enablePathMappings: boolean;
@@ -22,7 +23,8 @@ const PathMappingsPivot: React.FC<FormikProps<AppSettingsFormValues> & PathMappi
   const { enablePathMappings, enableAzureStorageMount } = props;
   const { t } = useTranslation();
   const { app_write } = useContext(PermissionsContext);
-  const site = useContext(SiteContext);
+  const siteState = useContext(SiteStateContext);
+  const site = siteState && siteState.site;
   const scenarioCheckerRef = useRef(new ScenarioService(t));
   const scenarioChecker = scenarioCheckerRef.current!;
 
