a9f6bd0438280016c296be205f1a3ad73db56e7e
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2021-06-07 12:38:51 -0700

    [DC] Update .NET 6 GitHub Actions workflows for preview (#5996)
    * dotnet6version
    
    * new update

--- client-react/src/pages/app/deployment-center/utility/GitHubActionUtility.ts
+++ client-react/src/pages/app/deployment-center/utility/GitHubActionUtility.ts
@@ -573,6 +573,7 @@ jobs:
       uses: actions/setup-dotnet@v1
       with:
         dotnet-version: '${runtimeStackVersion}'
+        include-prerelease: true
 
     - name: Build with dotnet
       run: dotnet build --configuration Release
