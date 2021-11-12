751f708d503f5eafaeaf98cafd4825a51aa357bf
Krrish Mittal <krrishmittal15@gmail.com>
2021-11-12 13:12:27 -0800

    merge changes

--- client-react/src/ApiHelpers/FunctionsService.ts
+++ client-react/src/ApiHelpers/FunctionsService.ts
@@ -12,6 +12,8 @@ import { Host } from '../models/functions/host';
 import { VfsObject } from '../models/functions/vfs';
 import { KeyValue } from '../models/portal-models';
 import { ContainerItem, ShareItem } from '../pages/app/app-settings/AppSettings.types';
+import { Method } from 'axios';
+import { NetAjaxSettings } from '../models/ajax-request-model';
 
 export default class FunctionsService {
   public static getHostStatus = (resourceId: string) => {
@@ -229,6 +231,27 @@ export default class FunctionsService {
       url: `${Url.serviceHost}/api/getStorageFileShares?accountName=${accountName}`,
       method: 'POST',
     });
+  }
+
+  public static runFunction(settings: NetAjaxSettings) {
+    const url = settings.uri;
+    const method = settings.type as Method;
+    const headers = settings.headers || {};
+    const data = settings.data;
+
+    return sendHttpRequest({ url, method, headers, data }).catch(err => {
+      return this.tryPassThroughController(err, url, method, headers, data);
+    });
+  }
+
+  private static tryPassThroughController(err: any, url: string, method: Method, headers: KeyValue<string>, body: any) {
+    const passthroughBody = {
+      url,
+      headers,
+      method,
+      body,
+    };
+    return sendHttpRequest({ url: `${Url.serviceHost}api/passthrough`, method: 'POST', data: passthroughBody });
   }
 
   private static _getVfsApiForRuntimeVersion(endpoint: string, runtimeVersion?: string) {
