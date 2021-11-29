9b19c7e1f4dd2be257f16a7b32866698f3627c14
Krrish Mittal 🕺 ⚽️ <krmitta@microsoft.com>
2021-11-29 15:21:45 -0800

    add dropdowns

--- client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeVersionNew.tsx
+++ client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeVersionNew.tsx
@@ -1,5 +1,7 @@
 // TODO (krmitta):  Rename the file after this version is tested
 import React, { useContext, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import DropdownNoFormik from '../../../../components/form-controls/DropDownnoFormik';
 import { RuntimeExtensionMajorVersions } from '../../../../models/functions/runtime-extension';
 import { SiteStateContext } from '../../../../SiteState';
 import { CommonConstants } from '../../../../utils/CommonConstants';
@@ -10,14 +12,16 @@ import {
   isWindowsNodeApp,
 } from '../../../../utils/stacks-utils';
 import { AppSettingsFormProps } from '../AppSettings.types';
-import { FunctionAppStacksContext } from '../Contexts';
+import { FunctionAppStacksContext, PermissionsContext } from '../Contexts';
 
 const RuntimeVersion: React.FC<AppSettingsFormProps> = props => {
   const { values } = props;
+  const { t } = useTranslation();
 
   const [stackSupportedRuntimeVersions, setStackSupportedRuntimeVersions] = useState<RuntimeExtensionMajorVersions[]>([]);
   const [selectedRuntimeVersion, setselectedRuntimeVersion] = useState<string>(RuntimeExtensionMajorVersions.custom);
 
+  const { app_write, editable } = useContext(PermissionsContext);
   const siteStateContext = useContext(SiteStateContext);
   const functionAppStacksContext = useContext(FunctionAppStacksContext);
 
@@ -89,7 +93,31 @@ const RuntimeVersion: React.FC<AppSettingsFormProps> = props => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [values.currentlySelectedStack, values.appSettings, values.config]);
 
-  return <></>;
+  return (
+    <>
+      {app_write && editable ? (
+        <DropdownNoFormik
+          onChange={() => null}
+          options={stackSupportedRuntimeVersions.map(version => ({
+            key: version.toLocaleLowerCase(),
+            text: version.toLocaleLowerCase(),
+          }))}
+          selectedKey={selectedRuntimeVersion}
+          disabled={false}
+          label={t('runtimeVersion')}
+          id="function-app-settings-runtime-version"
+        />
+      ) : (
+        <DropdownNoFormik
+          onChange={() => null}
+          options={[]}
+          disabled={true}
+          label={t('runtimeVersion')}
+          id="function-app-settings-runtime-version"
+        />
+      )}
+    </>
+  );
 };
 
 export default RuntimeVersion;
