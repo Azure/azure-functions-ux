d907f6c8ec057a1cec6183c5e3974e9f70384f24
Krrish Mittal <krrishmittal15@gmail.com>
2022-01-12 16:08:55 -0800

    Update functions and test_data calls (#6441)
    * get correct jqXHR object
    
    * update 1
    
    Co-authored-by: Krrish Mittal 🕺 ⚽️ <krmitta@microsoft.com>

--- client-react/src/pages/app/functions/function/function-editor/FunctionEditorDataLoader.tsx
+++ client-react/src/pages/app/functions/function/function-editor/FunctionEditorDataLoader.tsx
@@ -30,7 +30,7 @@ import { useTranslation } from 'react-i18next';
 import { CommonConstants } from '../../../../../utils/CommonConstants';
 import { NetAjaxSettings } from '../../../../../models/ajax-request-model';
 import { PortalContext } from '../../../../../PortalContext';
-import { isPortalCommunicationStatusSuccess } from '../../../../../utils/portal-utils';
+import { getJQXHR, isPortalCommunicationStatusSuccess } from '../../../../../utils/portal-utils';
 import { getJsonHeaders } from '../../../../../ApiHelpers/HttpClient';
 import { SiteStateContext } from '../../../../../SiteState';
 import SiteHelper from '../../../../../utils/SiteHelper';
@@ -342,7 +342,7 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
         uri: url,
         type: testDataObject.method as string,
         headers: headers,
-        data: JSON.stringify(testDataObject.body),
+        data: testDataObject.body,
       };
     }
     return undefined;
@@ -402,7 +402,10 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
 
     const runFunctionResponse = await portalContext.makeHttpRequestsViaPortal(settings);
     const runFunctionResponseResult = runFunctionResponse.result;
-    response.code = runFunctionResponseResult.jqXHR.status;
+    const jqXHR = getJQXHR(runFunctionResponse, LogCategories.FunctionEdit, 'makeHttpRequestForRunFunction');
+    if (!!jqXHR) {
+      response.code = jqXHR.status;
+    }
 
     if (isPortalCommunicationStatusSuccess(runFunctionResponse.status)) {
       response.text = runFunctionResponseResult.content;
@@ -410,14 +413,16 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
       if (response.code !== 200) {
         LogService.error(
           LogCategories.FunctionEdit,
-          'runFunction',
+          'makeHttpRequestForRunFunction',
           `Failed to run function: ${getErrorMessageOrStringify(runFunctionResponseResult)}`
         );
       }
     } else {
       // NOTE(krmitta): This happens when the http request on the portal fails for some reason,
       // not the api returning the error
-      response.text = runFunctionResponseResult;
+      if (!!jqXHR) {
+        response.text = jqXHR.statusText;
+      }
       LogService.error(
         LogCategories.FunctionEdit,
         'makeHttpRequestForRunFunction',
@@ -467,13 +472,6 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
     }
   };
 
-  const getAuthorizationHeaders = (): KeyValue<string> => {
-    return {
-      Authorization: `Bearer ${startupInfoContext.token}`,
-      FunctionsPortal: '1',
-    };
-  };
-
   const getAndSetTestData = async () => {
     if (!!functionInfo && !!site && !!functionInfo.properties.test_data_href) {
       const testDataHrefObjects = functionInfo.properties.test_data_href.split('/vfs/');
@@ -497,12 +495,11 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
       // Note (krmitta): Almost always we should be able to get the test_data through VFS Arm.
       // Adding the below fallback logic just on the off-chance that it doesn't.
       if (!testDataResponseSuccess) {
-        const headers = getAuthorizationHeaders();
+        const headers = getHeaders([], getDefaultXFunctionKey());
         const functionHrefTestDataResponse = await portalContext.makeHttpRequestsViaPortal({
           uri: functionInfo.properties.test_data_href,
           type: 'GET',
           headers: headers,
-          setAuthorizationHeader: true,
         });
 
         if (isPortalCommunicationStatusSuccess(functionHrefTestDataResponse.status)) {
@@ -511,7 +508,7 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
           LogService.error(
             LogCategories.FunctionEdit,
             'GetTestDataUsingFunctionHref',
-            `Failed to get test data: ${getErrorMessageOrStringify(functionHrefTestDataResponse.result)}`
+            `Failed to get test data: ${getErrorMessageOrStringify(functionHrefTestDataResponse)}`
           );
         }
       }
