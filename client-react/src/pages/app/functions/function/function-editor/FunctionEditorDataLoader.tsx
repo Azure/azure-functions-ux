61df325f7b23dabd618ff2bb30d40cef706e3869
Krrish Mittal <krrishmittal15@gmail.com>
2022-01-24 10:39:18 -0800

    cherry-pick (#6456)
    Co-authored-by: Krrish Mittal 🕺 ⚽️ <krmitta@microsoft.com>

--- client-react/src/pages/app/functions/function/function-editor/FunctionEditorDataLoader.tsx
+++ client-react/src/pages/app/functions/function/function-editor/FunctionEditorDataLoader.tsx
@@ -361,7 +361,7 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
         uri: url,
         type: 'POST',
         headers: headers,
-        data: newFunctionInfo.properties.test_data || '',
+        data: JSON.stringify({ input: newFunctionInfo.properties.test_data || '' }),
       };
     }
     return undefined;
@@ -502,8 +502,10 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = props 
           headers: headers,
         });
 
+        const result = functionHrefTestDataResponse.result;
+
         if (isPortalCommunicationStatusSuccess(functionHrefTestDataResponse.status)) {
-          testData = functionHrefTestDataResponse.result;
+          testData = !!result && !!result.content ? result.content : result;
         } else {
           LogService.error(
             LogCategories.FunctionEdit,
