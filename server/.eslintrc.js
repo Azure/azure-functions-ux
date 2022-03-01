3da52765c6f401839085a7d5860022e2fe7a35b9
Joe Chung <joechung@microsoft.com>
2022-02-28 20:51:46 -0500

    chore: Enable linting with ESLint in server

---
+++ server/.eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {},
+};
