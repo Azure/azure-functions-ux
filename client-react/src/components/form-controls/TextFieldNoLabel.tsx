b96c8617607636a14258ced0b51790d59915acd7
Joe Chung <joechung@microsoft.com>
2022-03-10 20:51:04 -0500

    fix: Address `react/no-deprecated` lint errors (#6542)

--- client-react/src/components/form-controls/TextFieldNoLabel.tsx
+++ client-react/src/components/form-controls/TextFieldNoLabel.tsx
@@ -12,7 +12,7 @@ interface EventMsg {
 class TextField extends React.Component<FieldProps & ITextFieldProps, any> {
   private inputDebouncer = new Subject<EventMsg>();
   private readonly DEBOUNCE_TIME = 300;
-  public componentWillMount() {
+  public UNSAFE_componentWillMount() {
     const { field, form } = this.props;
     this.inputDebouncer.pipe(debounceTime(this.DEBOUNCE_TIME)).subscribe(({ e, value }) => {
       form.setFieldValue(field.name, value);
