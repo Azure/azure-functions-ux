5b6399261e9f27302bede5bd25a3a5eb8cf3a836
miyukiabe-baxter <miyuki0809@gmail.com>
2022-03-23 16:55:24 -0700

    Config Menu Change Only - 10023302 [SWA] - Additional Changes request - stable environments (#6594)
    Co-authored-by: Miyuki Abe-Baxter <miabebax@microsoft.com>

--- client-react/src/pages/static-app/StaticSiteUtility.tsx
+++ client-react/src/pages/static-app/StaticSiteUtility.tsx
@@ -1,6 +1,8 @@
 import { KeyValue } from '../../models/portal-models';
 import { LogLevel, TelemetryInfo } from '../../models/telemetry';
 import { PasswordProtectionTypes } from './configuration/Configuration.types';
+import { ArmObj } from '../../models/arm-obj';
+import { Environment } from '../../models/static-site/environment';
 
 export const getTelemetryInfo = (
   logLevel: LogLevel,
@@ -41,4 +43,11 @@ export const stringToPasswordProtectionType = (passwordProtection: string) => {
     default:
       return PasswordProtectionTypes.Disabled;
   }
+};
+
+// NOTE: This is for non-stable and stable environment (non-prod). 'buildId' property value should be available for both stable and non-stable,
+// but not 'pullRequestTitle' property value. Stable env's 'pullRequestTitle' will be 'null'.
+// If 'pullRequestTitle' has value, we would like to display the title. Otherwise, we will display buildId.
+export const getPreviewsTitleValue = (environment: ArmObj<Environment>) => {
+  return environment.properties.pullRequestTitle || environment.properties.buildId;
 };
