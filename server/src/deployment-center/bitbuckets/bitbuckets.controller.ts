978e07b0b3281e29b676d8335f27fd28b69713cc
TianxiangSun <tianxiangsun@microsoft.com>
2022-04-06 11:42:34 -0700

    Disable bitbucket environment check in OnPrem

--- server/src/deployment-center/bitbuckets/bitbuckets.controller.ts
+++ server/src/deployment-center/bitbuckets/bitbuckets.controller.ts
@@ -59,7 +59,7 @@ export class BitbucketsController {
   async getToken(@Session() session, @Body('redirUrl') redirUrl: string, @Headers('origin') origin: string) {
     const state = this.dcService.getParameterByName('state', redirUrl);
     const environment = this.dcService.getEnvironment(origin);
-    if (!environment) {
+    if (!environment && !this.envIsOnPrem) {
       throw new HttpException('Invalid Environment', 403);
     }
     if (
