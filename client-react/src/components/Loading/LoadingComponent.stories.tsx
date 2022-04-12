09cc19dcfeaffa0fd557e112fae22ffbe72b7a68
Joe Chung <joechung@microsoft.com>
2022-04-11 20:40:34 -0400

    Remove unused Storybook code and dependencies (#6635)

--- client-react/src/components/Loading/LoadingComponent.stories.tsx
+++
@@ -1,4 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import LoadingComponent from './LoadingComponent';
-
-storiesOf('Loading Component', module).add('Loading with no errors', () => <LoadingComponent />);
