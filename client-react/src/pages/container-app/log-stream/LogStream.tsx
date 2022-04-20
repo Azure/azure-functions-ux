e58e1283acd9bf53b0136cb163853cb70ad7a258
Eva Theodoridou <23189351+etheodoridou@users.noreply.github.com>
2022-04-20 16:52:21 -0700

    [ContainerApps][Console][LogStream] Console resize (#6652)
    * [ContainerApps][Console][LogStream] Console resize
    
    Co-authored-by: Aniello Scotto Di Marco <andimarc@microsoft.com>

--- client-react/src/pages/container-app/log-stream/LogStream.tsx
+++ client-react/src/pages/container-app/log-stream/LogStream.tsx
@@ -1,6 +1,8 @@
-import React, { useContext, useEffect, useRef } from 'react';
-import { PortalContext } from '../../../PortalContext';
+import React, { useContext, useEffect, useMemo, useRef } from 'react';
+import useWindowSize from 'react-use/lib/useWindowSize';
+import { debounce } from 'lodash-es';
 import { XTerm } from 'xterm-for-react';
+import { PortalContext } from '../../../PortalContext';
 
 export interface LogStreamProps {
   resourceId: string;
@@ -8,10 +10,12 @@ export interface LogStreamProps {
   line?: string;
 }
 
-const LogStream: React.SFC<LogStreamProps> = props => {
+const LogStream: React.SFC<LogStreamProps> = (props) => {
   const portalCommunicator = useContext(PortalContext);
 
+  const { width, height } = useWindowSize();
   const terminalRef = useRef<XTerm>(null);
+  const timeoutRef = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
     portalCommunicator.loadComplete();
@@ -29,15 +33,34 @@ const LogStream: React.SFC<LogStreamProps> = props => {
     }
   }, [props.line]);
 
+  const resizeHandler = (width: number, height: number) => {
+    const columns = Math.floor(width / 9 - 0.5) - 2;
+    const rows = Math.floor(height / 17 - 0.5);
+    if (terminalRef.current?.terminal) {
+      terminalRef.current?.terminal.resize(columns, rows);
+      timeoutRef.current = undefined;
+    }
+  };
+
+  const debouncedResizeHandler = useMemo(() => debounce(resizeHandler, 300, { trailing: true, leading: true }), []);
+
+  useEffect(() => {
+    return () => debouncedResizeHandler.cancel();
+  }, []);
+
+  useEffect(() => {
+    debouncedResizeHandler(width, height);
+  }, [width, height]);
+
   return (
-    <>
+    <div style={{ height: '100vh', overflow: 'hidden' }}>
       <XTerm
         options={{
           disableStdin: true,
         }}
         ref={terminalRef}
       />
-    </>
+    </div>
   );
 };
 
