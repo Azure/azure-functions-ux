f8a80079bb7221496e53a14f2bdc81dc8e72d04d
Eva Theodoridou <evtheodo@microsoft.com>
2022-04-25 10:12:57 -0700

    oopsie

--- client-react/src/pages/container-app/ContainerApp.styles.ts
+++ client-react/src/pages/container-app/ContainerApp.styles.ts
@@ -1,16 +1,8 @@
 import { mergeStyleSets } from '@fluentui/react/lib/Styling';
 
 export const containerAppStyles = mergeStyleSets({
-  customTextField: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
   divContainer: {
     height: '100vh',
     overflow: 'hidden',
-  },
-  dialogFooterAlignLeft: {
-    textAlign: 'left',
   },
 });
