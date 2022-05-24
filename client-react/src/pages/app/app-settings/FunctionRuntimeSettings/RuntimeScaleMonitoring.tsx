5e94e92edfc83d2a46feada1ac51cf1745adfff7
Krrish Mittal <takyyon@Krrishs-MacBook-Pro.local>
2022-05-24 14:57:04 -0700

    update

--- client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeScaleMonitoring.tsx
+++ client-react/src/pages/app/app-settings/FunctionRuntimeSettings/RuntimeScaleMonitoring.tsx
@@ -17,7 +17,7 @@ const RuntimeScaleMonitoring: React.FC<AppSettingsFormProps & WithTranslation> =
   const disableAllControls = !app_write || !editable || saving;
 
   const scaleMonitoringSupported = () => {
-    let isSupported = false;
+    let isSupported = true;
     const initialRuntimeVersion = findFormAppSettingValue(
       initialValues.appSettings,
       CommonConstants.AppSettingNames.functionsExtensionVersion
@@ -28,10 +28,10 @@ const RuntimeScaleMonitoring: React.FC<AppSettingsFormProps & WithTranslation> =
       const exactRuntimeVersionParsed = FunctionsRuntimeVersionHelper.parseExactRuntimeVersion(exactRuntimeVersion || null);
       const initialRuntimeVersionParsed = FunctionsRuntimeVersionHelper.parseConfiguredRuntimeVersion(initialRuntimeVersion);
 
-      const supportedVersions = [RuntimeExtensionMajorVersions.v2, RuntimeExtensionMajorVersions.v3, RuntimeExtensionMajorVersions.v4];
-      supportedVersions.forEach(version => {
+      const nonSupportedVersions = [RuntimeExtensionMajorVersions.v1];
+      nonSupportedVersions.forEach(version => {
         if (initialRuntimeVersionParsed === version || exactRuntimeVersionParsed === version) {
-          isSupported = true;
+          isSupported = false;
         }
       });
     }
