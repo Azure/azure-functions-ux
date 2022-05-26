1cd3e7cb1284ac6fe808c1dab798f39a6d53f264
spellegrino021 <stpelleg@microsoft.com>
2022-05-26 14:17:28 -0700

    |

--- client-react/src/pages/app/log-stream/LogStream.types.ts
+++ client-react/src/pages/app/log-stream/LogStream.types.ts
@@ -29,7 +29,7 @@ export class LogRegex {
   public static readonly infoLogUTC: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+)(\Z|Z:) (\[Info|INFO|\[INFO)/;
   public static readonly errorLog: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+) (\[Error|ERROR|\[ERROR)/;
   public static readonly errorLogUTC: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+)(\Z|Z:) (\[Error|ERROR|\[ERROR)/;
-  public static readonly warningLog: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+) (\[Warning|WARNING\[WARNING)/;
-  public static readonly warningLogUTC: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+)(\Z|Z:) (\[Warning|WARNING\[WARNING)/;
+  public static readonly warningLog: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+) (\[Warning|WARNING|\[WARNING)/;
+  public static readonly warningLogUTC: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2}\.\d+)(\Z|Z:) (\[Warning|WARNING|\[WARNING)/;
   public static readonly log: RegExp = /^(\d{4}-\d{2}-\d{2})[T\s](\d{2}:\d{2}:\d{2})/;
 }
