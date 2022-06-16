acdc339533e8b2cfb6cc2ddc887990c39ec0b78f
kaibocai <89094811+kaibocai@users.noreply.github.com>
2022-06-16 08:51:22 -0500

    add java17

--- server/src/stacks/2020-10-01/stacks/function-app-stacks/Java.ts
+++ server/src/stacks/2020-10-01/stacks/function-app-stacks/Java.ts
@@ -6,6 +6,63 @@ export const javaStack: FunctionAppStack = {
   preferredOs: 'windows',
   majorVersions: [
     {
+      displayText: 'Java 17',
+      value: '17',
+      minorVersions: [
+        {
+          displayText: 'Java 17',
+          value: '17.0',
+          stackSettings: {
+            windowsRuntimeSettings: {
+              runtimeVersion: '17',
+              isPreview: true,
+              isHidden: true,
+              isAutoUpdate: true,
+              remoteDebuggingSupported: false,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              gitHubActionSettings: {
+                isSupported: true,
+                supportedVersion: '17',
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'java',
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: true,
+                javaVersion: '17',
+                netFrameworkVersion: 'v6.0',
+              },
+              supportedFunctionsExtensionVersions: ['~4'],
+            },
+            linuxRuntimeSettings: {
+              runtimeVersion: 'Java|17',
+              isPreview: true,
+              isHidden: true,
+              isAutoUpdate: true,
+              remoteDebuggingSupported: false,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              gitHubActionSettings: {
+                isSupported: true,
+                supportedVersion: '17',
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'java',
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: false,
+                linuxFxVersion: 'Java|17',
+              },
+              supportedFunctionsExtensionVersions: ['~4'],
+            },
+          },
+        },
+      ],
+    },
+    {
       displayText: 'Java 11',
       value: '11',
       minorVersions: [
