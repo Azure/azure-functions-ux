7986dd307047d8abe085d1f13facaca40dd81663
Byron Tardif <byvinyal@microsoft.com>
2022-08-19 11:33:29 -0700

    missed updateing 7.4 EOL in 2020-10-01

--- server/src/stacks/2020-10-01/stacks/web-app-stacks/Php.ts
+++ server/src/stacks/2020-10-01/stacks/web-app-stacks/Php.ts
@@ -5,7 +5,7 @@ const getPhpStack: (useIsoDateFormat: boolean) => WebAppStack = (useIsoDateForma
   const php8Point2EOL = getDateString(new Date(2025, 11, 30), useIsoDateFormat);
   const php8Point1EOL = getDateString(new Date(2024, 11, 30), useIsoDateFormat);
   const php8Point0EOL = getDateString(new Date(2023, 11, 30), useIsoDateFormat);
-  const php7Point4EOL = getDateString(new Date(2022, 11, 28), useIsoDateFormat);
+  const php7Point4EOL = getDateString(new Date(2022, 11, 30), useIsoDateFormat);
   const php7Point3EOL = getDateString(new Date(2021, 12, 6), useIsoDateFormat);
   const php7Point2EOL = getDateString(new Date(2020, 11, 30), useIsoDateFormat);
   const php7Point1EOL = getDateString(new Date(2020, 2, 1), useIsoDateFormat);
