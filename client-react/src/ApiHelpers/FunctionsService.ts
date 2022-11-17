09e5584f1d58ce73a8669fc86951640dc90c6cd1
Qi Liu <liuqi@microsoft.com>
2022-11-17 14:26:08 -0800

    remove changes

--- client-react/src/ApiHelpers/FunctionsService.ts
+++ client-react/src/ApiHelpers/FunctionsService.ts
@@ -70,14 +70,14 @@ export default class FunctionsService {
   };
 
   public static runFunction(settings: NetAjaxSettings, runFunctionsControllerOptions: RunFunctionControllerOptions) {
-    // const url = settings.uri;
-    // const method = settings.type as Method;
-    // const headers = settings.headers || {};
-    // const data = settings.data;
+    const url = settings.uri;
+    const method = settings.type as Method;
+    const headers = settings.headers || {};
+    const data = settings.data;
 
-    // return sendHttpRequest({ url, method, headers, data }).catch(() => {
-    return this.tryRunFunctionsController(runFunctionsControllerOptions);
-    // });
+    return sendHttpRequest({ url, method, headers, data }).catch(() => {
+      return this.tryRunFunctionsController(runFunctionsControllerOptions);
+    });
   }
 
   public static getDataFromFunctionHref(url: string, method: Method, headers: KeyValue<string>, body?: any) {
