bf53809762c6267acc6f58e29a0d84d3fd7aff6f
Qi Liu <liuqi@microsoft.com>
2022-11-18 10:37:14 -0800

    remove import

--- server/src/utilities/url.util.ts
+++ server/src/utilities/url.util.ts
@@ -1,8 +1,7 @@
-import { Site } from '@azure/arm-appservice';
 import { ArmObj } from '../types/arm-obj';
 
 export class Url {
-  public static getMainUrl(site: ArmObj<Site>) {
+  public static getMainUrl(site: ArmObj<any>) {
     return `https://${site.properties.defaultHostName}`;
   }
 }
