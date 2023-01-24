7a23e88df97cd9285237ab727579759dc047e994
miyukiabe-baxter <miyuki0809@gmail.com>
2023-01-24 13:17:33 -0800

    Update client-react/src/pages/app/functions/function/function-editor/function-test/FunctionTest.tsx
    Updating PR based on a suggestion.
    
    Co-authored-by: Joe Chung <joechung@microsoft.com>

--- client-react/src/pages/app/functions/function/function-editor/function-test/FunctionTest.tsx
+++ client-react/src/pages/app/functions/function/function-editor/function-test/FunctionTest.tsx
@@ -141,11 +141,7 @@ const FunctionTest: React.SFC<FunctionTestProps> = props => {
           // Get methods list and if POST method is available, assign it as a default.
           // Otherwise, assign the first method in the array as a default.
           if (methods?.length > 0) {
-            if (methods.find(m => StringUtils.equalsIgnoreCase(m, HttpMethods.post))) {
-              updatedFormValues.method = HttpMethods.post;
-            } else {
-              updatedFormValues.method = methods[0].toLowerCase();
-            }
+            updatedFormValues.method = methods.some(m => StringUtils.equalsIgnoreCase(m, HttpMethods.post) ? HttpMethods.post : methods[0].toLowerCase();
           } else {
             updatedFormValues.method = HttpMethods.post;
           }
