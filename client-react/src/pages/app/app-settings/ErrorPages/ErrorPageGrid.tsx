5ce62b6fa17e4b2185d2677b1a27526b5c0be585
Joy2469 <aditijain0424@gmail.com>
2023-03-08 12:43:11 -0600

    4

--- client-react/src/pages/app/app-settings/ErrorPages/ErrorPageGrid.tsx
+++ client-react/src/pages/app/app-settings/ErrorPages/ErrorPageGrid.tsx
@@ -17,68 +17,75 @@ const ErrorPageGrid: React.FC<FormikProps<AppSettingsFormValues>> = props => {
 
   const removeItem = React.useCallback((index: number) => {}, []);
 
-  const onShowPanel =  React.useCallback((item: IColumnItem, index: number) => {}, []);
+  const onShowPanel = React.useCallback((item: IColumnItem, index: number) => {}, []);
 
-  const _columnErrorCode = React.useMemo(() =>  [{
-      key: '403',
-      errorCode: '403',
-      status: t('errorPage_columnStatus_notConfigured'),
-    },
-    {
-      key: '502',
-      errorCode: '502',
-      status: t('errorPage_columnStatus_notConfigured'),
-    },
-    {
-      key: '503',
-      errorCode: '503',
-      status: t('errorPage_columnStatus_notConfigured'),
-    }]
-  }, []);
+  const _columnErrorCode = React.useMemo(
+    () => [
+      {
+        key: '403',
+        errorCode: '403',
+        status: t('errorPage_columnStatus_notConfigured'),
+      },
+      {
+        key: '502',
+        errorCode: '502',
+        status: t('errorPage_columnStatus_notConfigured'),
+      },
+      {
+        key: '503',
+        errorCode: '503',
+        status: t('errorPage_columnStatus_notConfigured'),
+      },
+    ],
+    []
+  );
 
-  const onRenderItemColumn = React.useCallback((item: IColumnItem, index: number, column: IColumn) => {
-    if (!column || !item) {
-      return null;
-    }
+  const onRenderItemColumn = React.useCallback(
+    (item: IColumnItem, index: number, column: IColumn) => {
+      if (!column || !item) {
+        return null;
+      }
 
-    if (column.key === 'delete') {
-      return (
-        <TooltipHost
-          content={t('delete')}
-          id={`app-settings-errorPages-delete-tooltip-${index}`}
-          calloutProps={{ gapSpace: 0 }}
-          closeDelay={500}>
-          <IconButton
-            className={defaultCellStyle}
-            disabled={disableAllControls || item.status === t('errorPage_columnStatus_notConfigured')}
+      if (column.key === 'delete') {
+        return (
+          <TooltipHost
+            content={t('delete')}
             id={`app-settings-errorPages-delete-tooltip-${index}`}
-            iconProps={{ iconName: 'Delete' }}
-            ariaLabel={t('delete')}
-            onClick={() => removeItem(index)}
-          />
-        </TooltipHost>
-      );
-    }
-    if (column.key === 'edit') {
-      return (
-        <TooltipHost
-          content={t('edit')}
-          id={`app-settings-errorPages-edit-tooltip-${index}`}
-          calloutProps={{ gapSpace: 0 }}
-          closeDelay={500}>
-          <IconButton
-            className={defaultCellStyle}
-            disabled={disableAllControls}
-            id={`app-settings-errorPages-edit-${index}`}
-            iconProps={{ iconName: 'Edit' }}
-            ariaLabel={t('edit')}
-            onClick={() => onShowPanel(item, index)}
-          />
-        </TooltipHost>
-      );
-    }
-    return <div className={defaultCellStyle}>{item[column.fieldName!]}</div>;
-  }, [removeItem, onShowPanel, disableAllControls]);
+            calloutProps={{ gapSpace: 0 }}
+            closeDelay={500}>
+            <IconButton
+              className={defaultCellStyle}
+              disabled={disableAllControls || item.status === t('errorPage_columnStatus_notConfigured')}
+              id={`app-settings-errorPages-delete-tooltip-${index}`}
+              iconProps={{ iconName: 'Delete' }}
+              ariaLabel={t('delete')}
+              onClick={() => removeItem(index)}
+            />
+          </TooltipHost>
+        );
+      }
+      if (column.key === 'edit') {
+        return (
+          <TooltipHost
+            content={t('edit')}
+            id={`app-settings-errorPages-edit-tooltip-${index}`}
+            calloutProps={{ gapSpace: 0 }}
+            closeDelay={500}>
+            <IconButton
+              className={defaultCellStyle}
+              disabled={disableAllControls}
+              id={`app-settings-errorPages-edit-${index}`}
+              iconProps={{ iconName: 'Edit' }}
+              ariaLabel={t('edit')}
+              onClick={() => onShowPanel(item, index)}
+            />
+          </TooltipHost>
+        );
+      }
+      return <div className={defaultCellStyle}>{item[column.fieldName!]}</div>;
+    },
+    [removeItem, onShowPanel, disableAllControls]
+  );
 
   const getColumns = (): IColumn[] => {
     return [
@@ -134,14 +141,14 @@ const ErrorPageGrid: React.FC<FormikProps<AppSettingsFormValues>> = props => {
   };
 
   return (
-      <DisplayTableWithEmptyMessage
-        columns={getColumns()}
-        items={_columnErrorCode() || []}
-        isHeaderVisible={true}
-        layoutMode={DetailsListLayoutMode.justified}
-        selectionMode={SelectionMode.none}
-        selectionPreservedOnEmptyClick={true}
-      />
+    <DisplayTableWithEmptyMessage
+      columns={getColumns()}
+      items={_columnErrorCode || []}
+      isHeaderVisible={true}
+      layoutMode={DetailsListLayoutMode.justified}
+      selectionMode={SelectionMode.none}
+      selectionPreservedOnEmptyClick={true}
+    />
   );
 };
 
