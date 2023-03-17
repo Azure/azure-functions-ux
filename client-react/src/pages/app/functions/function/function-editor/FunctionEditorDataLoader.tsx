e79773d1fcad23cb35da1188b879eaa363f8ea25
spellegrino021 <stpelleg@microsoft.com>
2023-03-17 15:39:48 -0700

    updating function test sample data

--- client-react/src/pages/app/functions/function/function-editor/FunctionEditorDataLoader.tsx
+++ client-react/src/pages/app/functions/function/function-editor/FunctionEditorDataLoader.tsx
@@ -485,7 +485,7 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = ({ res
       let testData;
 
       if (testDataHrefObjects.length === 2) {
-        const vfsArmTestDataResponse = await FunctionsService.getTestDataOverVfsArm(site.id, testDataHrefObjects[1], runtimeVersion);
+        const vfsArmTestDataResponse = await FunctionsService.getTestDataOverVfsArm(site.id, testDataHrefObjects[1]);
         if (vfsArmTestDataResponse.metadata.success) {
           testData = vfsArmTestDataResponse.data;
           testDataResponseSuccess = true;
@@ -742,11 +742,11 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = ({ res
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [site, functionInfo, hostKeys, functionKeys]);
 
-  useEffect(() => {
-    getAndSetTestData();
+  // useEffect(() => {
+  //   getAndSetTestData();
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [functionInfo, hostKeys]);
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [functionInfo, hostKeys]);
 
   // TODO (krmitta): Show a loading error message site or functionInfo call fails
   if (initialLoading || !site) {
@@ -758,6 +758,8 @@ const FunctionEditorDataLoader: React.FC<FunctionEditorDataLoaderProps> = ({ res
       {functionInfo ? (
         <div style={showTestPanel ? shrinkEditorStyle(window.innerWidth) : undefined}>
           <FunctionEditor
+            getAndSetTestData={getAndSetTestData}
+            hostKeys={hostKeys}
             functionInfo={functionInfo}
             site={site}
             run={run}
