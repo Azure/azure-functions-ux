faa50b6836788dd64d5e87c4e3c01ce2fbacc202
Joe Chung <joechung@microsoft.com>
2023-04-04 14:22:06 -0400

    fix: Remove Functions Collateral experiment (#7136)

--- client-react/src/pages/app/functions/common/useCreateFunctionFormBuilderFactory.ts
+++ client-react/src/pages/app/functions/common/useCreateFunctionFormBuilderFactory.ts
@@ -1,14 +1,10 @@
 import i18next from 'i18next';
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { ArmObj } from '../../../../models/arm-obj';
 import { Binding } from '../../../../models/functions/binding';
-import { BindingInfo, BindingType } from '../../../../models/functions/function-binding';
+import { BindingInfo } from '../../../../models/functions/function-binding';
 import { FunctionInfo } from '../../../../models/functions/function-info';
-import { PortalContext } from '../../../../PortalContext';
-import { ExperimentationConstants } from '../../../../utils/CommonConstants';
-import CosmosDbFunctionFormBuilder from './CosmosDbFunctionFormBuilder';
 import { CreateFunctionFormBuilder } from './CreateFunctionFormBuilder';
-import { usePermissions } from './usePermissions';
 
 export type CreateFunctionFormBuilderFactory = (
   bindingInfo: BindingInfo[],
@@ -19,24 +15,8 @@ export type CreateFunctionFormBuilderFactory = (
   t: i18next.TFunction
 ) => CreateFunctionFormBuilder;
 
-const COSMOS_DB_TRIGGER = BindingType.cosmosDBTrigger.toLowerCase();
-
-export const useCreateFunctionFormBuilderFactory = (resourceId: string, templateId: string) => {
-  const portalCommunicator = useContext(PortalContext);
-  const { hasAppSettingsPermissions, hasResourceGroupWritePermission, hasSubscriptionWritePermission } = usePermissions(resourceId);
-  const [hasFlightEnabled, setHasFlightEnabled] = useState(false);
+export const useCreateFunctionFormBuilderFactory = () => {
   const [factory, setFactory] = useState<CreateFunctionFormBuilderFactory>();
-
-  const cosmosDbFunctionFormBuilder = useCallback<() => CreateFunctionFormBuilderFactory>(
-    () => (bindingInfo, bindings, resourceId, functionInfos, defaultName, t) => {
-      return new CosmosDbFunctionFormBuilder(bindingInfo, bindings, resourceId, functionInfos, defaultName, t, {
-        hasAppSettingsPermissions,
-        hasResourceGroupWritePermission,
-        hasSubscriptionWritePermission,
-      });
-    },
-    [hasAppSettingsPermissions, hasResourceGroupWritePermission, hasSubscriptionWritePermission]
-  );
 
   const createFunctionFormBuilder = useCallback<() => CreateFunctionFormBuilderFactory>(
     () => (bindingInfo, bindings, resourceId, functionInfos, defaultName, t) => {
@@ -46,22 +26,8 @@ export const useCreateFunctionFormBuilderFactory = (resourceId: string, template
   );
 
   useEffect(() => {
-    portalCommunicator.hasFlightEnabled(ExperimentationConstants.TreatmentFlight.functionsCollateral).then(setHasFlightEnabled);
-  }, [portalCommunicator]);
-
-  useEffect(() => {
-    const [lowerTemplateIdWithoutLanguage] = templateId.toLowerCase().split('-');
-
-    switch (lowerTemplateIdWithoutLanguage) {
-      case COSMOS_DB_TRIGGER:
-        setFactory(hasFlightEnabled ? cosmosDbFunctionFormBuilder : createFunctionFormBuilder);
-        break;
-
-      default:
-        setFactory(createFunctionFormBuilder);
-        break;
-    }
-  }, [cosmosDbFunctionFormBuilder, createFunctionFormBuilder, hasFlightEnabled, templateId]);
+    setFactory(createFunctionFormBuilder);
+  }, [createFunctionFormBuilder]);
 
   return factory;
 };
