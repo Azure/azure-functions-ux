38778ad37881ca8a9fd39d749298d7a0262a4744
Joe Chung <joechung@microsoft.com>
2023-04-18 17:52:54 -0400

    [Functions][Python] Add column to template list in Create Function

---
+++ client-react/src/pages/app/functions/new-create-preview/portal-create/useTemplateListColumns.tsx
@@ -0,0 +1,103 @@
+import { IButtonStyles, IColumn, IIconProps, IconButton } from '@fluentui/react';
+import { useCallback, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { ArmObj } from '../../../../../models/arm-obj';
+import { FunctionTemplate } from '../../../../../models/functions/function-template';
+import { HostStatus } from '../../../../../models/functions/host-status';
+import { RuntimeExtensionMajorVersions } from '../../../../../models/functions/runtime-extension';
+import { CommonConstants } from '../../../../../utils/CommonConstants';
+import StringUtils from '../../../../../utils/string';
+import Url from '../../../../../utils/url';
+import { templateListNameColumnStyle } from '../FunctionCreate.styles';
+
+const enableNewProgrammingModel = Url.getFeatureValue(CommonConstants.FeatureFlags.enableNewProgrammingModel);
+
+const iconButtonStyles: IButtonStyles = {
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
+};
+
+const iconProps: IIconProps = {
+  iconName: 'FileCode',
+};
+
+export function useTemplateListColumns(hostStatus: ArmObj<HostStatus>, useNewProgrammingModel = false) {
+  const { t } = useTranslation();
+
+  const runtimeVersion = useMemo(
+    () => (hostStatus?.properties.version ? StringUtils.getRuntimeVersionString(hostStatus.properties.version) : ''),
+    [hostStatus?.properties.version]
+  );
+
+  const getTemplateDisplayName = useCallback(
+    (item: FunctionTemplate) => (runtimeVersion === RuntimeExtensionMajorVersions.v1 ? `${item.name}: ${item.language}` : item.name),
+    [runtimeVersion]
+  );
+
+  const onRender = useCallback(
+    (item?: FunctionTemplate, _?, column?: IColumn): JSX.Element | null => {
+      if (!column || !item) {
+        return null;
+      }
+
+      switch (column.key) {
+        case 'template':
+          return <div className={templateListNameColumnStyle}>{getTemplateDisplayName(item)}</div>;
+
+        case 'view-template':
+          return (
+            <IconButton
+              ariaLabel={t('viewTemplateFormat').format(getTemplateDisplayName(item))}
+              href={'https://aka.ms/todo' /** @todo (joechung) */}
+              iconProps={iconProps}
+              styles={iconButtonStyles}
+              target="_blank"
+            />
+          );
+        default:
+          return column.fieldName ? <div>{item[column.fieldName]}</div> : null;
+      }
+    },
+    [getTemplateDisplayName, t]
+  );
+
+  const columns = useMemo<IColumn[]>(() => {
+    return [
+      {
+        key: 'template',
+        name: t('template'),
+        fieldName: 'template',
+        isMultiline: true,
+        isResizable: true,
+        maxWidth: 225,
+        minWidth: 100,
+        onRender,
+      },
+      {
+        key: 'description',
+        name: t('description'),
+        fieldName: 'description',
+        isMultiline: true,
+        isResizable: true,
+        minWidth: 100,
+        onRender,
+      },
+      ...(enableNewProgrammingModel && useNewProgrammingModel
+        ? [
+            {
+              key: 'view-template',
+              name: t('viewTemplate'),
+              fieldName: 'view-template' /** @todo (joechung) */,
+              minWidth: 100,
+              onRender,
+            },
+          ]
+        : []),
+    ];
+  }, [onRender, t, useNewProgrammingModel]);
+
+  return columns;
+}
