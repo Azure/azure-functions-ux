ea6a45cc9017c5e56163da82bb8463d6d433f380
Aniello Scotto Di Marco <andimarc@microsoft.com>
2023-04-19 17:16:39 -0700

    enable slots for P0MV3 SKUs (#7159)

--- client/src/app/shared/services/scenario/azure.environment.ts
+++ client/src/app/shared/services/scenario/azure.environment.ts
@@ -222,6 +222,7 @@ export class AzureEnvironment extends Environment {
       case Tier.premiumV2:
       case Tier.premiumV3:
       case Tier.premiumMV3:
+      case Tier.premium0V3:
       case Tier.isolated:
       case Tier.isolatedV2:
       case Tier.premiumContainer:
