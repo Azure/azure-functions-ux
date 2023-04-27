3667e4965f73889f08975546ab6354990092381b
Joe Chung <joechung@microsoft.com>
2023-04-27 14:25:22 -0400

    [Functions][Python] Add v2 APIs to data loader for Create Function (#7175)

---
+++ client-react/src/pages/app/functions/function/hooks/useUserPromptQuery.ts
@@ -0,0 +1,27 @@
+import { useCallback, useMemo } from 'react';
+import FunctionsService from '../../../../../ApiHelpers/FunctionsService';
+import { ArmObj } from '../../../../../models/arm-obj';
+import { UserPrompt } from '../../../../../models/functions/user-prompt';
+import { getTelemetryInfo } from '../../../../../utils/TelemetryUtils';
+import { useHttpResponseObjectQuery } from '../../../../../utils/useHttpResponseObjectQuery';
+
+export function useUserPromptQuery(resourceId: string) {
+  const promise = useMemo(() => (!resourceId ? undefined : FunctionsService.getUserPrompts(resourceId)), [resourceId]);
+
+  const onSuccess = useCallback((response: ArmObj<UserPrompt[]>) => response.properties, []);
+
+  const onError = useCallback(
+    error =>
+      getTelemetryInfo('error', 'getUserPrompts', 'failed', {
+        error,
+        message: 'Failed to get user prompts',
+      }),
+    []
+  );
+
+  const { data } = useHttpResponseObjectQuery(promise, onSuccess, onError);
+
+  return {
+    userPrompts: data,
+  };
+}
