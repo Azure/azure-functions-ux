16657d26f0a4d09c769487b728adf7d2ad8fd9af
Joe Chung <joechung@microsoft.com>
2023-05-03 17:08:05 -0400

    [Functions] Add error banner with link to Download app content blade
    Show error banner which, when clicked, opens the Download app content
    blade if either Code + Test or Integrate is unable to fetch
    function info due to 404 errors.
    
    This could happen if the user made mistakes while creating or editing
    v2 programming model Python functions.
    
    Revert "[Functions][Python] Ensure Code+Test works in v2 programming
    model (#7176)" (commit hash `cb6f31da9`)

--- client/src/app/shared/models/portal-resources.ts
+++ client/src/app/shared/models/portal-resources.ts
@@ -2321,6 +2321,7 @@ export class PortalResources {
   public static pricing_ACU_Pv3 = 'pricing_ACU_Pv3';
   public static pricing_vCores = 'pricing_vCores';
   public static functionInfoFetchError = 'functionInfoFetchError';
+  public static functionInfoUnavailableError = 'functionInfoUnavailableError';
   public static createHostKeyNotification = 'createHostKeyNotification';
   public static createSystemKeyNotification = 'createSystemKeyNotification';
   public static createFunctionKeyNotification = 'createFunctionKeyNotification';
