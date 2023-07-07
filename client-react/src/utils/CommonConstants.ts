a160ee7aa940e3d98bdbd93441030061038bf49a
Nicolas Layne <nicolaslayne@microsoft.com>
2023-07-07 14:42:32 -0500

    Update ContainerAppService API version to match the one used in Ibiza

--- client-react/src/utils/CommonConstants.ts
+++ client-react/src/utils/CommonConstants.ts
@@ -50,7 +50,7 @@ export class CommonConstants {
     roleAssignmentApiVersion20180701: '2018-07-01',
     roleAssignmentApiVersion20180901Preview: '2018-09-01-preview',
     enableSystemAssignedIdentityApiVersion20210201: '2021-02-01',
-    containerAppApiVersion20220101preview: '2022-01-01-preview',
+    containerAppApiVersion20221101Preview: '2022-11-01-preview',
     antaresApiVersion20141101: '2014-11-01',
   };
 
