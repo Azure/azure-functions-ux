e56ef84c8da859b7b499ebfff3758a58e6ec4f20
Yoona Oh <jyoonaoh@gmail.com>
2023-08-15 11:14:29 -0700

    Revert "[DC] Show cross-sub ACR with no read access as private registry (#7320)"
    This reverts commit 46cda8d02b64250ddbc12526e4666ce038d0a531.

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerSettings.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerSettings.tsx
@@ -18,7 +18,7 @@ import DeploymentCenterGitHubWorkflowConfigSelector from '../github-provider/Dep
 import DeploymentCenterGitHubWorkflowConfigPreview from '../github-provider/DeploymentCenterGitHubWorkflowConfigPreview';
 import { DeploymentCenterContext } from '../DeploymentCenterContext';
 import { useTranslation } from 'react-i18next';
-import { getAcrNameFromLoginServer, getTelemetryInfo, getWorkflowFileName } from '../utility/DeploymentCenterUtility';
+import { getTelemetryInfo, getWorkflowFileName } from '../utility/DeploymentCenterUtility';
 import { Guid } from '../../../../utils/Guid';
 import DeploymentCenterContainerContinuousDeploymentSettings from './DeploymentCenterContainerContinuousDeploymentSettings';
 import { DeploymentCenterConstants } from '../DeploymentCenterConstants';
@@ -31,7 +31,6 @@ import { AppOs } from '../../../../models/site/site';
 import DeploymentCenterData from '../DeploymentCenter.data';
 import { PortalContext } from '../../../../PortalContext';
 import { CommonConstants } from '../../../../utils/CommonConstants';
-import { AcrDependency } from '../../../../utils/dependency/Dependency';
 
 const DeploymentCenterContainerSettings: React.FC<DeploymentCenterFieldProps<DeploymentCenterContainerFormData>> = props => {
   const { formProps, isDataRefreshing } = props;
@@ -59,52 +58,9 @@ const DeploymentCenterContainerSettings: React.FC<DeploymentCenterFieldProps<Dep
 
   const isGitHubActionSelected = formProps.values.scmType === ScmType.GitHubAction;
   const isVstsSelected = formProps.values.scmType === ScmType.Vsts;
-  const [isAcrConfigured, setIsAcrConfigured] = useState<boolean>(formProps.values.registrySource === ContainerRegistrySources.acr);
+  const isAcrConfigured = formProps.values.registrySource === ContainerRegistrySources.acr;
   const isDockerHubConfigured = formProps.values.registrySource === ContainerRegistrySources.docker;
   const isPrivateRegistryConfigured = formProps.values.registrySource === ContainerRegistrySources.privateRegistry;
-
-  const [isDataLoading, setIsDataLoading] = useState<boolean>(false);
-
-  const hasAcrReadAccess = React.useCallback(() => {
-    // NOTE(yoonaoh): Checking to see if the user has read access to the ACR itself
-    // If not, we turn this into a private registry instead
-    if (formProps.values.registrySource === ContainerRegistrySources.acr) {
-      setIsDataLoading(true);
-      const acrTagInstance = new AcrDependency();
-      const acrName = getAcrNameFromLoginServer(formProps.values.acrLoginServer);
-      acrTagInstance.discoverResourceId(portalContext, acrName).then(response => {
-        const isValidAcr = !!response?.subscriptionId;
-        setIsAcrConfigured(isValidAcr);
-        if (!isValidAcr) {
-          const formattedAcrLoginServer = CommonConstants.DeploymentCenterConstants.https + formProps.values.acrLoginServer;
-          // Change initial values to not affect the dirty state checks
-          formProps.initialValues.registrySource = ContainerRegistrySources.privateRegistry;
-          formProps.initialValues.privateRegistryServerUrl = formattedAcrLoginServer;
-          formProps.initialValues.privateRegistryImageAndTag = `${formProps.values.acrImage}:${formProps.values.acrTag}`;
-          formProps.initialValues.privateRegistryComposeYml = formProps.values.acrComposeYml;
-          formProps.initialValues.privateRegistryUsername = formProps.values.acrUsername;
-          formProps.initialValues.privateRegistryPassword = formProps.values.acrPassword;
-
-          // Switch to private registry
-          formProps.setFieldValue('registrySource', ContainerRegistrySources.privateRegistry);
-          formProps.setFieldValue('privateRegistryServerUrl', formattedAcrLoginServer);
-          formProps.setFieldValue('privateRegistryImageAndTag', `${formProps.values.acrImage}:${formProps.values.acrTag}`);
-          formProps.setFieldValue('privateRegistryComposeYml', formProps.values.acrComposeYml);
-          formProps.setFieldValue('privateRegistryUsername', formProps.values.acrUsername);
-          formProps.setFieldValue('privateRegistryPassword', formProps.values.acrPassword);
-
-          // Reset the values of the initial acr fields
-          formProps.initialValues.acrLoginServer = '';
-          formProps.initialValues.acrImage = '';
-          formProps.initialValues.acrTag = '';
-          formProps.initialValues.acrComposeYml = '';
-          formProps.initialValues.acrUsername = '';
-          formProps.initialValues.acrPassword = '';
-        }
-        setIsDataLoading(false);
-      });
-    }
-  }, [formProps.values.registrySource, formProps.values.acrLoginServer]);
 
   const getWorkflowFileVariables = () => {
     const slotName = deploymentCenterContext.siteDescriptor?.slot ?? '';
@@ -319,8 +275,6 @@ const DeploymentCenterContainerSettings: React.FC<DeploymentCenterFieldProps<Dep
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [siteStateContext.isLinuxApp]);
 
-  useEffect(() => hasAcrReadAccess(), [hasAcrReadAccess]);
-
   const renderSetupView = () => {
     return (
       <>
@@ -379,7 +333,7 @@ const DeploymentCenterContainerSettings: React.FC<DeploymentCenterFieldProps<Dep
     <ProgressIndicator description={t('deploymentCenterSettingsLoading')} ariaValueText={t('deploymentCenterSettingsLoadingAriaValue')} />
   );
 
-  return isDataLoading || isDataRefreshing ? getProgressIndicator() : getSettingsControls();
+  return isDataRefreshing ? getProgressIndicator() : getSettingsControls();
 };
 
 export default DeploymentCenterContainerSettings;
