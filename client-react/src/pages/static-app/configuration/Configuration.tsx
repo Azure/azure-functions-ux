4c845f123a93752ffef03aaf069c97ad760a371f
Stephanie Pellegrino <35281019+spellegrino021@users.noreply.github.com>
2023-08-15 13:40:46 -0700

    [SWA] Add aria labels to eye button (#7335)
    * labels
    
    * swap

--- client-react/src/pages/static-app/configuration/Configuration.tsx
+++ client-react/src/pages/static-app/configuration/Configuration.tsx
@@ -172,7 +172,8 @@ const Configuration: React.FC<ConfigurationProps> = (props: ConfigurationProps) 
               id={`environment-variable-show-${index}`}
               className={defaultCellStyle}
               onClick={() => onShowHideButtonClick(itemKey)}
-              iconProps={{ iconName: 'RedEye' }}>
+              ariaLabel={t('showButton')}
+              iconProps={{ iconName: 'RedEye', ariaLabel: t('showButton') }}>
               <div className={defaultCellStyle}>{t('hiddenValueClickAboveToShow')}</div>
             </ActionButton>
           ) : (
@@ -180,7 +181,8 @@ const Configuration: React.FC<ConfigurationProps> = (props: ConfigurationProps) 
               <IconButton
                 id={`environment-variable-hide-${index}`}
                 className={tableValueIconStyle(theme)}
-                iconProps={{ iconName: 'Hide' }}
+                iconProps={{ iconName: 'Hide', ariaLabel: t('hideButton') }}
+                ariaLabel={t('hideButton')}
                 onClick={() => onShowHideButtonClick(itemKey)}
               />
               <div className={tableValueTextFieldStyle}>
