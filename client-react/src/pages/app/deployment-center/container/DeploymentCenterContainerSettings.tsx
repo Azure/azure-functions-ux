2584a3374e57b599ff289af11f0e8990f3da6375
yoonaoh <jyoonaoh@gmail.com>
2023-08-22 14:40:49 -0700

    [DC] Add OIDC authentication settings

--- client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerSettings.tsx
+++ client-react/src/pages/app/deployment-center/container/DeploymentCenterContainerSettings.tsx
@@ -355,12 +355,15 @@ const DeploymentCenterContainerSettings: React.FC<DeploymentCenterFieldProps<Dep
         )}
 
         {isGitHubActionSelected && (
-          <DeploymentCenterGitHubWorkflowConfigPreview
-            isPreviewFileButtonDisabled={isPreviewFileButtonDisabled}
-            getWorkflowFileContent={getWorkflowFileContent}
-            workflowFilePath={workflowFilePath}
-            panelMessage={panelMessage}
-          />
+          <>
+            <DeploymentCenterAuthenticationSettings formProps={formProps} />
+            <DeploymentCenterGitHubWorkflowConfigPreview
+              isPreviewFileButtonDisabled={isPreviewFileButtonDisabled}
+              getWorkflowFileContent={getWorkflowFileContent}
+              workflowFilePath={workflowFilePath}
+              panelMessage={panelMessage}
+            />
+          </>
         )}
       </>
     );
