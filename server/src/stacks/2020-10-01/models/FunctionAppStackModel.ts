f66c5549079c055a0826e9b62ed7b086d5f9d38b
shimedh <shimedh@microsoft.com>
2023-10-02 21:21:59 -0700

    [DRAFT] Proposal to Update FunctionAppStackModel.ts

--- server/src/stacks/2020-10-01/models/FunctionAppStackModel.ts
+++ server/src/stacks/2020-10-01/models/FunctionAppStackModel.ts
@@ -16,12 +16,29 @@ export interface AppSettingsDictionary {
   WEBSITE_NODE_DEFAULT_VERSION?: string;
 }
 
+export interface InstanceSize {
+  size: string;
+  isDefault: boolean;
+}
+
+export interface ConcurrencySetting {
+  // TBD - Need to get more details on what this would look like
+}
+
 export interface SiteConfigPropertiesDictionary {
   use32BitWorkerProcess: boolean;
   linuxFxVersion?: string;
   javaVersion?: string;
   powerShellVersion?: string;
   netFrameworkVersion?: string;
+  instanceSizes?: InstanceSize[];
+  alwaysReady?: boolean;
+  concurrencySettings?: ConcurrencySetting;
+}
+
+export interface FunctionsExtensionVersionNew {
+  version: string;
+  isDeprecated: boolean;
 }
 
 export interface FunctionAppRuntimeSettings extends CommonSettings {
@@ -32,4 +49,6 @@ export interface FunctionAppRuntimeSettings extends CommonSettings {
   appSettingsDictionary: AppSettingsDictionary;
   siteConfigPropertiesDictionary: SiteConfigPropertiesDictionary;
   supportedFunctionsExtensionVersions: FunctionsExtensionVersion[];
+  supportedFunctionsExtensionVersionsNew: FunctionsExtensionVersionNew[];
+  unsupportedSkuCodes?: string[];
 }
