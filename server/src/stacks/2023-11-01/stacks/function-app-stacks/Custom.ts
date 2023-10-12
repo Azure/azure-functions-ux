d3c47d1672f45d987294e557a1e65f9e79c38750
shimedh <shimedh@microsoft.com>
2023-10-12 13:26:33 -0700

    Create Custom.ts

---
+++ server/src/stacks/2023-11-01/stacks/function-app-stacks/Custom.ts
@@ -0,0 +1,58 @@
+import { FunctionAppStack } from '../../models/FunctionAppStackModel';
+
+export const customStack: FunctionAppStack = {
+  displayText: 'Custom Handler',
+  value: 'custom',
+  preferredOs: 'windows',
+  majorVersions: [
+    {
+      displayText: 'Custom Handler',
+      value: 'custom',
+      minorVersions: [
+        {
+          displayText: 'Custom Handler',
+          value: 'custom',
+          stackSettings: {
+            windowsRuntimeSettings: {
+              runtimeVersion: 'custom',
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              remoteDebuggingSupported: false,
+              gitHubActionSettings: {
+                isSupported: false,
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'custom',
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: true,
+                netFrameworkVersion: 'v6.0',
+              },
+              supportedFunctionsExtensionVersions: ['~4', '~3', '~2'],
+            },
+            linuxRuntimeSettings: {
+              runtimeVersion: '',
+              isPreview: false,
+              appInsightsSettings: {
+                isSupported: true,
+              },
+              remoteDebuggingSupported: false,
+              gitHubActionSettings: {
+                isSupported: false,
+              },
+              appSettingsDictionary: {
+                FUNCTIONS_WORKER_RUNTIME: 'custom',
+              },
+              siteConfigPropertiesDictionary: {
+                use32BitWorkerProcess: false,
+                linuxFxVersion: '',
+              },
+              supportedFunctionsExtensionVersions: ['~4', '~3', '~2'],
+            },
+          },
+        },
+      ],
+    },
+  ],
+};
