9e100ffbf552dc5d16013fd9067f0d2effca156b
shimedh <shimedh@microsoft.com>
2023-10-20 14:25:01 -0700

    Hide General Settings tab for Flex Consumption Apps (#7444)
    * Hide General Settings tab for Flex Consumption Apps
    
    * Update client-react/src/utils/scenario-checker/flex-consumption-site.environment.ts
    
    Co-authored-by: Krrish Mittal <krrishmittal15@gmail.com>
    
    ---------
    
    Co-authored-by: Shibani Medhekar <shibanimedhekar@Shibanis-MacBook-Pro.local>
    Co-authored-by: Krrish Mittal <krrishmittal15@gmail.com>

---
+++ client-react/src/utils/scenario-checker/flex-consumption-site.environment.ts
@@ -0,0 +1,22 @@
+import { ScenarioIds } from './scenario-ids';
+import { ScenarioCheckInput, Environment } from './scenario.models';
+import { isFlexConsumption } from '../arm-utils';
+
+export class FlexConsumptionEnvironment extends Environment {
+  public name = 'FlexConsumption';
+
+  constructor() {
+    super();
+
+    this.scenarioChecks[ScenarioIds.showGeneralSettings] = {
+      id: ScenarioIds.showGeneralSettings,
+      runCheck: () => {
+        return { status: 'disabled' };
+      },
+    };
+  }
+
+  public isCurrentEnvironment(input?: ScenarioCheckInput): boolean {
+    return !!input && !!input.site && isFlexConsumption(input.site);
+  }
+}
