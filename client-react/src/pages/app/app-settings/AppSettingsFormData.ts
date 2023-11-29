c2002b02065cf9e29b6fb71bf3a216dbd438a17e
Qi Liu <liuqi@microsoft.com>
2023-11-29 11:10:03 -0800

    update

--- client-react/src/pages/app/app-settings/AppSettingsFormData.ts
+++ client-react/src/pages/app/app-settings/AppSettingsFormData.ts
@@ -35,7 +35,6 @@ import { IconConstants } from '../../../utils/constants/IconConstants';
 import { ThemeExtended } from '../../../theme/SemanticColorsExtended';
 import i18next from 'i18next';
 import { isJBossClusteringShown } from '../../../utils/stacks-utils';
-import Url from '../../../utils/url';
 
 export const findFormAppSettingIndex = (appSettings: FormAppSetting[], settingName: string) => {
   return settingName ? appSettings.findIndex(x => x.name.toLowerCase() === settingName.toLowerCase()) : -1;
