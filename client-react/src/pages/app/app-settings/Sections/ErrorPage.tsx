a2fcfb7f8682a1de53b073fcd2581be97c0bf6bd
Aditi Jain <45466137+Joy2469@users.noreply.github.com>
2024-01-16 15:51:50 -0600

    [Configuration] Banner link update (#7526)
    * update
    
    * Update client-react/src/pages/app/app-settings/Sections/ErrorPage.tsx
    
    Co-authored-by: Joe Chung <joechung@microsoft.com>
    
    ---------
    
    Co-authored-by: Aditi Jain <jainadit@microsoft.com>
    Co-authored-by: Joe Chung <joechung@microsoft.com>

--- client-react/src/pages/app/app-settings/Sections/ErrorPage.tsx
+++ client-react/src/pages/app/app-settings/Sections/ErrorPage.tsx
@@ -4,6 +4,9 @@ import { AppSettingsFormValues } from '../AppSettings.types';
 import { useTranslation } from 'react-i18next';
 import ErrorPageGrid from '../ErrorPages/ErrorPageGrid';
 import { isEqual } from 'lodash-es';
+import { Link } from '@fluentui/react';
+import { learnMoreLinkStyle } from '../../../../components/form-controls/formControl.override.styles';
+import { Links } from '../../../../utils/FwLinks';
 
 const ErrorPagePivot: React.FC<FormikProps<AppSettingsFormValues>> = props => {
   const { t } = useTranslation();
@@ -11,7 +14,17 @@ const ErrorPagePivot: React.FC<FormikProps<AppSettingsFormValues>> = props => {
   return (
     <>
       <h3>{t('customErrorPage')}</h3>
-      <p id="default-documents-info-message">{t('errorPagesInfoMessage')}</p>
+      <p>
+        <span id="default-documents-info-message">{t('errorPagesInfoMessage')}</span>
+        <Link
+          id="error-pages-info-learnMore"
+          href={Links.customErrorPagesLearnMore}
+          target="_blank"
+          className={learnMoreLinkStyle}
+          aria-labelledby="default-documents-info-message error-pages-info-learnMore">
+          {` ${t('learnMore')}`}
+        </Link>
+      </p>
       <ErrorPageGrid {...props} />
     </>
   );
